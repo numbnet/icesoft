5346e219fff381dcaed8685480fb883d06745134
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-05-16 22:36:57 +0000

    Putting back portlet enhanced version.

--- icefaces/scratchpads/portlets/icefaces/core/src/com/icesoft/faces/application/D2DViewHandler.java
+++ icefaces/scratchpads/portlets/icefaces/core/src/com/icesoft/faces/application/D2DViewHandler.java
@@ -37,6 +37,7 @@ import com.icesoft.faces.context.BridgeExternalContext;
 import com.icesoft.faces.context.BridgeFacesContext;
 import com.icesoft.faces.context.DOMResponseWriter;
 import com.icesoft.faces.webapp.http.servlet.ServletExternalContext;
+import com.icesoft.faces.component.NamespacingViewRoot;
 import com.icesoft.faces.webapp.parser.JspPageToDocument;
 import com.icesoft.faces.webapp.parser.Parser;
 import com.icesoft.faces.webapp.xmlhttp.PersistentFacesCommonlet;
@@ -155,7 +156,7 @@ public class D2DViewHandler extends ViewHandler {
             // JSF 1.1 removes transient components here, but I don't think that 1.2 does
         }
 
-       // This has been moved to the ServletView 
+       // This has been moved to the ServletView
 
     }
 
@@ -174,7 +175,8 @@ public class D2DViewHandler extends ViewHandler {
             return delegate.createView(context, viewId);
         }
 
-        UIViewRoot root = new UIViewRoot();
+        UIViewRoot root = new NamespacingViewRoot(context);
+//        UIViewRoot root = new UIViewRoot();
         root.setRenderKitId(RenderKitFactory.HTML_BASIC_RENDER_KIT);
 
         Map contextServletTable =
@@ -369,23 +371,7 @@ public class D2DViewHandler extends ViewHandler {
     }
 
     public String getResourceURL(FacesContext context, String path) {
-
-        String resourcePath = path;
-
-        if ( path != null ){
-            int startIndex = 0;
-            if (path.startsWith("/") ){
-                startIndex = 1;
-            }
-            resourcePath = LocationUtil.getAppBase(context) +
-                           path.substring(startIndex);
-        }
-
-        if( log.isTraceEnabled() ){
-            log.trace( "\n  original path is " + path +
-                       "\n  new resource path " + resourcePath );
-        }
-        return resourcePath;
+        return LocationUtil.getResourcePath(context,path);
     }
 
     protected long getTimeAttribute(UIComponent root, String key) {
