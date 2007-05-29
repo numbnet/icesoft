8412d56180633c63cb23339071bcf11dc3bdcd04
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-05-29 14:59:33 +0000

    Added a folder remotely

---
+++ projects/icefaces-seam-gen/trunk/SunRI-merged-seam-gen/src/ActionBean.java
@@ -0,0 +1,27 @@
+package @actionPackage@;
+
+import javax.ejb.Stateless;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.core.FacesMessages;
+
+@Stateless
+@Name("@componentName@")
+public class @beanName@ implements @interfaceName@ {
+	
+    @Logger private Log log;
+	
+    @In FacesMessages facesMessages;
+    
+    public void @methodName@()
+    {
+        //implement your business logic here
+        log.info("@componentName@.@methodName@() action called");
+        facesMessages.add("@methodName@");
+    }
+    
+    //add additional action methods
+    
+}
