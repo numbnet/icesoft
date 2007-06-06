184e48ed6f51590d986760e8e94b9b5af5d24e47
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-06-06 15:43:59 +0000

    This is for the bid information table that starts empty each time the application is run

---
+++ projects/seam-auctionMonitor/trunk/seam-auctionMonitor/src/model/com/icesoft/eb/Bid.java
@@ -0,0 +1,153 @@
+/*
+ * Bid.java
+ *
+ * Created on June 6, 2007, 9:36 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package model.com.icesoft.eb;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+import org.hibernate.validator.Pattern;
+import org.jboss.seam.annotations.Name;
+
+@Entity
+@Name("bid")
+public class Bid implements Serializable
+{
+   private Long id;
+   private User user;
+   private AuctionItem bidItem;
+   private DateTime timestamp;
+   private String creditCard;
+   private String creditCardName;
+   private int creditCardExpiryMonth;
+   private int creditCardExpiryYear;
+   
+   public Bid() {}
+   
+   public Bid(AuctionItem bidItem, User user)
+   {
+      this.bidItem = user;
+      this.user = user;
+   }
+
+   @Id @GeneratedValue
+   public Long getId()
+   {
+      return id;
+   }
+   public void setId(Long id)
+   {
+      this.id = id;
+   }
+   
+   @NotNull
+   @Basic @Temporal(TemporalType.DATETIME) 
+   public DateTime getTimestamp()
+   {
+      return timestamp;
+   }
+   public void setTimestamp(DateTime timestamp)
+   {
+      this.timestamp = timestamp;
+   }
+
+   @ManyToOne @NotNull
+   public AuctionItem getAuctionItem()
+   {
+      return bidItem;
+   }
+   public void setAuctionItem(AuctionItem bidItem)
+   {
+      this.bidItem = bidItem;
+   }
+   
+   @ManyToOne @NotNull
+   public User getUser()
+   {
+      return user;
+   }
+   public void setUser(User user)
+   {
+      this.user = user;
+   }
+   
+   
+   @NotNull(message="Credit card number is required")
+   @Length(min=16, max=16, message="Credit card number must 16 digits long")
+   @Pattern(regex="^\\d*$", message="Credit card number must be numeric")
+   public String getCreditCard()
+   {
+      return creditCard;
+   }
+
+   public void setCreditCard(String creditCard)
+   {
+      this.creditCard = creditCard;
+   }
+   
+   @Transient
+   public String getDescription()
+   {
+      return this.bidItem.toString();
+   
+   }
+
+
+
+   @NotNull(message="Credit card name is required")
+   @Length(min=3, max=70, message="Credit card name is required")
+   public String getCreditCardName()
+   {
+      return creditCardName;
+   }
+
+   public void setCreditCardName(String creditCardName)
+   {
+      this.creditCardName = creditCardName;
+   }
+
+   public int getCreditCardExpiryMonth()
+   {
+      return creditCardExpiryMonth;
+   }
+
+   public void setCreditCardExpiryMonth(int creditCardExpiryMonth)
+   {
+      this.creditCardExpiryMonth = creditCardExpiryMonth;
+   }
+
+   public int getCreditCardExpiryYear()
+   {
+      return creditCardExpiryYear;
+   }
+
+   public void setCreditCardExpiryYear(int creditCardExpiryYear)
+   {
+      this.creditCardExpiryYear = creditCardExpiryYear;
+   }
+   
+   @Override
+   public String toString()
+   {
+      return "Bid(" + user + ","+ bidItem + ")";
+   }
+
+}
