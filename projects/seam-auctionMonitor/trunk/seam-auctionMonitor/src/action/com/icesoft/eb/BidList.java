e3f93ba8b1e6eaea1e772fa5852643dc178c545c
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-06-06 16:20:37 +0000

    interface for BidListAction

---
+++ projects/seam-auctionMonitor/trunk/seam-auctionMonitor/src/action/com/icesoft/eb/BidList.java
@@ -0,0 +1,21 @@
+/*
+ * BidList.java
+ *
+ * Created on June 6, 2007, 10:16 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package action.com.icesoft.eb;
+
+import javax.ejb.Local;
+
+@Local
+public interface  BidList {
+
+   public void getBids();
+   public String cancel();
+   public void destroy();
+}
+
