2cdd165eb231ae3ddb97bf5d4a15fb3e673b9828
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-06-26 23:33:15 +0000

    ICE-1676 Added user registration functionality.

--- projects/seam-auctionMonitor/trunk/seam-auctionMonitor/src/action/com/icesoft/eb/RegisterAction.java
+++ projects/seam-auctionMonitor/trunk/seam-auctionMonitor/src/action/com/icesoft/eb/RegisterAction.java
@@ -25,62 +25,63 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.core.FacesMessages;
+import org.jboss.seam.security.Identity;
 import com.icesoft.eb.User;
 
 @Stateful
 @Scope(EVENT)
 @Name("register")
-public class RegisterAction implements Register
-{
+public class RegisterAction implements Register {
 
-   @In
-   private User user;
-   
-   @PersistenceContext
-   private EntityManager em;
-   
-   @In
-   private transient FacesMessages facesMessages;
-   
-   private String verify;
-   
-   public String register()
-   {
-      if ( user.getPassword().equals(verify) )
-      {
-         List existing = em.createQuery("select u.username from User u where u.username=:username")
-            .setParameter("username", user.getUsername())
-            .getResultList();
-         if (existing.size()==0)
-         {
-            em.persist(user);
-            facesMessages.add("Successfully registered as #{user.username}");
-            return "login";
-         }
-         else
-         {
-            facesMessages.add("Username #{user.username} already exists");
+    @In
+    private User user;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @In
+    private transient FacesMessages facesMessages;
+
+    @In
+    Identity identity;
+
+    private String verify;
+
+    public String register() {
+        if (user.getPassword().equals(verify)) {
+            List existing = em.createQuery("select u.username from User u where u.username=:username")
+                    .setParameter("username", user.getUsername())
+                    .getResultList();
+            if (existing.size() == 0) {
+                user.setRole("user");
+                em.persist(user);
+                identity.setUsername(user.getUsername());
+                identity.setPassword(user.getPassword());
+                identity.setRememberMe(true);
+                identity.login();
+                facesMessages.add("Successfully registered as #{user.username}");
+                return "login";
+            } else {
+                facesMessages.add("Username #{user.username} already exists");
+                return null;
+            }
+        } else {
+            facesMessages.add("verify", "Re-enter your password");
+            verify = null;
             return null;
-         }
-      }
-      else 
-      {
-         facesMessages.add("verify", "Re-enter your password");
-         verify=null;
-         return null;
-      }
-   }
+        }
+    }
 
-   public String getVerify()
-   {
-      return verify;
-   }
+    public String getVerify() {
+        return verify;
+    }
 
-   public void setVerify(String verify)
-   {
-      this.verify = verify;
-   }
-   
-   @Destroy @Remove
-   public void destroy() {}
+    public void setVerify(String verify) {
+        this.verify = verify;
+    }
+
+    @Destroy
+    @Remove
+    public void destroy() {
+    }
 }
