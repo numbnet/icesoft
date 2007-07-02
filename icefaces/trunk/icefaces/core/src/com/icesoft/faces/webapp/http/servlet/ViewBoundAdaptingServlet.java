bab5b972bdf0547e34662804cf5cbfd937241a64
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-07-02 16:13:39 +0000

    ICE-1799 Fixes NPE when session expires in synchronous mode

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/ViewBoundAdaptingServlet.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/ViewBoundAdaptingServlet.java
@@ -1,13 +1,20 @@
 package com.icesoft.faces.webapp.http.servlet;
 
 import com.icesoft.faces.webapp.http.common.Server;
+import com.icesoft.faces.webapp.http.common.Request;
+import com.icesoft.faces.webapp.http.common.standard.FixedXMLContentHandler;
+import com.icesoft.faces.webapp.command.Command;
+import com.icesoft.faces.webapp.command.SessionExpired;
 
 import javax.faces.FacesException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
+import java.io.Writer;
+import java.io.IOException;
 
 public class ViewBoundAdaptingServlet extends BasicAdaptingServlet {
+    private final static Command SessionExpired = new SessionExpired();
     private Map views;
     private SessionDispatcher.Listener.Monitor sessionMonitor;
 
@@ -23,20 +30,29 @@ public class ViewBoundAdaptingServlet extends BasicAdaptingServlet {
             response.sendError(500, "Cannot match view instance. 'viewNumber' parameter is missing.");
         } else {
             ServletView view = (ServletView) views.get(viewNumber);
-            try {
-                view.updateOnXMLHttpRequest(request, response);
-                sessionMonitor.touchSession();
-                super.service(request, response);
-            } catch (FacesException e) {
-                //"workaround" for exceptions zealously captured & wrapped by the JSF implementations
-                Throwable nestedException = e.getCause();
-                if (nestedException == null || nestedException instanceof Error) {
-                    throw e;
-                } else {
-                    throw (Exception) nestedException;
+            if (view == null) {
+                //todo: revisit this -- maybe the session was not created yet
+                new ServletRequestResponse(request, response).respondWith(new FixedXMLContentHandler() {
+                    public void writeTo(Writer writer) throws IOException {
+                        SessionExpired.serializeTo(writer);
+                    }
+                });
+            } else {
+                try {
+                    view.updateOnXMLHttpRequest(request, response);
+                    sessionMonitor.touchSession();
+                    super.service(request, response);
+                } catch (FacesException e) {
+                    //"workaround" for exceptions zealously captured & wrapped by the JSF implementations
+                    Throwable nestedException = e.getCause();
+                    if (nestedException == null || nestedException instanceof Error) {
+                        throw e;
+                    } else {
+                        throw (Exception) nestedException;
+                    }
+                } finally {
+                    view.release();
                 }
-            } finally {
-                view.release();
             }
         }
     }
