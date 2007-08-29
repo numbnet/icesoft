206ac7f2b974f1bcf9de77106e25d40d95f86bcc
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-08-29 21:29:04 +0000

    Close UI connection -- ICE-2059.

--- icefaces/trunk/icefaces/bridge/src/connection.js
+++ icefaces/trunk/icefaces/bridge/src/connection.js
@@ -36,6 +36,10 @@
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
     };
 
+    This.Close = function(response) {
+        response.close();
+    };
+
     This.BadResponse = function(request) {
         return request.isComplete() && !request.isResponseValid();
     }
@@ -100,6 +104,7 @@
                 request.on(Connection.Receive, this.receiveCallback);
                 request.on(Connection.BadResponse, this.badResponseCallback);
                 request.on(Connection.ServerError, this.serverErrorCallback);
+                request.on(Connection.Receive, Connection.Close);
                 this.onSendListeners.broadcast(request);
             }.bind(this));
         },
@@ -130,13 +135,19 @@
 
         sendDisposeViews: function() {
             try {
-                this.channel.postAsynchronously(this.disposeViewsURI, this.defaultQuery().asURIEncodedString(), Connection.FormPost);
+                this.channel.postAsynchronously(this.disposeViewsURI, this.defaultQuery().asURIEncodedString(), function(request) {
+                    Connection.FormPost(request);
+                    request.close();
+                });
             } catch (e) {
                 this.logger.warn('Failed to notify view disposal', e);
             }
         },
 
         shutdown: function() {
+            //shutdown once
+            this.shutdown = Function.NOOP;
+            //avoid sending XMLHTTP requests that might create new sessions on the server
             this.send = Function.NOOP;
             [ this.onSendListeners, this.onReceiveListeners, this.onServerErrorListeners, this.connectionDownListeners ].eachWithGuard(function(listeners) {
                 listeners.clear();
