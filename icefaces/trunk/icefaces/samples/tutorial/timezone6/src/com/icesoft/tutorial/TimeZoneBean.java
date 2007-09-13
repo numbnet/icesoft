f54cb302eb6adf1c602ad42e99827d5e63648246
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-09-13 22:44:56 +0000

    Bring code up to date. See issues ICE-1989 and ICE-1990.

--- icefaces/trunk/icefaces/samples/tutorial/timezone6/src/com/icesoft/tutorial/TimeZoneBean.java
+++ icefaces/trunk/icefaces/samples/tutorial/timezone6/src/com/icesoft/tutorial/TimeZoneBean.java
@@ -36,11 +36,11 @@ package com.icesoft.tutorial;
 import com.icesoft.faces.async.render.IntervalRenderer;
 import com.icesoft.faces.async.render.RenderManager;
 import com.icesoft.faces.async.render.Renderable;
-import com.icesoft.faces.context.ViewListener;
+import com.icesoft.faces.context.DisposableBean;
 import com.icesoft.faces.webapp.xmlhttp.PersistentFacesState;
 import com.icesoft.faces.webapp.xmlhttp.RenderingException;
-
-import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -51,9 +51,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.TimeZone;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * Bean backing the Time Zone application. This bean uses the RenderManager to
@@ -61,7 +60,7 @@ import org.apache.commons.logging.LogFactory;
  * during the session.
  */
 
-public class TimeZoneBean implements Renderable, ViewListener {
+public class TimeZoneBean implements Renderable, DisposableBean {
     private static Log log = LogFactory.getLog(TimeZoneBean.class);
     /**
      * The default {@link TimeZone} for this host server.
@@ -142,39 +141,38 @@ public class TimeZoneBean implements Renderable, ViewListener {
         allTimeZoneList = new ArrayList(7);
         allTimeZoneList
                 .add(new TimeZoneWrapper("Pacific/Honolulu", "GMTminus10",
-                                         hawaiiXCoords, hawaiiYCoords,
-                                         hawaiiXCoords.length));
+                        hawaiiXCoords, hawaiiYCoords,
+                        hawaiiXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("America/Anchorage", "GMTminus9",
-                                         alaskaXCoords, alaskaYCoords,
-                                         alaskaXCoords.length));
+                        alaskaXCoords, alaskaYCoords,
+                        alaskaXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("America/Los_Angeles", "GMTminus8",
-                                         pacificXCoords, pacificYCoords,
-                                         pacificXCoords.length));
+                        pacificXCoords, pacificYCoords,
+                        pacificXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("America/Denver", "GMTminus7",
-                                         mountainXCoords, mountainYCoords,
-                                         mountainXCoords.length));
+                        mountainXCoords, mountainYCoords,
+                        mountainXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("America/Chicago", "GMTminus6",
-                                         centralXCoords, centralYCoords,
-                                         centralXCoords.length));
+                        centralXCoords, centralYCoords,
+                        centralXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("America/New_York", "GMTminus5",
-                                         easternXCoords, easternYCoords,
-                                         easternXCoords.length));
+                        easternXCoords, easternYCoords,
+                        easternXCoords.length));
         allTimeZoneList
                 .add(new TimeZoneWrapper("Canada/Newfoundland", "GMTminus4",
-                                         nfldXCoords, nfldYCoords,
-                                         nfldXCoords.length));
+                        nfldXCoords, nfldYCoords,
+                        nfldXCoords.length));
 
         // create main timezone outlines
 
         checkedTimeZoneList = new ArrayList();
 
         state = PersistentFacesState.getInstance();
-        state.addViewListener(this);
         checkboxStates = new Hashtable(6);
         checkboxStates.put("GMTminus10", "false");
         checkboxStates.put("GMTminus9", "false");
@@ -328,26 +326,26 @@ public class TimeZoneBean implements Renderable, ViewListener {
     public void renderingException(RenderingException renderingException) {
         if (log.isDebugEnabled()) {
             log.debug("Rendering exception called because of " +
-                      renderingException);
+                    renderingException);
         }
         performCleanup();
     }
-    
+
     /**
      * Used to properly shut-off the ticking clock.
      *
      * @return true if properly shut-off, false if not.
      */
-    protected boolean performCleanup(){
-        try{
-            if(clock != null){
+    protected boolean performCleanup() {
+        try {
+            if (clock != null) {
                 clock.requestStop();
                 clock.remove(this);
                 clock.dispose();
                 clock = null;
             }
             return true;
-        } catch (Exception failedCleanup){
+        } catch (Exception failedCleanup) {
             if (log.isErrorEnabled()) {
                 log.error("Failed to cleanup a clock bean", failedCleanup);
             }
@@ -355,24 +353,15 @@ public class TimeZoneBean implements Renderable, ViewListener {
         }
         return false;
     }
-    
-    //
-    // ViewListener interface
-    //
-    
+
+
     /**
-     * New view has been created.
-     */
-    public void viewCreated() {
-    }
-    
-    /**
-     * Disposes a view either due to a window closing 
+     * Disposes a view either due to a window closing
      * or a timeout.
      */
-    public void viewDisposed() {
+    public void dispose() throws Exception {
         if (log.isInfoEnabled()) {
-            log.info("ViewListener of TimeZoneBean fired for a user - cleaning up");
+            log.info("Dispose TimeZoneBean for a user - cleaning up");
         }
         performCleanup();
     }
@@ -466,80 +455,80 @@ public class TimeZoneBean implements Renderable, ViewListener {
 
     private static int[] alaskaXCoords =
             {117, 118, 125, 132, 135, 138, 141, 146, 147, 157, 164, 165, 162,
-             156, 144, 120, 75, 72, 60, 45, 1, 0, 0, 14};
+                    156, 144, 120, 75, 72, 60, 45, 1, 0, 0, 14};
     private static int[] alaskaYCoords =
             {0, 4, 5, 12, 12, 8, 7, 14, 14, 28, 31, 37, 38, 41, 41, 16, 16, 25,
-             35, 38, 55, 55, 1, 0};
+                    35, 38, 55, 55, 1, 0};
 
     private static int[] pacificXCoords =
             {176, 176, 187, 187, 181, 185, 191, 192, 207, 207, 214, 214,
-             218, 222, 222, 221, 221, 222, 224, 230, 229, 225,
-             222, 219, 220, 218, 214, 214, 219, 107, 219, 232,
-             231, 230, 228, 228, 229, 228, 226, 226, 229, 231,
-             238, 233, 226, 217, 205, 198, 195, 197, 194, 187,
-             188, 189, 190, 186, 169, 152, 145, 158, 164, 164,
-             155, 141, 136, 134, 132, 125, 118, 118};
+                    218, 222, 222, 221, 221, 222, 224, 230, 229, 225,
+                    222, 219, 220, 218, 214, 214, 219, 107, 219, 232,
+                    231, 230, 228, 228, 229, 228, 226, 226, 229, 231,
+                    238, 233, 226, 217, 205, 198, 195, 197, 194, 187,
+                    188, 189, 190, 186, 169, 152, 145, 158, 164, 164,
+                    155, 141, 136, 134, 132, 125, 118, 118};
     private static int[] pacificYCoords =
             {0, 3, 3, 7, 7, 20, 19, 25, 32, 43, 47, 50, 54, 59, 64, 68, 67,
-             71, 71, 80, 86, 90, 92, 89, 90, 93, 95, 97, 106,
-             107, 112, 112, 137, 139, 140, 148, 149, 157, 158,
-             162, 163, 171, 179, 179, 171, 154, 148, 138, 133,
-             130, 130, 118, 114, 103, 88, 77, 61, 54, 41, 41,
-             37, 32, 25, 7, 9, 11, 11, 4, 4, 0};
+                    71, 71, 80, 86, 90, 92, 89, 90, 93, 95, 97, 106,
+                    107, 112, 112, 137, 139, 140, 148, 149, 157, 158,
+                    162, 163, 171, 179, 179, 171, 154, 148, 138, 133,
+                    130, 130, 118, 114, 103, 88, 77, 61, 54, 41, 41,
+                    37, 32, 25, 7, 9, 11, 11, 4, 4, 0};
 
     private static int[] mountainXCoords =
             {177, 287, 287, 268, 268, 258, 259, 249, 249, 254, 254,
-             250, 253, 250, 253, 254, 250, 250, 277, 277, 284,
-             289, 288, 290, 290, 285, 286, 281, 281, 272, 270,
-             265, 258, 256, 256, 264, 263, 268, 269, 275, 276,
-             272, 244, 216, 217, 231, 218, 226, 232, 239, 230,
-             228, 230, 230, 228, 229, 231, 233, 220, 220, 215,
-             215, 221, 220, 223, 225, 231, 231, 225, 222, 222,
-             215, 215, 208, 208, 193, 192, 185, 182, 182, 189,
-             189};
+                    250, 253, 250, 253, 254, 250, 250, 277, 277, 284,
+                    289, 288, 290, 290, 285, 286, 281, 281, 272, 270,
+                    265, 258, 256, 256, 264, 263, 268, 269, 275, 276,
+                    272, 244, 216, 217, 231, 218, 226, 232, 239, 230,
+                    228, 230, 230, 228, 229, 231, 233, 220, 220, 215,
+                    215, 221, 220, 223, 225, 231, 231, 225, 222, 222,
+                    215, 215, 208, 208, 193, 192, 185, 182, 182, 189,
+                    189};
     private static int[] mountainYCoords =
             {0, 0, 8, 8, 45, 45, 41, 40, 48, 48, 52, 52, 63, 63, 63, 69, 69,
-             75, 75, 80, 81, 86, 98, 101, 110, 116, 137, 138,
-             160, 160, 164, 161, 161, 165, 181, 190, 194, 198,
-             201, 204, 210, 214, 233, 231, 216, 208, 176, 174,
-             179, 179, 163, 158, 153, 147, 144, 140, 139, 109,
-             110, 106, 104, 98, 93, 89, 91, 93, 88, 84, 77, 70,
-             61, 53, 48, 44, 41, 30, 24, 16, 17, 8, 7, 2};
+                    75, 75, 80, 81, 86, 98, 101, 110, 116, 137, 138,
+                    160, 160, 164, 161, 161, 165, 181, 190, 194, 198,
+                    201, 204, 210, 214, 233, 231, 216, 208, 176, 174,
+                    179, 179, 163, 158, 153, 147, 144, 140, 139, 109,
+                    110, 106, 104, 98, 93, 89, 91, 93, 88, 84, 77, 70,
+                    61, 53, 48, 44, 41, 30, 24, 16, 17, 8, 7, 2};
 
     private static int[] centralXCoords =
             {288, 317, 314, 314, 321, 325, 330, 340, 336, 336, 338, 346,
-             348, 349, 350, 351, 347, 347, 357, 356, 358, 357,
-             352, 378, 380, 381, 291, 291, 269, 277, 276, 269,
-             264, 264, 257, 257, 260, 267, 270, 273, 283, 282,
-             287, 286, 292, 291, 289, 290, 284, 277, 277, 252,
-             252, 255, 255, 252, 255, 255, 251, 251, 256, 256,
-             270, 270, 289};
+                    348, 349, 350, 351, 347, 347, 357, 356, 358, 357,
+                    352, 378, 380, 381, 291, 291, 269, 277, 276, 269,
+                    264, 264, 257, 257, 260, 267, 270, 273, 283, 282,
+                    287, 286, 292, 291, 289, 290, 284, 277, 277, 252,
+                    252, 255, 255, 252, 255, 255, 251, 251, 256, 256,
+                    270, 270, 289};
     private static final int[] centralYCoords =
             {0, 0, 9, 15, 15, 26, 25, 30, 35, 74, 86, 89, 94, 111, 113, 115,
-             118, 129, 137, 145, 155, 170, 210, 242, 243, 252,
-             252, 241, 217, 210, 203, 198, 193, 189, 179, 164,
-             162, 163, 165, 163, 162, 139, 138, 116, 109, 98,
-             97, 85, 78, 78, 72, 73, 69, 69, 62, 54, 52, 47, 46,
-             41, 43, 47, 47, 9, 9};
+                    118, 129, 137, 145, 155, 170, 210, 242, 243, 252,
+                    252, 241, 217, 210, 203, 198, 193, 189, 179, 164,
+                    162, 163, 165, 163, 162, 139, 138, 116, 109, 98,
+                    97, 85, 78, 78, 72, 73, 69, 69, 62, 54, 52, 47, 46,
+                    41, 43, 47, 47, 9, 9};
 
     private static int[] easternXCoords =
             {388, 417, 446, 446, 449, 447, 448, 447, 450, 449, 442, 438,
-             431, 437, 437, 446, 447, 449, 449, 450, 451, 441,
-             429, 430, 433, 433, 435, 424, 419, 415, 415, 463,
-             464, 382, 381, 353, 360, 357, 359, 348, 348, 353,
-             349, 348, 340, 337, 337, 341, 373};
+                    431, 437, 437, 446, 447, 449, 449, 450, 451, 441,
+                    429, 430, 433, 433, 435, 424, 419, 415, 415, 463,
+                    464, 382, 381, 353, 360, 357, 359, 348, 348, 353,
+                    349, 348, 340, 337, 337, 341, 373};
     private static int[] easternYCoords =
             {0, 0, 5, 10, 16, 19, 21, 28, 36, 41, 40, 38, 44, 52, 57, 57, 51,
-             50, 54, 66, 74, 78, 81, 83, 84, 91, 96, 209, 215,
-             216, 226, 242, 255, 253, 241, 208, 154, 144, 136,
-             127, 118, 114, 110, 92, 84, 78, 35, 29, 9};
+                    50, 54, 66, 74, 78, 81, 83, 84, 91, 96, 209, 215,
+                    216, 226, 242, 255, 253, 241, 208, 154, 144, 136,
+                    127, 118, 114, 110, 92, 84, 78, 35, 29, 9};
 
     private static int[] nfldXCoords =
             {448, 465, 465, 415, 416, 418, 434, 434, 434, 432, 433, 436, 452,
-             452, 450, 453, 450, 447, 447, 444, 440, 440, 434, 440, 443, 450,
-             453, 450, 451, 448, 450, 450, 448};
+                    452, 450, 453, 450, 447, 447, 444, 440, 440, 434, 440, 443, 450,
+                    453, 450, 451, 448, 450, 450, 448};
     private static int[] nfldYCoords =
             {0, 0, 242, 242, 226, 217, 209, 95, 85, 83, 80, 82, 75, 56, 54, 51,
-             50, 52, 56, 54, 55, 50, 45, 40, 42, 42, 37, 29, 23, 20, 17, 14,
-             14};
+                    50, 52, 56, 54, 55, 50, 45, 40, 42, 42, 37, 29, 23, 20, 17, 14,
+                    14};
 } 
