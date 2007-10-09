9b979e150c59af7ba2d731aea8e4169c8100896f
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-10-09 22:20:52 +0000

    inputRichText component added [1979]

---
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/inputrichtext/InputRichText.java
@@ -0,0 +1,118 @@
+package com.icesoft.faces.component.inputrichtext;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Map;
+
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.el.ValueBinding;
+
+import com.icesoft.faces.context.JarResource;
+import com.icesoft.faces.context.Resource;
+import com.icesoft.faces.context.ResourceRegistry;
+import com.icesoft.faces.context.effects.JavascriptContext;
+
+public class InputRichText extends UIInput{
+	public static final String COMPONENET_TYPE = "com.icesoft.faces.InputRichText";
+    public static final String DEFAULT_RENDERER_TYPE = "com.icesoft.faces.InputRichTextRenderer";
+    private String language;
+    private String _for;
+    private Boolean html; 
+    private Boolean toolbarOnly;   
+	private final Resource FCK_EDITOR_BASE = new 
+	 JarResource("com/icesoft/faces/component/inputrichtext/fckeditor.zip");
+	private final Resource ICE_FCK_EDITOR_JS = new 
+	 JarResource("com/icesoft/faces/component/inputrichtext/fckeditor_ext.js");	
+	private URI baseURI;
+	 
+    public InputRichText() {
+    	ResourceRegistry registry = (ResourceRegistry) 
+    	FacesContext.getCurrentInstance();
+		try {
+			
+			baseURI = registry.registerZippedResources(FCK_EDITOR_BASE);
+			String path = baseURI + "/fckeditor.js";
+			path = path.substring(path.indexOf("block"), path.length());
+			JavascriptContext.includeLib(path, FacesContext.getCurrentInstance());
+			
+			URI jsURI = registry.registerResource("application/x-javascript",ICE_FCK_EDITOR_JS);
+			path = jsURI.getPath();
+			path = path.substring(path.indexOf("block"), path.length());
+			JavascriptContext.includeLib(path, FacesContext.getCurrentInstance());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    public String getRendererType() {
+        return DEFAULT_RENDERER_TYPE;
+    }
+    
+    public String getComponentType() {
+        return COMPONENET_TYPE;
+    }
+    
+    public void decode(FacesContext facesContext) {
+		 Map map = facesContext.getExternalContext().getRequestParameterMap();
+		 String clientId = getClientId(facesContext);
+		 if (map.containsKey(clientId)) {
+			 String newValue = map.get(clientId).toString().replace('\n', ' ');
+			 System.out.println(newValue);
+			 setSubmittedValue(newValue);			 
+		 }
+		 super.decode(facesContext);
+    }
+    
+    /**
+     * <p>Set the value of the <code>language</code> property.</p>
+     */
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    /**
+     * <p>Return the value of the <code>language</code> property.</p>
+     */
+    public String getLanguage() {
+        if (language != null) {
+            return language;
+        }
+        ValueBinding vb = getValueBinding("language");
+        return vb != null ? (String) vb.getValue(getFacesContext()) : "da";
+    }
+    
+    /**
+     * <p>Set the value of the <code>for</code> property.</p>
+     */
+    public void setFor(String _for) {
+        this._for = _for;
+    }
+
+    /**
+     * <p>Return the value of the <code>language</code> property.</p>
+     */
+    public String getFor() {
+        if (_for != null) {
+            return _for;
+        }
+        ValueBinding vb = getValueBinding("for");
+        return vb != null ? (String) vb.getValue(getFacesContext()) : "";
+    }
+	
+	public boolean isToolbarOnly() {
+        if (toolbarOnly != null) {
+            return toolbarOnly.booleanValue();
+        }
+        ValueBinding vb = getValueBinding("toolbarOnly");
+        return vb != null ?
+               ((Boolean) vb.getValue(getFacesContext())).booleanValue() :
+               false;
+	}
+
+	public void setToolbarOnly(boolean toolbarOnly) {
+		this.toolbarOnly = new Boolean(toolbarOnly);
+	}
+	public URI getBaseURI() {
+		return baseURI;
+	}	
+}
---
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/inputrichtext/InputRichTextRenderer.java
@@ -0,0 +1,67 @@
+package com.icesoft.faces.component.inputrichtext;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.w3c.dom.Element;
+
+import com.icesoft.faces.component.inputrichtext.InputRichText;
+import com.icesoft.faces.context.DOMContext;
+import com.icesoft.faces.context.effects.JavascriptContext;
+import com.icesoft.faces.renderkit.dom_html_basic.DomBasicInputRenderer;
+import com.icesoft.faces.renderkit.dom_html_basic.HTML;
+
+public class InputRichTextRenderer extends DomBasicInputRenderer{
+	 
+	 public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
+     throws IOException {
+	        String clientId = uiComponent.getClientId(facesContext);
+	        InputRichText inputRichText = (InputRichText) uiComponent;
+	        DOMContext domContext =
+	                DOMContext.attachDOMContext(facesContext, uiComponent);
+	        if (!domContext.isInitialized()) {
+	        	Element root = domContext.createRootElement(HTML.DIV_ELEM);
+	        	root.setAttribute(HTML.ID_ATTR, clientId + "container");
+	        	Element div = domContext.createElement(HTML.DIV_ELEM);
+	        	root.appendChild(div);
+	        	if(inputRichText.isToolbarOnly()) {
+	        		div.setAttribute(HTML.ID_ATTR, inputRichText.getId());
+		        	domContext.stepOver();
+		        	return;
+	        	} else {
+	        		div.setAttribute(HTML.ID_ATTR, clientId+"editor");
+	        	}
+	        	StringBuffer call = new StringBuffer();
+	        	if (inputRichText.getValue() != null) {
+	        		call.append("Ice.FCKeditor.register ('"+ clientId+"', new Ice.FCKeditor('"+ clientId+"', '','"+ inputRichText.getLanguage() +"', '"+ inputRichText.getFor()  +"', '"+inputRichText.getBaseURI().getPath() +"/'));");
+	        		call.append("Ice.FCKeditor.getInstance('"+ clientId+"').value('"+ clientId+"','');");
+	        	} else {
+	        		call.append("Ice.FCKeditor.register ('"+ clientId+"', new Ice.FCKeditor('"+ clientId+"','','"+ inputRichText.getLanguage() +"', '"+ inputRichText.getFor()  +"', '"+ inputRichText.getBaseURI().getPath() +"/'));");
+	        	}
+
+
+	        	Element textFormat = (Element) domContext.createElement(HTML.INPUT_ELEM);
+	        	textFormat.setAttribute(HTML.TYPE_ATTR, "hidden");
+	        	textFormat.setAttribute(HTML.VALUE_ATTR, "text");
+	        	textFormat.setAttribute(HTML.ID_ATTR, clientId+"Format");
+	        	textFormat.setAttribute(HTML.NAME_ATTR, clientId+"Format");
+	        	root.appendChild(textFormat);
+
+	        	Element hiddenValueHolder = domContext.createElement(HTML.INPUT_ELEM);
+	        	hiddenValueHolder.setAttribute(HTML.TYPE_ATTR, "hidden");
+	        	hiddenValueHolder.setAttribute(HTML.ID_ATTR, clientId+ "valueHolder");
+	        	root.appendChild(hiddenValueHolder);
+	        	
+	        	if (inputRichText.getValue() != null) {
+	        		hiddenValueHolder.setAttribute(HTML.VALUE_ATTR, inputRichText.getValue().toString());
+	        	}
+	        	
+	        	JavascriptContext.addJavascriptCall(facesContext, call.toString());
+	        	domContext.stepOver();
+	        }
+	 }
+
+
+}
