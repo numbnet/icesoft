f28aa5b203e21a9f7f002b8fc709e165c276e488
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-10-11 18:07:12 +0000

    adding missing file

---
+++ icefaces/trunk/icefaces/samples/component-showcase/src/com/icesoft/icefaces/samples/showcase/components/inputrichtext/InputRichTextBean.java
@@ -0,0 +1,20 @@
+package com.icesoft.icefaces.samples.showcase.components.inputrichtext;
+import javax.faces.event.ValueChangeEvent;
+
+
+public class InputRichTextBean {
+	private String value;
+
+	public void valueChange(ValueChangeEvent event) {
+		System.out.println("Value has been changed");
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
