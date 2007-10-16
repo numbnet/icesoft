4883f3680add9608b57616e7c2355ddb391318e4
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-10-16 18:08:41 +0000

    initial checkin of hot-deployable app

---
+++ projects/seam-component-showcase/branches/war-seam-comp-showcase/src/com/icesoft/icefaces/samples/showcase/layoutPanels/tabSetPanel/PanelTabSetWrapper.java
@@ -0,0 +1,15 @@
+package com.icesoft.icefaces.samples.showcase.layoutPanels.tabSetPanel;
+
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.ScopeType;
+
+@Name("tabSetWrapper")
+@Scope(ScopeType.EVENT)
+public class PanelTabSetWrapper {
+	
+	public void bind(){
+		
+	}
+
+}
