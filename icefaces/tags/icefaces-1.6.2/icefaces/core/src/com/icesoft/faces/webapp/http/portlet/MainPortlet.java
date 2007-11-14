03a83a921eb7956f81782e5ea5d0f79987f7d60d
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-11-14 20:47:18 +0000

    ICEfaces 1.6.2 official release tag.

---
+++ icefaces/tags/icefaces-1.6.2/icefaces/core/src/com/icesoft/faces/webapp/http/portlet/MainPortlet.java
@@ -0,0 +1,148 @@
+package com.icesoft.faces.webapp.http.portlet;
+
+import com.icesoft.jasper.Constants;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletException;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletRequestDispatcher;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import java.io.IOException;
+
+/**
+ * The MainPortlet is the entry point for ICEfaces-based portlets.  The goal is
+ * we set up the environment as required and then dispatch the request to the
+ * MainServlet and let the framework do all the normal processing.  It's
+ * basically only the initial page load that we care about.  The rest of the
+ * processing is handled between the ICEfaces JavaScript bridge and the
+ * MainServlet via AJAX mechanisms.  The main activities we do on the first page
+ * load are:
+ * <p/>
+ * - Get the initial view from the portlet config and set it as a request
+ * attribute.  We use the key "javax.servlet.include.request_uri" as portlets
+ * are fragrments and so to the framework, they are treated much like includes.
+ * <p/>
+ * - Get a request dispatcher for the view (typically an .iface resource) and
+ * call the include() method of the dispatcher.  By checking for the include
+ * attribute on the request, the framework should process it correctly.
+ */
+public class MainPortlet implements Portlet {
+
+    private static Log log = LogFactory.getLog(MainPortlet.class);
+
+    private static final String PORTLET_MARKER = "portlet";
+    private PortletConfig portletConfig;
+
+    public void init(PortletConfig portletConfig)
+            throws PortletException {
+
+        this.portletConfig = portletConfig;
+
+        if (log.isTraceEnabled()) {
+            log.trace("portlet config: " + portletConfig);
+        }
+    }
+
+    public void destroy() {
+
+        if( log == null ){
+            return;
+        }
+        
+        if (log.isTraceEnabled()) {
+            log.trace("portlet config: " + portletConfig);
+        }
+    }
+
+    public void processAction(
+            ActionRequest actionRequest, ActionResponse actionResponse)
+            throws IOException, PortletException {
+    }
+
+    public void render(
+            RenderRequest renderRequest, RenderResponse renderResponse)
+            throws IOException, PortletException {
+
+        //Portlets are provided in a namespace which is used to uniquely
+        //identify aspects (e.g. JavaScript, JSF component IDs) of the portlet.
+        //JSF uses the ExternalContext.encodeNamespace() method to do this.
+        //Because we are dispatching, we have to ensure that we make this
+        //setting available to the ICEfaces framework.
+        addAttribute(renderRequest, Constants.NAMESPACE_KEY, renderResponse.getNamespace());
+
+        //General marker attribute that shows that this request originated from
+        //a portlet environment.
+        addAttribute(renderRequest, Constants.PORTLET_KEY, PORTLET_MARKER);
+
+        //Get the inital view that is configured in the portlet.xml file
+        PortletMode portletMode = portletMode = renderRequest.getPortletMode();
+        String viewId = null;
+        if (portletMode == PortletMode.VIEW) {
+            viewId = portletConfig.getInitParameter(Constants.VIEW_KEY);
+            if (viewId == null) {
+                if (log.isErrorEnabled()) {
+                    log.error(Constants.VIEW_KEY + " is not properly configured");
+                }
+                throw new PortletException(Constants.VIEW_KEY + " is not properly configured");
+            }
+        } else if (portletMode == PortletMode.EDIT) {
+            viewId = portletConfig.getInitParameter(Constants.EDIT_KEY);
+            if (viewId == null) {
+                if (log.isErrorEnabled()) {
+                    log.error(Constants.EDIT_KEY + " is not properly configured");
+                }
+                throw new PortletException(Constants.EDIT_KEY + " is not properly configured");
+            }
+        } else if (portletMode == PortletMode.HELP) {
+            viewId = portletConfig.getInitParameter(Constants.HELP_KEY);
+            if (viewId == null) {
+                if (log.isErrorEnabled()) {
+                    log.error(Constants.HELP_KEY + " is not properly configured");
+                }
+                throw new PortletException(Constants.HELP_KEY + " is not properly configured");
+            }
+        }
+
+        //We request a dispatcher for the actual resource which is typically
+        //an .iface.  This maps to the proper handler, typically the ICEfaces
+        //MainServlet which takes over the processing.
+        PortletContext ctxt = portletConfig.getPortletContext();
+        PortletRequestDispatcher disp = ctxt.getRequestDispatcher(viewId);
+
+        if (disp == null) {
+            throw new PortletException("could not find dispatcher for " + viewId);
+        }
+
+        //IMPORTANT: See the JavaDoc for this class
+        PortletArtifactWrapper wrapper =
+                new PortletArtifactWrapper(portletConfig, renderRequest, renderResponse);
+        addAttribute(renderRequest, PortletArtifactWrapper.PORTLET_ARTIFACT_KEY, wrapper);
+
+        // Jack: This is a temporary fix for JBoss Portal. We should come up
+        //       with a better fix in our framework that makes sure the
+        //       Content-Type is set either before or when the
+        //       ServletExternalContext.getWriter(String encoding) method is
+        //       invoked.
+        renderResponse.setContentType("text/html");
+        disp.include(renderRequest, renderResponse);
+
+    }
+
+
+    private static void addAttribute(RenderRequest req, String key, Object value) {
+        if (key != null && value != null) {
+            req.setAttribute(key, value);
+        }
+        if (log.isTraceEnabled()) {
+            log.trace(key + ": " + value);
+        }
+    }
+
+}
---
+++ icefaces/tags/icefaces-1.6.2/icefaces/samples/component-showcase/src/com/icesoft/icefaces/samples/showcase/layoutPanels/tabSetPanel/StaticTabSetBean.java
@@ -0,0 +1,259 @@
+/*
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * "The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2006 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"
+ * License), in which case the provisions of the LGPL License are
+ * applicable instead of those above. If you wish to allow use of your
+ * version of this file only under the terms of the LGPL License and not to
+ * allow others to use your version of this file under the MPL, indicate
+ * your decision by deleting the provisions above and replace them with
+ * the notice and other provisions required by the LGPL License. If you do
+ * not delete the provisions above, a recipient may use your version of
+ * this file under either the MPL or the LGPL License."
+ *
+ */
+package com.icesoft.icefaces.samples.showcase.layoutPanels.tabSetPanel;
+
+import com.icesoft.faces.component.paneltabset.PanelTabSet;
+import com.icesoft.faces.component.paneltabset.TabChangeEvent;
+import com.icesoft.faces.component.paneltabset.TabChangeListener;
+import javax.faces.component.UIInput;
+import javax.faces.component.html.HtmlSelectOneRadio;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ValueChangeEvent;
+
+/**
+ * The StaticTabSetBean class is a backing bean for the TabbedPane showcase
+ * demonstration and is used to store the various states of the the
+ * ice:panelTabSet component.  These states are visibility, tab selection and
+ * tab placement. 
+ *
+ * @since 0.3.0
+ */
+public class StaticTabSetBean implements TabChangeListener{
+    
+    /**
+     * The demo contains three tabs and thus we need three variables to store
+     * their respective rendered states.
+     */
+    private boolean tabbedPane1Visible;
+    private boolean tabbedPane2Visible;
+    private boolean tabbedPane3Visible;
+    private HtmlSelectOneRadio selectedTabObject;
+    
+    /**
+     * Tabbed placement, possible values are "top" and "bottom", the default is
+     * "bottom".
+     */
+    private String tabPlacement = "bottom";
+
+    // default tab focus.
+    private String selectedTabFocus = "1";
+
+    /**
+     * Binding used by example to listen
+     */
+    private PanelTabSet tabSet;
+    
+    /**
+     * Return the visibility of tab panel 1.
+     *
+     * @return true if tab is visible; false, otherwise.
+     */
+    public boolean isTabbedPane1Visible() {
+        return tabbedPane1Visible;
+    }
+
+    /**
+     * Sets the tabbed pane visibility
+     *
+     * @param tabbedPane1Visible true to make the panel visible; false,
+     *                           otherwise.
+     */
+    public void setTabbedPane1Visible(boolean tabbedPane1Visible) {
+        this.tabbedPane1Visible = tabbedPane1Visible;
+    }
+
+    /**
+     * Return the visibility of tab panel 2.
+     *
+     * @return true if tab is visible; false, otherwise.
+     */
+    public boolean isTabbedPane2Visible() {
+        return tabbedPane2Visible;
+    }
+
+    /**
+     * Sets the tabbed pane visibility
+     *
+     * @param tabbedPane2Visible true to make the panel visible; false,
+     *                           otherwise.
+     */
+    public void setTabbedPane2Visible(boolean tabbedPane2Visible) {
+        this.tabbedPane2Visible = tabbedPane2Visible;
+    }
+
+    /**
+     * Return the visibility of tab panel 3.
+     *
+     * @return true if tab is visible; false, otherwise.
+     */
+    public boolean isTabbedPane3Visible() {
+        return tabbedPane3Visible;
+    }
+
+    /**
+     * Sets the tabbed pane visibility
+     *
+     * @param tabbedPane3Visible true to make the panel visible; false,
+     *                           otherwise.
+     */
+    public void setTabbedPane3Visible(boolean tabbedPane3Visible) {
+        this.tabbedPane3Visible = tabbedPane3Visible;
+    }
+    
+    /**
+     * Gets the tabbed pane object bound to this bean.
+     *
+     * @return bound tabbed pane.
+     */
+    public PanelTabSet getTabSet() {
+        return tabSet;
+    }
+    
+    /**
+     * Set a tabbed pane object which will be bound to this object
+     *
+     * @param tabSet new PanelTabSet object.
+     */
+    public void setTabSet(PanelTabSet tabSet) {
+        this.tabSet = tabSet;
+    }
+    
+    /**
+     * Called when the tab pane focus is to be changed.
+     *
+     * @param event new value is the new selected tab index.
+     */
+    public void selectTabFocus(ValueChangeEvent event) {
+        int index = Integer.parseInt((String) event.getNewValue());
+        tabSet.setSelectedIndex(index);
+    }
+    
+    /**
+     * Called when a tab is selected.
+     *
+     * @param event value is the selected tab.
+     */
+    public void selectTab(ValueChangeEvent event) {
+        UIInput component = (UIInput) event.getComponent();
+        int index = 1;
+        try {
+            index = Integer.parseInt(component.getValue().toString());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        tabSet.setSelectedIndex(index);
+    }
+    
+    /**
+     * Gets the tab placement, either top or bottom.
+     *
+     * @return top if the tab is on the top of the component, bottom if the tab
+     *         is on the bottom of the component.
+     */
+    public String getTabPlacement() {
+        return tabPlacement;
+    }
+
+    /**
+     * Sets the tab placement.
+     *
+     * @param tabPlacement two options top or bottom.
+     */
+    public void setTabPlacement(String tabPlacement) {
+        this.tabPlacement = tabPlacement;
+    }
+
+    /**
+     * Method is called when there has been a request to change the tab
+     * placement.
+     *
+     * @param event contains new tab placement data.
+     */
+    public void selectTabPlacement(ValueChangeEvent event) {
+        tabPlacement = (String) event.getNewValue();
+    }
+    
+     /**
+     * Called when the table binding's tab focus changes.
+     *
+     * @param tabChangeEvent used to set the tab focus.
+     * @throws AbortProcessingException An exception that may be thrown by event
+      * listeners to terminate the processing of the current event.
+     */
+    public void processTabChange(TabChangeEvent tabChangeEvent)
+            throws AbortProcessingException {
+        setSelectedTabFocus(String.valueOf(tabChangeEvent.getNewTabIndex()));
+        if (selectedTabObject != null) {
+            selectedTabObject.setSubmittedValue(selectedTabFocus);
+        }
+    }
+    
+    /**
+     * Gets the currently selected tab.
+     *
+     * @return selectedTabFocus of the currently selected tab.
+     */
+    public String getSelectedTabFocus() {
+        return selectedTabFocus;
+    }
+
+    /**
+     * Sets the currently selected tab.
+     *
+     * @param selectedTabFocus new selected tab.
+     */
+    public void setSelectedTabFocus(String selectedTabFocus) {
+        this.selectedTabFocus = selectedTabFocus;
+    }
+    
+    /**
+     * Gets the currently selected tab object.
+     *
+     * @return selectedTabObject of the currently selected tab.
+     */
+    public HtmlSelectOneRadio getBindSelectedTabObject() {
+        return selectedTabObject;
+    }
+    
+    /**
+     * Sets the cuurently selected tab object.
+     *
+     * @param selectedTabObject new HtmlSelectOneRadia object.
+     */
+    public void setBindSelectedTabObject(HtmlSelectOneRadio selectedTabObject) {
+        this.selectedTabObject = selectedTabObject;
+    }
+    
+}
