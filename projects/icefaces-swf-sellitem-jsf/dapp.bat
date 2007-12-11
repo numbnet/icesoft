6d2e64f84c9d34ade4fd082a974e4fde70124d06
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-12-11 18:27:11 +0000

    Not needed

--- projects/icefaces-swf-sellitem-jsf/dapp.bat
+++
@@ -1,3 +0,0 @@
-rm  c:\apache-tomcat-6.0.14\webapps\swf-sellitem-jsf.war
-rm -R c:\apache-tomcat-6.0.14\webapps\swf-sellitem-jsf
-rm -R c:\apache-tomcat-6.0.14\work\Catalina\localhost\swf-sellitem-jsf
