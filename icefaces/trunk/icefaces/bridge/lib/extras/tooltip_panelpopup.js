1e655e05179c547d5d58df16ad7ed392eb14e2b4
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2007-12-13 21:58:16 +0000

    tooltip js enhanced

--- icefaces/trunk/icefaces/bridge/lib/extras/tooltip_panelpopup.js
+++ icefaces/trunk/icefaces/bridge/lib/extras/tooltip_panelpopup.js
@@ -30,26 +30,35 @@
  * this file under either the MPL or the LGPL License."
  *
  */
+ var visibleTooltipList = new Array();
+ 
 ToolTipPanelPopup = Class.create({
-  initialize: function(srcComp, tooltipCompId, event, autoHide, delay, dynamic) {
+  initialize: function(srcComp, tooltipCompId, event, autoHide, delay, dynamic, formId) {
+   logger.info ("1- ToolTip initilized");
     this.src = srcComp;
     this.delay = delay || 500;
-    this.dynamic = (dynamic == "true"); ;
-    this.tooltip = this.getToolTipElement(tooltipCompId);
+    this.dynamic = (dynamic == "true"); 
+    this.tooltipCompId = tooltipCompId;
+    this.srcCompId = srcComp.id;
     this.autoHide = "mouseout";
+    this.x = Event.pointerX(event);
+    this.y = Event.pointerY(event);
+    this.formId = formId;
+    logger.info('event found'+ event + 'Evene x' +Event.pointerX(event));
+
     //cancel bubbling
     event.cancelBubble = true;
     //attach events
 
     if (autoHide == "onClick") {
-        this.autoHide = "click"
+        this.autoHide = "mousedown"
         this.hideEvent = this.hidePopupOnMouseClick.bindAsEventListener(this);
-          $(action).innerHTML += "event registered as "+ this.autoHide ;
+      logger.info ("event registered as "+ this.autoHide ) ;
 
     } else if (autoHide == "onExit") {
         this.autoHide = "mouseout"
         this.hideEvent = this.hidePopupOnMouseOut.bindAsEventListener(this);
-        $(action).innerHTML += "event registered as "+ this.autoHide ;
+     logger.info ("event registered as "+ this.autoHide) ;
     } else {
         this.autoHide = false;
     }
@@ -65,59 +74,95 @@ ToolTipPanelPopup = Class.create({
   },
 
   showPopup: function() {
-    this.state = "show";
+    logger.info("2- Show popup");
+    if (this.isTooltipVisible()) return;
+    this.addToVisibleList();
+
     if (this.dynamic) {
-        $("action").innerHTML += "dynamic request <br/> set hidden field tooltip_id = tooltipCompId, tooltip_src_id <br/> set tooltip_action=show\hide, <br/> set tooltip_x, <br/> set tooltip_y <br/> partialSubmit"
+    //dynamic? set status=show, populatefields, and submit
+      this.submit("show");
     } else {
-        $("action").innerHTML += "set PanelPopup's visible proprty to true"
-        this.tooltip.style.visibility = "visible";
-        this.tooltip.style.top = parseInt(this.y) + parseInt(8);
-        this.tooltip.style.left = this.x;
+        //static? just set the visibility= true 
+       var tooltip = this.getTooltip();
+       if (this.tooltipCompId.charAt(0)==':') {
+            //make visible true, so the page won't sbmuit
+             tooltip.innerHTML = this.tooltipCompId.substring(1);
+        }
+        tooltip.style.visibility = "visible";
+        tooltip.style.top = (parseInt(this.y) + parseInt(8)) +"px";
+        tooltip.style.left = this.x+"px";
+        tooltip.style.position = "absolute" ;
+        tooltip.style.display = "";
     }
-
-
+    
   },
 
   hidePopupOnMouseOut: function(event) {
-    var eventSrc = Event.element(event);
-        if(eventSrc == this.src) {
-            if (this.dynamic) {
-                $(action).innerHTML = "dynamic request<br/> call above Submitmethod with action=hide"
-            } else {
-                if (this.tooltip)
-                    this.tooltip.style.visibility = "hidden";
-            }
-        }
+    if (!this.isTooltipVisible()) return;
+    logger.info("3- Hide popup mouseout");
     this.hidePopup(event);
+    this.state = "hide";
+    this.populateFields();
+    if (this.autoHide == "mouseout") {
+        this.removedFromVisibleList();
+    }
+    this.dispose(event);
   },
 
   hidePopupOnMouseClick: function(event) {
+    if (!this.isTooltipVisible() || !Event.isLeftClick(event)) return;
+   
+    logger.info("3- Hide popup mouseclick" + this.tooltipCompId);
     var eventSrc = Event.element(event);
     if(this.srcOrchildOfSrcElement(eventSrc)) {
+       logger.info( "can not hide, its a src child");
         return;
     } else {
-        if (this.dynamic) {
-            $(action).innerHTML = "dynamic request<br/> call above Submitmethod with action=hide"
-        } else {
-            if (this.tooltip)
-                this.tooltip.style.visibility = "hidden";
-        }
+        this.hidePopup(event);
     }
-
-    this.hidePopup(event);
+    if (this.autoHide == "mousedown") {
+        logger.info("TOTAL LENGTH : " + visibleTooltipList.length);
+        this.removedFromVisibleList();
+        logger.info("TOTAL LENGTH After Remove: " + visibleTooltipList.length);
+    }
+    this.dispose(event);
   },
 
 
-  hidePopup: function(event) {
+ dispose: function(event) {
+    logger.info( "Hide pop called NEW LIST INDEX "+ visibleTooltipList.length);
+    Event.stopObserving(document, this.autoHide, this.hideEvent);
+    Event.stopObserving(document, "mousemove", this.eventMouseMove);
 
-  $(action).innerHTML += "<br/> Hide pop"+ this.autoHide;
+   },
 
-        Event.stopObserving(document, this.autoHide, this.hideEvent);
-        Event.stopObserving(document, "mousemove", this.eventMouseMove);
+  hidePopup:function(event) {
+    if(this.dynamic) {
+    //dynamic? set status=hide, populatefiels and submit 
+        this.submit("hide");
+    } else {
+        //static? set visibility = false;
+        tooltip =  this.getTooltip();
+        tooltip.style.visibility = "hidden";
+        tooltip.style.display = "none";
+    }
   },
-
+  
+  
+  submit:function(state, event) {
+      logger.info("Submitting the Form");
+      if (!event) event = new Object();
+      this.state = state;
+      this.populateFields();
+      var element = $(this.srcCompId);
+      try {
+        var form = Ice.util.findForm(element);
+        iceSubmitPartial(form,element,event);
+      } catch (e) {logger.info("Form not found" + e);}
+  },
+  
   clearTimer:function() {
-        $(action).innerHTML += "<br/> Clearing the event";
+     //   $(action).innerHTML += "<br/> Clearing the event";
         Event.stopObserving(document, "mouseout", this.clearTimerEvent);
         clearTimeout(this.timer);
 
@@ -125,45 +170,103 @@ ToolTipPanelPopup = Class.create({
 
   updateCordinate: function(event) {
     if (Event.element(event) != this.src) return;
+     logger.info( "updating cordinates"+ this.autoHide);
     this.x = Event.pointerX(event);
     this.y = Event.pointerY(event);
   },
 
   srcOrchildOfSrcElement: function(ele) {
-     if (this.tooltip  == ele) return true;
+     var tooltip =  this.getTooltip();
+     if (tooltip  == ele) return true;
      while (ele.parentNode) {
         ele = ele.parentNode;
-        if (this.tooltip  == ele){
+        if (tooltip  == ele){
             return true;
         }
      }
   },
 
-  getToolTipElement: function (tooltipCompId) {
-    if (tooltipCompId.charAt(0)==':') {
-
+  getTooltip: function () {
+    if (this.tooltipCompId.charAt(0)==':') {
         var tooltip = $("ice_tooltip_element");
         if (!tooltip) {
-
             tooltip = document.createElement('div');
+            tooltip.id = "ice_tooltip_element";
             tooltip.style.visibility = "hidden";
             tooltip.style.position = "absolute" ;
             tooltip.style.border = "1px solid" ;
             tooltip.style.backgroundColor = "white";
+            
             document.body.appendChild(tooltip);
         }
-            tooltip.innerHTML = tooltipCompId.substring(1);
         return tooltip;
     }
-    if (this.dynamic) { // component is not rendered yet so just assign the id to the component
-     $(action).innerHTML += "<br/> getTooltip called dynainc";
-        return tooltipCompId;
+      return $(this.tooltipCompId);
+  },
+  
+  populateFields: function() {
+  // the following field should be rendered by the panelPoupRenderer if rendered as tooltip
 
-    } else {
-         $(action).innerHTML += "<br/> getTooltip called not dynainc " + tooltipCompId;
-        return $(tooltipCompId);
-
+    var form = $(this.formId);
+    if (form == null) return;
+    var iceTooltipInfo = form.getElements().find( function(element) {
+        if (element.id == "iceTooltipInfo") return element;
+    });
+    if (!iceTooltipInfo) { 
+        logger.info("TOOLTIP Element not found creating one");
+	    iceTooltipInfo = document.createElement('input');
+	    iceTooltipInfo.id="iceTooltipInfo";
+	    iceTooltipInfo.name="iceTooltipInfo";            
+	    iceTooltipInfo.type="hidden";
+        form.appendChild(iceTooltipInfo);
+    }  else {
+            logger.info("TOOLTIP Element found REUSING IT");
     }
-  }
-
+    iceTooltipInfo.value = "tooltip_id=" + this.tooltipCompId + 
+                     "; tooltip_src_id="+ this.src.id+ 
+                     "; tooltip_state="+ this.state +
+                     "; tooltip_x="+ this.x +
+                     "; tooltip_y="+ this.y;
+     logger.info("Fields are populated :: "+ iceTooltipInfo.value);  
+    },
+    
+    addToVisibleList: function() {
+        if (!this.isTooltipVisible()) {
+            logger.info("ADD: NOT in the visible list ADDINGGGGGG");
+            visibleTooltipList[parseInt(visibleTooltipList.length)] = this.tooltipCompId;
+        } else {
+            logger.info("ADD: Already found in the visible list NOT ADDINGGGGGG");
+        }
+    },
+    
+    removedFromVisibleList: function() {
+     logger.info("REMOVE: TRYING TO REMOVING");
+        if (this.isTooltipVisible()) {
+             logger.info("REMOVE: FOUND IN REMOVE LIST");
+	        var newList = new Array();
+		    var index = -1;
+		    for (i=0; i < visibleTooltipList.length; i++) {
+		        logger.info ("REMOVELIST : "+ visibleTooltipList[i]);
+		        if (visibleTooltipList[i] != this.tooltipCompId) {
+		            index = parseInt(index)+ 1;
+		            newList[index] = visibleTooltipList[i];
+		        }else {
+		         logger.info("REMOVE: FOUND in the visible list REMOVING");
+		        }
+		    }
+		    visibleTooltipList = newList;
+		} else {
+		logger.info("REMOVE: NOT FOUND in the visible list CANT REMOVE");
+		}
+    },
+    
+    isTooltipVisible: function() {
+        for (i=0; i < visibleTooltipList.length; i++) {
+            if (visibleTooltipList[i]== this.tooltipCompId) {
+                return true;
+            }   
+        }
+        return false;
+    }
+    
 });
