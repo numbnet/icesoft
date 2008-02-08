2a0959add075bc7ef81834f68d4a4251471d6598
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-02-08 21:37:24 +0000

    Revert change for now. ICE-2607

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/xmlhttp/BlockingServlet.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/xmlhttp/BlockingServlet.java
@@ -33,8 +33,24 @@
 
 package com.icesoft.faces.webapp.xmlhttp;
 
-import com.icesoft.faces.webapp.http.servlet.MainServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
 //todo: deprecate this class
-public class BlockingServlet extends MainServlet {
+public class BlockingServlet extends HttpServlet {
+    private ServletContext context;
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        context = servletConfig.getServletContext();
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        context.getNamedDispatcher("Persistent Faces Servlet").forward(servletRequest, servletResponse);
+    }
 }
