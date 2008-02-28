9f9d5847bb57bd9c65bc4f2b6110b0c207f1a270
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-02-28 20:34:36 +0000

    Clear only the attributes created in the JSF environment. ICE-2700

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/portlet/PortletRequestAttributeMap.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/portlet/PortletRequestAttributeMap.java
@@ -3,9 +3,14 @@ package com.icesoft.faces.webapp.http.portlet;
 import com.icesoft.faces.context.AbstractCopyingAttributeMap;
 
 import javax.portlet.PortletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class PortletRequestAttributeMap extends AbstractCopyingAttributeMap {
+    private Collection localAttributes = new HashSet();
     private PortletRequest request;
 
     public PortletRequestAttributeMap(PortletRequest request) {
@@ -22,10 +27,21 @@ public class PortletRequestAttributeMap extends AbstractCopyingAttributeMap {
     }
 
     public void setAttribute(String name, Object value) {
+        localAttributes.add(name);
         request.setAttribute(name, value);
     }
 
     public void removeAttribute(String name) {
+        localAttributes.remove(name);
         request.removeAttribute(name);
+    }
+
+    public void clear() {
+        Iterator i = new ArrayList(localAttributes).iterator();
+        while (i.hasNext()) {
+            Object name = i.next();
+            super.remove(name);
+        }
+        localAttributes.clear();
     }
 }
