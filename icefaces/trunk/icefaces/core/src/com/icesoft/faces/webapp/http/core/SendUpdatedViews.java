6cc2ab807adfa133e358ac4beccc446455e83182
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-03-11 00:19:29 +0000

    Avoid to block responses after shutdown. ICE-2842

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/SendUpdatedViews.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/SendUpdatedViews.java
@@ -17,22 +17,31 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SendUpdatedViews implements Server {
+public class SendUpdatedViews implements Server, Runnable {
     private static final NOOP NOOPCommand = new NOOP();
-    private static final ResponseHandler NOOPResponseHandler = new FixedXMLContentHandler() {
+    private static final Runnable NOOP = new Runnable() {
+        public void run() {
+        }
+    };
+    private static final ResponseHandler NOOPResponse = new FixedXMLContentHandler() {
         public void writeTo(Writer writer) throws IOException {
             NOOPCommand.serializeTo(writer);
         }
     };
-    private static final ResponseHandler EmptyResponseHandler = new ResponseHandler() {
+    private static final ResponseHandler CloseResponse = new ResponseHandler() {
         public void respond(Response response) throws Exception {
             //let the bridge know that this blocking connection should not be re-initialized
             response.setHeader("X-Connection", "close");
             response.setHeader("Content-Length", 0);
         }
     };
-    private static final Runnable Noop = new Runnable() {
-        public void run() {
+    private static final Server AfterShutdown = new Server() {
+        public void service(Request request) throws Exception {
+            request.respondWith(CloseResponse);
+        }
+
+        public void shutdown() {
+            //do nothing
         }
     };
 
@@ -40,13 +49,11 @@ public class SendUpdatedViews implements Server {
     private final ViewQueue allUpdatedViews;
     private final String sessionID;
     private final long timeoutInterval;
-    private final Runnable inactivityMonitor;
-    private final MonitorRunner monitorRunner;
     private long responseTimeoutTime;
+    private Server server;
 
-    public SendUpdatedViews(String sessionID, final Collection synchronouslyUpdatedViews, final ViewQueue allUpdatedViews, MonitorRunner monitorRunner, Configuration configuration) {
+    public SendUpdatedViews(String sessionID, final Collection synchronouslyUpdatedViews, final ViewQueue allUpdatedViews, final MonitorRunner monitorRunner, Configuration configuration) {
         this.timeoutInterval = configuration.getAttributeAsLong("blockingConnectionTimeout", 90000);
-        this.monitorRunner = monitorRunner;
         this.sessionID = sessionID;
         this.allUpdatedViews = allUpdatedViews;
         this.allUpdatedViews.onPut(new Runnable() {
@@ -67,46 +74,50 @@ public class SendUpdatedViews implements Server {
             }
         });
 
-        this.inactivityMonitor = new Runnable() {
-            public void run() {
-                try {
-                    if ((System.currentTimeMillis() > responseTimeoutTime) && (!pendingRequest.isEmpty())) {
-                        Request request = (Request) pendingRequest.poll();
-                        if (request != null) {
-                            request.respondWith(NOOPResponseHandler);
-                        }
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
+        //add monitor
+        monitorRunner.registerMonitor(SendUpdatedViews.this);
+        //initialize blocking server
+        this.server = new Server() {
+            public void service(Request request) throws Exception {
+                responseTimeoutTime = System.currentTimeMillis() + timeoutInterval;
+                respondIfPreviousRequest(CloseResponse);
+                pendingRequest.put(request);
+            }
+
+            public void shutdown() {
+                //remove monitor
+                monitorRunner.unregisterMonitor(SendUpdatedViews.this);
+                allUpdatedViews.onPut(NOOP);
+                //avoid creating new blocking connections after shutdown
+                server = AfterShutdown;
+                respondIfPreviousRequest(CloseResponse);
             }
         };
-        //add monitor
-        this.monitorRunner.registerMonitor(inactivityMonitor);
     }
 
     public void service(final Request request) throws Exception {
-        responseTimeoutTime = System.currentTimeMillis() + timeoutInterval;
-        respondToPreviousRequest();
-        pendingRequest.put(request);
-    }
-
-    private void respondToPreviousRequest() {
-        Request previousRequest = (Request) pendingRequest.poll();
-        if (previousRequest != null) {
-            try {
-                previousRequest.respondWith(EmptyResponseHandler);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        server.service(request);
     }
 
     public void shutdown() {
-        //remove monitor
-        monitorRunner.unregisterMonitor(inactivityMonitor);
-        allUpdatedViews.onPut(Noop);
-        respondToPreviousRequest();
+        server.shutdown();
+    }
+
+    public void run() {
+        if ((System.currentTimeMillis() > responseTimeoutTime) && (!pendingRequest.isEmpty())) {
+            respondIfPreviousRequest(NOOPResponse);
+        }
+    }
+
+    private void respondIfPreviousRequest(ResponseHandler handler) {
+        Request previousRequest = (Request) pendingRequest.poll();
+        if (previousRequest != null) {
+            try {
+                previousRequest.respondWith(handler);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     private class UpdatedViewsHandler extends FixedXMLContentHandler {
