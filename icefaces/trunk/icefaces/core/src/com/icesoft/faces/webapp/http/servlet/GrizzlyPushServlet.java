f39903e5c4fa8262c20c6bc85a528200dc0e834f
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-03-28 22:27:54 +0000

    Partial Fix for JIRA ICE-2921 : Grizzly Ajax Push integration (continued)

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/GrizzlyPushServlet.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/GrizzlyPushServlet.java
@@ -33,13 +33,26 @@
 
 package com.icesoft.faces.webapp.http.servlet;
 
+import com.icesoft.faces.util.event.servlet.ContextDestroyedEvent;
+import com.icesoft.faces.util.event.servlet.ContextEventListener;
+import com.icesoft.faces.util.event.servlet.ContextEventRepeater;
+import com.icesoft.faces.util.event.servlet.ICEfacesIDDisposedEvent;
+import com.icesoft.faces.util.event.servlet.ICEfacesIDRetrievedEvent;
+import com.icesoft.faces.util.event.servlet.SessionDestroyedEvent;
+import com.icesoft.faces.util.event.servlet.ViewNumberRetrievedEvent;
 import com.icesoft.faces.webapp.http.core.ViewQueue;
 import com.sun.enterprise.web.connector.grizzly.comet.CometContext;
 import com.sun.enterprise.web.connector.grizzly.comet.CometEngine;
 import com.sun.enterprise.web.connector.grizzly.comet.CometEvent;
 import com.sun.enterprise.web.connector.grizzly.comet.CometHandler;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -47,191 +60,293 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-public class GrizzlyPushServlet
-        extends HttpServlet {
+public class GrizzlyPushServlet extends HttpServlet
+implements ContextEventListener {
+    private static final Log LOG = LogFactory.getLog(GrizzlyPushServlet.class);
 
-    private static Log log = LogFactory.getLog(GrizzlyPushServlet.class);
+    private final Map<HttpSession, EventResponder> eventResponderMap =
+        new HashMap<HttpSession, EventResponder>();
 
-    private String contextPath;
+    private CometContext cometContext;
 
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        Method getContextPathMethod;
-        ServletContext servletContext = config.getServletContext();
-        try {
-            getContextPathMethod =
-                    ServletContext.class
-                            .getMethod("getContextPath", new Class[]{});
-            contextPath = getContextPathMethod
-                    .invoke(servletContext, (Object[]) null) +
-                    "/block/receive-updated-views";
-        } catch (Exception e) {
-            throw new ServletException(
-                    "ServletContext.getContextPath not defined", e);
-        }
-        CometEngine cometEngine = CometEngine.getEngine();
-        CometContext context = cometEngine.register(contextPath);
-        //disable timeout thread
-        context.setExpirationDelay(-1);
+    public void contextDestroyed(final ContextDestroyedEvent event) {
+        // do nothing.
     }
 
     public void destroy() {
     }
 
-    protected void begin(CometEvent event, HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-    }
-
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        //We need access to the CometEvent, but this is not passed down through
-        //our dispatcher chain, so we obtain the queues directly here
-        MainSessionBoundServlet mainBound = (MainSessionBoundServlet)
-                SessionDispatcher.getSingletonSessionServlet(request.getSession());
-        ViewQueue allUpdatedViews = mainBound.getAllUpdatedViews();
-        Collection synchronouslyUpdatedViews =
-                mainBound.getSynchronouslyUpdatedViews();
-        String sessionID = mainBound.getSessionID();
-
-        /*Returning pending updated views here immediately seemed to 
-          return updated views several times until the updates were
-          fetched.
-        */
-        /*
-        if ( GrizzlyEventResponder.writeViews(
-                allUpdatedViews, synchronouslyUpdatedViews,
-                response.getWriter()) )  {
-                return;
-        }
-        */
-
-        GrizzlyEventResponder grizzlyResponder = new GrizzlyEventResponder(
-                synchronouslyUpdatedViews, allUpdatedViews, sessionID);
-        CometEngine cometEngine = CometEngine.getEngine();
-        CometContext cometContext = cometEngine.getCometContext(contextPath);
-        request.getParameter("rand");
-        grizzlyResponder.attach(response);
-        cometContext.addCometHandler(grizzlyResponder);
-    }
-
-
-}
-
-
-class GrizzlyEventResponder implements Runnable,
-        CometHandler<HttpServletResponse> {
-    ViewQueue allUpdatedViews;
-    Collection synchronouslyUpdatedViews;
-    String sessionID;
-    HttpServletResponse response;
-    boolean valid = true;
-    CometContext cometContext;
-    Writer writer;
-
-    public GrizzlyEventResponder(
-            Collection synchronouslyUpdatedViews,
-            ViewQueue allUpdatedViews, String sessionID) {
-        this.synchronouslyUpdatedViews = synchronouslyUpdatedViews;
-        this.allUpdatedViews = allUpdatedViews;
-        this.sessionID = sessionID;
-    }
-
-    /**
-     * Attach an intance of E to this class.
-     */
-    public void attach(HttpServletResponse httpResponse) {
-        response = httpResponse;
+    public void iceFacesIdDisposed(final ICEfacesIDDisposedEvent event) {
         try {
-            writer = response.getWriter();
-            response.setContentType("text/xml;charset=UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
+            Thread.sleep(1000);
+        } catch (InterruptedException exception) {
+            // ignore interrupts.
+        }
+        HttpSession session = event.getHttpSession();
+        synchronized (session) {
+            if (eventResponderMap.containsKey(session)) {
+                eventResponderMap.get(session).dispose();
+            }
         }
     }
 
-
-    /**
-     * Receive <code>CometEvent</code> notification.
-     */
-    public void onEvent(CometEvent event) throws IOException {
+    public void iceFacesIdRetrieved(final ICEfacesIDRetrievedEvent event) {
+        // do nothing.
     }
 
-
-    /**
-     * Receive <code>CometEvent</code> notification when the underlying
-     * tcp communication is started by the client
-     */
-    public void onInitialize(CometEvent event) throws IOException {
-        cometContext = event.getCometContext();
-        allUpdatedViews.onPut(this);
-    }
-
-
-    /**
-     * Receive <code>CometEvent</code> notification when the underlying
-     * tcp communication is closed by the <code>CometHandler</code>
-     */
-    public void onTerminate(CometEvent event) throws IOException {
-        onInterrupt(event);
-    }
-
-    /**
-     * Receive <code>CometEvent</code> notification when the underlying
-     * tcp communication is resumed by the Grizzly ARP.
-     */
-    public void onInterrupt(CometEvent event) throws IOException {
-        CometContext cometContext = event.getCometContext();
-        cometContext.removeCometHandler(this);
-    }
-
-    public void run() {
+    public void init(final ServletConfig servletConfig)
+    throws ServletException {
+        ContextEventRepeater.addListener(this);
+        super.init(servletConfig);
         try {
-            synchronized (this) {
-                if (!valid) {
-                    return;
-                } else {
-                    valid = false;
+            cometContext =
+                CometEngine.getEngine().register(
+                    ServletContext.class.getMethod("getContextPath").
+                        invoke(
+                            servletConfig.getServletContext(),
+                            (Object[])null) +
+                        "/block/receive-updated-views");
+            cometContext.setExpirationDelay(-1);
+        } catch (Exception exception) {
+            throw
+                new ServletException(
+                    "ServletContext.getContextPath not defined", exception);
+        }
+    }
+
+    public boolean receiveBufferedEvents() {
+        return true;
+    }
+
+    public void sessionDestroyed(final SessionDestroyedEvent event) {
+        HttpSession session = event.getHttpSession();
+        synchronized (session) {
+            if (eventResponderMap.containsKey(session)) {
+                eventResponderMap.remove(session);
+            }
+        }
+    }
+
+    public void viewNumberRetrieved(final ViewNumberRetrievedEvent event) {
+        // do nothing.
+    }
+
+    protected void service(
+        final HttpServletRequest request, final HttpServletResponse response)
+    throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setContentType("text/xml; charset=UTF-8");
+            PrintWriter writer = response.getWriter();
+            writer.write("<session-expired/>");
+            writer.flush();
+            return;
+        }
+        EventResponder eventResponder;
+        synchronized (session) {
+            if (!eventResponderMap.containsKey(session)) {
+                MainSessionBoundServlet servlet =
+                    (MainSessionBoundServlet)
+                        SessionDispatcher.getSingletonSessionServlet(session);
+                eventResponder =
+                    new EventResponder(
+                        servlet.getSessionID(),
+                        servlet.getAllUpdatedViews(),
+                        servlet.getSynchronouslyUpdatedViews(),
+                        cometContext);
+                eventResponderMap.put(session, eventResponder);
+            } else {
+                eventResponder = eventResponderMap.get(session);
+            }
+        }
+        synchronized (eventResponder.allUpdatedViews) {
+            Set<String> updatedViewSet = eventResponder.getUpdatedViews();
+            if (!updatedViewSet.isEmpty()) {
+                eventResponder.sendResponse(response, updatedViewSet);
+            } else {
+                eventResponder.attach(response);
+                cometContext.addCometHandler(eventResponder);
+            }
+        }
+    }
+
+    private static class EventResponder
+    implements CometHandler<HttpServletResponse>, Runnable {
+        private static final Log LOG = LogFactory.getLog(EventResponder.class);
+
+        private final String sessionID;
+        private final ViewQueue allUpdatedViews;
+        private final Collection synchronouslyUpdatedViews;
+        private final CometContext cometContext;
+
+        private HttpServletResponse response;
+
+        private EventResponder(
+            final String sessionID, final ViewQueue allUpdatedViews,
+            final Collection synchronouslyUpdatedViews,
+            final CometContext cometContext) {
+
+            this.sessionID = sessionID;
+            this.allUpdatedViews = allUpdatedViews;
+            this.allUpdatedViews.onPut(this);
+            this.synchronouslyUpdatedViews = synchronouslyUpdatedViews;
+            this.cometContext = cometContext;
+        }
+
+        public void attach(final HttpServletResponse response) {
+            this.response = response;
+        }
+
+        public void dispose() {
+            synchronized (allUpdatedViews) {
+                if (response != null) {
+                    Set<String> updatedViewSet = getUpdatedViews();
+//                    LOG.info("dispose():: updatedViewSet: " + updatedViewSet);
+                    if (!updatedViewSet.isEmpty()) {
+                        try {
+                            while (!cometContext.isActive(this)) {
+                                Thread.sleep(50);
+                            }
+                        } catch (InterruptedException exception) {
+                            // ignoring interrupts.
+                        }
+                        try {
+                            sendResponse(response, updatedViewSet);
+                            response = null;
+                            cometContext.resumeCometHandler(this);
+                        } catch (IOException exception) {
+                            if (LOG.isErrorEnabled()) {
+                                LOG.error(
+                                    "An I/O error occurred " +
+                                        "while trying to send a response.",
+                                    exception);
+                            }
+                        }
+                    }
+                }
+                responseSender = CONNECTION_CLOSE_SENDER;
+            }
+        }
+
+        public Set<String> getUpdatedViews() {
+            Set<String> viewIdentifierSet;
+            synchronized (allUpdatedViews) {
+                allUpdatedViews.removeAll(synchronouslyUpdatedViews);
+                synchronouslyUpdatedViews.clear();
+                viewIdentifierSet = new HashSet<String>(allUpdatedViews);
+                allUpdatedViews.clear();
+            }
+            return viewIdentifierSet;
+        }
+
+        public synchronized void onEvent(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void onInitialize(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+
+        public void onInterrupt(CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void onTerminate(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void run() {
+            synchronized (allUpdatedViews) {
+                if (response != null) {
+                    Set<String> updatedViewSet = getUpdatedViews();
+                    if (!updatedViewSet.isEmpty()) {
+                        try {
+                            while (!cometContext.isActive(this)) {
+                                Thread.sleep(50);
+                            }
+                        } catch (InterruptedException exception) {
+                            // ignoring interrupts.
+                        }
+                        try {
+                            sendResponse(response, updatedViewSet);
+                            response = null;
+                            cometContext.resumeCometHandler(this);
+                        } catch (IOException exception) {
+                            if (LOG.isErrorEnabled()) {
+                                LOG.error(
+                                    "An I/O error occurred " +
+                                        "while trying to send a response.",
+                                    exception);
+                            }
+                        }
+                    }
                 }
             }
-            writeViews(sessionID, allUpdatedViews, synchronouslyUpdatedViews, writer);
-            cometContext.resumeCometHandler(this);
-        } catch (Throwable e) {
-            e.printStackTrace();
+        }
+
+        private void sendResponse(
+            final HttpServletResponse response,
+            final Set<String> updatedViewSet)
+        throws IOException {
+            responseSender.send(sessionID, response, updatedViewSet);
+        }
+
+        private static final ResponseSender UPDATED_VIEWS_SENDER =
+            new ResponseSender() {
+                public void send(
+                    final String sessionID, final HttpServletResponse response,
+                    final Set<String> updatedViewSet)
+                throws IOException {
+                    response.setContentType("text/xml; charset=UTF-8");
+                    String[] viewIdentifiers =
+                        updatedViewSet.toArray(new String[updatedViewSet.size()]);
+                    PrintWriter writer = response.getWriter();
+                    writer.write("<updated-views>");
+                    for (int i = 0; i < viewIdentifiers.length; i++) {
+                        if (i != 0) {
+                            writer.write(' ');
+                        }
+                        writer.write(sessionID + ":" + viewIdentifiers[i]);
+                    }
+                    writer.write("</updated-views>");
+                    writer.flush();
+                }
+            };
+        private static final ResponseSender CONNECTION_CLOSE_SENDER =
+            new ResponseSender() {
+                public void send(
+                    final String sessionID, final HttpServletResponse response,
+                    final Set<String> updatedViewSet)
+                throws IOException {
+                    /*
+                     * let the bridge know that this blocking connection should
+                     * not be re-initialized...
+                     */
+                    // entity header fields
+                    response.setHeader("Content-Length", "0");
+                    // extension header fields
+                    response.setHeader("X-Connection", "close");
+                }
+            };
+
+        private ResponseSender responseSender = UPDATED_VIEWS_SENDER;
+
+        private static interface ResponseSender {
+            public void send(
+                final String sessionID,
+                final HttpServletResponse response,
+                final Set<String> updatedViewSet)
+            throws IOException;
         }
     }
-
-    public static boolean writeViews(
-            String sessionID, ViewQueue views, Collection synchronousViews, Writer writer)
-            throws IOException {
-        boolean result = false;
-        views.removeAll(synchronousViews);
-        synchronousViews.clear();
-        Set viewIdentifiers = new HashSet(views);
-        //potential race condition with views being updated before
-        //being cleared here.  We need an atomic getAndClear()
-        views.clear();
-        if (!viewIdentifiers.isEmpty()) {
-            result = true;
-            Iterator identifiers = viewIdentifiers.iterator();
-            writer.write("<updated-views>");
-            while (identifiers.hasNext()) {
-                Object identifier = identifiers.next();
-                writer.write(sessionID + ":" + identifier);
-                writer.write(' ');
-            }
-            writer.write("</updated-views>");
-            writer.flush();
-        }
-        return result;
-    }
-
 }
