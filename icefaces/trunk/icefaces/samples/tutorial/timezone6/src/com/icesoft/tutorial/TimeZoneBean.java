74bcf899c165d45ee60f5fef75c2dd9e41d468ec
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-03-28 17:46:08 +0000

    Removed over-aggressive cleanup of IntervalRenderer. If a single bean stops the renderer (that is the same renderer as every other user because it has been aquired by name) then everyone else's clocks stop too!

--- icefaces/trunk/icefaces/samples/tutorial/timezone6/src/com/icesoft/tutorial/TimeZoneBean.java
+++ icefaces/trunk/icefaces/samples/tutorial/timezone6/src/com/icesoft/tutorial/TimeZoneBean.java
@@ -39,6 +39,7 @@ import com.icesoft.faces.async.render.Renderable;
 import com.icesoft.faces.context.DisposableBean;
 import com.icesoft.faces.webapp.xmlhttp.PersistentFacesState;
 import com.icesoft.faces.webapp.xmlhttp.RenderingException;
+import com.icesoft.faces.webapp.xmlhttp.FatalRenderingException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -319,16 +320,21 @@ public class TimeZoneBean implements Renderable, DisposableBean {
     }
 
     /**
-     * Callback to inform us that there was an Exception while rendering
+     * Callback to inform us that there was an Exception while rendering.
+     * Continue from a transientRenderingException but not
+     * from a FatalRenderingException
      *
-     * @param renderingException rendering exception from framework
+     * @param renderingException render exception passed in frome framework.
      */
     public void renderingException(RenderingException renderingException) {
+
         if (log.isDebugEnabled()) {
-            log.debug("Rendering exception called because of " +
-                    renderingException);
+            log.debug("Rendering exception: ", renderingException);
         }
-        performCleanup();
+
+        if (renderingException instanceof FatalRenderingException)  {
+            performCleanup();
+        }
     }
 
     /**
@@ -339,9 +345,13 @@ public class TimeZoneBean implements Renderable, DisposableBean {
     protected boolean performCleanup() {
         try {
             if (clock != null) {
-                clock.requestStop();
                 clock.remove(this);
-                clock.dispose();
+                // whether or not this is necessary depends on how 'shutdown'
+                // you want an empty renderer. If it's emptied often, the cost
+                // of shutdown+startup is too great
+                if (clock.isEmpty() ) {
+                    clock.dispose();
+                }
                 clock = null;
             }
             return true;
