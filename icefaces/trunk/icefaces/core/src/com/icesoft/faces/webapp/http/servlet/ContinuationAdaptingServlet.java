0019b10498f026f1844fcb5ba3a5eac8df524c7d
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-04-10 21:52:41 +0000

    Fixed JIRA ICE-2980 : Jetty Continuations: some updated views responses contain no updated views

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/ContinuationAdaptingServlet.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/ContinuationAdaptingServlet.java
@@ -2,23 +2,25 @@ package com.icesoft.faces.webapp.http.servlet;
 
 import com.icesoft.faces.webapp.http.common.ResponseHandler;
 import com.icesoft.faces.webapp.http.common.Server;
-import org.mortbay.util.ajax.Continuation;
-import org.mortbay.util.ajax.ContinuationSupport;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.mortbay.util.ajax.Continuation;
+import org.mortbay.util.ajax.ContinuationSupport;
 
 public class ContinuationAdaptingServlet implements PseudoServlet {
     private Map requests = new HashMap();
     private Server server;
 
-    public ContinuationAdaptingServlet(Server server) {
+    public ContinuationAdaptingServlet(final Server server) {
         this.server = server;
     }
 
-    public void service(final HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void service(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         if (requests.containsKey(request)) {
             ResponseHandler handler = (ResponseHandler) requests.remove(request);
             handler.respond(new ServletRequestResponse(request, response));
@@ -37,17 +39,17 @@ public class ContinuationAdaptingServlet implements PseudoServlet {
         private boolean captureContinuation = true;
         private Continuation continuation;
 
-        public ContinuationRequestResponse(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        public ContinuationRequestResponse(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
             super(request, response);
         }
 
-        public synchronized void respondWith(ResponseHandler handler) throws Exception {
+        public synchronized void respondWith(final ResponseHandler handler) throws Exception {
             if (continuation == null) {
                 captureContinuation = false;
                 super.respondWith(handler);
             } else {
-                continuation.resume();
                 requests.put(request, handler);
+                continuation.resume();
             }
         }
 
