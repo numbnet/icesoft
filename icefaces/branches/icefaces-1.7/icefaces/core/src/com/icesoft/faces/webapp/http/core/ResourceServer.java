295e879b93c2a0c680acfad4fb141dd0ccc9c824
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-05-08 21:42:20 +0000

    Backport fixes for ICE-3032

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/core/ResourceServer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/core/ResourceServer.java
@@ -1,10 +1,6 @@
 package com.icesoft.faces.webapp.http.core;
 
-import com.icesoft.faces.webapp.http.common.Configuration;
-import com.icesoft.faces.webapp.http.common.FileLocator;
-import com.icesoft.faces.webapp.http.common.MimeTypeMatcher;
-import com.icesoft.faces.webapp.http.common.Request;
-import com.icesoft.faces.webapp.http.common.Server;
+import com.icesoft.faces.webapp.http.common.*;
 import com.icesoft.faces.webapp.http.common.standard.CacheControlledServer;
 import com.icesoft.faces.webapp.http.common.standard.CompressingServer;
 import com.icesoft.faces.webapp.http.common.standard.PathDispatcherServer;
@@ -18,7 +14,8 @@ public class ResourceServer implements Server {
         pathDispatcher.dispatchOn(".*xmlhttp\\/.*\\/.*\\.js$", new CacheControlledServer(new ServeJSCode()));
         pathDispatcher.dispatchOn(".*xmlhttp\\/css\\/.*", new CacheControlledServer(new ServeCSSResource(mimeTypeMatcher)));
         pathDispatcher.dispatchOn(".*xmlhttp\\/blank$", new CacheControlledServer(new ServeBlankPage()));
-        pathDispatcher.dispatchOn(".*", new FileServer(fileLocator, mimeTypeMatcher));
+        //match any path that does not point to WEB-INF directory
+        pathDispatcher.dispatchOn("^(?!.*WEB\\-INF.*).*$", new FileServer(fileLocator, mimeTypeMatcher));
         if (configuration.getAttributeAsBoolean("compressResources", true)) {
             dispatcher = new CompressingServer(pathDispatcher);
         } else {
