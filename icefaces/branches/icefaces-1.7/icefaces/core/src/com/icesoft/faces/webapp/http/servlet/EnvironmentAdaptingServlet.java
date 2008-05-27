f7148970893a2d1e450716d745405f8598e5448c
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-05-27 18:31:43 +0000

    Fixed JIRA ICE-2945 : Grizzly NIO: Integrate the GrizzlyPushServlet functionality into the ICEfaces core framework

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/EnvironmentAdaptingServlet.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/EnvironmentAdaptingServlet.java
@@ -3,33 +3,97 @@ package com.icesoft.faces.webapp.http.servlet;
 import com.icesoft.faces.webapp.http.common.Configuration;
 import com.icesoft.faces.webapp.http.common.Server;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class EnvironmentAdaptingServlet implements PseudoServlet {
+    private static final Log LOG = LogFactory.getLog(EnvironmentAdaptingServlet.class);
+    private static final Object LOCK = new Object();
+
+    private static EnvironmentAdaptingServletFactory factory;
+
     private PseudoServlet servlet;
 
-    public EnvironmentAdaptingServlet(Server server, Configuration configuration) {
-        boolean useJettyContinuationsByDefault;
-        try {
-            this.getClass().getClassLoader().loadClass("org.mortbay.util.ajax.Continuation");
-            useJettyContinuationsByDefault = true;
-        } catch (ClassNotFoundException e) {
-            useJettyContinuationsByDefault = false;
+    public EnvironmentAdaptingServlet(final Server server, final Configuration configuration, final ServletContext servletContext) {
+        if (factory == null) {
+            synchronized (LOCK) {
+                if (factory == null) {
+                    // checking if GlassFish ARP is available...
+                    boolean isGlassFishARPAvailable;
+                    try {
+                        this.getClass().getClassLoader().loadClass("com.sun.enterprise.web.connector.grizzly.comet.CometEngine");
+                        isGlassFishARPAvailable = true;
+                    } catch (ClassNotFoundException exception) {
+                        isGlassFishARPAvailable = false;
+                    }
+                    if (LOG.isInfoEnabled()) {
+                        LOG.info("GlassFish ARP available: " + isGlassFishARPAvailable);
+                    }
+                    // checking if Jetty ARP is available...
+                    boolean isJettyARPAvailable;
+                    try {
+                        this.getClass().getClassLoader().loadClass("org.mortbay.util.ajax.Continuation");
+                        isJettyARPAvailable = true;
+                    } catch (ClassNotFoundException exception) {
+                        isJettyARPAvailable = false;
+                    }
+                    if (LOG.isInfoEnabled()) {
+                        LOG.info("Jetty ARP available: " + isJettyARPAvailable);
+                    }
+                    if (isGlassFishARPAvailable && configuration.getAttributeAsBoolean("useARP", isGlassFishARPAvailable)) {
+                        LOG.info("Adapting to GlassFish ARP environment");
+                        factory = new GlassFishAdaptingServletFactory();
+                    } else if (isJettyARPAvailable && configuration.getAttributeAsBoolean("useARP", configuration.getAttributeAsBoolean("useJettyContinuations", isJettyARPAvailable))) {
+                        LOG.info("Adapting to Jetty ARP environment");
+                        factory = new JettyAdaptingServletFactory();
+                    } else {
+                        LOG.info("Adapting to Thread Blocking environment");
+                        factory = new ThreadBlockingAdaptingServletFactory();
+                    }
+                }
+            }
         }
-
-        if (configuration.getAttributeAsBoolean("useJettyContinuations", useJettyContinuationsByDefault)) {
-            servlet = new ContinuationAdaptingServlet(server);
-        } else {
-            servlet = new ThreadBlockingAdaptingServlet(server);
-        }
+        servlet = factory.newServlet(server, servletContext);
     }
 
-    public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void service(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
         servlet.service(request, response);
     }
 
     public void shutdown() {
         servlet.shutdown();
+    }
+
+    private static interface EnvironmentAdaptingServletFactory {
+        public PseudoServlet newServlet(final Server server, final ServletContext servletContext);
+    }
+
+    private static class GlassFishAdaptingServletFactory implements EnvironmentAdaptingServletFactory {
+        public PseudoServlet newServlet(final Server server, final ServletContext servletContext) {
+            try {
+                return new GlassFishAdaptingServlet(server, servletContext);
+            } catch (ServletException exception) {
+                LOG.warn("Failed to adapt to GlassFish ARP environment. Falling back to Thread Blocking environment.", exception);
+                factory = new ThreadBlockingAdaptingServletFactory();
+                return factory.newServlet(server, servletContext);
+            }
+        }
+    }
+
+    private static class JettyAdaptingServletFactory implements EnvironmentAdaptingServletFactory {
+        public PseudoServlet newServlet(final Server server, final ServletContext servletContext) {
+            return new ContinuationAdaptingServlet(server);
+        }
+    }
+
+    private static class ThreadBlockingAdaptingServletFactory implements EnvironmentAdaptingServletFactory {
+        public PseudoServlet newServlet(final Server server, final ServletContext servletContext) {
+            return new ThreadBlockingAdaptingServlet(server);
+        }
     }
 }
---
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/GlassFishAdaptingServlet.java
@@ -0,0 +1,158 @@
+package com.icesoft.faces.webapp.http.servlet;
+
+import com.icesoft.faces.webapp.http.common.Server;
+import com.icesoft.faces.webapp.http.common.ResponseHandler;
+import com.sun.enterprise.web.connector.grizzly.comet.CometContext;
+import com.sun.enterprise.web.connector.grizzly.comet.CometEngine;
+import com.sun.enterprise.web.connector.grizzly.comet.CometHandler;
+import com.sun.enterprise.web.connector.grizzly.comet.CometEvent;
+
+import java.lang.reflect.InvocationTargetException;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class GlassFishAdaptingServlet
+implements PseudoServlet {
+    private static final Log LOG =
+        LogFactory.getLog(GlassFishAdaptingServlet.class);
+
+    private final Server server;
+    private final String contextPath;
+
+    public GlassFishAdaptingServlet(
+        final Server server, final ServletContext servletContext)
+    throws ServletException {
+        this.server = server;
+        try {
+            contextPath =
+                ServletContext.class.
+                    getMethod("getContextPath", new Class[] {}).
+                        invoke(servletContext, (Object[])null) + "/";
+        } catch (NoSuchMethodException exception) {
+            throw
+                new ServletException(
+                    "No such method: ServletContext.getContextPath", exception);
+        } catch (IllegalAccessException exception) {
+            throw
+                new ServletException(
+                    "Illegal access: ServletContext.getContextPath", exception);
+        } catch (InvocationTargetException exception) {
+            throw
+                new ServletException(
+                    "Invocation target: ServletContext.getContextPath",
+                    exception);
+        }
+        CometEngine.getEngine().register(contextPath).setExpirationDelay(-1);
+    }
+
+    public void service(
+        final HttpServletRequest request, final HttpServletResponse response)
+    throws Exception {
+        GlassFishRequestResponse requestResponse =
+            new GlassFishRequestResponse(request, response);
+        server.service(requestResponse);
+        synchronized (requestResponse) {
+            if (!requestResponse.isDone()) {
+                requestResponse.park();
+                /*
+                 * This will make sure that the HTTP request and response are
+                 * being "parked" by Grizzly's Comet Engine.
+                 *
+                 * Please note that, the actual "parking" happens somewhere
+                 * after the onInitialize(CometEvent) method of the CometHandler
+                 * has been invoked.
+                 */
+                CometEngine.getEngine().register(contextPath).
+                    addCometHandler(requestResponse);
+            }
+        }
+    }
+
+    public void shutdown() {
+        server.shutdown();
+    }
+
+    private class GlassFishRequestResponse
+    extends ServletRequestResponse
+    implements CometHandler {
+        private boolean parked = false;
+        private boolean done = false;
+
+        public GlassFishRequestResponse(
+            final HttpServletRequest request,
+            final HttpServletResponse response)
+        throws Exception {
+            super(request, response);
+        }
+
+        public void attach(final Object object) {
+            // do nothing.
+        }
+
+        public boolean isDone() {
+            return done;
+        }
+
+        public boolean isParked() {
+            return parked;
+        }
+
+        public void onEvent(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void onInitialize(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void onTerminate(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void onInterrupt(final CometEvent event)
+        throws IOException {
+            // do nothing.
+        }
+
+        public void park() {
+            parked = true;
+        }
+
+        public void respondWith(final ResponseHandler handler)
+        throws Exception {
+            synchronized (this) {
+                if (!isParked()) {
+                    handler.respond(this);
+                    done = true;
+                } else {
+                    CometContext cometContext =
+                        CometEngine.getEngine().register(contextPath);
+                    while (!cometContext.isActive(this)) {
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException exception) {
+                            // ignoring interrupts...
+                        }
+                    }
+                    handler.respond(this);
+                    cometContext.resumeCometHandler(this);
+                    unpark();
+                }
+            }
+        }
+
+        public void unpark() {
+            parked = false;
+        }
+    }
+}
