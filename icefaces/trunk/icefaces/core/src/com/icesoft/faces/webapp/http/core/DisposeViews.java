9a79d1ea453165829b836fcff95d394243985222
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-06-26 10:43:20 +0000

    ICE-3229 - Refactor to send the list of disposed views using one request before unloading the document.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/DisposeViews.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/DisposeViews.java
@@ -8,18 +8,22 @@ import com.icesoft.faces.webapp.http.common.standard.OKHandler;
 import java.util.Map;
 
 public class DisposeViews implements Server {
+    private String sessionID;
     private Map views;
 
-    public DisposeViews(Map views) {
+    public DisposeViews(String sessionID, Map views) {
+        this.sessionID = sessionID;
         this.views = views;
     }
 
     public void service(Request request) throws Exception {
-        String viewIdentifier = request.getParameter("ice.view");
-        View view = (View) views.remove(viewIdentifier);
-        // Jira 1616 Logout throws NPE.
-        if (view != null) {
-            view.dispose();
+        String[] viewIdentifiers = request.getParameterAsStrings(sessionID);
+        for (int i = 0; i < viewIdentifiers.length; i++) {
+            View view = (View) views.remove(viewIdentifiers[i]);
+            // Jira 1616 Logout throws NPE.
+            if (view != null) {
+                view.dispose();
+            }
         }
 
         request.respondWith(OKHandler.HANDLER);
