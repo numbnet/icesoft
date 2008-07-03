dffaccaea33be641cbc4507d68d6c0d9fed056cf
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-07-03 17:24:29 +0000

    ICE-3016 Load messages from bundles based on chosen locale.

--- icefaces/branches/icefaces-1.7/icefaces/bridge/src/status.js
+++ icefaces/branches/icefaces-1.7/icefaces/bridge/src/status.js
@@ -138,9 +138,10 @@
     });
 
     This.OverlayIndicator = Object.subclass({
-        initialize: function(message, description, iconPath, panel) {
+        initialize: function(message, description, buttonText, iconPath, panel) {
             this.message = message;
             this.description = description;
+            this.buttonText = buttonText;
             this.iconPath = iconPath;
             this.panel = panel;
         },
@@ -188,7 +189,7 @@
 
             var buttonElement = document.createElement('input');
             buttonElement.type = 'button';
-            buttonElement.value = 'Reload';
+            buttonElement.value = this.buttonText;
             var buttonElementStyle = buttonElement.style;
             buttonElementStyle.fontSize = '11px';
             buttonElementStyle.fontWeight = 'normal';
@@ -212,14 +213,14 @@
             this.container = container;
             this.connectionLostRedirect = configuration.connectionLostRedirectURI ? new This.RedirectIndicator(configuration.connectionLostRedirectURI) : null;
             this.sessionExpiredRedirect = configuration.sessionExpiredRedirectURI ? new This.RedirectIndicator(configuration.sessionExpiredRedirectURI) : null;
-            var description = 'To reconnect click the Reload button on the browser or click the button below';
+            var messages = configuration.messages;
             var sessionExpiredIcon = configuration.connection.context + '/xmlhttp/css/xp/css-images/connect_disconnected.gif';
             var connectionLostIcon = configuration.connection.context + '/xmlhttp/css/xp/css-images/connect_caution.gif';
 
             this.busy = new This.PointerIndicator(container);
-            this.sessionExpired = this.sessionExpiredRedirect ? this.sessionExpiredRedirect : new This.OverlayIndicator('User Session Expired', description, sessionExpiredIcon, this)
-            this.connectionLost = this.connectionLostRedirect ? this.connectionLostRedirect : new This.OverlayIndicator('Network Connection Interrupted', description, connectionLostIcon, this);
-            this.serverError = new This.OverlayIndicator('Server Internal Error', description, connectionLostIcon, this)
+            this.sessionExpired = this.sessionExpiredRedirect ? this.sessionExpiredRedirect : new This.OverlayIndicator(messages.sessionExpired, messages.description, messages.buttonText, sessionExpiredIcon, this)
+            this.connectionLost = this.connectionLostRedirect ? this.connectionLostRedirect : new This.OverlayIndicator(messages.connectionLost, messages.description, messages.buttonText, connectionLostIcon, this);
+            this.serverError = new This.OverlayIndicator(messages.serverError, messages.description, messages.buttonText, connectionLostIcon, this)
             this.connectionTrouble = { on: Function.NOOP, off: Function.NOOP };
         },
 
