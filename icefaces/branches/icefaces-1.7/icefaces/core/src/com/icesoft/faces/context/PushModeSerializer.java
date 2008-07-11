453a10eac84707872c1a7ce31fb40085ed253a08
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-07-11 15:53:43 +0000

    ICE-3300 undo last changes

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/context/PushModeSerializer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/context/PushModeSerializer.java
@@ -11,9 +11,6 @@ import org.w3c.dom.Node;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.HashSet;
-import java.util.HashMap;
-import java.util.Collection;
 
 public class PushModeSerializer implements DOMSerializer {
     private Document oldDocument;
@@ -28,39 +25,10 @@ public class PushModeSerializer implements DOMSerializer {
 
     public void serialize(Document document) throws IOException {
         Node[] changed = DOMUtils.domDiff(oldDocument, document);
-        HashMap depthMaps = new HashMap();
         for (int i = 0; i < changed.length; i++) {
             Element changeRoot =
                     DOMUtils.ascendToNodeWithID(changed[i]);
-            Integer depth = new Integer(getDepth(changeRoot));
-            HashSet peers = (HashSet) depthMaps.get(depth);
-            if (null == peers)  {
-                peers = new HashSet();
-                depthMaps.put(depth, peers);
-            }
-            //place the node in a collection of all nodes
-            //at its same depth in the DOM
-            peers.add(changeRoot);
-        }
-        HashSet elementList = new HashSet();
-        Iterator allDepths = depthMaps.keySet().iterator();
-        while (allDepths.hasNext())  {
-            Integer baseDepth = (Integer) allDepths.next();
-            Iterator checkDepths = depthMaps.keySet().iterator();
-            while (checkDepths.hasNext()) {
-                Integer checkDepth = (Integer) checkDepths.next();
-                if (baseDepth.intValue() < checkDepth.intValue())  {
-                    pruneAncestors(baseDepth, (HashSet) depthMaps.get(baseDepth),
-                                   checkDepth, (HashSet) depthMaps.get(checkDepth) );
-                }
-            }
-        }
-        Iterator allDepthMaps = depthMaps.values().iterator();
-        while (allDepthMaps.hasNext())  {
-            elementList.addAll((HashSet) allDepthMaps.next());
-        }
-/*        for (int i = 0; i < changed.length; i++) {
-           for (int j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (changed[j] == null)
                     continue;
                 // If new is already in, then discard new
@@ -88,13 +56,11 @@ public class PushModeSerializer implements DOMSerializer {
                 elementList.add(element);
             }
         }
-*/
         if (!elementList.isEmpty()) {
             Iterator i = elementList.iterator();
             boolean reload = false;
             while (i.hasNext()) {
                 String tag = ((Element) i.next()).getTagName();
-//System.out.println("update node " + tag);
                 //send reload command if 'html', 'body', or 'head' elements need to be updated (see: ICE-3063)
                 reload = reload || "html".equalsIgnoreCase(tag) || "head".equalsIgnoreCase(tag);
             }
@@ -108,53 +74,6 @@ public class PushModeSerializer implements DOMSerializer {
         }
 
         oldDocument = document;
-//        System.out.println();
-    }
-
-    //prune the children by looking for ancestors in the parents collection 
-    private void pruneAncestors(Integer parentDepth, Collection parents,
-                                Integer childDepth, Collection children)  {
-//System.out.println("checking level " + parentDepth + " for children in " + childDepth);
-        Iterator parentList = parents.iterator();
-        while (parentList.hasNext())  {
-            Node parent = (Node) parentList.next();
-            Iterator childList = children.iterator();
-            while (childList.hasNext())  {
-                Node child = (Node) childList.next();
-                if (isAncestor(parentDepth, parent, childDepth, child))  {
-//System.out.println("pruning " + child);
-                    childList.remove();
-                }
-            }
-
-        }
-    }
-
-    private int getDepth(Node node)  {
-        int depth = 0;
-        Node parent = node;
-        while ((parent = parent.getParentNode()) != null) {
-            depth++;
-        }
-        return depth;
-    }
-
-    private boolean isAncestor(Integer parentDepth, Node parent,
-                               Integer childDepth, Node child)  {
-        if (!parent.hasChildNodes()) {
-            return false;
-        }
-        Node testParent = child;
-        int testDepth = childDepth.intValue();
-        int stopDepth = parentDepth.intValue();
-        while ( ((testParent = testParent.getParentNode()) != null) &&
-                (testDepth > stopDepth) ){
-            testDepth--;
-            if (testParent.equals(parent)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     private boolean isAncestor(Node test, Node child) {
