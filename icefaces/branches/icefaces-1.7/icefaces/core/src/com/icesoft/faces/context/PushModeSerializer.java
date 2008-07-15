f62f649b6a0391d8baf2681381f297d8920a6419
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-07-15 20:11:35 +0000

    keep track of duplicates in pruned ancestors (ICE-3300)

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/context/PushModeSerializer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/context/PushModeSerializer.java
@@ -11,6 +11,9 @@ import org.w3c.dom.Node;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Collection;
 
 public class PushModeSerializer implements DOMSerializer {
     private Document oldDocument;
@@ -25,55 +28,114 @@ public class PushModeSerializer implements DOMSerializer {
 
     public void serialize(Document document) throws IOException {
         Node[] changed = DOMUtils.domDiff(oldDocument, document);
+        HashMap depthMaps = new HashMap();
         for (int i = 0; i < changed.length; i++) {
             Element changeRoot =
                     DOMUtils.ascendToNodeWithID(changed[i]);
-            for (int j = 0; j < i; j++) {
-                if (changed[j] == null)
-                    continue;
-                // If new is already in, then discard new
-                if (changeRoot == changed[j]) {
-                    changeRoot = null;
-                    break;
-                }
-                // If new is parent of old, then replace old with new
-                else if (isAncestor(changeRoot, changed[j])) {
-                    changed[j] = null;
-                }
-                // If new is a child of old, then discard new
-                else if (isAncestor(changed[j], changeRoot)) {
-                    changeRoot = null;
-                    break;
+            changed[i] = changeRoot;
+            Integer depth = new Integer(getDepth(changeRoot));
+            HashSet peers = (HashSet) depthMaps.get(depth);
+            if (null == peers)  {
+                peers = new HashSet();
+                depthMaps.put(depth, peers);
+            }
+            //place the node in a collection of all nodes
+            //at its same depth in the DOM
+            peers.add(changeRoot);
+        }
+        Iterator allDepths = depthMaps.keySet().iterator();
+        while (allDepths.hasNext())  {
+            Integer baseDepth = (Integer) allDepths.next();
+            Iterator checkDepths = depthMaps.keySet().iterator();
+            while (checkDepths.hasNext()) {
+                Integer checkDepth = (Integer) checkDepths.next();
+                if (baseDepth.intValue() < checkDepth.intValue())  {
+                    pruneAncestors(baseDepth, (HashSet) depthMaps.get(baseDepth),
+                                   checkDepth, (HashSet) depthMaps.get(checkDepth) );
                 }
             }
-            changed[i] = changeRoot;
         }
 
-        ArrayList elementList = new ArrayList(changed.length);
-        for (int i = 0; i < changed.length; i++) {
-            Element element = (Element) changed[i];
-            if (null != element) {
-                elementList.add(element);
-            }
+        //Merge all remaining elements at different depths
+        //Collection is a Set so duplicates will be discarded
+        HashSet topElements = new HashSet();
+        Iterator allDepthMaps = depthMaps.values().iterator();
+        while (allDepthMaps.hasNext())  {
+            topElements.addAll((HashSet) allDepthMaps.next());
         }
-        if (!elementList.isEmpty()) {
-            Iterator i = elementList.iterator();
+        
+        if (!topElements.isEmpty()) {
             boolean reload = false;
-            while (i.hasNext()) {
-                String tag = ((Element) i.next()).getTagName();
+            int j = 0;
+            Element[] elements = new Element[topElements.size()];
+            HashSet dupCheck = new HashSet();
+            //copy the succsessful changed elements and check for change
+            //to head or body
+            for (int i = 0; i < changed.length; i++) {
+                Element element = (Element) changed[i];
+                String tag = element.getTagName();
                 //send reload command if 'html', 'body', or 'head' elements need to be updated (see: ICE-3063)
                 reload = reload || "html".equalsIgnoreCase(tag) || "head".equalsIgnoreCase(tag);
+                if (topElements.contains(element))  {
+                   if (!dupCheck.contains(element))  {
+                        dupCheck.add(element);
+                        elements[j++] = element;
+                    }
+                }
             }
             if (reload) {
                 //reload document instead of applying an update for the entire page (see: ICE-2189)
                 commandQueue.put(new Reload(viewNumber));
             } else {
-                Element[] elements = (Element[]) elementList.toArray(new Element[elementList.size()]);
                 commandQueue.put(new UpdateElements(elements));
             }
         }
 
         oldDocument = document;
+    }
+
+    //prune the children by looking for ancestors in the parents collection 
+    private void pruneAncestors(Integer parentDepth, Collection parents,
+                                Integer childDepth, Collection children)  {
+        Iterator parentList = parents.iterator();
+        while (parentList.hasNext())  {
+            Node parent = (Node) parentList.next();
+            Iterator childList = children.iterator();
+            while (childList.hasNext())  {
+                Node child = (Node) childList.next();
+                if (isAncestor(parentDepth, parent, childDepth, child))  {
+                    childList.remove();
+                }
+            }
+
+        }
+    }
+
+    private int getDepth(Node node)  {
+        int depth = 0;
+        Node parent = node;
+        while ((parent = parent.getParentNode()) != null) {
+            depth++;
+        }
+        return depth;
+    }
+
+    private boolean isAncestor(Integer parentDepth, Node parent,
+                               Integer childDepth, Node child)  {
+        if (!parent.hasChildNodes()) {
+            return false;
+        }
+        Node testParent = child;
+        int testDepth = childDepth.intValue();
+        int stopDepth = parentDepth.intValue();
+        while ( ((testParent = testParent.getParentNode()) != null) &&
+                (testDepth > stopDepth) ){
+            testDepth--;
+            if (testParent.equals(parent)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isAncestor(Node test, Node child) {
