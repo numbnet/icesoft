284f27e3b81d3c97203d900683a96740042b858e
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-07-16 16:17:15 +0000

    ICE-3229 Create message handlers per session. Change 'dispose-views' request format. Refactor & simplify.

--- icefaces/branches/icefaces-1.7.1/icefaces/ahs/src/com/icesoft/faces/async/servlet/SessionBoundServlet.java
+++ icefaces/branches/icefaces-1.7.1/icefaces/ahs/src/com/icesoft/faces/async/servlet/SessionBoundServlet.java
@@ -3,6 +3,7 @@ package com.icesoft.faces.async.servlet;
 import com.icesoft.faces.async.common.ExecuteQueue;
 import com.icesoft.faces.async.common.SessionManager;
 import com.icesoft.faces.webapp.http.common.Configuration;
+import com.icesoft.faces.webapp.http.common.Server;
 import com.icesoft.faces.webapp.http.common.standard.PathDispatcherServer;
 import com.icesoft.faces.webapp.http.servlet.EnvironmentAdaptingServlet;
 import com.icesoft.faces.webapp.http.servlet.PseudoServlet;
@@ -36,10 +37,16 @@ implements PseudoServlet {
         if (executeQueue == null) {
             throw new IllegalArgumentException("executeQueue is null");
         }
+        // todo: I should've done this differently... When revisiting this I
+        //       should create a separate DisposeViewsServer and register that
+        //       with the appropriate Request URI.
+        Server _server =
+            new SendUpdatedViewsServer(sessionManager, executeQueue, monitor);
         PathDispatcherServer _pathDispatcherServer = new PathDispatcherServer();
         _pathDispatcherServer.dispatchOn(
-            ".*block\\/receive\\-updated\\-views$",
-            new SendUpdatedViewsServer(sessionManager, executeQueue, monitor));
+            ".*block\\/receive\\-updated\\-views$", _server);
+        _pathDispatcherServer.dispatchOn(
+            ".*block\\/dispose\\-views$", _server);
         servlet =
             new EnvironmentAdaptingServlet(
                 _pathDispatcherServer, configuration, servletContext);
