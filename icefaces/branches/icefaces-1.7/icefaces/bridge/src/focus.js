bcf41cb86e549ab55e9fa4083c62bb3140ad1adc
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-07 20:27:07 +0000

    ICE-3363 Verify element IDs before moving focus.

--- icefaces/branches/icefaces-1.7/icefaces/bridge/src/focus.js
+++ icefaces/branches/icefaces-1.7/icefaces/bridge/src/focus.js
@@ -31,6 +31,11 @@
  *
  */
 
+//todo: is this really necessary?!
+Ice.isValidID = function(id) {
+    return /^\w[\w\d\-\:]*$/g.test(id);
+};
+
 //todo: implement focus management!
 var currentFocus;
 Ice.Focus = new Object();
@@ -54,7 +59,7 @@ Ice.Focus.setFocus = function(id) {
 };
 
 Ice.Focus.setFocusNow = function(id) {
-    if ((Ice.Focus.userInterupt == false) && (id != '') && (id != 'undefined')) {
+    if ((Ice.Focus.userInterupt == false) && (id != '') && (id != 'undefined') && Ice.isValidID(id)) {
         try {
             id.asExtendedElement().focus();
             setFocus(id);
