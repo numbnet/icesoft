f9e8c9912bddf633d7dec0af1e4ffd87324b6442
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-12 21:31:17 +0000

    ICE-3402 Refactor for better memory usage.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/PathDispatcher.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/PathDispatcher.java
@@ -1,71 +1,43 @@
 package com.icesoft.faces.webapp.http.servlet;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.regex.Pattern;
 
 public class PathDispatcher implements PseudoServlet {
-
-    private static Log log = LogFactory.getLog(PathDispatcher.class);
-
     private List matchers = new ArrayList();
+    private List servlets = new ArrayList();
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String path = request.getRequestURI();
-
-        if( log.isDebugEnabled() ){
-            log.debug( "path: " + path );
-        }
-
-        Iterator i = matchers.iterator();
-        boolean matched = false;
-        while (!matched && i.hasNext()) {
-            matched = ((Matcher) i.next()).serviceOnMatch(path, request, response);
-        }
-
-        if (!matched) {
-            response.sendError(404, "Resource not found.");
-        }
-    }
-
-    public void dispatchOn(String pathExpression, PseudoServlet toServer) {
-        matchers.add(new Matcher(pathExpression, toServer));
-    }
-
-    private class Matcher {
-        private Pattern pattern;
-        private PseudoServlet server;
-
-        public Matcher(String expression, PseudoServlet server) {
-            this.pattern = Pattern.compile(expression);
-            this.server = server;
-        }
-
-        boolean serviceOnMatch(String path, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ListIterator i = matchers.listIterator();
+        while (i.hasNext()) {
+            int index = i.nextIndex();
+            Pattern pattern = (Pattern) i.next();
             if (pattern.matcher(path).find()) {
+                PseudoServlet server = (PseudoServlet) servlets.get(index);
                 server.service(request, response);
-                return true;
-            } else {
-                return false;
+                return;
             }
         }
 
-        void shutdown() {
-            server.shutdown();
-        }
+        response.sendError(404, "Could not find resource at " + path);
+    }
+
+    public void dispatchOn(String pathExpression, PseudoServlet toServlet) {
+        matchers.add(Pattern.compile(pathExpression));
+        servlets.add(toServlet);
     }
 
     public void shutdown() {
-        Iterator i = matchers.iterator();
+        Iterator i = servlets.iterator();
         while (i.hasNext()) {
-            Matcher matcher = (Matcher) i.next();
-            matcher.shutdown();
+            PseudoServlet servlet = (PseudoServlet) i.next();
+            servlet.shutdown();
         }
     }
 }
