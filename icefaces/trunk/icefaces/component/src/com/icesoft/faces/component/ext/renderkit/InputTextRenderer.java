a2a0753709220a86e056aa935b20fbf5ff073163
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-22 16:03:47 +0000

    Converter fixed related to ICE-3321

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/InputTextRenderer.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/InputTextRenderer.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import javax.faces.event.ActionEvent;
 
 import com.icesoft.faces.component.IceExtended;
 import com.icesoft.faces.component.ext.HtmlInputText;
+import com.icesoft.faces.component.ext.KeyEvent;
+import com.icesoft.faces.component.ext.taglib.Util;
 import com.icesoft.faces.context.effects.LocalEffectEncoder;
 import com.icesoft.faces.renderkit.dom_html_basic.DomBasicRenderer;
 import com.icesoft.faces.renderkit.dom_html_basic.HTML;
 import com.icesoft.faces.renderkit.dom_html_basic.PassThruAttributeWriter;
 
-public class InputTextRenderer extends com.icesoft.faces.renderkit.dom_html_basic.InputTextRender {
+public class InputTextRenderer extends com.icesoft.faces.renderkit.dom_html_basic.InputTextRenderer {
     
     //private static final String[] passThruAttributes = ExtendedAttributeConstants.getAttributes(ExtendedAttributeConstants.ICE_INPUTTEXT);
     //handled HTML.ONKEYPRESS_ATTR, HTML.ONFOCUS_ATTR, HTML.ONBLUR_ATTR
@@ -52,4 +55,37 @@ public class InputTextRenderer extends com.icesoft.faces.renderkit.dom_html_basi
         LocalEffectEncoder.encodeLocalEffects(uiComponent, writer,
                 FacesContext.getCurrentInstance());
     }
+    
+    public void decode(FacesContext facesContext, UIComponent uiComponent) {
+        super.decode(facesContext, uiComponent);
+        Object focusId = facesContext.getExternalContext()
+                .getRequestParameterMap().get(FormRenderer.getFocusElementId());
+        if (focusId != null) {
+            if (focusId.toString()
+                    .equals(uiComponent.getClientId(facesContext))) {
+                ((HtmlInputText) uiComponent).setFocus(true);
+            } else {
+                ((HtmlInputText) uiComponent).setFocus(false);
+            }
+        }
+
+        if (Util.isEventSource(facesContext, uiComponent)) {
+            queueEventIfEnterKeyPressed(facesContext, uiComponent);
+        }
+    }
+
+
+    public void queueEventIfEnterKeyPressed(FacesContext facesContext,
+                                            UIComponent uiComponent) {
+        try {
+            KeyEvent keyEvent =
+                    new KeyEvent(uiComponent, facesContext.getExternalContext()
+                .getRequestParameterMap());
+            if (keyEvent.getKeyCode() == KeyEvent.CARRIAGE_RETURN) {
+                uiComponent.queueEvent(new ActionEvent(uiComponent));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }    
 }
