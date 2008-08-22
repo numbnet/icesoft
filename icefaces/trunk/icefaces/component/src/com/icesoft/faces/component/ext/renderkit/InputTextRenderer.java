26ae949e5afd89577b15f9c69502c3c87def92ce
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-22 20:10:49 +0000

    JSHandler fixed, related to ICE-3321

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/InputTextRenderer.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/InputTextRenderer.java
@@ -7,53 +7,28 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.event.ActionEvent;
 
-import com.icesoft.faces.component.IceExtended;
+import com.icesoft.faces.component.ExtendedAttributeConstants;
 import com.icesoft.faces.component.ext.HtmlInputText;
 import com.icesoft.faces.component.ext.KeyEvent;
 import com.icesoft.faces.component.ext.taglib.Util;
 import com.icesoft.faces.context.effects.LocalEffectEncoder;
-import com.icesoft.faces.renderkit.dom_html_basic.DomBasicRenderer;
-import com.icesoft.faces.renderkit.dom_html_basic.HTML;
 import com.icesoft.faces.renderkit.dom_html_basic.PassThruAttributeWriter;
 
 public class InputTextRenderer extends com.icesoft.faces.renderkit.dom_html_basic.InputTextRenderer {
     
-    //private static final String[] passThruAttributes = ExtendedAttributeConstants.getAttributes(ExtendedAttributeConstants.ICE_INPUTTEXT);
-    //handled HTML.ONKEYPRESS_ATTR, HTML.ONFOCUS_ATTR, HTML.ONBLUR_ATTR
-    private static final String[] passThruAttributes = 
-            new String[]{ HTML.ACCESSKEY_ATTR, HTML.ALT_ATTR, HTML.AUTOCOMPLETE_ATTR, HTML.DIR_ATTR, HTML.LANG_ATTR,  HTML.MAXLENGTH_ATTR ,  HTML.ONCHANGE_ATTR,  HTML.ONCLICK_ATTR,  HTML.ONDBLCLICK_ATTR, HTML.ONKEYDOWN_ATTR,  HTML.ONKEYUP_ATTR,  HTML.ONMOUSEDOWN_ATTR,  HTML.ONMOUSEMOVE_ATTR,  HTML.ONMOUSEOUT_ATTR,  HTML.ONMOUSEOVER_ATTR,  HTML.ONMOUSEUP_ATTR,  HTML.ONSELECT_ATTR,  HTML.SIZE_ATTR,  HTML.STYLE_ATTR,  HTML.TABINDEX_ATTR,  HTML.TITLE_ATTR };                     
+    private static final String[] passThruAttributes = ExtendedAttributeConstants.getAttributes(ExtendedAttributeConstants.ICE_INPUTTEXT);
            
     public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
             throws IOException {
+        super.encodeBegin(facesContext, uiComponent);
         ResponseWriter writer = facesContext.getResponseWriter();
-        String clientId = uiComponent.getClientId(facesContext);
-        writer.startElement(HTML.INPUT_ELEM, uiComponent);
-        writer.writeAttribute(HTML.ID_ATTR, clientId, HTML.ID_ATTR);
-
-        PassThruAttributeWriter.renderHtmlAttributes(writer, uiComponent, passThruAttributes);
-
-        writer.writeAttribute(HTML.NAME_ATTR, clientId, null);
-        Object styleClass = uiComponent.getAttributes().get("styleClass");
-        if (styleClass != null) {
-            writer.writeAttribute(HTML.CLASS_ATTR, styleClass, null);
-        }
-
-        String onkeypress = ((HtmlInputText) uiComponent).getOnkeypress() != null ? ((HtmlInputText) uiComponent).getOnkeypress() : "";
-        String onfocus = ((HtmlInputText) uiComponent).getOnfocus() != null ? ((HtmlInputText) uiComponent).getOnfocus() : "";
-        String onblur = ((HtmlInputText) uiComponent).getOnblur() != null ? ((HtmlInputText) uiComponent).getOnblur() : "";
-
-        writer.writeAttribute(HTML.ONKEYPRESS_ATTR, onkeypress + DomBasicRenderer.ICESUBMIT, null);
-        writer.writeAttribute(HTML.ONFOCUS_ATTR, onfocus + "setFocus(this.id);", null);
-
-
-        if (((IceExtended) uiComponent).getPartialSubmit()) {
-            writer.writeAttribute("onblur", onblur + "setFocus('');" +
-                    "iceSubmitPartial(form,this,event); return false;", null);
-        } else {
-            writer.writeAttribute(HTML.ONBLUR_ATTR, onblur + "setFocus('');", null);
-        }
         LocalEffectEncoder.encodeLocalEffects(uiComponent, writer,
                 FacesContext.getCurrentInstance());
+    }
+    
+    protected void renderHtmlAttributes(ResponseWriter writer, UIComponent uiComponent)
+            throws IOException{
+        PassThruAttributeWriter.renderHtmlAttributes(writer, uiComponent, passThruAttributes);
     }
     
     public void decode(FacesContext facesContext, UIComponent uiComponent) {
