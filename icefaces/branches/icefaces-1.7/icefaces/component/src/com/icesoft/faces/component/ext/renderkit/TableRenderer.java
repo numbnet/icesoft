b3f8d726f810f26eac725951f010ee90844ae4b0
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-28 21:48:10 +0000

    Fix for ICE-3433 (Add a style for the sorted header (datatable)) User supplied patch applied

--- icefaces/branches/icefaces-1.7/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/TableRenderer.java
+++ icefaces/branches/icefaces-1.7/icefaces/component/src/com/icesoft/faces/component/ext/renderkit/TableRenderer.java
@@ -35,6 +35,7 @@ package com.icesoft.faces.component.ext.renderkit;
 
 import com.icesoft.faces.component.CSS_DEFAULT;
 import com.icesoft.faces.component.PORTLET_CSS_DEFAULT;
+import com.icesoft.faces.component.commandsortheader.CommandSortHeader;
 import com.icesoft.faces.component.ext.HeaderRow;
 import com.icesoft.faces.component.ext.ColumnGroup;
 import com.icesoft.faces.component.ext.HtmlDataTable;
@@ -325,6 +326,25 @@ public class TableRenderer
                     } else {
                         styleClass = baseClass.replaceAll(sourceClass, sourceClass+"Col ")+ styleClass;                        
                     }
+                    String sortColumn = htmlDataTable.getSortColumn();
+
+                    if (sortColumn != null) {
+                        Iterator it = column.getChildren().iterator();
+
+                        while (it.hasNext()) {
+                            UIComponent columnChild = (UIComponent) it.next();
+
+                            if (columnChild instanceof CommandSortHeader) {
+                                String columnName = ((CommandSortHeader) columnChild).getColumnName();
+
+                                if (sortColumn.equals(columnName)) {
+                                    styleClass += CSS_DEFAULT.TABLE_ACTIVE_SORT_COLUMN ;
+                                }
+
+                                break;
+                            }
+                        }
+                    }                     
                     th.setAttribute(HTML.CLASS_ATTR, styleClass);
                     Integer colspan = null;
                     try {
@@ -437,7 +457,18 @@ public class TableRenderer
             cursorParent = columnHeaderDiv;
         }
         if ("header".equalsIgnoreCase(facet) ){
-            th.setAttribute("class",getHeaderStyles(uiComponent)[styleIndex]);
+            String styles = this.getHeaderStyles(uiComponent)[styleIndex];
+            String sortColumn = htmlDataTable.getSortColumn();
+            UIComponent headerFacet = nextColumn.getFacet("header");
+
+            if (sortColumn != null && (headerFacet instanceof CommandSortHeader)) {
+                String columnName = ((CommandSortHeader) headerFacet).getColumnName();
+
+                if (sortColumn.equals(columnName)) {
+                    styles += CSS_DEFAULT.TABLE_ACTIVE_SORT_COLUMN;
+                }
+            }            
+            th.setAttribute("class",styles);
         } else {
             th.setAttribute("class",getFooterClass(htmlDataTable));
         }
