c5af2a5b5351d577a6f538a334baf9928a6c672d
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-08-29 20:01:53 +0000

    ICE-3344 Make event handler return false to cancel default action for anchors.

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/core/MultiViewServer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/webapp/http/core/MultiViewServer.java
@@ -39,15 +39,15 @@ public class MultiViewServer implements Server {
             String redirectViewNumber = request.getParameter("rvn");
             //synchronize to atomically test and possibly put new View into the map
             synchronized (views) {
-            view = (View) views.get(redirectViewNumber);
-            if (view == null) {
-                view = new View(redirectViewNumber, sessionID, request, asynchronouslyUpdatedViews, configuration, sessionMonitor, resourceDispatcher);
-                views.put(redirectViewNumber, view);
-                ContextEventRepeater.viewNumberRetrieved(session, sessionID, Integer.parseInt(redirectViewNumber));
-            } else {
-                view.updateOnPageRequest(request);
-                view.switchToNormalMode();
-            }
+                view = (View) views.get(redirectViewNumber);
+                if (view == null) {
+                    view = new View(redirectViewNumber, sessionID, request, asynchronouslyUpdatedViews, configuration, sessionMonitor, resourceDispatcher);
+                    views.put(redirectViewNumber, view);
+                    ContextEventRepeater.viewNumberRetrieved(session, sessionID, Integer.parseInt(redirectViewNumber));
+                } else {
+                    view.updateOnPageRequest(request);
+                    view.switchToNormalMode();
+                }
             }
         } else {
             String viewNumber = String.valueOf(++viewCount);
