c8e286e348a6f4b0c8f04a7707721a8b62a38e66
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-09-17 22:45:13 +0000

    ICE-3528 Added rendering of disabled and readonly attributes.

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/renderkit/dom_html_basic/MenuRenderer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/renderkit/dom_html_basic/MenuRenderer.java
@@ -373,6 +373,14 @@ public class MenuRenderer extends DomBasicInputRenderer {
 
         PassThruAttributeRenderer.renderAttributes(facesContext, uiComponent,
                                                    getExcludesArray(excludes));
+        String[] attributes = new String[]{HTML.DISABLED_ATTR, HTML.READONLY_ATTR};
+        Object attribute;
+        for (int i = 0; i < attributes.length; i++) {
+            attribute = uiComponent.getAttributes().get(attributes[i]);
+            if (attribute instanceof Boolean && ((Boolean) attribute).booleanValue()) {
+                root.setAttribute(attributes[i], attributes[i]);
+            }
+        }
         excludes.clear();
 
         domContext.stepInto(uiComponent);
