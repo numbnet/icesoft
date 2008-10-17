12fbaf558ddb3c8125c1473ca950c5782f7521b1
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-10-17 20:16:05 +0000

    ICE-3308 Reverted changes for now because they caused regressions for ICE-2722.

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopupRenderer.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopupRenderer.java
@@ -242,7 +242,6 @@ public class PanelPopupRenderer extends GroupRenderer {
 
 		// Rebroadcast Javascript to survive refresh
 		if (dndType != null) {
-            JavascriptContext.addJavascriptCall(facesContext, "Ice.DnD.adjustPosition('" + uiComponent.getClientId(facesContext) + "');");
             String call = addJavascriptCalls(uiComponent, "DRAG", handleId,
 					facesContext);
 			JavascriptContext.addJavascriptCall(facesContext, call);
