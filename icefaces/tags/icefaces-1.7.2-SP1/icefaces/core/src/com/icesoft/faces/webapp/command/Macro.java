1359dec8e7338c892effa2acaa3aaca13956862a
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2008-11-13 21:23:18 +0000

    ICEfaces 1.7.2 SP1 official release tag.

---
+++ icefaces/tags/icefaces-1.7.2-SP1/icefaces/core/src/com/icesoft/faces/webapp/command/Macro.java
@@ -0,0 +1,77 @@
+package com.icesoft.faces.webapp.command;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Macro implements Command {
+    private Collection commands = new ArrayList();
+
+    public Macro(Command commandA, Command commandB) {
+        commands.add(commandA);
+        commands.add(commandB);
+    }
+
+    public Macro(Command[] commands) {
+        this.commands.addAll(Arrays.asList(commands));
+    }
+
+    public Command coalesceWith(Command command) {
+        return command.coalesceWith(this);
+    }
+
+    public Command coalesceWith(UpdateElements updateElements) {
+        commands.add(updateElements);
+        return this;
+    }
+
+    public Command coalesceWith(Redirect redirect) {
+        commands.add(redirect);
+        return this;
+    }
+
+    public Command coalesceWith(Reload reload) {
+        commands.add(reload);
+        return this;
+    }
+
+    public Command coalesceWith(SessionExpired sessionExpired) {
+        return sessionExpired;
+    }
+
+    public Command coalesceWith(Macro macro) {
+        commands.addAll(macro.commands);
+        return this;
+    }
+
+    public void addCommand(Command command) {
+        commands.add(command);
+    }
+
+    public Command coalesceWith(SetCookie setCookie) {
+        commands.add(setCookie);
+        return this;
+    }
+
+    public Command coalesceWith(Pong pong) {
+        commands.add(pong);
+        return this;
+    }
+
+    public Command coalesceWith(NOOP noop) {
+        return this;
+    }
+
+    public void serializeTo(Writer writer) throws IOException {
+        Iterator i = commands.iterator();
+        writer.write("<macro>");
+        while (i.hasNext()) {
+            Command command = (Command) i.next();
+            command.serializeTo(writer);
+        }
+        writer.write("</macro>");
+    }
+}
