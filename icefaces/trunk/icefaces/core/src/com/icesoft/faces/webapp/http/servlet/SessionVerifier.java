35ae67443615024b7d71a79aadf345276af40760
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-01-12 12:55:23 +0000

    ICE-3871 Modify SessionVerifier.class to also send normal HTTP error responses for coresponding resource requests.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/SessionVerifier.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/servlet/SessionVerifier.java
@@ -1,6 +1,7 @@
 package com.icesoft.faces.webapp.http.servlet;
 
 import com.icesoft.faces.webapp.http.common.standard.ResponseHandlerServer;
+import com.icesoft.faces.webapp.http.core.SessionExpiredException;
 import com.icesoft.faces.webapp.http.core.SessionExpiredResponse;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,16 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 public class SessionVerifier implements PseudoServlet {
     private static final PseudoServlet SessionExpiredServlet = new BasicAdaptingServlet(new ResponseHandlerServer(SessionExpiredResponse.Handler));
     private PseudoServlet servlet;
+    private boolean xmlResponse;
 
-    public SessionVerifier(PseudoServlet servlet) {
+    public SessionVerifier(PseudoServlet servlet, boolean xmlResponse) {
         this.servlet = servlet;
+        this.xmlResponse = xmlResponse;
     }
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (request.isRequestedSessionIdValid()) {
             servlet.service(request, response);
         } else {
-            SessionExpiredServlet.service(request, response);
+            if (xmlResponse) {
+                SessionExpiredServlet.service(request, response);
+            } else {
+                throw new SessionExpiredException();
+            }
         }
     }
 
