93207030ae657f6ca6cf07b81f8bc0ff6d25795f
frank.ye <frank.ye@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-02 22:13:07 +0000

    ICE-3768 fail duplicate tag name

--- icefaces/trunk/icefaces/component/test/java/com/icesoft/faces/metadata/RendererTypeTest.java
+++ icefaces/trunk/icefaces/component/test/java/com/icesoft/faces/metadata/RendererTypeTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestSuite;
 
 import com.sun.rave.jsfmeta.beans.ComponentBean;
 import com.sun.rave.jsfmeta.beans.RendererBean;
+import java.util.HashMap;
 import javax.faces.component.UIComponent;
 
 public class RendererTypeTest extends ICECompsTestCase {
@@ -28,7 +29,7 @@ public class RendererTypeTest extends ICECompsTestCase {
         UIComponent[] uiComponentBases = new UIComponent[oldUIComponentBases.length];
         System.arraycopy(oldUIComponentBases, 0, uiComponentBases, 0, oldUIComponentBases.length);
 
-
+        HashMap tagNameMap = new HashMap();
         for (int i = 0; i < componentBeans.length; i++) {
             String renderTypeUIComponent = uiComponentBases[i].getRendererType();
             String renderTypeComponentBean = componentBeans[i].getRendererType();
@@ -41,6 +42,21 @@ public class RendererTypeTest extends ICECompsTestCase {
 
             RendererBean myRendererBean = ICECompsListHelper.getRenderer(uiComponentBases[i].getFamily(), uiComponentBases[i].getRendererType());
 
+            if (myRendererBean == null || myRendererBean.getRendererType() == null || myRendererBean.getRendererType() == null) {
+                //TODO fail this situation.
+                message = "No Renderer defined for " + uiComponentBases[i].getClass().getName();
+            //System.out.println(""+message);
+            //fail(message);
+            } else {
+                if (renderTypeUIComponent == null) {
+                    if (!tagNameMap.containsKey(myRendererBean.getTagName())) {
+                        tagNameMap.put(myRendererBean.getTagName(), myRendererBean.getTagName());
+                    } else {
+                        message = "Tag name="+myRendererBean.getTagName()+" already defined, please check component =" + uiComponentBases[i].getClass().getName();
+                        fail(message);
+                    }
+                }
+            }
             if (myRendererBean != null && myRendererBean.getRendererType() != null) {
                 boolean notSameType = renderTypeUIComponent != null && !(renderTypeUIComponent.trim().equalsIgnoreCase(myRendererBean.getRendererType()));
                 message = "RenderType not the same for Component Class=" + componentBeans[i].getComponentClass() + "\n component renderType=" + renderTypeUIComponent + "\n faces-config declared renderType=" + renderTypeComponentBean +
