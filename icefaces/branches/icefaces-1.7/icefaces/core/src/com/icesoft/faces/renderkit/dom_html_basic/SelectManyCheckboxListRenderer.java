aefacc181e7cd214c69b469ce2905eec0d4aebf6
mark.collette <mark.collette@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-02 22:03:38 +0000

    ICE-3977 : Preselection of radio or checkbox with layout=spread not working

--- icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/renderkit/dom_html_basic/SelectManyCheckboxListRenderer.java
+++ icefaces/branches/icefaces-1.7/icefaces/core/src/com/icesoft/faces/renderkit/dom_html_basic/SelectManyCheckboxListRenderer.java
@@ -275,7 +275,7 @@ public class SelectManyCheckboxListRenderer extends MenuRenderer {
             throw new IllegalStateException("Could not find select items for UISelectMany component.");
         }
         
-        Object[] submittedValue = getSubmittedSelectedValues(uiComponent);
+        Object[] submittedValue = getSubmittedSelectedValues(forComponent);
         Object componentValue = (submittedValue != null) ? null :
             getCurrentSelectedValues(forComponent);
 
