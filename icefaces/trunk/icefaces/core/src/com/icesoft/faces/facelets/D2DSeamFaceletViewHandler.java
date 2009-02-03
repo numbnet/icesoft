bfefd8e48819f5af9703268fd81cebef82b6a846
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-03 20:12:44 +0000

    ICE-2147 Delegate calculateLocale() call to SeamViewHandler. LocaleSelector assumes that SeamViewHandler is the ViewHandler.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/facelets/D2DSeamFaceletViewHandler.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/facelets/D2DSeamFaceletViewHandler.java
@@ -1,7 +1,5 @@
 package com.icesoft.faces.facelets;
 
-import javax.faces.application.ViewHandler;
-
 import com.icesoft.util.SeamUtilities;
 import com.sun.facelets.compiler.Compiler;
 import com.sun.facelets.compiler.SAXCompiler;
@@ -9,14 +7,18 @@ import com.sun.facelets.impl.ResourceResolver;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import javax.faces.application.ViewHandler;
+import javax.faces.context.FacesContext;
+import java.util.Locale;
+
 /**
  * @author ICEsoft Technologies, Inc.
  */
 public class D2DSeamFaceletViewHandler extends D2DFaceletViewHandler {
 
     private static final String SEAM_EXPRESSION_FACTORY = "org.jboss.seam.ui.facelet.SeamExpressionFactory";
-    
-     // Log instance for this class
+
+    // Log instance for this class
     private static Log log = LogFactory.getLog(D2DSeamFaceletViewHandler.class);
 
     public D2DSeamFaceletViewHandler(ViewHandler delegate) {
@@ -24,25 +26,25 @@ public class D2DSeamFaceletViewHandler extends D2DFaceletViewHandler {
     }
 
     protected void faceletInitialize() {
-    /*
-     * If Seam1.2.1 or before then the SeamExpressionFactory exists in Seam so instantiate
-     * the faceletFactory that way; otherwise, just initialize the compiler without any
-     * options for Seam1.3.0.  
-     * Need SeamUtilities class to determine if we require SeamExpressionFactory
-     * 
-     */
+        /*
+        * If Seam1.2.1 or before then the SeamExpressionFactory exists in Seam so instantiate
+        * the faceletFactory that way; otherwise, just initialize the compiler without any
+        * options for Seam1.3.0.
+        * Need SeamUtilities class to determine if we require SeamExpressionFactory
+        *
+        */
         try {
-            if( faceletFactory == null ) {
+            if (faceletFactory == null) {
                 com.sun.facelets.compiler.Compiler c = new SAXCompiler();
-                if ( SeamUtilities.requiresSeamExpressionFactory()){              
-                    c.setFeature( Compiler.EXPRESSION_FACTORY, SEAM_EXPRESSION_FACTORY );
+                if (SeamUtilities.requiresSeamExpressionFactory()) {
+                    c.setFeature(Compiler.EXPRESSION_FACTORY, SEAM_EXPRESSION_FACTORY);
                 } //else just initialize the compiler without any extra seam settings
-                initializeCompiler( c );
-                faceletFactory = createFaceletFactory( c );
+                initializeCompiler(c);
+                faceletFactory = createFaceletFactory(c);
             }
         }
         catch (Throwable t) {
-            if( log.isDebugEnabled() ) {
+            if (log.isDebugEnabled()) {
                 log.debug("Failed initializing facelet instance", t);
             }
         }
@@ -53,11 +55,14 @@ public class D2DSeamFaceletViewHandler extends D2DFaceletViewHandler {
      */
     protected ResourceResolver preChainResourceResolver(ResourceResolver after) {
         ResourceResolver seamDebugResourceResolver =
-            SeamDebugResourceResolver.build(after);
-        if( seamDebugResourceResolver != null )
+                SeamDebugResourceResolver.build(after);
+        if (seamDebugResourceResolver != null)
             return seamDebugResourceResolver;
         else
             return after;
     }
-    
+
+    public Locale calculateLocale(FacesContext facesContext) {
+        return delegate.calculateLocale(facesContext);
+    }
 }
