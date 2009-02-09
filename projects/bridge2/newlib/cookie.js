32a36ef47305a86925c84c06d361a9dc91e48002
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-09 20:47:24 +0000

    Add new libraries from Sandbox.

---
+++ projects/bridge2/newlib/cookie.js
@@ -0,0 +1,55 @@
+function lookupCookieValue(name) {
+    var tupleString = detect(split(asString(document.cookie), '; '), function(tuple) {
+        return startsWith(tuple, name);
+    }, function() {
+        throw 'Cannot find value for cookie: ' + name;
+    });
+
+    return contains(tupleString, '=') ? split(tupleString, '=')[1] : '';
+}
+
+function lookupCookie(name, failThunk) {
+    try {
+        return Cookie(name, lookupCookieValue(name));
+    } catch (e) {
+        if (failThunk) {
+            return failThunk();
+        } else {
+            throw e;
+        }
+    }
+}
+
+function existsCookie(name) {
+    var exists = true;
+    lookupCookie(name, function() {
+        exists = false;
+    });
+    return exists;
+}
+
+var update = operator();
+var remove = operator();
+function Cookie(name, val) {
+    val = val ? val : '';
+    document.cookie = name + '=' + val;
+
+    return object(function(method) {
+        method(value, function(self) {
+            return lookupCookieValue(name);
+        });
+
+        method(update, function(self, val) {
+            document.cookie = name + '=' + val;
+            return self;
+        });
+
+        method(remove, function(self) {
+            document.cookie = name + '=0; expires=' + (new Date).toGMTString();
+        });
+
+        method(asString, function(self) {
+            return 'Cookie[' + name + ', ' + value(self) + ']';
+        });
+    });
+}
