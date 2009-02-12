ff167a1c54977bf64b0205bf00fc9f2fd6f6da25
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-12 16:47:41 +0000

    backing out inadvertent commit

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/outputconnectionstatus/OutputConnectionStatusRenderer.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/outputconnectionstatus/OutputConnectionStatusRenderer.java
@@ -37,13 +37,14 @@ import com.icesoft.faces.context.DOMContext;
 import com.icesoft.faces.renderkit.dom_html_basic.DomBasicRenderer;
 import com.icesoft.faces.renderkit.dom_html_basic.HTML;
 import com.icesoft.faces.util.DOMUtils;
-import com.icesoft.util.pooling.ClientIdPool;
 import org.w3c.dom.Element;
 import org.w3c.dom.Text;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import java.io.IOException;
+
+import com.icesoft.util.pooling.ClientIdPool;
 
 public class OutputConnectionStatusRenderer extends DomBasicRenderer {
 
@@ -89,7 +90,7 @@ public class OutputConnectionStatusRenderer extends DomBasicRenderer {
             Element script = domContext.createElement(HTML.SCRIPT_ELEM);
             script.setAttribute(HTML.TYPE_ATTR, "text/javascript");
             script.appendChild(domContext.createTextNode(
-                    "attachStatusManager('" + id + "'.asExtendedElement().findContainerFor('bridge').bridge, " +
+                    "'" + id + "'.asExtendedElement().findContainerFor('bridge').bridge.attachStatusManager(" +
                             "function(defaultStatusManager) {" +
                             "return new Ice.Status.ComponentStatusManager('" + workingID + "', '" + idleID + "', '" + troubleID + "', '" + lostID + "', defaultStatusManager, " + component.isShowPopupOnDisconnect() + ", " + component.isDisplayHourglassWhenActive() + ");" +
                             "});"
@@ -108,7 +109,7 @@ public class OutputConnectionStatusRenderer extends DomBasicRenderer {
         if (!visible) {
             div.setAttribute(HTML.STYLE_ATTR, "visibility: hidden;");
         }
-        if ((null == label) || ("".equals(label))) {
+        if ( (null == label) || ("".equals(label)) )  {
             return div;
         }
         if (label != null) {
