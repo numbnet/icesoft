d4c5fd309e6dba83b3efda87e9f1ae4278af63b4
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-20 19:18:32 +0000

    ICE-3862..new components and modifications to RowSelectController for ICEfaces-1.8

--- projects/seam-component-showcase/trunk/seam-comp-showcase/src/com/icesoft/icefaces/samples/showcase/components/table/SeamRowSelectorController.java
+++ projects/seam-component-showcase/trunk/seam-comp-showcase/src/com/icesoft/icefaces/samples/showcase/components/table/SeamRowSelectorController.java
@@ -3,16 +3,21 @@ package com.icesoft.icefaces.samples.showcase.components.table;
 import static org.jboss.seam.ScopeType.PAGE;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.io.Serializable;
-import com.icesoft.icefaces.samples.showcase.common.Employee;
+
 import javax.faces.event.ValueChangeEvent;
 
+import com.icesoft.icefaces.samples.showcase.common.Employee;
+
+
+import org.icefaces.application.showcase.view.bean.examples.component.rowSelector.RowSelectController;
+import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Logger;
+
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.framework.EntityQuery;
+
+import org.jboss.seam.log.Log;
 
 import com.icesoft.faces.component.ext.RowSelectorEvent;
 
@@ -21,26 +26,45 @@ import com.icesoft.faces.component.ext.RowSelectorEvent;
 //in components.xml 
 @Scope(PAGE)
 @Name("rowSelectController")
-public class SeamRowSelectorController implements Serializable{
+public class SeamRowSelectorController extends RowSelectController{
 	
+	@Logger Log log;
 	@In("#{employeesListRowSelector.resultList}")
-	private List employees;
+	private ArrayList seamEmployees;
 	   // list of selected employees
-    private ArrayList selectedEmployees = new ArrayList();
 
-    // flat to indicate multiselect row enabled.
-    private boolean multiRowSelect;
 
  
+    @Create
+    public void init(){
+      super.employees = this.seamEmployees;
+    }
+ 
+    @Override
+    public ArrayList getEmployees(){return this.employees;}
     /**
      * SelectionListener bound to the ice:rowSelector component.  Called
      * when a row is selected in the UI.
+     * Had to override this one too as Seam uses annotated Entity class
+     * with data in the DB
      *
      * @param event from the ice:rowSelector component
      */
+    @Override
     public void rowSelectionListener(RowSelectorEvent event) {
         // clear our list, so that we can build a new one
         selectedEmployees.clear();
+        
+        /* If application developers rely on validation to control submission of the form or use the result of
+           the selection in cascading control set up the may want to defer procession of the event to
+           INVOKE_APPLICATION stage by using this code fragment
+		    if (event.getPhaseId() != PhaseId.INVOKE_APPLICATION) {
+		       event.setPhaseId( PhaseId.INVOKE_APPLICATION );
+		       event.queue();
+		       return;
+		    }
+
+         */
 
         // build the new selected list
         Employee employee;
@@ -50,16 +74,23 @@ public class SeamRowSelectorController implements Serializable{
                 selectedEmployees.add(employee);
             }
         }
+        /* If application developers do not rely on validation and want to bypass UPDATE_MODEL and
+           INVOKE_APPLICATION stages, they may be able to use the following statement:
+           FacesContext.getCurrentInstance().renderResponse();
+           to send application to RENDER_RESPONSE phase shortening the app. cycle
+         */
     }
-
     /**
      * Clear the selection list if going from multi select to single select.
-     *
+     *  Again...have to override due to different Employee Object used
      * @param event jsf action event.
      */
-    public void rowSelectionListener(ValueChangeEvent event) {
-        // if multi select then want to make sure we clear the selected states
-        if (!((Boolean) event.getNewValue()).booleanValue()) {
+    @Override
+    public void changeSelectionMode(ValueChangeEvent event) {
+        String newValue = event.getNewValue().toString(); 
+        multiple = false;
+        enhancedMultiple = false;
+        if ("Single".equals(newValue)){
             selectedEmployees.clear();
 
             // build the new selected list
@@ -68,33 +99,11 @@ public class SeamRowSelectorController implements Serializable{
                 employee = (Employee)employees.get(i);
                 employee.setSelected(false);
             }
+        } else if ("Multiple".equals(newValue)){
+            multiple = true;
+        } else if ("Enhanced Multiple".equals(newValue)){
+            enhancedMultiple = true;
         }
     }
-
-    public ArrayList getSelectedEmployees() {
-        return selectedEmployees;
-    }
-
-    public void setSelectedEmployees(ArrayList selectedEmployees) {
-        this.selectedEmployees = selectedEmployees;
-    }
-
-    public boolean isMultiRowSelect() {
-        return multiRowSelect;
-    }
-
-    /**
-     * Sets the selection more of the rowSelector.
-     *
-     * @param multiRowSelect true indicates multi-row select and false indicates
-     *                       single row selection mode.
-     */
-    public void setMultiRowSelect(boolean multiRowSelect) {
-        this.multiRowSelect = multiRowSelect;
-    }
-	public List getEmployees(){
-		return this.employees;
-	}
-	
 	
 }
