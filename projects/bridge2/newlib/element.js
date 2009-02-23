bc703c08a7d3976896f11978d769f27a220ad021
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-23 13:23:27 +0000

    Removed dependency on Prototype for BodyElement, IFrame, TitleElement, ScriptElement.

--- projects/bridge2/newlib/element.js
+++ projects/bridge2/newlib/element.js
@@ -14,8 +14,6 @@ onLoad(window, function() {
 
 var id = operator();
 var tag = operator();
-var property = operator();
-var attribute = operator();
 var parents = operator();
 var enclosingForm = operator();
 var enclosingBridge = operator();
@@ -24,12 +22,14 @@ var enclosingBridge = operator();
 var updateElement = operator();
 var removeElement = operator();
 
+var EventListenerNames = [
+    'onkeydown', 'onkeypress', 'onkeyup', 'onhelp', 'onclick', 'ondblclick',
+    'onmousedown', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup',
+    'onblur', 'onfocus', 'onchange', 'onreset', 'onsubmit'
+];
+
 function disconnectEventListeners(element) {
-    each([
-        'onkeydown', 'onkeypress', 'onkeyup', 'onhelp', 'onclick', 'ondblclick',
-        'onmousedown', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup',
-        'onblur', 'onfocus', 'onchange', 'onreset', 'onsubmit'
-    ], function(listenerName) {
+    each(EventListenerNames, function(listenerName) {
         try {
             element[listenerName] = null;
         } catch (e) {
@@ -106,10 +106,6 @@ function Element(element) {
 
         method(tag, function(self) {
             return toLowerCase(element.tagName);
-        });
-
-        method(property, function(self, name) {
-            return element[name];
         });
 
         method(parents, function(self) {
@@ -231,6 +227,17 @@ function ButtonElement(element) {
     }, InputElement(element));
 }
 
+function AnchorElement(element) {
+    return objectWithAncestors(function(method) {
+        method(canSubmitForm, any);
+
+        method(serializeOn, function(self, query) {
+            var name = element.name;
+            if (name) addNameValue(query, name, name);
+        });
+    }, InputElement(element));
+}
+
 var detectDefaultSubmit = operator();
 var captureAndRedirectSubmit = operator();
 var submit = operator();
@@ -241,6 +248,7 @@ function FormElement(element) {
             throw 'forms cannot be nested';
         });
 
+        //todo: this method should go away since it encourages special treatment for elements with ID ending in ':default' 
         method(detectDefaultSubmit, function(self) {
             var defaultID = this.element.id + ':default';
             return $element(detect(element.elements, function(e) {
@@ -288,22 +296,114 @@ function FormElement(element) {
     }, Element(element));
 }
 
+function ScriptElement(element) {
+    return objectWithAncestors(function(method) {
+        method(updateElement, function(self, update) {
+            //if script element is updated its code will be evaluate in IE (thus evaluating it twice)
+            //evaluate code in the 'window' context
+            var scriptCode = content(update);
+            if (scriptCode != '' && scriptCode != ';') {
+                var evalFunc = function() {
+                    eval(scriptCode);
+                };
+                evalFunc.apply(window);
+            }
+        });
+    }, Element(element));
+}
+
+function TitleElement(element) {
+    return objectWithAncestors(function(method) {
+        method(updateElement, function(self, update) {
+            element.ownerDocument.title = content(update);
+        });
+    }, Element(element));
+}
+
+function TableCellElement(element) {
+    return objectWithAncestors(function(method) {
+        method(updateElement, function(self, update) {
+            HTMLParser(function(parse) {
+                var newElement = parse('<TABLE>' + asHTML(update) + '</TABLE>')[0];
+                each(element.getElementsByTagName('*'), disconnectEventListeners);
+                while ((null != newElement) && (element.id != newElement.id)) {
+                    newElement = newElement.firstChild;
+                }
+                element.parentNode.replaceChild(newElement, element);
+                element = newElement;
+            });
+        });
+    }, Element(element));
+}
+
+function BodyElement(element) {
+    return objectWithAncestors(function(method) {
+        method(updateElement, function(self, update) {
+            each(element.getElementsByTagName('*'), disconnectEventListeners);
+            //strip <noscript> tag to fix Safari bug
+            // #3131 If this is a response from an error code, there may not be a <noscript> tag.
+            var html = content(update);
+            var start = new RegExp('\<noscript\>', 'g').exec(html);
+            if (start == null) {
+                element.innerHTML = html;
+            } else {
+                var end = new RegExp('\<\/noscript\>', 'g').exec(html);
+                element.innerHTML = substring(html, 0, start.index) + substring(html, end.index + 11, html.length);
+            }
+        });
+    }, Element(element));
+}
+
+function IFrameElement(element) {
+    return objectWithAncestors(function(method) {
+        method(updateElement, function(self, update) {
+            HTMLParser(function(parse) {
+                var newElement = parse(asHTML(update))[0];
+
+                each(['title', 'lang', 'dir', 'class', 'style', 'align', 'frameborder',
+                    'width', 'height', 'hspace', 'ismap', 'longdesc', 'marginwidth',
+                    'marginheight', 'name', 'scrolling'], function(attributeName) {
+                    var value = newElement.getAttribute(attributeName);
+                    if (value == null) {
+                        element.removeAttribute(attributeName);
+                    } else {
+                        element.setAttribute(attributeName, value);
+                    }
+                });
+
+                //special case for the 'src' attribute (Safari bug?)
+                var oldLocation = element.contentWindow.location.href;
+                var newLocation = newElement.contentWindow.location.href;
+                if (oldLocation != newLocation) {
+                    element.contentWindow.location = newLocation;
+                }
+
+                //overwrite listeners and bind them to the existing element
+                each(EventListenerNames, function(name) {
+                    element[name] = newElement[name] ? newElement[name] : null;
+                    newElement[name] = null;
+                });
+            });
+        });
+    }, Element(element));
+}
+
 function $element(e) {
     //no polymophism here...'switch' is the way then.
     switch (toLowerCase(e.tagName)) {
         case 'textarea':
         case 'input': return InputElement(e);
-        //            case 'th':
-        //            case 'td':
-        //            case 'tr': return TableCellElement(e);
+        case 'th':
+        case 'td':
+        case 'tr': return TableCellElement(e);
         case 'button': return ButtonElement(e);
         case 'select': return SelectElement(e);
         case 'form': return FormElement(e);
-        //            case 'body': return BodyElement(e);
-        //            case 'script': return ScriptElement(e);
-        //            case 'title': return TitleElement(e);
-        //            case 'a': return AnchorElement(e);
-        //            case 'iframe': return IFrameElement(e);
+        case 'body': return BodyElement(e);
+        case 'script': return ScriptElement(e);
+        case 'title': return TitleElement(e);
+        case 'a': return AnchorElement(e);
+        case 'iframe': return IFrameElement(e);
         default : return Element(e);
     }
 }
