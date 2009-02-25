67d763b7c466828a68c1c2f7d7927da411850245
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-02-25 21:09:47 +0000

    Ran code through Intellij Reformat code function.

--- tutorials/trunk/tutorials/samples/dataTable-JPA/dataTable/src/com/icesoft/icefaces/samples/datatable/jpa/EntityManagerHelper.java
+++ tutorials/trunk/tutorials/samples/dataTable-JPA/dataTable/src/com/icesoft/icefaces/samples/datatable/jpa/EntityManagerHelper.java
@@ -41,43 +41,43 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class EntityManagerHelper {
-	
-	private static final EntityManagerFactory emf; 
-	private static final ThreadLocal<EntityManager> threadLocal;
-	private static final Logger logger;
-	
-	static {
-		emf = Persistence.createEntityManagerFactory("tutorialPU"); 		
-		threadLocal = new ThreadLocal<EntityManager>();
-		logger = Logger.getLogger("tutorialPU");
-		logger.setLevel(Level.ALL);
-	}
-		
-	public static EntityManager getEntityManager() {
-		EntityManager manager = threadLocal.get();		
-		if (manager == null || !manager.isOpen()) {
-			manager = emf.createEntityManager();
-			threadLocal.set(manager);
-		}
-		return manager;
-	}
-	
-	 public static void closeEntityManager() {
+
+    private static final EntityManagerFactory emf;
+    private static final ThreadLocal<EntityManager> threadLocal;
+    private static final Logger logger;
+
+    static {
+        emf = Persistence.createEntityManagerFactory("tutorialPU");
+        threadLocal = new ThreadLocal<EntityManager>();
+        logger = Logger.getLogger("tutorialPU");
+        logger.setLevel(Level.ALL);
+    }
+
+    public static EntityManager getEntityManager() {
+        EntityManager manager = threadLocal.get();
+        if (manager == null || !manager.isOpen()) {
+            manager = emf.createEntityManager();
+            threadLocal.set(manager);
+        }
+        return manager;
+    }
+
+    public static void closeEntityManager() {
         EntityManager em = threadLocal.get();
         threadLocal.set(null);
         if (em != null) em.close();
     }
-    
+
     public static void beginTransaction() {
-    	getEntityManager().getTransaction().begin();
+        getEntityManager().getTransaction().begin();
     }
-    
+
     public static void commit() {
-    	getEntityManager().getTransaction().commit();
-    }  
-	
-	public static void log(String info, Level level, Throwable ex) {
-    	logger.log(level, info, ex);
+        getEntityManager().getTransaction().commit();
     }
-    
+
+    public static void log(String info, Level level, Throwable ex) {
+        logger.log(level, info, ex);
+    }
+
 }
