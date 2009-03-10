ac194e68c9252b337d4ae11a5ae1c291f348aee9
mark.collette <mark.collette@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-03-10 21:32:17 +0000

    ICE-4068 : Component Showcase SetEventPhase example updates

--- icefaces/trunk/icefaces/samples/component-showcase/common-src/org/icefaces/application/showcase/view/bean/examples/component/setEventPhase/SetEventPhaseBean.java
+++ icefaces/trunk/icefaces/samples/component-showcase/common-src/org/icefaces/application/showcase/view/bean/examples/component/setEventPhase/SetEventPhaseBean.java
@@ -90,7 +90,7 @@ public class SetEventPhaseBean extends BaseBean {
      */
     public SelectItem[] getCountryItems() {
         SelectItem[] countryItems =
-            build("bean.selection.country", ".value", 1, 5);
+            buildSelectItemArray("bean.selection.country", ".value", 1, 5);
         return countryItems;
     }
 
@@ -105,7 +105,7 @@ public class SetEventPhaseBean extends BaseBean {
             SelectItem[] countryItems = getCountryItems();            
             for(int i = 0; i < countryItems.length; i++) {
                 if (selectedCountry.equals(countryItems[i].getValue())) {
-                    cityItems = build(
+                    cityItems = buildSelectItemArray(
                         "bean.selection.country"+Integer.toString(i+1)+".city",
                         ".value", 1, 5);
                     break;
@@ -159,41 +159,5 @@ public class SetEventPhaseBean extends BaseBean {
      */
     public String getSelectedCitiesStrings() {
         return convertToString(selectedCities);
-    }
-
-    private static SelectItem[] build(
-            String prefix, String suffix, int first, int last) {
-        int num = last - first + 1;
-        SelectItem[] ret = new SelectItem[num];
-        for(int i = 0; i < num; i++) {
-            String key = prefix + Integer.toString(first+i) + suffix;
-            ret[i] = build(key);
-        }
-        return ret;
-    }
-    
-    private static SelectItem build(String key) {
-        return new SelectItem(
-                key, MessageBundleLoader.getMessage(key));
-    }
-    
-    /**
-     * Converts string arrays for displays.
-     *
-     * @param stringArray string array to convert
-     * @return a string concatenating the elements of the string array
-     */
-    private static String convertToString(String[] stringArray) {
-        if (stringArray == null) {
-            return "";
-        }
-        StringBuffer itemBuffer = new StringBuffer();
-        for (int i = 0, max = stringArray.length; i < max; i++) {
-            if (i > 0) {
-                itemBuffer.append(" , ");
-            }
-            itemBuffer.append(MessageBundleLoader.getMessage(stringArray[i]));
-        }
-        return itemBuffer.toString();
     }
 }
