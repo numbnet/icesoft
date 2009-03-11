47d7dac365eb3dd7b1da7f08fa19ff2659b8e205
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-03-11 15:32:37 +0000

    Fix problems with directory re-naming.

--- tutorials/trunk/tutorials/samples/spring-security/spring-security-basic/src/com/icesoft/icefaces/security/beans/LoginBean.java
+++ tutorials/trunk/tutorials/samples/spring-security/spring-security-basic/src/com/icesoft/icefaces/security/beans/LoginBean.java
@@ -1,63 +1,56 @@
 package com.icesoft.icefaces.security.beans;
 
+import org.springframework.security.ui.AbstractProcessingFilter;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
-import org.springframework.security.ui.AbstractProcessingFilter;
-
 public class LoginBean {
 
-	// properties
-	private String userId;
+    // properties
+    private String userId;
 
-	private String password;
+    private String password;
 
-	/**
-	 * default empty constructor
-	 */
-	public LoginBean() {
+    /**
+     * default empty constructor
+     */
+    public LoginBean() {
 
-		Exception ex = (Exception) FacesContext
-				.getCurrentInstance()
-				.getExternalContext()
-				.getSessionMap()
-				.get(AbstractProcessingFilter.SPRING_SECURITY_LAST_EXCEPTION_KEY);
+        Exception ex = (Exception) FacesContext
+                .getCurrentInstance()
+                .getExternalContext()
+                .getSessionMap()
+                .get(AbstractProcessingFilter.SPRING_SECURITY_LAST_EXCEPTION_KEY);
 
-		if (ex != null)
-			FacesContext.getCurrentInstance().addMessage(
-					null,
-					new FacesMessage(FacesMessage.SEVERITY_ERROR, ex
-							.getMessage(), ex.getMessage()));
+        if (ex != null)
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, ex
+                            .getMessage(), ex.getMessage()));
 
-	}
+    }
 
-	/**
-	 * Method that is backed to a submit button of a form.
-	 */
-	public String send() {
+    public String getPassword() {
+        return password;
+    }
 
-		// do real logic
-		return ("success");
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public String getUserId() {
-		return userId;
-	}
+    public void login(ActionEvent e) throws java.io.IOException {
+        FacesContext.getCurrentInstance().getExternalContext().redirect("/spring-authentication/j_spring_security_check.jsp?j_username=" + userId + "&j_password=" + password);
+    }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	
-	public void login(ActionEvent e) throws java.io.IOException {
-		FacesContext.getCurrentInstance().getExternalContext().redirect("/spring-authentication/j_spring_security_check.jsp?j_username="+userId+"&j_password="+password);
-	}
+
 }
