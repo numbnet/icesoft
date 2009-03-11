1c3939bae93b027865c8172dccf5b8f3705a3c2c
frank.ye <frank.ye@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-03-11 20:44:19 +0000

    ICE-4196 list class<Type> Error with Boolean binding

--- icefaces/trunk/icefaces/component/test/java/com/icesoft/faces/metadata/BeanImpPropertiesTest.java
+++ icefaces/trunk/icefaces/component/test/java/com/icesoft/faces/metadata/BeanImpPropertiesTest.java
@@ -1,9 +1,11 @@
 package com.icesoft.faces.metadata;
 
+import com.sun.faces.util.ReflectionUtils;
 import com.sun.rave.jsfmeta.beans.ComponentBean;
 import com.sun.rave.jsfmeta.beans.PropertyBean;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import javax.faces.component.UIComponent;
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -46,6 +48,13 @@ public class BeanImpPropertiesTest extends ICECompsTestCase {
 
                     message = "Failed under test Component= " + components[j].getClass().getName() + " property name= " + propertyName + "\n" + "\tproperty class=" + pds[i].getPropertyClass();
                     Object propValue = testDataProvider.getSimpleTestObject(pds[i].getPropertyClass());
+                    if(pds[i].getPropertyClass().equals("boolean")){
+                        String methodName = propertyName.substring(0, 1).toUpperCase()+propertyName.substring(1);
+                        Method setMethod = ReflectionUtils.lookupMethod(components[j].getClass(), "set"+methodName, boolean.class);
+                        if(setMethod == null){
+                            System.out.println(" "+message);
+                        }
+                    }
                     BeanUtils.setProperty(components[j], propertyName, propValue);
                 } catch (java.lang.IllegalArgumentException ex) {
                     ex.printStackTrace();
