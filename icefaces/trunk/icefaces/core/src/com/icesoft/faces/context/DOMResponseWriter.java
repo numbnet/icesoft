e27ae995c79b27b9d4ba22663bf5cf2d59fbd754
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-03-26 22:47:00 +0000

    ICE-3784: Modified algorithm for massaging the context path so that slashes were not removed

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/context/DOMResponseWriter.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/context/DOMResponseWriter.java
@@ -351,7 +351,22 @@ public class DOMResponseWriter extends ResponseWriter {
 
         String contextPath = handler.getResourceURL(context, "/");
         if (blockingRequestHandlerContext == null) {
-            blockingRequestHandlerContext = URI.create("/").resolve(contextPath.replaceAll("/", "") + "/").toString();
+            //ICE-3784
+            //Only need to strip leading and trailing slashes. Removing all slashes
+            //will cause problems with contexts that actually contain slashes e.g /myapp/mysubapp.
+            String normalizedPath = contextPath;
+            if( normalizedPath.length() > 0 ){
+                if(normalizedPath.charAt(0) == '/'){
+                    normalizedPath = normalizedPath.substring(1);
+                }
+                if( normalizedPath.length() > 0 ){
+                    if(normalizedPath.charAt(normalizedPath.length() -1 ) == '/'){
+                        normalizedPath = normalizedPath.substring(0,normalizedPath.length()-1);
+                    }
+                }
+            }
+            blockingRequestHandlerContext = URI.create("/").resolve(normalizedPath + "/").toString();
+            System.out.println("DOMResponseWriter.enhanceBody: " + contextPath + " - " + blockingRequestHandlerContext);
         }
         String connectionLostRedirectURI;
         try {
