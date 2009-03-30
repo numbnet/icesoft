3357a7146a51fac0c6ae0239f3cbff50ec7e79b1
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-03-30 21:36:32 +0000

    ICE-4297 Test if ice.cookie is still around.

--- icefaces/trunk/icefaces/bridge/src/application.js
+++ icefaces/trunk/icefaces/bridge/src/application.js
@@ -57,22 +57,25 @@ window.console && window.console.firebug ? new Ice.Log.FirebugLogHandler(window.
     };
 
     var deregisterSession = function(sessionID) {
-        var sessionsCookie = Ice.Cookie.lookup('ice.sessions');
-        var sessions = sessionsCookie.loadValue().split(' ');
-        sessionsCookie.saveValue(sessions.inject([], function(tally, s) {
-            if (s && s.length == 2) {
-                var entry = s.split('#');
-                var id = entry[0];
-                var occurences = entry[1].asNumber();
-                if (id == sessionID) {
-                    --occurences;
+        //cookie can be gone because its value was emptied when all the windows where disposed
+        if (Ice.Cookie.exists('ice.sessions')) {
+            var sessionsCookie = Ice.Cookie.lookup('ice.sessions');
+            var sessions = sessionsCookie.loadValue().split(' ');
+            sessionsCookie.saveValue(sessions.inject([], function(tally, s) {
+                if (s && s.length == 2) {
+                    var entry = s.split('#');
+                    var id = entry[0];
+                    var occurences = entry[1].asNumber();
+                    if (id == sessionID) {
+                        --occurences;
+                    }
+                    if (occurences > 0) {
+                        tally.push(entry[0] + '#' + occurences);
+                    }
                 }
-                if (occurences > 0) {
-                    tally.push(entry[0] + '#' + occurences);
-                }
-            }
-            return tally;
-        }).join(' '));
+                return tally;
+            }).join(' '));
+        }
     };
 
     var views = window.views = window.views ? window.views : [];
