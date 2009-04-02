58b5eae5c3f78e6b694181b21acfce076595f213
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-04-02 01:45:19 +0000

    ICEfaces 1.8.0 official release tag.

---
+++ icefaces/tags/icefaces-1.8.0/icefaces/bridge/lib/cookie.js
@@ -0,0 +1,71 @@
+[ Ice ].as(function(This) {
+    This.Cookie = This.Parameter.Association.subclass({
+        initialize: function(name, value, path) {
+            this.name = name;
+            this.value = value || '';
+            this.path = path || '/';
+            this.save();
+        },
+
+        saveValue: function(value) {
+            this.value = value;
+            this.save();
+        },
+
+        loadValue: function() {
+            this.load();
+            return this.value;
+        },
+
+        save: function() {
+            document.cookie = this.name + '=' + this.value + ';path=' + this.path;
+            return this;
+        },
+
+        load: function() {
+            var foundTuple = This.Cookie.parse().detect(function(tuple) {
+                return this.name == tuple[0];
+            }.bind(this));
+            this.value = foundTuple ? foundTuple[1] : null;
+            return this;
+        },
+
+        remove: function() {
+            document.cookie = this.name + '=0; path=' + this.path + '; expires=' + (new Date).toGMTString();
+        }
+    });
+
+    This.Cookie.all = function() {
+        return This.Cookie.parse().collect(function(tuple) {
+            var name = tuple[0];
+            var value = tuple[1];
+            return new This.Cookie(name, value);
+        });
+    };
+
+    This.Cookie.lookup = function(name, value) {
+        var foundTuple = This.Cookie.parse().detect(function(tuple) {
+            return name == tuple[0];
+        });
+        if (foundTuple) {
+            return new This.Cookie(name, foundTuple[1]);
+        } else {
+            if (value) {
+                return new This.Cookie(name, value);
+            } else {
+                throw 'Cannot find cookie named: ' + name;
+            }
+        }
+    };
+
+    This.Cookie.exists = function(name) {
+        return document.cookie.contains(name + '=');
+    };
+
+    //private
+    This.Cookie.parse = function() {
+        return document.cookie.split('; ').collect(function(tupleDetails) {
+            return tupleDetails.contains('=') ? tupleDetails.split('=') : [tupleDetails, ''];
+        });
+    };
+});
