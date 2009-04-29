75375650479e238a2f5d34c089778c72ccad2caf
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-04-29 17:16:42 +0000

    ICE-3356 Removed unsupported resizable attribute.

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopup.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopup.java
@@ -273,9 +273,11 @@ public class PanelPopup extends HtmlPanelGroup {
 	 * 
 	 * @return false
 	 */
+/*
 	public boolean isResizable() {
 		return false; // resizable functionality is not added yet
 	}
+*/
 
 	/**
 	 * Sets the resizable attribute of the component. Note: The resizable
@@ -284,9 +286,11 @@ public class PanelPopup extends HtmlPanelGroup {
 	 * @param resizable
 	 *            a value of true will set the component to be resizable
 	 */
+/*
 	public void setResizable(boolean resizable) {
 		this.resizable = Boolean.valueOf(resizable);
 	}
+*/
 
 	/**
 	 * @return true if the component is modal.
