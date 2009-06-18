78e716ce08985502a274c500132a06a9927304e6
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-06-18 23:11:55 +0000

    ICE-4543 Create and add new resource components instances every time a  view is created to avoid duplicate component ID errors.

--- icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
@@ -12,37 +12,6 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class BridgeSetup extends ViewHandlerWrapper {
-    private static final UIOutput BridgeCode = new UIOutput() {
-        public void encodeBegin(FacesContext context) throws IOException {
-            ResponseWriter writer = context.getResponseWriter();
-            Resource bridgeCode = FacesContext.getCurrentInstance().getApplication().getResourceHandler().createResource("bridge.js");
-            writer.startElement("script", this);
-            writer.writeAttribute("type", "text/javascript", null);
-            writer.writeAttribute("src", bridgeCode.getRequestPath(), null);
-            writer.endElement("script");
-        }
-    };
-    private static final UIOutput BridgeInitialization = new UIOutput() {
-        public void encodeBegin(FacesContext context) throws IOException {
-            ResponseWriter writer = context.getResponseWriter();
-            ExternalContext externalContext = context.getExternalContext();
-            String sessionID = ((HttpSession) externalContext.getSession(true)).getId();
-            String contextPath = externalContext.getRequestContextPath();
-            writer.startElement("script", this);
-            writer.writeAttribute("type", "text/javascript", null);
-            writer.writeText("ice.onLoad(function() { try {" +
-                    "document.body.bridge = ice.Application({" +
-                    "blockUI: false," +
-                    "session: '" + sessionID + "'," +
-                    "connection: {" +
-                    "heartbeat: {}," +
-                    "context: {current: '" + contextPath + "/',async: '" + contextPath + "/'}" +
-                    "}," +
-                    "messages: {sessionExpired: 'User Session Expired',connectionLost: 'Network Connection Interrupted',serverError: 'Server Internal Error',description: 'To reconnect click the Reload button on the browser or click the button below',buttonText: 'Reload'}}," +
-                    "document.body); } catch (e) { alert(e); }; });", null);
-            writer.endElement("script");
-        }
-    };
     private ViewHandler handler;
 
     public BridgeSetup() {
@@ -59,8 +28,40 @@ public class BridgeSetup extends ViewHandlerWrapper {
 
     public UIViewRoot createView(FacesContext context, String viewId) {
         UIViewRoot root = handler.createView(context, viewId);
-        root.addComponentResource(context, BridgeCode, "head");
-        root.addComponentResource(context, BridgeInitialization, "body");
+
+        root.addComponentResource(context, new UIOutput() {
+            public void encodeBegin(FacesContext context) throws IOException {
+                ResponseWriter writer = context.getResponseWriter();
+                Resource bridgeCode = context.getApplication().getResourceHandler().createResource("bridge.js");
+                writer.startElement("script", this);
+                writer.writeAttribute("type", "text/javascript", null);
+                writer.writeAttribute("src", bridgeCode.getRequestPath(), null);
+                writer.endElement("script");
+            }
+        }, "head");
+
+        root.addComponentResource(context, new UIOutput() {
+            public void encodeBegin(FacesContext context) throws IOException {
+                ResponseWriter writer = context.getResponseWriter();
+                ExternalContext externalContext = context.getExternalContext();
+                String sessionID = ((HttpSession) externalContext.getSession(true)).getId();
+                String contextPath = externalContext.getRequestContextPath();
+                writer.startElement("script", this);
+                writer.writeAttribute("type", "text/javascript", null);
+                writer.writeText("ice.onLoad(function() { try {" +
+                        "document.body.bridge = ice.Application({" +
+                        "blockUI: false," +
+                        "session: '" + sessionID + "'," +
+                        "connection: {" +
+                        "heartbeat: {}," +
+                        "context: {current: '" + contextPath + "/',async: '" + contextPath + "/'}" +
+                        "}," +
+                        "messages: {sessionExpired: 'User Session Expired',connectionLost: 'Network Connection Interrupted',serverError: 'Server Internal Error',description: 'To reconnect click the Reload button on the browser or click the button below',buttonText: 'Reload'}}," +
+                        "document.body); } catch (e) { alert(e); }; });", null);
+                writer.endElement("script");
+            }
+        }, "body");
+
         return root;
     }
 }
