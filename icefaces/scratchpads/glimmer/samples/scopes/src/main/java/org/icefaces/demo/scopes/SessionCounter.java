9ccc8f8c36c6734824faca6c9fa3c5575755d287
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-06-30 21:08:01 +0000

    ICE-4639 Add 'session' scope test.

---
+++ icefaces/scratchpads/glimmer/samples/scopes/src/main/java/org/icefaces/demo/scopes/SessionCounter.java
@@ -0,0 +1,25 @@
+package org.icefaces.demo.scopes;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "SessionCounter")
+@SessionScoped
+public class SessionCounter extends Counter implements Serializable {
+    public SessionCounter() {
+        System.out.println(this);
+    }
+
+    @PostConstruct
+    public void created() {
+        System.out.println("created >> " + this);
+    }
+
+    @PreDestroy
+    public void destroyed() {
+        System.out.println("destroyed >> " + this);
+    }
+}
