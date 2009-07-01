159425b6bd82ab850861d2f266288c77d7063863
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-07-01 15:44:12 +0000

    ICE-4639 Rename beans and classes. Add links to index page.

--- icefaces/scratchpads/glimmer/samples/scopes/src/main/java/org/icefaces/demo/scopes/ApplicationScopedCounter.java
+++ icefaces/scratchpads/glimmer/samples/scopes/src/main/java/org/icefaces/demo/scopes/ApplicationScopedCounter.java
@@ -2,14 +2,14 @@ package org.icefaces.demo.scopes;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.faces.bean.CustomScoped;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 
-@ManagedBean(name = "WindowCounter")
-@CustomScoped(value = "#{window}")
-public class WindowCounter extends Counter implements Serializable {
-    public WindowCounter() {
+@ManagedBean(name = "ApplicationScopedCounter")
+@ApplicationScoped
+public class ApplicationScopedCounter extends Counter implements Serializable {
+    public ApplicationScopedCounter() {
         System.out.println(this);
     }
 
