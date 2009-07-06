152b3ed06f51737fe74bd34f6154e2d16f6f6ef7
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-07-06 22:36:29 +0000

    ICE-4616 Serialize the element that triggered the submit.

--- icefaces/scratchpads/glimmer/core/src/main/javascript/submit.js
+++ icefaces/scratchpads/glimmer/core/src/main/javascript/submit.js
@@ -2,13 +2,16 @@ var submitEvent;
 var submitForm;
 
 (function() {
-    submitEvent = function(event, element, form) {
+    submitEvent = function(event, element) {
         var options = {execute: '@all', render: '@all', 'ice.window': namespace.window};
-        serializeOn($event(event, element), object(function(method) {
+        var collectingQuery = object(function(method) {
             method(addNameValue, function(self, name, value) {
                 options[name] = value;
             });
-        }));
+        });
+        each([$event(event, element), $element(element)], function(e) {
+            serializeOn(e, collectingQuery);
+        });
 
         jsf.ajax.request(element, event, options);
     };
@@ -17,11 +20,13 @@ var submitForm;
         //make event null if undefined
         event = event ? event : null;
         var options = {execute: '@all', render: '@all', 'ice.window': namespace.window};
-        serializeOn($event(event, form), object(function(method) {
+        var collectingQuery = object(function(method) {
             method(addNameValue, function(self, name, value) {
                 options[name] = value;
             });
-        }));
+        });
+        serializeOn($event(event, form), collectingQuery);
+
         jsf.ajax.request(form, event, options);
     };
 })();
