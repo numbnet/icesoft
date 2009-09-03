3bf145757de39cf1d2425044b16c6fff285991ec
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-09-03 15:54:47 +0000

    ICE-4839: JBoss implementation of AssociatedPageViews

---
+++ projects/portletviews/trunk/src/com/icesoft/faces/webapp/http/portlet/page/JBossAssociatedPageViews.java
@@ -0,0 +1,23 @@
+package com.icesoft.faces.webapp.http.portlet.page;
+
+import org.jboss.portal.api.node.PortalNode;
+import org.jboss.portal.core.aspects.controller.node.Navigation;
+
+public class JBossAssociatedPageViews extends AssociatedPageViewsImpl {
+
+    public String getPageId() {
+
+        try {
+            PortalNode node = Navigation.getCurrentNode();
+            PortalNode parent = node.getParent();
+            String pageName = parent.getName();
+            return pageName;
+        } catch (Exception e) {
+            if (log.isErrorEnabled()) {
+                log.warn("could not get page id", e);
+            }
+        }
+
+        return null;
+    }
+}
