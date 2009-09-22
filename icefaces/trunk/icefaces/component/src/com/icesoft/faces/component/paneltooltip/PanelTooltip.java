9ace53588d7ebf6a32124b1a76bca861cb3c422d
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-09-22 18:47:33 +0000

    ICE-4952: Added optional support for having a panelTooltip track with the mouse.

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/paneltooltip/PanelTooltip.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/paneltooltip/PanelTooltip.java
@@ -47,6 +47,7 @@ public class PanelTooltip extends PanelPopup{
     
     private MethodBinding displayListener;
     private String displayOn;
+    private Boolean moveWithMouse;
 
     public PanelTooltip() {
         setRendererType(DEFAULT_RENDERER_TYPE);
@@ -370,11 +371,12 @@ public class PanelTooltip extends PanelPopup{
         hideOn = (String)states[3];
         hoverDelay = (Integer)states[4];
         displayOn = (String) states[5];
+        moveWithMouse = (Boolean) states[6];
     }
 
     public Object saveState(FacesContext context) {
         if(states == null){
-            states = new Object[6];
+            states = new Object[7];
         }
         states[0] = super.saveState(context);
         states[1] = saveAttachedState(context, displayListener);
@@ -382,6 +384,7 @@ public class PanelTooltip extends PanelPopup{
         states[3] = hideOn;
         states[4] = hoverDelay;
         states[5] = displayOn;
+        states[6] = moveWithMouse;
         return states;
     }
 
@@ -398,5 +401,18 @@ public class PanelTooltip extends PanelPopup{
             return (String) vb.getValue(getFacesContext());
         }
         return "hover";
+    }
+
+    public boolean isMoveWithMouse() {
+        if (moveWithMouse != null) return moveWithMouse.booleanValue();
+        ValueBinding vb = getValueBinding("moveWithMouse");
+        if (vb == null) return false;
+        Object o = vb.getValue(getFacesContext());
+        if (o == null) return false;
+        return (Boolean.valueOf(o.toString())).booleanValue();
+    }
+
+    public void setMoveWithMouse(boolean moveWithMouse) {
+        this.moveWithMouse = Boolean.valueOf(moveWithMouse);
     }
 }
