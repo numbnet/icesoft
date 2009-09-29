920c0fb39574659012529534d5e88b9d69bcca0a
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-09-29 22:06:52 +0000

    sketch of PushContext (ICE-4980)

---
+++ scratchpads/snowplow/src/main/java/org/icepush/PushContext.java
@@ -0,0 +1,29 @@
+package org.icepush;
+import javax.servlet.http.HttpServletRequest;
+
+public class PushContext  {
+
+    public static synchronized PushContext getInstance(HttpServletRequest request)  {
+        //the returned PushContext must be aware of the BROWSERID cookie and
+        //must have a reference to the application scope notification service
+        //request.getSession().getServletContext().getAttribute(NOTIFY_SERVICE);
+       return null;
+    }
+    
+    private PushContext(String browserId)  {
+        this.browserId = browserId;
+    }
+
+    public String createPushId(String browserId)  {
+        return "next pushid";
+    }
+
+    public void notify(String targetName)  {
+    }
+    
+    public void addGroupMember(String groupName, String pushId)  {
+    }
+
+    public void removeGroupMember(String groupName, String pushId)  {
+    }
+}
