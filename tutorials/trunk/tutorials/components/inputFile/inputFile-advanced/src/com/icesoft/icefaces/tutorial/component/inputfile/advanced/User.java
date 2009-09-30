c3cf564b2d6583381c317c9503185800e2306626
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-09-30 22:24:41 +0000

    Update inputfile advanced for 1.8.2 release.

--- tutorials/trunk/tutorials/components/inputFile/inputFile-advanced/src/com/icesoft/icefaces/tutorial/component/inputfile/advanced/User.java
+++ tutorials/trunk/tutorials/components/inputFile/inputFile-advanced/src/com/icesoft/icefaces/tutorial/component/inputfile/advanced/User.java
@@ -1,30 +1,58 @@
 package com.icesoft.icefaces.tutorial.component.inputfile.advanced;
 
-import java.io.File;
-
-/**
- * <p>
- * A basic backing bean for a ice:inputFile component.  This bean contains a
- * reference to the file that is uploaded by the inputFile component.
- * </p>
- */
-
 import javax.faces.event.*;
 
 import com.icesoft.faces.component.inputfile.InputFile;
+import com.icesoft.faces.component.inputfile.FileInfo;
 
 public class User {
 
     private String componentStatus;
     private String fileLocation2;
     private String fileLocation3;
-    
-    public String getComponentStatus() {
-        return componentStatus;
+
+    public void uploadActionListener(ActionEvent actionEvent) {
+        InputFile inputFile =(InputFile) actionEvent.getSource();
+        FileInfo fileInfo = inputFile.getFileInfo();
+        //file has been saved
+        if (fileInfo.isSaved()) {
+            componentStatus = "Custom saved message.";
+        }
+        //upload failed, generate custom messages
+        if (fileInfo.isFailed()) {
+            if(fileInfo.getStatus() == FileInfo.INVALID){
+                componentStatus = "Custom invalid message.";
+            }
+            if(fileInfo.getStatus() == FileInfo.SIZE_LIMIT_EXCEEDED){
+                componentStatus = "Custom size limit exceeded message.";
+            }
+            if(fileInfo.getStatus() == FileInfo.INVALID_CONTENT_TYPE){
+                componentStatus = "Custom invalid content type message.";
+            }
+            if(fileInfo.getStatus() == FileInfo.INVALID_NAME_PATTERN){
+                componentStatus = "Custom invalid name pattern message - we have set the fileNamePattern attribute to only accept .pdf files.";
+            }
+        }
     }
 
-    public void setComponentStatus(String componentStatus) {
-        this.componentStatus = componentStatus;
+    public void checkFileLocation(ActionEvent event){
+        InputFile inputFile =(InputFile) event.getSource();
+        FileInfo fileInfo = inputFile.getFileInfo();
+        //file has been saved
+         if (fileInfo.isSaved()) {
+             // Path with uniqueFolder attribute default
+             if(inputFile.getId().endsWith("2")){
+                 fileLocation2 = fileInfo.getPhysicalPath();
+             }
+             // Path with uniqueFolder attribute set to 'false'
+             if(inputFile.getId().endsWith("3")){
+                 fileLocation3 = fileInfo.getPhysicalPath();
+             }
+        }
+    }
+
+    public String getComponentStatus() {
+        return componentStatus;
     }
 
     public String getFileLocation2() {
@@ -34,57 +62,5 @@ public class User {
     public String getFileLocation3() {
         return fileLocation3;
     }
-
-    public void setFileLocation3(String fileLocation3) {
-        this.fileLocation3 = fileLocation3;
-    }
-
-    public void setFileLocation2(String fileLocation2) {
-        this.fileLocation2 = fileLocation2;
-    }
-
-    public void action(ActionEvent event){
-        InputFile inputFile =(InputFile) event.getSource();
-        //file has been saved
-         if (inputFile.getStatus() == InputFile.SAVED) {
-             componentStatus = "SAVED";
-        }
-         //invalid file, happens when clicking on upload without selecting a file, or a file with no contents.
-         if (inputFile.getStatus() == InputFile.INVALID) {
-             componentStatus = "INVALID";
-         }
-       //file size exceeded the limit
-        if (inputFile.getStatus() == InputFile.SIZE_LIMIT_EXCEEDED) {
-            componentStatus = "SIZE_LIMIT_EXCEEDED";
-        }
-       //indicate that the request size is not specified.
-        if (inputFile.getStatus() == InputFile.UNKNOWN_SIZE) {
-            componentStatus = "UNKNOWN_SIZE";
-       }
-    }
-    
-    public void checkFileLocation(ActionEvent event){
-        InputFile inputFile =(InputFile) event.getSource();
-        //file has been saved
-         if (inputFile.getStatus() == InputFile.SAVED) {
-             if(inputFile.getId().endsWith("2")){
-                 fileLocation2 = inputFile.getFile().getPath();
-             }
-             if(inputFile.getId().endsWith("3")){
-                 fileLocation3 = inputFile.getFile().getPath();
-             }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-    
 
 }
