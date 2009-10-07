1b2c556a0aa04e63d0f44586869ecdee5c792655
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-07 22:32:20 +0000

    Add blocking connection support classes. Add servlet container specific libraries.

---
+++ scratchpads/snowplow/src/main/java/org/icepush/PushNotifications.java
@@ -0,0 +1,156 @@
+/*
+ * Version: MPL 1.1
+ *
+ * "The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2009 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ *
+*/
+
+package org.icepush;
+
+import org.icepush.http.Request;
+import org.icepush.http.Response;
+import org.icepush.http.ResponseHandler;
+import org.icepush.http.Server;
+import org.icepush.http.standard.FixedXMLContentHandler;
+import org.icepush.http.standard.ResponseHandlerServer;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PushNotifications extends TimerTask implements Server {
+    private static final ResponseHandler CloseResponse = new ResponseHandler() {
+        public void respond(Response response) throws Exception {
+            //let the bridge know that this blocking connection should not be re-initialized
+            response.setHeader("X-Connection", "close");
+            response.setHeader("Content-Length", 0);
+        }
+    };
+    //Define here to avoid classloading problems after application exit
+    private static final ResponseHandler NoopHandler = new FixedXMLContentHandler() {
+        public void writeTo(Writer writer) throws IOException {
+            writer.write("<noop/>");
+        }
+    };
+    private static final ResponseHandler PongHandler = new FixedXMLContentHandler() {
+        public void writeTo(Writer writer) throws IOException {
+            writer.write("<pong/>");
+        }
+    };
+    private static final ResponseHandler AfterShutdownHandler = new FixedXMLContentHandler() {
+        public void writeTo(Writer writer) throws IOException {
+            writer.write("<session-expired/>");
+        }
+    };
+    private static final Server AfterShutdown = new ResponseHandlerServer(AfterShutdownHandler);
+
+    private final BlockingQueue pendingRequest = new LinkedBlockingQueue(1);
+    private final long timeoutInterval;
+    private long responseTimeoutTime;
+    private Server activeServer;
+    private String[] updatedViews;
+    private String[] participatingViews;
+
+    public PushNotifications(HttpSession session, final Timer monitorRunner, Configuration configuration) {
+        this.timeoutInterval = configuration.getAttributeAsLong("blockingConnectionTimeout", 90000);
+
+        //add monitor
+        monitorRunner.scheduleAtFixedRate(this, System.currentTimeMillis(), 2000);
+
+        //define blocking server
+        activeServer = new Server() {
+            public void service(final Request request) throws Exception {
+                responseTimeoutTime = System.currentTimeMillis() + timeoutInterval;
+                respondIfPendingRequest(CloseResponse);
+                participatingViews = request.getParameterAsStrings("ice.view");
+                System.out.println("view >> " + Arrays.asList(participatingViews));
+                pendingRequest.put(request);
+                respondIfViewsAvailable();
+            }
+
+            public void shutdown() {
+                //avoid creating new blocking connections after shutdown
+                activeServer = AfterShutdown;
+                respondIfPendingRequest(AfterShutdownHandler);
+            }
+        };
+    }
+
+    public void service(final Request request) throws Exception {
+        activeServer.service(request);
+    }
+
+    public void shutdown() {
+        cancel();
+        activeServer.shutdown();
+    }
+
+    public void run() {
+        if ((System.currentTimeMillis() > responseTimeoutTime) && (!pendingRequest.isEmpty())) {
+            respondIfPendingRequest(NoopHandler);
+        }
+    }
+
+    private void respondIfViewsAvailable() {
+        if (updatedViews != null && updatedViews.length > 0) {
+            respondIfPendingRequest(new UpdatedViewsHandler(updatedViews));
+        }
+    }
+
+    private void respondIfPendingRequest(ResponseHandler handler) {
+        Request previousRequest = (Request) pendingRequest.poll();
+        if (previousRequest != null) {
+            try {
+                previousRequest.respondWith(handler);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void respondWhenRequestAvailable() {
+        try {
+            Request request = (Request) pendingRequest.take();
+            request.respondWith(PongHandler);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private class UpdatedViewsHandler extends FixedXMLContentHandler {
+        private String[] views;
+
+        private UpdatedViewsHandler(String[] views) {
+            this.views = views;
+        }
+
+        public void writeTo(Writer writer) throws IOException {
+            writer.write("<updated-views>");
+            for (String updatedView : views) {
+                writer.write(updatedView);
+                writer.write(' ');
+            }
+            writer.write("</updated-views>");
+        }
+    }
+}
---
+++ scratchpads/snowplow/src/main/java/org/icepush/servlet/ICEpushServlet.java
@@ -0,0 +1,72 @@
+package org.icepush.servlet;
+
+import org.icepush.Configuration;
+import org.icepush.PushNotifications;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.Timer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ICEpushServlet extends HttpServlet {
+    private static Logger log = Logger.getLogger(ICEpushServlet.class.getName());
+    private SessionDispatcher dispatcher;
+    private Timer timer;
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+
+        final ServletContext context = servletConfig.getServletContext();
+        final Configuration configuration = new ServletContextConfiguration("org.icepush", context);
+        timer = new Timer(true);
+        dispatcher = new SessionDispatcher(context) {
+            protected PseudoServlet newServer(HttpSession session) {
+                return new EnvironmentAdaptingServlet(new PushNotifications(session, timer, configuration), configuration, context);
+            }
+        };
+    }
+
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            dispatcher.service(request, response);
+        } catch (SocketException e) {
+            if ("Broken pipe".equals(e.getMessage())) {
+                // client left the page
+                if (log.isLoggable(Level.FINEST)) {
+                    log.log(Level.FINEST, "Connection broken by client.", e);
+                } else if (log.isLoggable(Level.FINE)) {
+                    log.log(Level.FINE, "Connection broken by client: " + e.getMessage());
+                }
+            } else {
+                throw new ServletException(e);
+            }
+        } catch (RuntimeException e) {
+            //ICE-4261: We cannot wrap RuntimeExceptions as ServletExceptions because of support for Jetty
+            //Continuations.  However, if the message of a RuntimeException is null, Tomcat won't
+            //properly redirect to the configured error-page.  So we need a new RuntimeException
+            //that actually includes a message.
+            if ("org.mortbay.jetty.RetryRequest".equals(e.getClass().getName())) {
+                throw e;
+            } else if (e.getMessage() != null) {
+                throw e;
+            } else {
+                throw new RuntimeException("wrapped Exception: " + e, e);
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    public void destroy() {
+        dispatcher.shutdown();
+        timer.cancel();
+    }
+}
