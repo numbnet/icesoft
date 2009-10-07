306e0d3a02a1cac6c9275e3f091546fff9f26626
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-07 20:31:03 +0000

    Make class compilable.

--- scratchpads/snowplow/src/main/java/org/icepush/PushContext.java
+++ scratchpads/snowplow/src/main/java/org/icepush/PushContext.java
@@ -1,29 +1,31 @@
 package org.icepush;
+
 import javax.servlet.http.HttpServletRequest;
 
-public class PushContext  {
+public class PushContext {
+    private String browserId;
 
-    public static synchronized PushContext getInstance(HttpServletRequest request)  {
+    public static synchronized PushContext getInstance(HttpServletRequest request) {
         //the returned PushContext must be aware of the BROWSERID cookie and
         //must have a reference to the application scope notification service
         //request.getSession().getServletContext().getAttribute(NOTIFY_SERVICE);
-       return null;
+        return null;
     }
-    
-    private PushContext(String browserId)  {
+
+    private PushContext(String browserId) {
         this.browserId = browserId;
     }
 
-    public String createPushId(String browserId)  {
+    public String createPushId(String browserId) {
         return "next pushid";
     }
 
-    public void notify(String targetName)  {
-    }
-    
-    public void addGroupMember(String groupName, String pushId)  {
+    public void notify(String targetName) {
     }
 
-    public void removeGroupMember(String groupName, String pushId)  {
+    public void addGroupMember(String groupName, String pushId) {
+    }
+
+    public void removeGroupMember(String groupName, String pushId) {
     }
 }
