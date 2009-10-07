b11738435a5602db2290f5a83b99f37848183d5d
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-07 17:31:13 +0000

    Using StateHelper

--- icefaces/scratchpads/sparkle/generator/src/org/icefaces/generator/ComponentClassGenerator.java
+++ icefaces/scratchpads/sparkle/generator/src/org/icefaces/generator/ComponentClassGenerator.java
@@ -45,7 +45,6 @@ public class ComponentClassGenerator {
     }
     
     static void endComponentClass() {
-        addSaveRestoreState();
         generatedComponentClass.append("\n}");
         createJavaFile();
 
@@ -63,236 +62,125 @@ public class ComponentClassGenerator {
     }
 
     static void addProperties(Map<String, Field> properties) {
-        Iterator<Field> fields = properties.values().iterator();
-        while(fields.hasNext()) {
-            Field field = fields.next();
-            if(field.isAnnotationPresent(Property.class)){
-                Property prop = (Property)field.getAnnotation(Property.class);
-                generatedComponentProperties.add(field);
-                
-                //create a private member if not defined already, it will usually
-                //happen with includeProperties
-                try {
-                    System.out.println(field.getName() + " : "+ Generator.currentClass.getDeclaredField(field.getName()));
-                } catch (SecurityException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } catch (NoSuchFieldException e) {
-                    generatedComponentClass.append("\n\tprivate ");   
-                    generatedComponentClass.append(field.getType().getName());
-                    generatedComponentClass.append(" ");                    
-                    generatedComponentClass.append(field.getName());
-                    generatedComponentClass.append(";");                      
-                }
-                
-                
-                //set
-                generatedComponentClass.append("\t/**\n"); 
-                generatedComponentClass.append("\t* <p>Set the value of the <code>");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("</code> property.</p>");                
-                if (prop.javadocSet() != null && !"".equals(prop.javadocSet())) {
-                    String[] lines = prop.javadocSet().split("\n");
-                    generatedComponentClass.append("\n\t* <p>Contents:");
-                     
-                    for (int j=0; j < lines.length; j++){
-                        if (j>0) {
-                            generatedComponentClass.append("\n\t* ");
-                        }
-                        generatedComponentClass.append(lines[j]);
-                        if (j == (lines.length-1)) {
-                            generatedComponentClass.append("</p>");
-                        }                        
-                    }
-                } 
-                generatedComponentClass.append("\n\t*/\n"); 
-                
-                
-                generatedComponentClass.append("\tpublic void set");
-                generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
-                generatedComponentClass.append(field.getName().substring(1));
+      Iterator<Field> fields = properties.values().iterator();
+      while(fields.hasNext()) {
+          Field field = fields.next();
+          if(field.isAnnotationPresent(Property.class)){
+              generatedComponentProperties.add(field);
+          }
+      }
+      addPropertyEnum();
+      addGetterSetter();
+    }
+       
+    static void addPropertyEnum() {
 
-                generatedComponentClass.append("(");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(" ");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append(") {\n");
-                generatedComponentClass.append("\t\tthis."); 
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append(" = "); 
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(";\n\t}\n\n");
-                
-                
-                
-                //get
-                generatedComponentClass.append("\t/**\n"); 
-                generatedComponentClass.append("\t* <p>Return the value of the <code>");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("</code> property.</p>");                
-                if (prop.javadocGet() != null && !"".equals(prop.javadocGet())) {
-                    String[] lines = prop.javadocGet().split("\n");
-                    generatedComponentClass.append("\n\t* <p>Contents:");
-                     
-                    for (int j=0; j < lines.length; j++){
-                        if (j>0) {
-                            generatedComponentClass.append("\n\t* ");
-                        }
-                        generatedComponentClass.append(lines[j]);
-                        if (j == (lines.length-1)) {
-                            generatedComponentClass.append("</p>");
-                        }                        
-                    }
-                } 
-                generatedComponentClass.append("\n\t*/\n");    
-                
-
-                generatedComponentClass.append("\tpublic ");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(" ");
-                if (field.getType().getName().endsWith("boolean")||
-                        field.getType().getName().endsWith("java.lang.Boolean")) {
-                    generatedComponentClass.append("is");
-                } else {
-                    generatedComponentClass.append("get");                    
-                }
-                generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
-                generatedComponentClass.append(field.getName().substring(1));
-                generatedComponentClass.append("() {\n");
-                generatedComponentClass.append("\t\tif (null != this.");
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(") {\n"); 
-                generatedComponentClass.append("\t\t\treturn this.");
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(";\n\t\t}\n");
-                generatedComponentClass.append("\t\tValueExpression _ve = getValueExpression(\"");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("\");\n");    
-                generatedComponentClass.append("\t\tif (_ve != null) {\n");                
-                generatedComponentClass.append("\t\t\treturn (");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(") _ve.getValue(getFacesContext().getELContext());\n"); 
-                generatedComponentClass.append("\t\t} else {\n\t\t\treturn null;\n\t\t}\n\t}\n\n");
-
-              
-            }
-        }        
+        generatedComponentClass.append("\tprotected enum PropertyKeys {\n");        
+        for (int i = 0; i < generatedComponentProperties.size(); i++){
+            generatedComponentClass.append("\t\t");     
+            generatedComponentClass.append(generatedComponentProperties.get(i).getName()); 
+            generatedComponentClass.append(",\n");            
+        }
+        generatedComponentClass.append("\t\t;\n");
+        generatedComponentClass.append("\t\tString toString;\n"); 
+        generatedComponentClass.append("\t\tPropertyKeys(String toString) { this.toString = toString; }\n");        
+        generatedComponentClass.append("\t\tPropertyKeys() { }\n"); 
+        generatedComponentClass.append("\t\tpublic String toString() {\n");       
+        generatedComponentClass.append("\t\t\treturn ((toString != null) ? toString : super.toString());\n");       
+        generatedComponentClass.append("\t\t}\n\t}\n");   
     }
     
-    
-    static void addProperties(Class clazz) {
-        Field[] fields = clazz.getDeclaredFields();
-        for (int i=0; i<fields.length; i++) {
-            Field field = fields[i];
-            if(field.isAnnotationPresent(Property.class)){
-                Property prop = (Property)field.getAnnotation(Property.class);
-                generatedComponentProperties.add(field);
-                //set
-                if (prop.javadocSet() != null && !"".equals(prop.javadocSet())) {
-                    generatedComponentClass.append("\t");    
-                    generatedComponentClass.append(prop.javadocSet().replaceAll("\n", "\n\t"));
-                    generatedComponentClass.append("\n");                    
-                } else {
-                    generatedComponentClass.append("\n\t/**\n\t * <p>Set the value of the <code>");  
-                }
-
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("</code> property.</p>\n\t */\n");
-                generatedComponentClass.append("\tpublic void set");
-                generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
-                generatedComponentClass.append(field.getName().substring(1));
-
-                generatedComponentClass.append("(");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(" ");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append(") {\n");
-                generatedComponentClass.append("\t\tthis."); 
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append(" = "); 
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(";\n\t}\n\n");
-                
-                
-                
-                //get
-                generatedComponentClass.append("\t/**\n"); 
-                generatedComponentClass.append("\t* <p>Return the value of the <code>");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("</code> property.</p>");                
-                if (prop.javadocGet() != null && !"".equals(prop.javadocGet())) {
-                    String[] lines = prop.javadocGet().split("\n");
-                    for (int j=0; j < lines.length; j++){
-                        generatedComponentClass.append("\t* ");
-                        generatedComponentClass.append(lines[j]);
-                        generatedComponentClass.append("\n");   
-                    }
-                } 
-                generatedComponentClass.append("\t*/\n");                
-
-                generatedComponentClass.append("\tpublic ");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(" ");
-                if (field.getType().getName().endsWith("boolean")||
-                        field.getType().getName().endsWith("java.lang.Boolean")) {
-                    generatedComponentClass.append("is");
-                } else {
-                    generatedComponentClass.append("get");                    
-                }
-                generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
-                generatedComponentClass.append(field.getName().substring(1));
-                generatedComponentClass.append("() {\n");
-                generatedComponentClass.append("\t\tif (null != this.");
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(") {\n"); 
-                generatedComponentClass.append("\t\t\treturn this.");
-                generatedComponentClass.append(field.getName()); 
-                generatedComponentClass.append(";\n\t\t}\n");
-                generatedComponentClass.append("\t\tValueExpression _ve = getValueExpression(\"");
-                generatedComponentClass.append(field.getName());
-                generatedComponentClass.append("\");\n");    
-                generatedComponentClass.append("\t\tif (_ve != null) {\n");                
-                generatedComponentClass.append("\t\t\treturn (");
-                generatedComponentClass.append(field.getType().getName());
-                generatedComponentClass.append(") _ve.getValue(getFacesContext().getELContext());\n"); 
-                generatedComponentClass.append("\t\t} else {\n\t\t\treturn null;\n\t\t}\n\t}\n\n");
-
-              
-            }
-        }        
-    }
-    
-    static void addSaveRestoreState() {
-        generatedComponentClass.append("\tprivate Object[] _values;\n\n");
-
-        //savestate
-        generatedComponentClass.append("\tpublic Object saveState(FacesContext _context) {\n");
-        generatedComponentClass.append("\t\tif (_values == null) {\n\t\t\t_values = new Object[");
-        generatedComponentClass.append(generatedComponentProperties.size()+1);
-        generatedComponentClass.append("];\n\t\t}\n");
-        generatedComponentClass.append("\t\t_values[0] = super.saveState(_context);\n");        
-        for (int i = 0; i < generatedComponentProperties.size(); i++){
-            generatedComponentClass.append("\t\t_values[");
-            generatedComponentClass.append((i+1));
-            generatedComponentClass.append("] = ");     
-            generatedComponentClass.append(generatedComponentProperties.get(i).getName()); 
-            generatedComponentClass.append(";\n");            
+    static void addGetterSetter() {
+        for (int i = 0; i < generatedComponentProperties.size(); i++) {
+                  Field field = generatedComponentProperties.get(i);
+                  Property prop = (Property)field.getAnnotation(Property.class);
+                 
+                  //set
+                  generatedComponentClass.append("\t/**\n"); 
+                  generatedComponentClass.append("\t* <p>Set the value of the <code>");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append("</code> property.</p>");                
+                  if (prop.javadocSet() != null && !"".equals(prop.javadocSet())) {
+                      String[] lines = prop.javadocSet().split("\n");
+                      generatedComponentClass.append("\n\t* <p>Contents:");
+                       
+                      for (int j=0; j < lines.length; j++){
+                          if (j>0) {
+                              generatedComponentClass.append("\n\t* ");
+                          }
+                          generatedComponentClass.append(lines[j]);
+                          if (j == (lines.length-1)) {
+                              generatedComponentClass.append("</p>");
+                          }                        
+                      }
+                  } 
+                  generatedComponentClass.append("\n\t*/\n"); 
+                  
+                  
+                  generatedComponentClass.append("\tpublic void set");
+                  generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
+                  generatedComponentClass.append(field.getName().substring(1));
+  
+                  generatedComponentClass.append("(");
+                  generatedComponentClass.append(field.getType().getName());
+                  generatedComponentClass.append(" ");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append(") {\n\t\tgetStateHelper().put(PropertyKeys.");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append(", ");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append(");\n");
+                  generatedComponentClass.append("\t\t//handleAttribute(\"");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append("\", ");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append(");\n");
+                  generatedComponentClass.append("\n\t}\n");
+                  
+                  
+                  
+                  //get
+                  generatedComponentClass.append("\t/**\n"); 
+                  generatedComponentClass.append("\t* <p>Return the value of the <code>");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append("</code> property.</p>");                
+                  if (prop.javadocGet() != null && !"".equals(prop.javadocGet())) {
+                      String[] lines = prop.javadocGet().split("\n");
+                      generatedComponentClass.append("\n\t* <p>Contents:");
+                       
+                      for (int j=0; j < lines.length; j++){
+                          if (j>0) {
+                              generatedComponentClass.append("\n\t* ");
+                          }
+                          generatedComponentClass.append(lines[j]);
+                          if (j == (lines.length-1)) {
+                              generatedComponentClass.append("</p>");
+                          }                        
+                      }
+                  } 
+                  generatedComponentClass.append("\n\t*/\n");    
+                  
+  
+                  generatedComponentClass.append("\tpublic ");
+                  generatedComponentClass.append(field.getType().getName());
+                  generatedComponentClass.append(" ");
+                  if (field.getType().getName().endsWith("boolean")||
+                          field.getType().getName().endsWith("java.lang.Boolean")) {
+                      generatedComponentClass.append("is");
+                  } else {
+                      generatedComponentClass.append("get");                    
+                  }
+                  generatedComponentClass.append(field.getName().substring(0,1).toUpperCase());
+                  generatedComponentClass.append(field.getName().substring(1));
+                  generatedComponentClass.append("() {\n");
+                  generatedComponentClass.append("\t\t return (");
+                  generatedComponentClass.append(field.getType().getName());
+                  generatedComponentClass.append(") getStateHelper().eval(PropertyKeys.");
+                  generatedComponentClass.append(field.getName());
+                  generatedComponentClass.append(");\n\t}\n");
+                  
+                  
         }
-        generatedComponentClass.append("\t\treturn _values;\n\t}\n\n");
-        
-        //restoretate
-        generatedComponentClass.append("\tpublic void restoreState(FacesContext _context, Object _state) {\n");
-        generatedComponentClass.append("\t\t_values = (Object[]) _state;\n\t\tsuper.restoreState(_context, _values[0]);\n");
-        for (int i = 0; i < generatedComponentProperties.size(); i++){
-            generatedComponentClass.append("\t\tthis.");
-            generatedComponentClass.append(generatedComponentProperties.get(i).getName()); 
-            generatedComponentClass.append(" = (");    
-            generatedComponentClass.append(generatedComponentProperties.get(i).getType().getName()); 
-            generatedComponentClass.append(") _values[");
-            generatedComponentClass.append((i+1));
-            generatedComponentClass.append("];\n");
-        }
-        generatedComponentClass.append("\t}");        
     }
     
     static void create() {
