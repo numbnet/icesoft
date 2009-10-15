3c9bafb9b4fd9002b130c99331361276ef3581d0
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-15 16:52:37 +0000

    IPCK-22: stop attempting to reconnect when contextDestroyed events are received

---
+++ icefaces/trunk/icefaces/core/src/com/icesoft/net/messaging/DefaultMessageServiceContextListener.java
@@ -0,0 +1,29 @@
+package com.icesoft.net.messaging;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DefaultMessageServiceContextListener implements ServletContextListener {
+
+    private static DefaultMessageService messageService;
+
+    public DefaultMessageServiceContextListener() {
+        System.out.println("DefaultMessageServiceContextListener: created " + this);
+    }
+
+    public static void setMessageService(DefaultMessageService messageService) {
+        System.out.println("DefaultMessageServiceContextListener.setMessageService: " + messageService);
+        DefaultMessageServiceContextListener.messageService = messageService;
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        if (messageService != null) {
+            System.out.println("DefaultMessageServiceContextListener.contextDestroyed: requesting stop...");
+            messageService.stop();
+            System.out.println("DefaultMessageServiceContextListener.contextDestroyed: stopped");
+        }
+    }
+}
