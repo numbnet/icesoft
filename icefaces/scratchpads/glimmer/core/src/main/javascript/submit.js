9513bcde97813b4921fb2bbc6f498ac52c7a63e5
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-16 14:04:29 +0000

    ICE-5012 Created function for submitting only the event triggering element. Factor out common code for submit functions. Rename 'submitEvent' to 'submit' function. Add public function aliases.

--- icefaces/scratchpads/glimmer/core/src/main/javascript/submit.js
+++ icefaces/scratchpads/glimmer/core/src/main/javascript/submit.js
@@ -1,12 +1,42 @@
-function submitEvent(event, element) {
-    event = event || null;
-    var options = {execute: '@all', render: '@all', 'ice.window': namespace.window};
-    var collectingQuery = object(function(method) {
-        method(addNameValue, function(self, name, value) {
-            options[name] = value;
+var singleSubmit;
+var submit;
+(function() {
+    function serializeEventToOptions(event, element, options) {
+        var collectingQuery = object(function(method) {
+            method(addNameValue, function(self, name, value) {
+                options[name] = value;
+            });
         });
-    });
-    serializeOn($event(event, element), collectingQuery);
+        serializeOn($event(event, element), collectingQuery);
+    }
 
-    jsf.ajax.request(element, event, options);
-}
+    //set these variables only once
+    var singleSubmitForm;
+    var viewState;
+    namespace.onLoad(function() {
+        singleSubmitForm = document.createElement('form');
+        singleSubmitForm.action = window.location.pathname;
+        viewState = document.getElementById('javax.faces.ViewState').value;
+    });
+
+    singleSubmit = function (event, element) {
+        var clonedElement = element.cloneNode(true);
+        singleSubmitForm.appendChild(clonedElement);
+
+        try {
+            event = event || null;
+            var options = {execute: clonedElement.id, render: '@all', 'ice.window': namespace.window, 'javax.faces.ViewState': viewState};
+            serializeEventToOptions(event, element, options);
+            jsf.ajax.request(clonedElement, event, options);
+        } finally {
+            singleSubmitForm.removeChild(clonedElement);
+        }
+    };
+
+    submit = function (event, element) {
+        event = event || null;
+        var options = {execute: '@all', render: '@all', 'ice.window': namespace.window};
+        serializeEventToOptions(event, element, options);
+        jsf.ajax.request(element, event, options);
+    };
+})();
