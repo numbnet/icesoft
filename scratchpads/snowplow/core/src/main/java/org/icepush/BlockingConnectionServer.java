f9bdc313e8e5c3fc644b5fbd95db2ddeca0745e7
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-19 20:48:11 +0000

    Make PushContext a class again.

--- scratchpads/snowplow/core/src/main/java/org/icepush/BlockingConnectionServer.java
+++ scratchpads/snowplow/core/src/main/java/org/icepush/BlockingConnectionServer.java
@@ -33,13 +33,15 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class PushNotifications extends TimerTask implements Server {
+public class BlockingConnectionServer extends TimerTask implements Server {
     private static final ResponseHandler CloseResponse = new ResponseHandler() {
         public void respond(Response response) throws Exception {
             //let the bridge know that this blocking connection should not be re-initialized
@@ -62,31 +64,26 @@ public class PushNotifications extends TimerTask implements Server {
     private ConcurrentLinkedQueue updatedViews = new ConcurrentLinkedQueue();
     private String[] participatingViews;
 
-    public PushNotifications(HttpSession session, final Timer monitorRunner, Configuration configuration) {
+    public BlockingConnectionServer(HttpSession session, final Timer monitorRunner, Configuration configuration) {
         this.timeoutInterval = configuration.getAttributeAsLong("blockingConnectionTimeout", 3000);
 
         //add monitor
         monitorRunner.scheduleAtFixedRate(this, 0, 1000);
-        PushContext pushContext = new PushContext() {
-            private int counter = 0;
-
-            public synchronized String createPushId(String browserId) {
-                return System.currentTimeMillis() + "." + counter++;
+        Observable notifier = new Observable() {
+            public synchronized void notifyObservers(Object o) {
+                setChanged();
+                super.notifyObservers(o);
+                clearChanged();
             }
-
-            public synchronized void notify(String targetName) {
-                updatedViews.offer(targetName);
+        };
+        notifier.addObserver(new Observer() {
+            public void update(Observable observable, Object o) {
+                updatedViews.offer(o);
                 resetTimeout();
                 respondIfViewsAvailable();
             }
-
-            public void addGroupMember(String groupName, String pushId) {
-            }
-
-            public void removeGroupMember(String groupName, String pushId) {
-            }
-        };
-        session.setAttribute(PushContext.class.getName(), pushContext);
+        });
+        session.setAttribute(PushContext.class.getName(), new PushContext(notifier));
 
         //define blocking server
         activeServer = new Server() {
