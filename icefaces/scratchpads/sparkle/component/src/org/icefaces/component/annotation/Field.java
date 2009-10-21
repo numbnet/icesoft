eaa7a83022aec65ee8e9b874be70bc483b7ad5b1
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-21 17:51:38 +0000

    Field annotation added

---
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/annotation/Field.java
@@ -0,0 +1,15 @@
+package org.icefaces.component.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Field {
+    boolean isTransient() default false;
+    String defaultValue() default "";
+    String javadocGet() default "";    
+    String javadocSet() default "";
+}
