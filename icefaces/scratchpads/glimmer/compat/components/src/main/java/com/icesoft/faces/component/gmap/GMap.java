85d2fbd7ee9a313bf0d8b138851c07429ed4926d
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-10-23 15:06:32 +0000

    ICE-4992 Modify component to use UIViewRoot.addComponentResource instead of JavascriptContext.includeLib.

--- icefaces/scratchpads/glimmer/compat/components/src/main/java/com/icesoft/faces/component/gmap/GMap.java
+++ icefaces/scratchpads/glimmer/compat/components/src/main/java/com/icesoft/faces/component/gmap/GMap.java
@@ -1,19 +1,19 @@
 package com.icesoft.faces.component.gmap;
 
-import java.beans.Beans;
-import java.io.IOException;
-import java.util.Map;
-
-import javax.faces.component.UIPanel;
-import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
-
 import com.icesoft.faces.component.CSS_DEFAULT;
 import com.icesoft.faces.component.ext.taglib.Util;
 import com.icesoft.faces.context.effects.JavascriptContext;
 
-public class GMap extends UIPanel{
-	public static final String COMPONENET_TYPE = "com.icesoft.faces.GMap";
+import javax.faces.component.UIOutput;
+import javax.faces.component.UIPanel;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.el.ValueBinding;
+import java.io.IOException;
+import java.util.Map;
+
+public class GMap extends UIPanel {
+    public static final String COMPONENET_TYPE = "com.icesoft.faces.GMap";
     public static final String DEFAULT_RENDERER_TYPE = "com.icesoft.faces.GMapRenderer";
     private static final String DEFAULT_LONGITUDE = "-101.162109375";
     private static final String DEFAULT_LATITUDE = "56.46249048388979";
@@ -22,38 +22,41 @@ public class GMap extends UIPanel{
     private Integer zoomLevel;
 
 
-    private Boolean locateAddress ;
+    private Boolean locateAddress;
     private boolean initilized = false;
     private String address;
     private String type;
     private String style = null;
     private String styleClass = null;
     private String renderedOnUserRole = null;
-    private transient boolean jsLibraryLoaded = false;
-    
+
+    public GMap() {
+        loadJsLibrary(FacesContext.getCurrentInstance());
+    }
+
     public String getRendererType() {
         return DEFAULT_RENDERER_TYPE;
     }
-    
+
     public String getComponentType() {
         return COMPONENET_TYPE;
     }
-    
-	public String getLongitude() {
-       if (longitude != null) {
+
+    public String getLongitude() {
+        if (longitude != null) {
             return longitude;
         }
         ValueBinding vb = getValueBinding("longitude");
         return vb != null ? (String) vb.getValue(getFacesContext()) : DEFAULT_LONGITUDE;
-	}
+    }
 
-	public String getLatitude() {
-       if (latitude != null) {
+    public String getLatitude() {
+        if (latitude != null) {
             return latitude;
         }
         ValueBinding vb = getValueBinding("latitude");
         return vb != null ? (String) vb.getValue(getFacesContext()) : DEFAULT_LATITUDE;
-	}
+    }
 
     public void setLongitude(String longitude) {
         this.longitude = longitude;
@@ -74,141 +77,146 @@ public class GMap extends UIPanel{
         if (vb != null) {
             vb.setValue(getFacesContext(), latitude);
             this.latitude = null;
-        }		
-	}
-	
-    public void decode(FacesContext facesContext) {
-    	Map map = facesContext.getExternalContext().getRequestParameterMap();
-    	String clientId = getClientId(facesContext);
-    	if (map.get(clientId+ "event") != null && map.get(clientId+ "event")
-    						.toString().length() > 0) {
-	    	 if (map.containsKey(clientId + "lat")) {
-				 setLatitude(String.valueOf(map.get(clientId + "lat")));
-			 }
-			 if(map.containsKey(clientId + "lng")){
-				 setLongitude(String.valueOf(map.get(clientId + "lng")));				 
-			 }
-			 if(map.containsKey(clientId + "zoom")){
-				 setZoomLevel(Integer.valueOf(String.
-						 valueOf(map.get(clientId + "zoom"))).intValue());				 
-			 }
-			 if(map.containsKey(clientId + "type")){
-				 setType(String.valueOf(map.get(clientId + "type")));				 
-			 }			 
-    	}
-    	
+        }
     }
-    
+
+    public void decode(FacesContext facesContext) {
+        Map map = facesContext.getExternalContext().getRequestParameterMap();
+        String clientId = getClientId(facesContext);
+        if (map.get(clientId + "event") != null && map.get(clientId + "event")
+                .toString().length() > 0) {
+            if (map.containsKey(clientId + "lat")) {
+                setLatitude(String.valueOf(map.get(clientId + "lat")));
+            }
+            if (map.containsKey(clientId + "lng")) {
+                setLongitude(String.valueOf(map.get(clientId + "lng")));
+            }
+            if (map.containsKey(clientId + "zoom")) {
+                setZoomLevel(Integer.valueOf(String.
+                        valueOf(map.get(clientId + "zoom"))).intValue());
+            }
+            if (map.containsKey(clientId + "type")) {
+                setType(String.valueOf(map.get(clientId + "type")));
+            }
+        }
+
+    }
+
     public void encodeBegin(FacesContext context) throws IOException {
-        loadJsLibrary(context);
-    	super.encodeBegin(context);
-    	//assume it page refresh/redirect
-    	if(context.getExternalContext().getRequestParameterMap() != null &&
-    	        context.getExternalContext().getRequestParameterMap().size() <= 1) {
-    	    initilized = false;
-    	}
-    	if ((isLocateAddress() || !initilized) && (getAddress() != null 
-    			&& getAddress().length() > 2)) {
-        	JavascriptContext.addJavascriptCall(context, 
-        			"Ice.GoogleMap.locateAddress('"+ getClientId(context)+"', '"+ 
-        				getAddress() +"');");
-        	initilized = true;
-    	} else {
-    		if (isLocatedByGeocoder(context)) {
-    			JavascriptContext.addJavascriptCall(context, 
-    					"Ice.GoogleMap.getGMapWrapper('"+ getClientId(context)+
-    					"').getRealGMap().setZoom("+ getZoomLevel() +");");
-    		} else { 
-    		    String latitude = getLatitude();
-    		    String longitude = getLongitude();
-    		    try {
-    		        Float.parseFloat(latitude);
-    		        Float.parseFloat(longitude);
+        super.encodeBegin(context);
+        //assume it page refresh/redirect
+        if (context.getExternalContext().getRequestParameterMap() != null &&
+                context.getExternalContext().getRequestParameterMap().size() <= 1) {
+            initilized = false;
+        }
+        if ((isLocateAddress() || !initilized) && (getAddress() != null
+                && getAddress().length() > 2)) {
+            JavascriptContext.addJavascriptCall(context,
+                    "Ice.GoogleMap.locateAddress('" + getClientId(context) + "', '" +
+                            getAddress() + "');");
+            initilized = true;
+        } else {
+            if (isLocatedByGeocoder(context)) {
+                JavascriptContext.addJavascriptCall(context,
+                        "Ice.GoogleMap.getGMapWrapper('" + getClientId(context) +
+                                "').getRealGMap().setZoom(" + getZoomLevel() + ");");
+            } else {
+                String latitude = getLatitude();
+                String longitude = getLongitude();
+                try {
+                    Float.parseFloat(latitude);
+                    Float.parseFloat(longitude);
                 } catch (NumberFormatException e) {
                     latitude = DEFAULT_LATITUDE;
                     longitude = DEFAULT_LONGITUDE;
                 }
-                JavascriptContext.addJavascriptCall(context, 
-                        "Ice.GoogleMap.getGMapWrapper('"+ getClientId(context)+
-                        "').getRealGMap().setCenter(new GLatLng("+ latitude 
-                        +", "+ longitude +"), "+ getZoomLevel() +");");
-    		}
-    	}
-    	JavascriptContext.addJavascriptCall(context, 
-    			"Ice.GoogleMap.setMapType('"+ getClientId(context)+"', '"+ 
-    			getType() +"');");
-    }
-    
-    private void loadJsLibrary(FacesContext context) {
-        if (jsLibraryLoaded) return;
-        String key = context.getCurrentInstance().getExternalContext().getInitParameter("com.icesoft.faces.gmapKey");
-        if(key != null) {
-            JavascriptContext.includeLib("http://maps.google.com/maps?file=api&v=2&key="+ key, FacesContext.getCurrentInstance());
-            jsLibraryLoaded = true;
-        } else {
-            //log you must need to define googlemap key in web.xml
-        }        
+                JavascriptContext.addJavascriptCall(context,
+                        "Ice.GoogleMap.getGMapWrapper('" + getClientId(context) +
+                                "').getRealGMap().setCenter(new GLatLng(" + latitude
+                                + ", " + longitude + "), " + getZoomLevel() + ");");
+            }
+        }
+        JavascriptContext.addJavascriptCall(context,
+                "Ice.GoogleMap.setMapType('" + getClientId(context) + "', '" +
+                        getType() + "');");
     }
 
-	public int getZoomLevel() {
-       if (zoomLevel != null) {
+    private void loadJsLibrary(FacesContext context) {
+        final String key = context.getExternalContext().getInitParameter("com.icesoft.faces.gmapKey");
+        if (key != null) {
+            context.getViewRoot().addComponentResource(context, new UIOutput() {
+                public void encodeBegin(FacesContext context) throws IOException {
+                    ResponseWriter writer = context.getResponseWriter();
+                    writer.startElement("script", this);
+                    writer.writeAttribute("type", "text/javascript", null);
+                    writer.writeAttribute("src", "http://maps.google.com/maps?file=api&v=2&key=" + key, null);
+                    writer.endElement("script");
+                }
+            }, "head");
+        } else {
+            //log you must need to define googlemap key in web.xml
+        }
+    }
+
+    public int getZoomLevel() {
+        if (zoomLevel != null) {
             return zoomLevel.intValue();
         }
         ValueBinding vb = getValueBinding("zoomLevel");
         return vb != null ? ((Integer) vb.getValue(getFacesContext())).intValue() : 3;
-	}
+    }
 
-	public void setZoomLevel(int zoomLevel) {
-		this.zoomLevel = new Integer(zoomLevel);
-	}
-	
-	private boolean isLocatedByGeocoder(FacesContext context) {
-		Object event = context.getExternalContext().getRequestParameterMap()
-		.get(getClientId(context)+ "event");
-		if (event != null && "geocoder".equals(event)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public void setZoomLevel(int zoomLevel) {
+        this.zoomLevel = new Integer(zoomLevel);
+    }
 
-	public boolean isLocateAddress() {
+    private boolean isLocatedByGeocoder(FacesContext context) {
+        Object event = context.getExternalContext().getRequestParameterMap()
+                .get(getClientId(context) + "event");
+        if (event != null && "geocoder".equals(event)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isLocateAddress() {
         if (locateAddress != null) {
             return locateAddress.booleanValue();
         }
         ValueBinding vb = getValueBinding("locateAddress");
         return vb != null ?
-               ((Boolean) vb.getValue(getFacesContext())).booleanValue() :
-               false;
-	}
+                ((Boolean) vb.getValue(getFacesContext())).booleanValue() :
+                false;
+    }
 
-	public void setLocateAddress(boolean locateAddress) {
-		this.locateAddress = new Boolean(locateAddress);
-	}
+    public void setLocateAddress(boolean locateAddress) {
+        this.locateAddress = new Boolean(locateAddress);
+    }
 
-	public String getAddress() {
+    public String getAddress() {
         if (address != null) {
             return address;
         }
         ValueBinding vb = getValueBinding("address");
         return vb != null ? (String) vb.getValue(getFacesContext()) : null;
-	}
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
-    
-	public String getType() {
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
         if (type != null) {
             return type;
         }
         ValueBinding vb = getValueBinding("type");
         return vb != null ? (String) vb.getValue(getFacesContext()) : "Map";
-	}
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}	
+    public void setType(String type) {
+        this.type = type;
+    }
 
     /**
      * <p>Set the value of the <code>renderedOnUserRole</code> property.</p>
@@ -274,33 +282,34 @@ public class GMap extends UIPanel{
     }
 
     public String getMapTdStyleClass() {
-        return Util.getQualifiedStyleClass(this, 
+        return Util.getQualifiedStyleClass(this,
                 CSS_DEFAULT.GMAP_MAP_TD);
-    }    
-    
+    }
+
     public String getTxtTdStyleClass() {
-        return Util.getQualifiedStyleClass(this, 
+        return Util.getQualifiedStyleClass(this,
                 CSS_DEFAULT.GMAP_TXT_TD);
-    }    
-            
+    }
+
     private transient Object values[];
+
     public void restoreState(FacesContext context, Object state) {
-        values = (Object[])state;
+        values = (Object[]) state;
         super.restoreState(context, values[0]);
-        renderedOnUserRole = (String)values[1];
-        longitude = (String)values[2];
-        styleClass = (String)values[3];
-        zoomLevel = (Integer)values[4];
-        type = (String)values[5];
-        style = (String)values[6];
-        latitude = (String)values[7];
-        address = (String)values[8];
-        locateAddress = (Boolean)values[9];
-        initilized =  ((Boolean)values[10]).booleanValue();
+        renderedOnUserRole = (String) values[1];
+        longitude = (String) values[2];
+        styleClass = (String) values[3];
+        zoomLevel = (Integer) values[4];
+        type = (String) values[5];
+        style = (String) values[6];
+        latitude = (String) values[7];
+        address = (String) values[8];
+        locateAddress = (Boolean) values[9];
+        initilized = ((Boolean) values[10]).booleanValue();
     }
 
     public Object saveState(FacesContext context) {
-        if(values == null){
+        if (values == null) {
             values = new Object[11];
         }
         values[0] = super.saveState(context);
@@ -312,8 +321,8 @@ public class GMap extends UIPanel{
         values[6] = style;
         values[7] = latitude;
         values[8] = address;
-        values[9] = locateAddress;   
-        values[10] = initilized? Boolean.TRUE : Boolean.FALSE;
+        values[9] = locateAddress;
+        values[10] = initilized ? Boolean.TRUE : Boolean.FALSE;
         return values;
     }
 }
