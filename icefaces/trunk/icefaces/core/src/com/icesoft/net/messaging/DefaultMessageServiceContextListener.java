8b5c93ea23b932d2bdbab54a2cb18bd49cf13aff
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-11-02 20:42:45 +0000

    IPCK-22: removing System.out calls with proper logging

--- icefaces/trunk/icefaces/core/src/com/icesoft/net/messaging/DefaultMessageServiceContextListener.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/net/messaging/DefaultMessageServiceContextListener.java
@@ -1,18 +1,21 @@
 package com.icesoft.net.messaging;
 
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class DefaultMessageServiceContextListener implements ServletContextListener {
 
+    private static Log log = LogFactory.getLog("com.icesoft.net.messaging.DefaultMessageServiceContextListener");
+
     private static DefaultMessageService messageService;
 
     public DefaultMessageServiceContextListener() {
-        System.out.println("DefaultMessageServiceContextListener: created " + this);
     }
 
     public static void setMessageService(DefaultMessageService messageService) {
-        System.out.println("DefaultMessageServiceContextListener.setMessageService: " + messageService);
         DefaultMessageServiceContextListener.messageService = messageService;
     }
 
@@ -21,9 +24,10 @@ public class DefaultMessageServiceContextListener implements ServletContextListe
 
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         if (messageService != null) {
-            System.out.println("DefaultMessageServiceContextListener.contextDestroyed: requesting stop...");
+            if (log.isDebugEnabled()) {
+                log.debug("requesting close of message service");
+            }
             messageService.close();
-            System.out.println("DefaultMessageServiceContextListener.contextDestroyed: stopped");
         }
     }
 }
