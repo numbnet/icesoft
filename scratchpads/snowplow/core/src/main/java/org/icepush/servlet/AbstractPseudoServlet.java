b213a0d45ec64cf06b0521e8d84599dc2a58cffd
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-11-11 13:04:57 +0000

    Extract abstract superclass.

---
+++ scratchpads/snowplow/core/src/main/java/org/icepush/servlet/AbstractPseudoServlet.java
@@ -0,0 +1,13 @@
+package org.icepush.servlet;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mircea
+ * Date: Nov 11, 2009
+ * Time: 4:11:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class AbstractPseudoServlet implements PseudoServlet {
+    public void shutdown() {
+    }
+}
