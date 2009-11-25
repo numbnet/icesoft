7a5154ee7e9953c110de58a73b2be7a10f22f759
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-11-25 20:26:32 +0000

    ICE-5156 Disable optimized listener cleanup by default. Change configuration parameter name.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/context/DOMResponseWriter.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/context/DOMResponseWriter.java
@@ -395,7 +395,7 @@ public class DOMResponseWriter extends ResponseWriter {
                 "window.disposeViewsURI = '" + blockingRequestHandlerContext + "block/dispose-views';\n" +
                         "var container = '" + configurationID + "'.asElement().parentNode;\n" +
                         "container.bridge = new Ice.Community.Application({" +
-                        "fullListenerCleanup: " + configuration.getAttribute("fullJSListenerCleanup", "false") + "," +
+                        "optimizedJSListenerCleanup: " + configuration.getAttribute("optimizedJSListenerCleanup", "false") + "," +
                         "blockUI: " + configuration.getAttribute("blockUIOnSubmit", "false") + "," +
                         "session: '" + sessionIdentifier + "'," +
                         "view: " + viewIdentifier + "," +
