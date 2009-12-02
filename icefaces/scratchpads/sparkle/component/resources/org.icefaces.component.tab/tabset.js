691b0c8aa95c93109b19b028f70a09ca6daaa861
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-02 19:26:48 +0000

    Fix for ICE-5183 (Sparkle: add optional hover effect to the tabSet component)

--- icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.tab/tabset.js
+++ icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.tab/tabset.js
@@ -1,64 +1,96 @@
 Ice.component.tabset = {
+    hover:false,
     initialize:function(clientId) {
-       logger.info('1. tabset initialize');
+       //logger.info('1. tabset initialize');
        var tabview = new YAHOO.widget.TabView(clientId);  
        var o = Ice.component.getProperty(clientId, 'orientation');
        if(o) {
-          logger.info('ori  new -------'+  o['new'] + ' : pre '+  o['previous'] + ' : source '+ o['modifiedByClient']);
+          //logger.info('ori  new -------'+  o['new'] + ' : pre '+  o['previous'] + ' : source '+ o['modifiedByClient']);
           tabview.set('orientation', o['new']);
        }
        var thiz = this;
-       logger.info('2. tabset initialize');
+       //logger.info('2. tabset initialize');
         if (!Ice.component.registeredComponents[clientId]) {
-         logger.info('2.1 tabset initialize');
+         //logger.info('2.1 tabset initialize');
             var onupdate = Ice.component.getProperty(clientId, 'onupdate');
             if (onupdate["new"] != null) {
-	      	    logger.info('2.2 tabset initialize');	
-	            ice.onAfterUpdate(function() {
-	            	tabview = Ice.component.getInstance(clientId, Ice.component.tabset);
-	                onupdate["new"](clientId, tabview );
-	            });
-	           logger.info('2.3 tabset initialize');     
+          	    //logger.info('2.2 tabset initialize');	
+                ice.onAfterUpdate(function() {
+                	tabview = Ice.component.getInstance(clientId, Ice.component.tabset);
+                    onupdate["new"](clientId, tabview );
+                });
+               //logger.info('2.3 tabset initialize');     
             }       
-        }       
-       logger.info('3. tabset initialize');
+        }
+       
+ 
+       
+       //add hover effect 
+       var hover = Ice.component.tabset.hover; //temp var should be accessed from the component
+       if (hover) {
+           hoverEffect = function(event, attributes) {  
+               target = Ice.eventTarget(event);
+               target = target.parentNode;
+               if ("selected" == target.parentNode.className) return;         
+               anim = new YAHOO.util.ColorAnim(target, attributes); 
+               anim.animate(); 
+           };       
+           var tabs = tabview.get('tabs');
+           //logger.info('tabs.length  '+ tabs.length);       
+           for (i=0; i<tabs.length; i++) {
+              tab = tabs[i].get('labelEl');
+              tabs[i].get('labelEl').onmouseover = function(event) {
+                  var attributes = { 
+                       backgroundColor: { from: '#DEDBDE', to: '#9F9F9F' } 
+                  }; 
+                  hoverEffect(event, attributes);        
+              }
+              tabs[i].get('labelEl').onmouseout = function(event) {
+                  var attributes = { 
+                     backgroundColor: { from: '#9F9F9F', to: '#DEDBDE'  } 
+                  }; 
+                  hoverEffect(event, attributes);         
+              }          
+           }
+       }
+       //hover ends
+               
+       //logger.info('3. tabset initialize');
        var tabChange=function(event) {
-            logger.info('on tab change ');
-             var o = Ice.component.getProperty(clientId, 'orientation');
-             var t = Ice.component.getProperty(clientId, 'tabIdx');
-	  event.target = document.getElementById(clientId);
-	  tbset = document.getElementById(clientId);
-	  currentTabIndex = clientId + '='+ tabview.getTabIndex(event.newValue);
-	  var isClientSide = Ice.component.getProperty(clientId, 'isClientSide');
+            //logger.info('on tab change ');
+            var o = Ice.component.getProperty(clientId, 'orientation');
+            var t = Ice.component.getProperty(clientId, 'tabIdx');
+            event.target = document.getElementById(clientId);
+            tbset = document.getElementById(clientId);
+            currentTabIndex = clientId + '='+ tabview.getTabIndex(event.newValue);
+            var isClientSide = Ice.component.getProperty(clientId, 'isClientSide');
 
-	  if (isClientSide && isClientSide['new']){
-	     logger.info('Client side tab ');
-	  } else {
-	     logger.info('Server side tab '+ event);
-		     try {
-			ice.singleSubmit(event, tbset, function(parameter) {
-			   parameter('yti', currentTabIndex);
-			}); 
-		     } catch(e) {
-			logger.info(e);
-		     } 	             
-
-	  }                  
-                         
-       }//tabchange; 
-       tabview.addListener('activeTabChange', tabChange);       
-logger.info('4. tabset initialize');       
-       return tabview;
+            if (isClientSide && isClientSide['new']){
+                //logger.info('Client side tab ');
+            } else {
+                //logger.info('Server side tab '+ event);
+                try {
+                    ice.singleSubmit(event, tbset, function(parameter) {
+                        parameter('yti', currentTabIndex);
+                    }); 
+                } catch(e) {
+                    //logger.info(e);
+                } 	             
+			}//end if    
+	   }//tabchange; 
+	   tabview.addListener('activeTabChange', tabChange);       
+	   //logger.info('4. tabset initialize');       
+	   return tabview;
    },
    
    updateProperties:function(_id, props) {
-         logger.info('Updateing properties orientation ');
+         //logger.info('Updateing properties orientation ');
 	     //update properties first
 	     var tabview = Ice.component.updateProperties(_id, props, this);
-	     logger.info('Updateing tabview tabview ');
+	     //logger.info('Updateing tabview tabview ');
    },
    
-    getHdnFld: function(form) {  logger.info('getInxFld called: '+ form); 
+    getHdnFld: function(form) {  //logger.info('getInxFld called: '+ form); 
         var fn = "yti";
         var yti;
         if (form) {
@@ -66,14 +98,14 @@ logger.info('4. tabset initialize');
 		    if (ele.id == fn) return true;
 		}, 'input', form);
 		if(!yti) {
-		        logger.info('Form was fouond but filed not found adding to form');
+		        //logger.info('Form was fouond but filed not found adding to form');
 			yti = this.createHiddenField(fn);
 			form.appendChild(yti);
 		}
         } else {//now add to body
             yti = document.getElementById(fn);
 			if(!yti) {
-			    logger.info('Form was not fouond and filed not found adding to body');
+			    //logger.info('Form was not fouond and filed not found adding to body');
 				yti = this.createHiddenField(fn);
 				document.body.appendChild(yti);
 			}        
@@ -91,8 +123,8 @@ logger.info('4. tabset initialize');
     
     execute: function(_id) {
        var ele = document.getElementById(_id+'call');
-       logger.info('execute ele '+ ele);
-       logger.info('execute ele '+ ele.innerHTML);
+       //logger.info('execute ele '+ ele);
+       //logger.info('execute ele '+ ele.innerHTML);
        eval(ele.innerHTML);
     }   
 };
