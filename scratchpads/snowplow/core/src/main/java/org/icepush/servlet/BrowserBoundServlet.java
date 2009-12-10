c68e43d9a4a136930e0d1cbd73aa74d2a67c4a00
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-10 14:01:42 +0000

    ICE-5192 Add Javascript functions and corresponding server-side support for adding/removing pushIds to groups.

--- scratchpads/snowplow/core/src/main/java/org/icepush/servlet/BrowserBoundServlet.java
+++ scratchpads/snowplow/core/src/main/java/org/icepush/servlet/BrowserBoundServlet.java
@@ -10,38 +10,47 @@ import java.util.Observable;
 import java.util.Timer;
 
 public class BrowserBoundServlet extends PathDispatcher {
+    private PushContext pushContext;
 
     public BrowserBoundServlet(PushContext pushContext, Observable outboundNotifier, Observable inboundNotifier, final Timer monitorRunner, Configuration configuration) {
+        this.pushContext = pushContext;
+
         dispatchOn(".*listen\\.icepush", new EnvironmentAdaptingServlet(new BlockingConnectionServer(outboundNotifier, inboundNotifier, monitorRunner, configuration), configuration));
-        dispatchOn(".*create-push-id\\.icepush", new CreatePushID(pushContext));
-        dispatchOn(".*notify\\.icepush", new NotifyPushID(pushContext));
+        dispatchOn(".*create-push-id\\.icepush", new CreatePushID());
+        dispatchOn(".*notify\\.icepush", new NotifyPushID());
+        dispatchOn(".*add-group-member\\.icepush", new AddGroupMember());
+        dispatchOn(".*remove-group-member\\.icepush", new RemoveGroupMember());
     }
 
-    private static class CreatePushID extends AbstractPseudoServlet {
-        private final PushContext pushContext;
-
-        public CreatePushID(PushContext pushContext) {
-            this.pushContext = pushContext;
-        }
-
+    private class CreatePushID extends AbstractPseudoServlet {
         public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
             response.getWriter().write(pushContext.createPushId(request, response));
             response.getWriter().write("\n\n");
         }
     }
 
-    private static class NotifyPushID extends AbstractPseudoServlet {
-        private final PushContext pushContext;
-
-        public NotifyPushID(PushContext pushContext) {
-            this.pushContext = pushContext;
-        }
-
+    private class NotifyPushID extends AbstractPseudoServlet {
         public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
             String[] ids = request.getParameterValues("id");
-            for (int i = 0; i < ids.length; i++) {
-                pushContext.push(ids[i]);
+            for (String id : ids) {
+                pushContext.push(id);
             }
+        }
+    }
+
+    private class AddGroupMember extends AbstractPseudoServlet {
+        public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+            String group = request.getParameter("group");
+            String pushID = request.getParameter("id");
+            pushContext.addGroupMember(group, pushID);
+        }
+    }
+
+    private class RemoveGroupMember extends AbstractPseudoServlet {
+        public void service(HttpServletRequest request, HttpServletResponse response) throws Exception {
+            String group = request.getParameter("group");
+            String pushID = request.getParameter("id");
+            pushContext.removeGroupMember(group, pushID);
         }
     }
 }
