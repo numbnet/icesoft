96b8c3e7fd20a5913d08370625b0145736ec96bf
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-14 11:56:06 +0000

    BridgeSetup

--- icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
@@ -3,16 +3,11 @@ package org.icefaces.push;
 import org.icefaces.application.WindowScopeManager;
 import org.icefaces.util.EnvUtils;
 
-import javax.faces.FacesException;
-import javax.faces.application.Resource;
 import javax.faces.application.ViewHandler;
 import javax.faces.application.ViewHandlerWrapper;
 import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
-import java.io.IOException;
-import java.util.Map;
 
 public class BridgeSetup extends ViewHandlerWrapper {
     private ViewHandler handler;
@@ -30,19 +25,12 @@ public class BridgeSetup extends ViewHandlerWrapper {
         return handler;
     }
 
-    public void renderView(FacesContext context, UIViewRoot root) 
-    throws IOException, FacesException  {
+    public UIViewRoot createView(FacesContext context, String viewId) {
         if (!EnvUtils.isICEfacesView(context)) {
-            handler.renderView(context, root);
-            return;
+            return handler.createView(context, viewId);
         }
 
-        Map rootAttributes = root.getAttributes();
-        if (rootAttributes.containsKey(BRIDGE_SETUP_MARKER))  {
-            handler.renderView(context, root);
-            return;
-        }
-        rootAttributes.put(BRIDGE_SETUP_MARKER, BRIDGE_SETUP_MARKER);
+        UIViewRoot root = handler.createView(context, viewId);
 
         UIOutput output;
 
@@ -65,7 +53,7 @@ public class BridgeSetup extends ViewHandlerWrapper {
 
         try {
             String id = WindowScopeManager.lookup(context).determineWindowID(context);
-            
+
             output = new UIOutput();
             output.getAttributes().put("escape", "false");
             output.setValue("<script type=\"text/javascript\">window.ice.window = " + id + ";</script>");
@@ -76,13 +64,13 @@ public class BridgeSetup extends ViewHandlerWrapper {
             output.getAttributes().put("escape", "false");
             output.setValue("<script type=\"text/javascript\">ice.push.register(['" + windowID + "'], ice.retrieveUpdate);</script>");
             root.addComponentResource(context, output, "body");
-        } catch (Exception e)  {
+        } catch (Exception e) {
             //could re-throw as a FacesException, but WindowScope failure should
             //not be fatal to the application
             e.printStackTrace();
         }
 
-        handler.renderView(context, root);
-        return;
+
+        return root;
     }
 }
