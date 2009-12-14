a71ba07d56d8b4c298d372ae4a6c10ab16158f9c
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-14 18:50:07 +0000

    initial commit of fajax test application

---
+++ icefaces/scratchpads/glimmer/samples/test/fajax/src/main/java/org/icefaces/demo/ajax/AjaxBean.java
@@ -0,0 +1,37 @@
+
+package org.icefaces.demo.ajax;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean(name = "ajaxBean")
+@ViewScoped
+public class AjaxBean implements Serializable {
+    Boolean visible = false;
+    int toggleCount = 0;
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void toggle(ActionEvent ae) {
+        System.out.println("AjaxBean: toggling");
+        toggleCount++;
+        visible = !visible;
+    }
+
+    public String getResponseWriter(){
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ResponseWriter rw = fc.getResponseWriter();
+        return rw.getClass().getName();
+    }
+
+    public int getToggleCount(){
+        return toggleCount;
+    }
+
+}
