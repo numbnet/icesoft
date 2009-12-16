106e9378f0eb693c93595c0b9374063f4aec1eda
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-16 15:33:22 +0000

    ICE-5220 Invoke WindowScopeManager.determineWindowID before restore phase is invoked. Refactor tests in 'renderView' method. Remove unused WindowSCopeSetup class.

--- icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/push/BridgeSetup.java
@@ -13,8 +13,8 @@ import java.io.IOException;
 import java.util.Map;
 
 public class BridgeSetup extends ViewHandlerWrapper {
+    private static final String Marker = BridgeSetup.class.getName();
     private ViewHandler handler;
-    private static String BRIDGE_SETUP_MARKER = BridgeSetup.class.getName();
 
     public BridgeSetup() {
         super();
@@ -28,59 +28,57 @@ public class BridgeSetup extends ViewHandlerWrapper {
         return handler;
     }
 
-    public void renderView(FacesContext context, UIViewRoot root)
-            throws IOException, FacesException {
-        if (!EnvUtils.isICEfacesView(context)) {
-            handler.renderView(context, root);
-            return;
-        }
+    public UIViewRoot createView(FacesContext context, String viewId) {
+        WindowScopeManager.lookup(context).determineWindowID(context);
+        return handler.createView(context, viewId);
+    }
 
-        Map rootAttributes = root.getAttributes();
-        if (rootAttributes.containsKey(BRIDGE_SETUP_MARKER)) {
-            handler.renderView(context, root);
-            return;
-        }
-        rootAttributes.put(BRIDGE_SETUP_MARKER, BRIDGE_SETUP_MARKER);
+    public void renderView(FacesContext context, UIViewRoot root) throws IOException, FacesException {
+        if (EnvUtils.isICEfacesView(context)) {
+            Map rootAttributes = root.getAttributes();
+            //modify UIViewRoot only once
+            if (!rootAttributes.containsKey(Marker)) {
+                rootAttributes.put(Marker, BridgeSetup.class);
 
-        UIOutput output;
+                UIOutput output;
 
-        output = new UIOutput();
-        output.setRendererType("javax.faces.resource.Script");
-        output.getAttributes().put("name", "jsf.js");
-        output.getAttributes().put("library", "javax.faces");
-        root.addComponentResource(context, output, "head");
+                output = new UIOutput();
+                output.setRendererType("javax.faces.resource.Script");
+                output.getAttributes().put("name", "jsf.js");
+                output.getAttributes().put("library", "javax.faces");
+                root.addComponentResource(context, output, "head");
 
-        //replace with icepush.js resource in icepush.jar
-        output = new UIOutput();
-        output.getAttributes().put("escape", "false");
-        output.setValue("<script src='code.icepush.jsf' type='text/javascript'></script>");
-        root.addComponentResource(context, output, "head");
+                //replace with icepush.js resource in icepush.jar
+                output = new UIOutput();
+                output.getAttributes().put("escape", "false");
+                output.setValue("<script src='code.icepush.jsf' type='text/javascript'></script>");
+                root.addComponentResource(context, output, "head");
 
-        output = new UIOutput();
-        output.setRendererType("javax.faces.resource.Script");
-        output.getAttributes().put("name", "bridge.js");
-        root.addComponentResource(context, output, "head");
+                output = new UIOutput();
+                output.setRendererType("javax.faces.resource.Script");
+                output.getAttributes().put("name", "bridge.js");
+                root.addComponentResource(context, output, "head");
 
-        try {
-            String id = WindowScopeManager.lookup(context).determineWindowID(context);
+                try {
+                    String id = WindowScopeManager.lookup(context).lookupWindowScope().getId();
 
-            output = new UIOutput();
-            output.getAttributes().put("escape", "false");
-            output.setValue("<script type=\"text/javascript\">window.ice.window = " + id + ";</script>");
-            root.addComponentResource(context, output, "body");
+                    output = new UIOutput();
+                    output.getAttributes().put("escape", "false");
+                    output.setValue("<script type=\"text/javascript\">window.ice.window = " + id + ";</script>");
+                    root.addComponentResource(context, output, "body");
 
-            String windowID = WindowScopeManager.lookup(context).lookupWindowScope().getId();
-            output = new UIOutput();
-            output.getAttributes().put("escape", "false");
-            output.setValue("<script type=\"text/javascript\">ice.push.register(['" + windowID + "'], ice.retrieveUpdate);</script>");
-            root.addComponentResource(context, output, "body");
-        } catch (Exception e) {
-            //could re-throw as a FacesException, but WindowScope failure should
-            //not be fatal to the application
-            e.printStackTrace();
+                    output = new UIOutput();
+                    output.getAttributes().put("escape", "false");
+                    output.setValue("<script type=\"text/javascript\">ice.push.register(['" + id + "'], ice.retrieveUpdate);</script>");
+                    root.addComponentResource(context, output, "body");
+                } catch (Exception e) {
+                    //could re-throw as a FacesException, but WindowScope failure should
+                    //not be fatal to the application
+                    e.printStackTrace();
+                }
+            }
         }
 
         handler.renderView(context, root);
-        return;
     }
 }
