6d58556e542fc4e0cd842a17d4ee5ca549a4378e
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2009-12-17 21:00:48 +0000

    ICE-5224 Reset hidden fields after iceSubmit or iceSubmitPartial is executed.

--- icefaces/scratchpads/glimmer/compat/core/src/main/resources/META-INF/resources/icesubmit.js
+++ icefaces/scratchpads/glimmer/compat/core/src/main/resources/META-INF/resources/icesubmit.js
@@ -1,52 +1,71 @@
 //fckeditor needs Ice namespace
 window.Ice = {};
 
-function iceSubmitPartial(form, component, evt) {
-    ice.submit(evt, component || form, function(parameter) {
-        if (Ice.Menu != null && Ice.Menu.menuContext != null) {
-            parameter('ice.menuContext', Ice.Menu.menuContext);
-        }
+var iceSubmitPartial;
+var iceSubmit;
+var formOf;
+var setFocus;
 
-        parameter('ice.submit.partial', true);
-    });
-    return false;
-}
-
-function iceSubmit(form, component, evt) {
-    var code;
-    if (evt.keyCode) code = evt.keyCode;
-    else if (evt.which) code = evt.which;
-    if (code > 3) {
-        if (code != 13) {
-            return false;
+(function() {
+    function resetHiddenFieldsFor(aForm) {
+        var elements = aForm.elements;
+        var length = elements.length;
+        for (var i = 0; i < length; i++) {
+            var formElement = elements[i];
+            if (formElement.type == 'hidden' && formElement.id == '') formElement.value = '';
         }
     }
-    ice.submit(evt, component || form, function(parameter) {
-        if (Ice.Menu != null && Ice.Menu.menuContext != null) {
-            parameter('ice.menuContext', Ice.Menu.menuContext);
+
+    iceSubmitPartial = function(form, component, evt) {
+        ice.submit(evt, component || form, function(parameter) {
+            if (Ice.Menu != null && Ice.Menu.menuContext != null) {
+                parameter('ice.menuContext', Ice.Menu.menuContext);
+            }
+
+            parameter('ice.submit.partial', true);
+        });
+
+        resetHiddenFieldsFor(form);
+        return false;
+    };
+
+    iceSubmit = function(form, component, evt) {
+        var code;
+        if (evt.keyCode) code = evt.keyCode;
+        else if (evt.which) code = evt.which;
+        if (code > 3) {
+            if (code != 13) {
+                return false;
+            }
+        }
+        ice.submit(evt, component || form, function(parameter) {
+            if (Ice.Menu != null && Ice.Menu.menuContext != null) {
+                parameter('ice.menuContext', Ice.Menu.menuContext);
+            }
+
+        });
+        resetHiddenFieldsFor(form);
+
+        return false;
+    };
+
+    formOf = function(element) {
+        var parent = element.parentNode;
+        while (parent) {
+            if (parent.tagName && parent.tagName.toLowerCase() == 'form') return parent;
+            parent = parent.parentNode;
         }
 
-    });
-    return false;
-}
-
-function formOf(element) {
-    var parent = element.parentNode;
-    while (parent) {
-        if (parent.tagName && parent.tagName.toLowerCase() == 'form') return parent;
-        parent = parent.parentNode;
+        throw 'Cannot find enclosing form.';
     }
 
-    throw 'Cannot find enclosing form.';
-}
+    setFocus = noop;
 
-function setFocus() {
-}
-
-window.onLoad = ice.onLoad;
-window.onUnload = ice.onUnload;
-var noop = function() {
-};
-window.logger = {debug: noop, info: noop, warn: noop, error: noop, child: function() {
-    return window.logger
-}};
+    window.onLoad = ice.onLoad;
+    window.onUnload = ice.onUnload;
+    var noop = function() {
+    };
+    window.logger = {debug: noop, info: noop, warn: noop, error: noop, child: function() {
+        return window.logger
+    }};
+})();
