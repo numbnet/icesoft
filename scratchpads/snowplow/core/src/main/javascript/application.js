281da1e42e3ca226a840077b58e9bb0f9a17d769
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-04 21:29:23 +0000

    PUSH-14 Rename onServerError function to avoid conflicts.

--- scratchpads/snowplow/core/src/main/javascript/application.js
+++ scratchpads/snowplow/core/src/main/javascript/application.js
@@ -58,7 +58,7 @@ if (!window.ice.icepush) {
         };
 
         var serverErrorListeners = [];
-        namespace.onServerError = function(callback) {
+        namespace.onBlockingConnectionServerError = function(callback) {
             append(serverErrorListeners, callback);
         };
 
