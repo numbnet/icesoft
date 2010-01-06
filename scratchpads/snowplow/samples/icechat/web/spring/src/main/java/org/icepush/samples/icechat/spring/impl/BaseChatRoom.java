00e49d432093dbe5b2a7476d9781dbd545b6b258
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-06 21:42:08 +0000

    PUSH-15 - Added room creation and joining

---
+++ scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/impl/BaseChatRoom.java
@@ -0,0 +1,9 @@
+package org.icepush.samples.icechat.spring.impl;
+
+import org.icepush.samples.icechat.beans.model.BaseNewChatRoomBean;
+
+public class BaseChatRoom extends BaseNewChatRoomBean {
+    public BaseChatRoom() {
+        super();
+    }
+}
--- scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/mvc/ChatFormController.java
+++ scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/mvc/ChatFormController.java
@@ -1,28 +1,70 @@
 package org.icepush.samples.icechat.spring.mvc;
 
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.mvc.AbstractFormController;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.validation.BindException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.icepush.samples.icechat.spring.impl.BasePushRequestContext;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
-public class ChatFormController implements Controller {
+public class ChatFormController extends AbstractFormController {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
-    private LoginFormData loginFormData;
+    private ChatFormData chatFormData;
 
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        return new ModelAndView("chat", "user", loginFormData);
+    public ChatFormController() {
+        super();
     }
 
-    public LoginFormData getLoginFormData() { return loginFormData; }
+    public ModelAndView showForm(HttpServletRequest request, HttpServletResponse response,
+                                 BindException errors) throws Exception {
+        return new ModelAndView("chat", "chat", chatFormData);
+    }
 
-    public void setLoginFormData(LoginFormData loginFormData) { this.loginFormData = loginFormData; }
+    public ModelAndView processFormSubmission(HttpServletRequest request, HttpServletResponse response,
+                                                 Object command, BindException errors) throws Exception {
+        if (chatFormData.getPushRequestContext() == null) {
+            chatFormData.setPushRequestContext(new BasePushRequestContext(request, response));
+        }
+
+        if (request.getParameter("submit.newRoom") != null) {
+            newRoom();
+        }
+        else if (request.getParameter("submit.joinRoom") != null) {
+            joinRoom(request.getParameter("submit.joinRoom.name"));
+        }
+
+        return new ModelAndView("chat", "chat", chatFormData);
+    }
+
+    protected Object formBackingObject(HttpServletRequest request) throws ServletException {
+        return chatFormData;
+    }
+
+    private void newRoom() {
+        chatFormData.getChatManagerViewController().setNewChatRoomBean(chatFormData.getNewChatRoom());
+        chatFormData.getChatManagerViewController().createNewChatRoom();
+        
+        // Reset the desired name
+        chatFormData.getNewChatRoom().setName(null);
+    }
+
+    private void joinRoom(String name) {
+        if (name != null) {
+            chatFormData.getChatManagerViewController().openChatSession(name);
+        }
+    }
+
+    public ChatFormData getChatFormData() {
+        return chatFormData;
+    }
+
+    public void setChatFormData(ChatFormData chatFormData) {
+        this.chatFormData = chatFormData;
+    }
 }
