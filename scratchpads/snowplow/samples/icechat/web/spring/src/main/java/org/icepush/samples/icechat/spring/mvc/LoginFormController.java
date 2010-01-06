05b0a5c42fe26f48a6603dd520a7db0a0f55b459
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-06 17:25:44 +0000

    PUSH-15 - Initialize Push Context properly and use the existing model beans to login properly

--- scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/mvc/LoginFormController.java
+++ scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/mvc/LoginFormController.java
@@ -3,28 +3,59 @@ package org.icepush.samples.icechat.spring.mvc;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.validation.BindException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.icepush.samples.icechat.spring.impl.BaseLoginController;
+import org.icepush.samples.icechat.spring.impl.BasePushRequestContext;
+import org.icepush.samples.icechat.spring.impl.BaseChatService;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.io.IOException;
 
 public class LoginFormController extends SimpleFormController {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
     private LoginFormData loginFormData;
+    private BaseLoginController baseLoginController;
+    private BaseChatService baseChatService;
 
-    public ModelAndView onSubmit(Object command) throws ServletException {
-        LoginFormData currentForm = (LoginFormData)command;
+    public LoginFormController() {
+        super();
 
-        loginFormData.setUserName(currentForm.getUserName());
-        
-        return new ModelAndView(new RedirectView(getSuccessView()));
+        baseChatService = new BaseChatService();
+        baseLoginController = new BaseLoginController();
+
+        baseLoginController.setChatService(baseChatService);
+    }
+
+    public ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,
+                                 Object command, BindException errors)
+                                 throws Exception {
+        if ((errors != null) && (errors.getErrorCount() == 0)) {
+            try{
+                if (baseLoginController.getCredentialsBean() == null) {
+                    baseLoginController.setCredentialsBean(loginFormData);
+                }
+
+                if (baseLoginController.getPushRequestContext() == null) {
+                    baseLoginController.setPushRequestContext(new BasePushRequestContext(request, response));
+                }
+
+                baseLoginController.register();
+                baseLoginController.login();
+
+                return new ModelAndView(new RedirectView(getSuccessView()));
+            }catch (Exception failedLogin) {
+                logger.error("Failed to attempt to log a user in", failedLogin);
+            }
+        }
+
+        return new ModelAndView(new RedirectView("login.htm"));
     }
 
     protected Object formBackingObject(HttpServletRequest request) throws ServletException {
@@ -32,6 +63,10 @@ public class LoginFormController extends SimpleFormController {
     }
 
     public LoginFormData getLoginFormData() { return loginFormData; }
+    public BaseLoginController getBaseLoginController() { return baseLoginController; }
+    public BaseChatService getBaseChatService() { return baseChatService; }
     
     public void setLoginFormData(LoginFormData loginFormData) { this.loginFormData = loginFormData; }
+    public void setBaseLoginController(BaseLoginController baseLoginController) { this.baseLoginController = baseLoginController; }
+    public void setBaseChatService(BaseChatService baseChatService) { this.baseChatService = baseChatService; }
 }
