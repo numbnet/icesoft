b8152fcd8b519de4649217b46107938112131002
patrick.wilson <patrick.wilson@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-11 16:22:20 +0000

    transfered backend to use the common api

--- scratchpads/snowplow/samples/icechat/web/gwt/src/org/icepush/samples/icechat/gwt/server/service/ChatServiceImpl.java
+++ scratchpads/snowplow/samples/icechat/web/gwt/src/org/icepush/samples/icechat/gwt/server/service/ChatServiceImpl.java
@@ -4,8 +4,9 @@ import org.icepush.samples.icechat.gwt.client.chat.ChatRoomMessage;
 import org.icepush.samples.icechat.gwt.client.service.ChatService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.icepush.samples.icechat.gwt.push.adapter.GWTPushRequestContextAdaptor;
+import org.icepush.samples.icechat.gwt.server.service.beans.AuthenticationProvider;
+import org.icepush.samples.icechat.gwt.server.service.beans.ChatServiceBean;
 import org.icepush.PushContext;
-
 
 import java.util.List;
 import java.util.ArrayList;
@@ -13,111 +14,152 @@ import java.util.HashMap;
 import org.icepush.samples.icechat.gwt.client.chat.ChatHandleBuilder;
 import org.icepush.samples.icechat.gwt.client.chat.ChatMessageBuilder;
 import org.icepush.samples.icechat.gwt.client.chat.ChatRoomHandle;
+import org.icepush.samples.icechat.model.ChatRoom;
+import org.icepush.samples.icechat.model.Message;
+import org.icepush.samples.icechat.model.User;
+import org.icepush.samples.icechat.model.UserChatSession;
 
-public class ChatServiceImpl extends RemoteServiceServlet implements ChatService {
+public class ChatServiceImpl extends RemoteServiceServlet implements
+		ChatService {
 
-    private List<String> chatRooms = new ArrayList<String>();
-    private HashMap<String, List<String>> participants = new HashMap<String, List<String>>();
+	private List<String> chatRooms = new ArrayList<String>();
+	private HashMap<String, List<String>> participants = new HashMap<String, List<String>>();
+	private HashMap<String, String> participantTextBoxes = new HashMap<String, String>();
+	private HashMap<String, List<ChatRoomMessage>> chatMessages = new HashMap<String, List<ChatRoomMessage>>();
 
-    private HashMap<String, String> participantTextBoxes = new HashMap<String, String>();
+	public ChatRoomHandle createChatRoom(String name) {
 
-    private HashMap<String, List<ChatRoomMessage>> chatMessages  = new HashMap<String, List<ChatRoomMessage>>();
+		User currentUser = AuthenticationProvider
+				.getSessionUser(getThreadLocalRequest());
 
-    public ChatRoomHandle createChatRoom(String name) {
-        
-            this.chatRooms.add(name);
-            GWTPushRequestContextAdaptor pushAdaptor = GWTPushRequestContextAdaptor.getInstance(this.getServletContext(), getThreadLocalRequest(), getThreadLocalResponse());
-            PushContext pushContext = pushAdaptor.getPushContext();
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+				.getServletContext());
+		UserChatSession session = chatService.createNewChatRoom(name,
+				currentUser.getUserName(), currentUser.getPassword());
+		//    	
+		// this.chatRooms.add(name);
+		GWTPushRequestContextAdaptor pushAdaptor = GWTPushRequestContextAdaptor
+				.getInstance(this.getServletContext(), getThreadLocalRequest(),
+						getThreadLocalResponse());
+		PushContext pushContext = pushAdaptor.getPushContext();
 
-            ChatHandleBuilder builder = new ChatHandleBuilder();
-            ChatRoomHandle handle = builder.createHandle(name);
+		ChatHandleBuilder builder = new ChatHandleBuilder();
+		ChatRoomHandle handle = builder.createHandle(session.getRoom()
+				.getName());
 
-            pushContext.push("chatRoomIndex");
-            return handle;
-        
-    }
+		pushContext.push("chatRoomIndex");
+		return handle;
 
-    public List<ChatRoomHandle> getChatRooms() {
-        List<ChatRoomHandle> result = new ArrayList<ChatRoomHandle>(this.chatRooms.size());
+	}
 
-        for (String chatName : this.chatRooms) {
-            ChatHandleBuilder builder = new ChatHandleBuilder();
-            ChatRoomHandle handle = builder.createHandle(chatName);
-            result.add(handle);
-        }
-        return result;
-    }
+	public List<ChatRoomHandle> getChatRooms() {
 
-    public void joinChatRoom(ChatRoomHandle handle,String username) {
-        System.out.println("Joining Chat Room...");
-        List<String> participantList = this.participants.get(handle.getName());
-        if(participantList == null){
-            participantList = new ArrayList<String>();
-            this.participants.put(handle.getName(), participantList);
-        }
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+				.getServletContext());
 
-        if(participantList.contains(username)){
-            return; //already joined.
-        }
+		List<ChatRoom> chatRooms = chatService.getChatRooms();
+		List<ChatRoomHandle> result = new ArrayList<ChatRoomHandle>(chatRooms
+				.size());
 
-        this.participantTextBoxes.put(username + handle.getName(), "");
-        
-        participantList.add(username);
-        PushContext.getInstance(this.getServletContext()).push(handle.getName()+"-participants");
+		for (ChatRoom chatRoom : chatRooms) {
+			ChatHandleBuilder builder = new ChatHandleBuilder();
+			ChatRoomHandle handle = builder.createHandle(chatRoom.getName());
+			result.add(handle);
+		}
+		return result;
+	}
+
+	public void joinChatRoom(ChatRoomHandle handle, String username) {
+		
+		User currentUser = AuthenticationProvider
+				.getSessionUser(getThreadLocalRequest());
+
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+				.getServletContext());
+
+		chatService.loginToChatRoom(handle.getName(), currentUser.getUserName(), currentUser.getPassword());
+		
+		this.participantTextBoxes.put(username + handle.getName(), "");
+
+		PushContext.getInstance(this.getServletContext()).push(
+				handle.getName() + "-participants");
+
+	}
+
+	public List<String> getParticipants(ChatRoomHandle handle) {
+//		System.out.println("Getting Participants");
+//		return this.participants.get(handle.getName());
+		
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+				.getServletContext());
+		
+		List<User> users = chatService.getChatRoomParticipants(handle.getName());
+		
+		List<String> userNames = new ArrayList<String>(users.size());
+		for(User u: users){
+			userNames.add(u.getUserName());
+		}
+		return userNames;
+		
+	}
+
+	public void sendMessage(String message, String username,
+			ChatRoomHandle handle) {
+		
+		
+		User currentUser = AuthenticationProvider
+			.getSessionUser(getThreadLocalRequest());
+
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+			.getServletContext());
+		
+		chatService.sendNewMessage(handle.getName(), currentUser.getUserName(), currentUser.getPassword(), message);
 
 
-    }
+		participantTextBoxes.put(username + handle.getName(), "");
 
-    public List<String> getParticipants(ChatRoomHandle handle) {
-        System.out.println("Getting Participants");
-        return this.participants.get(handle.getName());
-    }
+		PushContext.getInstance(this.getServletContext())
+				.push(handle.getName());
+		PushContext.getInstance(this.getServletContext()).push(
+				username + handle.getName().replaceAll(" ", "_"));
+	}
 
-    public void sendMessage(String message, String username, ChatRoomHandle handle) {
-        System.out.println("Sending message: '" + message + "'");
-        List<ChatRoomMessage> messages = this.chatMessages.get(handle.getName());
-        if(messages == null){
-            messages = new ArrayList<ChatRoomMessage>();
-            this.chatMessages.put(handle.getName(), messages);
-        }
+	public ChatRoomHandle getMessages(ChatRoomHandle handle) {
+		try{
+		ChatServiceBean chatService = ChatServiceBean.getInstance(this
+			.getServletContext());
+		
+		List<ChatRoomMessage> result = new ArrayList<ChatRoomMessage>();
+		
+		List<Message> messages = chatService.getChatRoomMessagesFromIndex(handle.getName(), handle.getMessageIndex());
+		
+		ChatMessageBuilder messageBuilder = new ChatMessageBuilder();
+		for(Message m: messages){
+			result.add(messageBuilder.createChatMessage(m.getMessage(), m.getUserChatSession().getUser().getUserName()));
+		}
 
-        ChatMessageBuilder builder = new ChatMessageBuilder();
-        messages.add(builder.createChatMessage(message, username));
+		
 
-        participantTextBoxes.put(username + handle.getName(), "");
+		ChatHandleBuilder builder = new ChatHandleBuilder();
+		builder.addMessages(handle, result);
+		return handle;
+		}catch(RuntimeException e){
+			e.printStackTrace();
+			throw e;
+		}
+	}
 
-        PushContext.getInstance(this.getServletContext()).push(handle.getName());
-        PushContext.getInstance(this.getServletContext()).push(username+handle.getName().replaceAll(" ", "_"));
-    }
+	public void sendCharacterNotification(String username,
+			ChatRoomHandle handle, String newText) {
+		this.participantTextBoxes.put(username + handle.getName(), newText);
 
-    public ChatRoomHandle getMessages(ChatRoomHandle handle) {
-        
-        List<ChatRoomMessage> result = new ArrayList<ChatRoomMessage>();
-        if(this.chatMessages.get(handle.getName()) == null){
-            return handle; //return the unchanged handle.
-        }
+		PushContext.getInstance(this.getServletContext()).push(
+				username + handle.getName().replaceAll(" ", "_"));
 
-        for(int i = handle.getMessageIndex(); i < this.chatMessages.get(handle.getName()).size(); i++){
-            result.add(this.chatMessages.get(handle.getName()).get(i));
-        }
+	}
 
-        ChatHandleBuilder builder = new ChatHandleBuilder();
-        builder.addMessages(handle, result);
-        System.out.println("Getting new messages...found " + handle.getNextMessages().size() + " (new index: " + handle.getMessageIndex() + ")");
-        return handle;
-    }
+	public String getCurrentCharacters(String username, ChatRoomHandle handle) {
+		return this.participantTextBoxes.get(username + handle.getName());
+	}
 
-    public void sendCharacterNotification(String username, ChatRoomHandle handle, String newText) {
-        this.participantTextBoxes.put(username + handle.getName(), newText);
-
-        System.out.println("Push [" + username + handle.getName().replaceAll(" ", "_") + "]");
-        PushContext.getInstance(this.getServletContext()).push(username + handle.getName().replaceAll(" ", "_"));
-
-    }
-
-    public String getCurrentCharacters(String username, ChatRoomHandle handle){
-        return this.participantTextBoxes.get(username + handle.getName());
-    }
-
- 
 }
--- scratchpads/snowplow/samples/icechat/web/gwt/src/org/icepush/samples/icechat/gwt/server/service/beans/ChatServiceBean.java
+++ scratchpads/snowplow/samples/icechat/web/gwt/src/org/icepush/samples/icechat/gwt/server/service/beans/ChatServiceBean.java
@@ -1,8 +1,14 @@
 package org.icepush.samples.icechat.gwt.server.service.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletContext;
 
 import org.icepush.samples.icechat.beans.service.BaseChatServiceBean;
+import org.icepush.samples.icechat.model.ChatRoom;
+import org.icepush.samples.icechat.model.User;
+import org.icepush.samples.icechat.model.UserChatSession;
 
 public class ChatServiceBean extends BaseChatServiceBean{
 
@@ -15,5 +21,15 @@ public class ChatServiceBean extends BaseChatServiceBean{
 		}
 		
 		return (ChatServiceBean) context.getAttribute(ChatServiceBean.class.getName());
+	}
+	
+	public List<User> getChatRoomParticipants(String chatRoom){
+		List<User> result = new ArrayList<User>();
+		
+		ChatRoom room = this.chatRooms.get(chatRoom);
+		for(UserChatSession session: room.getUserChatSessions()){
+			result.add(session.getUser());
+		}
+		return result;
 	}
 }
