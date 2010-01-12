7fd7e133f27a605721ff39edbc7f689d3fbf3a32
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-12 16:04:59 +0000

    delete obsolete dir

--- scratchpads/snowplow/samples/icechat/jee6/web/gwt/src/org/icepush/samples/icechat/gwt/client/UserServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package org.icepush.samples.icechat.gwt.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-import org.icepush.samples.icechat.gwt.client.User;
-
-public interface UserServiceAsync{
-	void register(User name, AsyncCallback<Void> callback);
-}
