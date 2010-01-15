486a0ef3fb47665d3b72c4c34b2e7ec88868832e
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-15 19:33:25 +0000

    fix for ICE-5268 (loadBundle is not serializable)

--- icefaces/scratchpads/glimmer/compat/components/src/main/java/com/icesoft/faces/component/loadbundle/LoadBundle.java
+++ icefaces/scratchpads/glimmer/compat/components/src/main/java/com/icesoft/faces/component/loadbundle/LoadBundle.java
@@ -1,6 +1,7 @@
 package com.icesoft.faces.component.loadbundle;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
@@ -18,7 +19,8 @@ import javax.faces.el.ValueBinding;
 
 import com.icesoft.faces.utils.MessageUtils;
 
-public class LoadBundle extends UIOutput{
+public class LoadBundle extends UIOutput implements Serializable{
+    private static final long serialVersionUID = 1L;
     public static final String COMPONENT_TYPE = "com.icesoft.faces.LoadBundle";
     public static final String COMPONENT_FAMILY = "com.icesoft.faces.LoadBundle";
     private String basename;
@@ -55,101 +57,7 @@ public class LoadBundle extends UIOutput{
             bundle = ResourceBundle.getBundle(newBasename.trim(),
                     currentLocale,
                     MessageUtils.getClassLoader(this)); 
-            map = new Map() {
-
-                public void clear() {
-                    throw new UnsupportedOperationException();
-                }
-
-                public boolean containsKey(Object key) {
-                    return (null == key)?  false : (null != bundle.getObject(key.toString())) ;
-                }
-
-                public boolean containsValue(Object value) {
-                    boolean found = false;
-                    Object currentValue = null;
-                    Enumeration keys = bundle.getKeys();
-                    while (keys.hasMoreElements()) {
-                        currentValue = bundle.getObject((String) keys.nextElement());
-                        if ( (value == currentValue) ||
-                                ((null != currentValue) && currentValue.equals(value))) {
-                            found = true;
-                            break;
-                        }
-                    }
-                    return found;
-                }
-
-                public Set entrySet() {
-                    HashMap entries = new HashMap();
-                    Enumeration keys = bundle.getKeys();
-                    while (keys.hasMoreElements()) {
-                        Object key = keys.nextElement();
-                        Object value = bundle.getObject((String)key);
-                        entries.put(key, value);
-                    }
-                    return entries.entrySet();
-                }
-
-                public Object get(Object key) {
-                    if (null == key) return null;
-                    Object result = null;
-                    try {
-                        result = bundle.getObject(key.toString());
-                    } catch (MissingResourceException mre) {
-                        result = "???"+ key + "???";
-                    }
-                    return result;
-                }
-
-                public boolean isEmpty() {
-                    return !bundle.getKeys().hasMoreElements();
-                }
-
-                public Set keySet() {
-                    Set keySet = new HashSet();
-                    Enumeration keys = bundle.getKeys();
-                    while (keys.hasMoreElements()) {
-                        keySet.add(keys.nextElement());
-                    }
-                    return keySet;
-                }
-
-                public Object put(Object key, Object value) {
-                    throw new UnsupportedOperationException();
-                }
-
-                public void putAll(Map t) {
-                    throw new UnsupportedOperationException();                }
-
-                public Object remove(Object key) {
-                    throw new UnsupportedOperationException();
-                }
-
-                public int size() {
-                    int size = 0;
-                    Enumeration keys = bundle.getKeys();
-                    while (keys.hasMoreElements()) {
-                        keys.nextElement();
-                        size++;
-                    }
-                    return size;
-                }
-
-                public Collection values() {
-                    ArrayList values = new ArrayList();
-                    Enumeration keys = bundle.getKeys();
-                    while(keys.hasMoreElements()) {
-                        values.add(bundle.getObject((String)keys.nextElement()));
-                    }
-                    return values;
-                }
-                
-                public int hashCode() {
-                    return bundle.hashCode();
-                }
-                
-            };
+            map = new SerializableMap();
             context.getExternalContext().getRequestMap().put(getVar(), map); 
         }
         oldBasename = newBasename;
@@ -203,4 +111,101 @@ public class LoadBundle extends UIOutput{
         var = (String) values[2];    
         map = (Map) values[3];
     }    
+
+    class SerializableMap implements Map, Serializable {
+        private static final long serialVersionUID = 1L;
+
+            public void clear() {
+                throw new UnsupportedOperationException();
+            }
+
+            public boolean containsKey(Object key) {
+                return (null == key)?  false : (null != bundle.getObject(key.toString())) ;
+            }
+
+            public boolean containsValue(Object value) {
+                boolean found = false;
+                Object currentValue = null;
+                Enumeration keys = bundle.getKeys();
+                while (keys.hasMoreElements()) {
+                    currentValue = bundle.getObject((String) keys.nextElement());
+                    if ( (value == currentValue) ||
+                            ((null != currentValue) && currentValue.equals(value))) {
+                        found = true;
+                        break;
+                    }
+                }
+                return found;
+            }
+
+            public Set entrySet() {
+                HashMap entries = new HashMap();
+                Enumeration keys = bundle.getKeys();
+                while (keys.hasMoreElements()) {
+                    Object key = keys.nextElement();
+                    Object value = bundle.getObject((String)key);
+                    entries.put(key, value);
+                }
+                return entries.entrySet();
+            }
+
+            public Object get(Object key) {
+                if (null == key) return null;
+                Object result = null;
+                try {
+                    result = bundle.getObject(key.toString());
+                } catch (MissingResourceException mre) {
+                    result = "???"+ key + "???";
+                }
+                return result;
+            }
+
+            public boolean isEmpty() {
+                return !bundle.getKeys().hasMoreElements();
+            }
+
+            public Set keySet() {
+                Set keySet = new HashSet();
+                Enumeration keys = bundle.getKeys();
+                while (keys.hasMoreElements()) {
+                    keySet.add(keys.nextElement());
+                }
+                return keySet;
+            }
+
+            public Object put(Object key, Object value) {
+                throw new UnsupportedOperationException();
+            }
+
+            public void putAll(Map t) {
+                throw new UnsupportedOperationException();                }
+
+            public Object remove(Object key) {
+                throw new UnsupportedOperationException();
+            }
+
+            public int size() {
+                int size = 0;
+                Enumeration keys = bundle.getKeys();
+                while (keys.hasMoreElements()) {
+                    keys.nextElement();
+                    size++;
+                }
+                return size;
+            }
+
+            public Collection values() {
+                ArrayList values = new ArrayList();
+                Enumeration keys = bundle.getKeys();
+                while(keys.hasMoreElements()) {
+                    values.add(bundle.getObject((String)keys.nextElement()));
+                }
+                return values;
+            }
+            
+            public int hashCode() {
+                return bundle.hashCode();
+            }
+            
+        }
 }
