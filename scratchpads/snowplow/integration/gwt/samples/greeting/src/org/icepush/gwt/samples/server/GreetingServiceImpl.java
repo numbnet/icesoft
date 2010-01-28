f82af00131b9ec155b0cd1f89c6be4febf72af92
patrick.wilson <patrick.wilson@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-01-28 17:52:06 +0000

    added greeting extension sample for GWT

---
+++ scratchpads/snowplow/integration/gwt/samples/greeting/src/org/icepush/gwt/samples/server/GreetingServiceImpl.java
@@ -0,0 +1,24 @@
+package org.icepush.gwt.samples.server;
+
+import org.icepush.PushContext;
+import org.icepush.gwt.samples.client.GreetingService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+    GreetingService {
+
+  public String greetServer(String input) {
+    String serverInfo = getServletContext().getServerInfo();
+    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+    
+    PushContext pushContext = PushContext.getInstance(this.getServletContext());
+    pushContext.push("new-user");
+    
+    return "Hello, " + input + "!<br><br>I am running " + serverInfo
+        + ".<br><br>It looks like you are using:<br>" + userAgent;
+  }
+}
