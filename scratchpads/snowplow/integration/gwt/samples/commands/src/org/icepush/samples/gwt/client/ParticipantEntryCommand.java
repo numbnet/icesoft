93505713a139e452e1b9fe953aa0c434df6196f0
patrick.wilson <patrick.wilson@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-01 22:29:16 +0000

    PUSH-16: added basic sample application to demonstrate the command API.

---
+++ scratchpads/snowplow/integration/gwt/samples/commands/src/org/icepush/samples/gwt/client/ParticipantEntryCommand.java
@@ -0,0 +1,9 @@
+package org.icepush.samples.gwt.client;
+
+import java.io.Serializable;
+
+import org.icepush.integration.gwt.command.ICommand;
+
+public class ParticipantEntryCommand implements ICommand,Serializable{
+
+}
