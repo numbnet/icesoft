eb1baf17eabeb35af179228f49bfec081852facb
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-01 22:59:20 +0000

    add chaining functionality

--- scratchpads/snowplow/integration/jquery/core/src/main/javascript/jquery.icepush.js
+++ scratchpads/snowplow/integration/jquery/core/src/main/javascript/jquery.icepush.js
@@ -1,81 +1,80 @@
-jQuery.fn.appendPushUpdate = function(groupName, getURL) {
-	return this.each(function() {
-		var pushId = ice.push.createPushId();
-		ice.push.addGroupMember(groupName, pushId);
-		ice.push.register( [ pushId ], function(){ 
-			this.append($.get(getURL));
-		});
-	});
-};
-
-jQuery.fn.loadPushUpdate = function(groupName, getURL) {
-	return this.each(function() {
-		var pushId = ice.push.createPushId();
-		var elem = this;
-		ice.push.addGroupMember(groupName, pushId);
-		ice.push.register( [ pushId ], function(){ 
-			$(elem).load($.get(getURL));
-		});
-	});
-};
-
-jQuery.push = {
-	
-	groups : {},	
-	
-	listenToGroup : function(groupName, callback) {
-		var pushId = ice.push.createPushId();
-		if( this.groups[groupName] == null ){
-			this.groups[groupName] = new Array();			
-		}
-		this.groups[groupName].push([callback,pushId]);
-		ice.push.addGroupMember(groupName, pushId);
-		ice.push.register( [ pushId ], callback);
-	},
-	
-	stopListeningToGroup: function(groupName, callback){
-		var registrations = this.groups[groupName];
-		if( registrations ){
-			for( var i=0; i < registrations.length ; i++ ){
-				if( registrations[i][0] == callback ){
-					ice.push.removeGroupMember(groupName,registrations[i][1]);
-					registrations[i] = null;
-					if( registrations.length == 0 )
-						this.groups[groupName] = null;
-					break;
+(function($) {	
+	$.push = {
+		version : 0.1,
+		groups : {},
+		pushIds : {},
+		listenToGroup : function(groupName, callback) {
+			var pushId = ice.push.createPushId();
+			if (this.groups[groupName] == null) {
+				this.groups[groupName] = new Array();
+			}
+			this.groups[groupName].push( [ callback, pushId ]);
+			this.pushIds[pushId] = [groupName,callback];
+			ice.push.addGroupMember(groupName, pushId);
+			ice.push.register( [ pushId ], callback);
+		},		
+		stopListeningToGroup : function(groupName, callback) {
+			var registrations = this.groups[groupName];
+			if (registrations) {
+				//if callback passed, just remove registrations for this callback
+				if( callback ){						
+					for ( var i = 0; i < registrations.length; i++) {
+						if (registrations[i][0] == callback) {
+							ice.push.removeGroupMember(groupName,
+									registrations[i][1]);
+							$.push.pushIds[registrations[i][1]] = null;
+							registrations[i] = null;
+							if (registrations.length == 0)
+								this.groups[groupName] = null;
+							break;
+						}
+					}
+				}
+				else{
+					for ( var i = 0; i < registrations.length; i++) {
+						ice.push.removeGroupMember(groupName,
+								registrations[i][1]);
+						$.push.pushIds[registrations[i][1]] = null;
+						registrations[i] = null;
+					}
+					this.groups[groupName] = null;
 				}
 			}
 		}
-	},
-	
-	appendUpdates: function(selector, groupName, getURL){
-		var pushId = ice.push.createPushId();
-		if( this.groups[groupName] == null ){
-			this.groups[groupName] = new Array();			
-		}
-		this.groups[groupName].push([getURL,pushId]);
-		ice.push.addGroupMember(groupName, pushId);
-		ice.push.register( [ pushId ], function(){
-			$.get(getURL,function(data){
-				$(selector).each( function(idx,elem){
-					$(elem).append(data);
-				});
-			});			
-		});
-	},
-	loadUpdates: function(selector, groupName, getURL){
-		var pushId = ice.push.createPushId();
-		if( this.groups[groupName] == null ){
-			this.groups[groupName] = new Array();			
-		}
-		this.groups[groupName].push([getURL,pushId]);
-		ice.push.addGroupMember(groupName, pushId);
-		ice.push.register( [ pushId ], function(){
-			$.get(getURL,function(data){
-				$(selector).each( function(idx,elem){
-					$(elem).html(data);
-				});
-			});			
-		});
-	}
-}
+	};
+	$.fn.loadPushUpdates = function(groupName, getURL){
+		return this.each(function(idx,elem){
+        	var pushId = ice.push.createPushId();
+        	if ($.push.groups[groupName] == null) {
+        		$.push.groups[groupName] = new Array();
+			}
+        	$.push.groups[groupName].push( [ getURL, pushId ])
+			$.push.pushIds[pushId] = [elem,getURL];
+			ice.push.addGroupMember(groupName, pushId);
+    		ice.push.register( [ pushId ], function(pushId) {    			
+    			$($.push.pushIds[pushId][0]).load($.push.pushIds[pushId][1], function(){
+    				$($.push.pushIds[pushId][0]).ready( function(){    					
+    					$($.push.pushIds[pushId][0]).hide().fadeIn(1500);
+    				});
+    			});    			
+    		});
+        });
+    };    
+    $.fn.appendPushUpdates = function(groupName, getURL){
+    	return this.each(function(idx,elem){
+        	var pushId = ice.push.createPushId();
+        	if ($.push.groups[groupName] == null) {
+        		$.push.groups[groupName] = new Array();
+			}
+        	$.push.groups[groupName].push( [ getURL, pushId ])
+        	$.push.pushIds[pushId] = [elem,getURL];
+			ice.push.addGroupMember(groupName, pushId);
+    		ice.push.register( [ pushId ], function(pushId) {    
+    			$.get($.push.pushIds[pushId][1],function (data){
+    				$($.push.pushIds[pushId][0]).append(data);
+    			});
+    		});
+        });
+    };    
+	return this;
+})(jQuery);
