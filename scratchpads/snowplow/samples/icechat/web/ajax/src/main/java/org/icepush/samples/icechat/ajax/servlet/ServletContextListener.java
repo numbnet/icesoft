94f026048c07b0165d96ed5c5695aa60b0b88bf5
patrick.wilson <patrick.wilson@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-03 17:52:14 +0000

    fixed another faulty override annotation

--- scratchpads/snowplow/samples/icechat/web/ajax/src/main/java/org/icepush/samples/icechat/ajax/servlet/ServletContextListener.java
+++ scratchpads/snowplow/samples/icechat/web/ajax/src/main/java/org/icepush/samples/icechat/ajax/servlet/ServletContextListener.java
@@ -5,11 +5,9 @@ import javax.servlet.ServletContextEvent;
 public class ServletContextListener implements
 		javax.servlet.ServletContextListener {
 
-	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 	}
 
-	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 
 		sce.getServletContext().setAttribute("chatService", new SynchronizedChatServiceBean());
