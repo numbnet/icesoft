fdbc5b18e1bf4d34baa9c3cb135fb13238ff557e
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-04 21:58:57 +0000

    PUSH-6 Initial commit of Wicket integration.  The integration consists of PushPanel.java, PushPanel.html and WicketPushRequestContext.java.

---
+++ scratchpads/snowplow/integration/wicket/samples/pushpanel/src/main/java/org/icepush/integration/wicket/samples/pushpanel/PushPanel.java
@@ -0,0 +1,61 @@
+package org.icepush.integration.wicket.samples.pushpanel;
+
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebResponse;
+import org.icepush.PushContext;
+
+/**
+ * Template used to implement push for a specific region (Panel)
+ */
+public final class PushPanel extends Panel {
+
+    // Initialize PushContext (Creates PushId)
+    WicketPushRequestContext pushRequestContext = new WicketPushRequestContext((WebRequest)getRequest(),(WebResponse)getResponse());
+
+    public PushPanel(String id) {
+        super (id);
+        this.setOutputMarkupId(true);
+
+        // Create or add to your group(s).  In this case the group is 'ExamplePanel'.
+        PushContext.getInstance(getWebRequest().getHttpServletRequest().getSession().getServletContext()).addGroupMember("ExamplePanel",pushRequestContext.getCurrentPushId());
+
+        // Push Javascript
+        // USE THE callbackURLButton IN THIS TEMPLATE TO OUTPUT YOUR CALLBACK URL.  THIS URL WILL BE THE ARGUMENT IN THE WICKETAJAXGET METHOD CALL BELOW.
+/*        Label pushJavascript = new Label("pushJavascript", new Model("window.onload = function(){ice.push.register(['" + pushRequestContext.getCurrentPushId() + "'],function(){wicketAjaxGet('?wicket:interface=:0:pushPanel::IActivePageBehaviorListener:0:-1&wicket:ignoreIfNotActive=true')});};"));
+        pushJavascript.setEscapeModelStrings(false);
+        add(pushJavascript);
+*/
+        // Push callback.  Called from wicketAjaxGet method.
+        final AbstractDefaultAjaxBehavior behave = new AbstractDefaultAjaxBehavior() {
+            protected void respond(final AjaxRequestTarget target) {
+
+                // Update Model
+
+                // Render the Panel
+                target.addComponent(this.getComponent());
+            }
+        };
+        add(behave);
+
+        // Form used to retrieve the callback url.  Not necessary once you have the url.
+        Form form = new Form("pushPanelForm");
+        // BUTTON OUTPUTS CALLBACK URL THAT SHOULD BE THE ARGUMENT OF YOUR WICKETAJAXGET CALL.
+        form.add(new AjaxButton("callbackURLButton") {
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                // Copy this output into wicketAjaxGet method
+                System.out.println("callbackURL: " + behave.getCallbackUrl());
+                // An Example push call
+                PushContext.getInstance(getWebRequest().getHttpServletRequest().getSession().getServletContext()).push("ExamplePanel");
+            }
+        });
+        add(form);
+    }
+
+}
---
+++ scratchpads/snowplow/integration/wicket/samples/pushpanel/src/main/java/org/icepush/integration/wicket/samples/pushpanel/RightPushPanel.java
@@ -0,0 +1,84 @@
+package org.icepush.integration.wicket.samples.pushpanel;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebResponse;
+import org.icepush.PushContext;
+
+/**
+ *
+ * @author bkroeger
+ */
+public final class RightPushPanel extends Panel {
+
+    WicketPushRequestContext pushRequestContext = new WicketPushRequestContext((WebRequest)getRequest(),(WebResponse)getResponse());
+
+    final ListView pushListView;
+    private List pushList = new ArrayList();
+
+    boolean isPushMine = false;
+
+    public RightPushPanel(String id) {
+        super (id);
+        this.setOutputMarkupId(true);
+
+        // PUSH CALLBACK
+        final AbstractDefaultAjaxBehavior behave = new AbstractDefaultAjaxBehavior() {
+            protected void respond(final AjaxRequestTarget target) {
+                if(isPushMine){
+                    pushList.add("My Push.");
+                }else{
+                    pushList.add("Pushed From Another Session.");
+                }
+                isPushMine=false;
+                pushListView.modelChanged();
+                target.addComponent(this.getComponent());
+            }
+        };
+        add(behave);
+
+        PushContext.getInstance(getWebRequest().getHttpServletRequest().getSession().getServletContext()).addGroupMember("RightPanel",pushRequestContext.getCurrentPushId());
+        Label pushJavascript = new Label("pushJavascript", new Model("window.onload = function(){ice.push.register(['" + pushRequestContext.getCurrentPushId() + "'],function(){wicketAjaxGet('?wicket:interface=:0:rightPushPanel::IActivePageBehaviorListener:0:-1&wicket:ignoreIfNotActive=true')});};"));
+        pushJavascript.setEscapeModelStrings(false);
+        add(pushJavascript);
+
+        Form rightForm = new Form("rightForm");
+        rightForm.setOutputMarkupId(true);
+
+        rightForm.add(new AjaxButton("rightButton") {
+                protected void onSubmit(AjaxRequestTarget target, Form form) {
+                // Obtain url to paste in to wicketAjaxGet callback method.  This url will call AbstractDefaultAjaxBehavior's respond method.
+                System.out.println("Right BEHAVE callbackURL: " + behave.getCallbackUrl());
+                PushContext.getInstance(getWebRequest().getHttpServletRequest().getSession().getServletContext()).push("RightPanel");
+                isPushMine=true;
+                target.addComponent(this.getParent());
+        }
+        });
+
+        rightForm.add(new AjaxButton("rightClear") {
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                pushList.clear();
+                target.addComponent(this.getParent());
+            }
+        });
+
+        rightForm.add(pushListView = new ListView("pushListView", pushList){
+            public void populateItem(final ListItem listItem){
+                listItem.add(new Label("pushSource",(String)listItem.getModelObject()));
+            }
+        });
+
+        add(rightForm);
+    }
+
+}
