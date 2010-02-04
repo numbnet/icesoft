5a48fc5ac1ee7f6c6c9c4f6aaa1afd3dddabe71b
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-04 00:14:32 +0000

    PUSH-6 Clean up wicket icechat code.

--- scratchpads/snowplow/samples/icechat/web/wicket/src/main/java/org/icepush/samples/icechat/wicket/WicketPushContextAdapter.java
+++ scratchpads/snowplow/samples/icechat/web/wicket/src/main/java/org/icepush/samples/icechat/wicket/WicketPushContextAdapter.java
@@ -1,12 +1,7 @@
 package org.icepush.samples.icechat.wicket;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
-
 import org.icepush.samples.icechat.AbstractPushRequestContext;
 
-//@Named(value="pushRequestContext")
-//@RequestScoped
 public class WicketPushContextAdapter extends AbstractPushRequestContext {
 	
 	public WicketPushContextAdapter(){
