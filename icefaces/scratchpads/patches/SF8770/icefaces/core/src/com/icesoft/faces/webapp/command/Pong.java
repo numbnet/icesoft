31bb6338031fb51e6a1ca729404e577bd39c49cd
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-05 17:44:55 +0000

    patch for session expired logging

---
+++ icefaces/scratchpads/patches/SF8770/icefaces/core/src/com/icesoft/faces/webapp/command/Pong.java
@@ -0,0 +1,48 @@
+package com.icesoft.faces.webapp.command;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class Pong implements Command {
+
+    public Command coalesceWith(Command command) {
+        return command.coalesceWith(this);
+    }
+
+    public Command coalesceWith(Macro macro) {
+        macro.addCommand(this);
+        return macro;
+    }
+
+    public Command coalesceWith(UpdateElements updateElements) {
+        return new Macro(updateElements, this);
+    }
+
+    public Command coalesceWith(Redirect redirect) {
+        return redirect;
+    }
+
+    public Command coalesceWith(Reload reload) {
+        return reload;
+    }
+
+    public Command coalesceWith(SessionExpired sessionExpired) {
+        return sessionExpired;
+    }
+
+    public Command coalesceWith(SetCookie setCookie) {
+        return new Macro(setCookie, this);
+    }
+
+    public Command coalesceWith(NOOP noop) {
+        return this;
+    }
+
+    public Command coalesceWith(Pong pong) {
+        return pong;
+    }
+
+    public void serializeTo(Writer writer) throws IOException {
+        writer.write("<pong/>");
+    }
+}
