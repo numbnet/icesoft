e99bb0ca5e0cd98ef5b4af821f5b1545637394f8
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-10 16:20:13 +0000

    PUSH-36 - sample causes divide by zero error - also fix 'unchecked' cast compiler warning

--- scratchpads/snowplow/integration/jsp/samples/region/src/main/java/org/icepush/jsp/samples/region/GroupNotificationCounter.java
+++ scratchpads/snowplow/integration/jsp/samples/region/src/main/java/org/icepush/jsp/samples/region/GroupNotificationCounter.java
@@ -24,44 +24,48 @@
 
 package org.icepush.jsp.samples.region;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 import org.icepush.notify.GroupNotifier;
 
-import java.util.Hashtable;
+public class GroupNotificationCounter extends GroupNotifier {
+	private Map<String,Long> counters;
+	private long interval = 3000; //default to 3 secs
 
-public class GroupNotificationCounter extends GroupNotifier{
-    private Hashtable counters;
-    private long interval;
-
-    public GroupNotificationCounter() {
-	super();
-	counters = new Hashtable();
-    }
-
-    public void setGroup(String grp) {
-	super.setGroup(grp);
-	if (!counters.containsKey(grp)) {
-	    counters.put(grp, new Long(System.currentTimeMillis()));
+	public GroupNotificationCounter() {
+		super();
+		counters = new Hashtable<String,Long>();
 	}
-    }
 
-    public long getInterval() {
-	return interval;
-    }
-    public void setInterval(long interval) {
-	this.interval = interval;
-    }
-
-    public long getCounter() {
-	return getCounter(getGroup());
-    }
-
-    public long getCounter(String group) {
-	Long start = (Long)counters.get(group);
-	if (start == null) {
-	    return 0;
-	} else {
-	    long now = System.currentTimeMillis();
-	    return (now - start.longValue())/interval;
+	public void setGroup(String grp) {
+		super.setGroup(grp);
+		if (!counters.containsKey(grp)) {
+			counters.put(grp, new Long(System.currentTimeMillis()));
+		}
 	}
-    }
+
+	public long getInterval() {
+		return interval;
+	}
+
+	public void setInterval(long interval) {
+		this.interval = interval;
+	}
+
+	public long getCounter() {
+		return getCounter(getGroup());
+	}
+
+	public long getCounter(String group) {
+		Long start = (Long) counters.get(group);
+		if (start == null) {
+			return 0;
+		} else {
+			long now = System.currentTimeMillis();
+			if( interval < 1 ) //ensure interval always > 0 
+				interval = 1000;
+			return (now - start.longValue()) / interval;
+		}
+	}
 }
