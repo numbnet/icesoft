0e5c7f730baa33294f99fbbbdb89b77519981a0c
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-11 22:17:41 +0000

    move wicket top-level

---
+++ scratchpads/snowplow/samples/icechat/wicket/src/main/java/org/icepush/samples/icechat/wicket/ChatPage.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.icepush.samples.icechat.wicket;
+import javax.inject.Inject;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.icepush.samples.icechat.cdi.facade.ChatManagerFacadeBean;
+import org.icepush.samples.icechat.cdi.model.CurrentChatSessionHolderBean;
+import org.icepush.samples.icechat.controller.ILoginController;
+
+/**
+ *
+ * @author bkroeger
+ */
+public final class ChatPage extends AppBasePage {
+
+    // Session Scoped Beans
+    @Inject
+    ILoginController loginController;
+    CompoundPropertyModel compoundLoginController;
+    
+    @Inject
+    ChatManagerFacadeBean chatManagerFacadeBean;
+    CompoundPropertyModel compoundChatManagerFacadeBean;
+    
+    @Inject
+    ChatManagerViewControllerSessionBean chatManagerVC;
+
+    CurrentChatSessionHolderBean currentChatSessionHolderBean = new CurrentChatSessionHolderBean();
+
+
+    public ChatPage() {
+        super ();
+        // Initialize Model
+        chatManagerVC.setChatService(chatService);
+        chatManagerVC.setLoginController(loginController);
+        chatManagerVC.setCurrentChatSessionHolder(currentChatSessionHolderBean);
+        chatManagerFacadeBean.setChatService(chatService);
+
+        compoundLoginController = new CompoundPropertyModel(loginController);
+        compoundChatManagerFacadeBean = new CompoundPropertyModel(chatManagerFacadeBean);
+
+        // Add Components to page
+        Form chatSession = new Form("chatSession",compoundLoginController);
+        chatSession.add(new Label("credentialsBean.userName"));
+        chatSession.add(new Label("credentialsBean.nickName"));
+	chatSession.add(new AjaxButton("logout") {
+			protected void onSubmit(AjaxRequestTarget target, Form form) {
+                            loginController.logout();
+                            setResponsePage(LoginPage.class);
+			}
+		});
+        add(chatSession);
+
+        add(new ChatRoomsPanel("chatRoomsPanel",compoundChatManagerFacadeBean));
+        
+        add(new ChatPanel("chatPanel"));
+
+    }
+
+}
+
