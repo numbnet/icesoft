1a6e9ce2a54119a0ae1b6467aa5f5bcfe825b23f
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-16 18:12:15 +0000

    PUSH-15 - Removed pure Spring implementation of ICEchat, as just the jsp region tag version is needed

--- scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/impl/BaseChatRoom.java
+++
@@ -1,9 +0,0 @@
-package org.icepush.samples.icechat.spring.impl;
-
-import org.icepush.samples.icechat.beans.model.BaseNewChatRoomBean;
-
-public class BaseChatRoom extends BaseNewChatRoomBean {
-    public BaseChatRoom() {
-        super();
-    }
-}
--- scratchpads/snowplow/samples/icechat/web/spring/src/main/java/org/icepush/samples/icechat/spring/impl/BaseChatService.java
+++
@@ -1,9 +0,0 @@
-package org.icepush.samples.icechat.spring.impl;
-
-import org.icepush.samples.icechat.beans.service.BaseChatServiceBean;
-
-public class BaseChatService extends BaseChatServiceBean {
-    public BaseChatService() {
-        super();
-    }
-}
