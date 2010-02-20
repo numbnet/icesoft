e3c36af9c21ad274600fa424a96b5e6c14869ec4
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-02-20 17:14:41 +0000

    ICE-4808 Throw RuntimeException when calling PushRenderer methods without a current FacesContext. Avoid notifying browser when PortableRenderer.render is invoked and FacesContext is present.

--- icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/application/PortableRenderer.java
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/application/PortableRenderer.java
@@ -2,5 +2,14 @@ package org.icefaces.application;
 
 public interface PortableRenderer {
 
+    /**
+     * Render the specified group of sessions by performing the JavaServer Faces
+     * execute and render lifecycle phases.  If a FacesContext is in the
+     * scope of the current thread scope, the current view will not be
+     * asynchronously rendered
+     * (it is already rendered as a result of the user event being
+     * processed).  For more fine-grained control
+     * use the RenderManager API.
+     */
     void render(String group);
 }
