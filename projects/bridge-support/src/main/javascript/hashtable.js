b30547ff29769e76e44d4d56c97a974ffb76c426
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-03-02 21:49:12 +0000

    ICE-5361 Use method lookup provided by OO framework.

--- projects/bridge-support/src/main/javascript/hashtable.js
+++ projects/bridge-support/src/main/javascript/hashtable.js
@@ -23,49 +23,48 @@ var Hashtable;
 
         var buckets = [];
 
-        //        return object(function(method) {
-        //            method(at, function at(self, k, notFoundThunk) {
-        this.at = function(k, notFoundThunk) {
-            var index = h(k);
-            var bucket = buckets[index];
-            if (bucket) {
-                for (var i = 0, l = bucket.length; i < l; i++) {
-                    var entry = bucket[i];
-                    if (eq(entry.key, k)) {
-                        return entry.value;
+        return object(function(method) {
+            method(at, function at(self, k, notFoundThunk) {
+                var index = h(k);
+                var bucket = buckets[index];
+                if (bucket) {
+                    for (var i = 0, l = bucket.length; i < l; i++) {
+                        var entry = bucket[i];
+                        if (eq(entry.key, k)) {
+                            return entry.value;
+                        }
                     }
+                    return null;
+                } else {
+                    return null;
                 }
+            });
 
-                return null;
-            } else {
-                return null;
-            }
-        };
-        //            });
-
-        //            method(putAt, function putAt(self, k, v) {
-        this.putAt = function(k, v) {
-            var index = h(k);
-            var bucket = buckets[index];
-            if (bucket) {
-                for (var i = 0, l = bucket.length; i < l; i++) {
-                    var entry = bucket[i];
-                    if (eq(entry.key, k)) {
-                        var oldValue = entry.value;
-                        entry.value = v;
-                        return oldValue;
+            method(putAt, function putAt(self, k, v) {
+                var index = h(k);
+                var bucket = buckets[index];
+                if (bucket) {
+                    for (var i = 0, l = bucket.length; i < l; i++) {
+                        var entry = bucket[i];
+                        if (eq(entry.key, k)) {
+                            var oldValue = entry.value;
+                            entry.value = v;
+                            return oldValue;
+                        }
                     }
+                    bucket.push({ key:k, value: v });
+                    return null;
+                } else {
+                    bucket = [
+                        {
+                            key:k,
+                            value: v
+                        }
+                    ];
+                    buckets[index] = bucket;
+                    return null;
                 }
-                bucket.push({key:k, value: v});
-                return null;
-            } else {
-                bucket = [];
-                buckets[index] = bucket;
-                bucket.push({key:k, value: v});
-                return null;
-            }
-        };
-        //            });
-        //        });
+            });
+        });
     };
 })();
