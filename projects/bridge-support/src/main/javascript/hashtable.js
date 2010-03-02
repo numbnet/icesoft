18bbbb0d8a0b423f68d25462565c74ab34336e2b
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-03-02 17:10:13 +0000

    ICE-5361 Start Hashtable implementation & test.

---
+++ projects/bridge-support/src/main/javascript/hashtable.js
@@ -0,0 +1,71 @@
+var at = operator();
+var putAt = operator();
+var Hashtable;
+
+(function() {
+    function jenkinsOneAtATimeHash(k) {
+        var key = String(k);
+        var hash = 0;
+        for (var i = 0, length = key.length; i < length; i++) {
+            hash += parseInt(key[i], 36);
+            hash += (hash << 10);
+            hash ^= (hash >> 6);
+        }
+        hash += (hash << 3);
+        hash ^= (hash >> 11);
+        hash += (hash << 15);
+        return hash;
+    }
+
+    Hashtable = function(hashFunction, equalFunction) {
+        var h = hashFunction || jenkinsOneAtATimeHash;
+        var eq = equalFunction || equal;
+
+        var buckets = [];
+
+        //        return object(function(method) {
+        //            method(at, function at(self, k, notFoundThunk) {
+        this.at = function(k, notFoundThunk) {
+            var index = h(k);
+            var bucket = buckets[index];
+            if (bucket) {
+                for (var i = 0, l = bucket.length; i < l; i++) {
+                    var entry = bucket[i];
+                    if (eq(entry.key, k)) {
+                        return entry.value;
+                    }
+                }
+
+                return null;
+            } else {
+                return null;
+            }
+        };
+        //            });
+
+        //            method(putAt, function putAt(self, k, v) {
+        this.putAt = function(k, v) {
+            var index = h(k);
+            var bucket = buckets[index];
+            if (bucket) {
+                for (var i = 0, l = bucket.length; i < l; i++) {
+                    var entry = bucket[i];
+                    if (eq(entry.key, k)) {
+                        var oldValue = entry.value;
+                        entry.value = v;
+                        return oldValue;
+                    }
+                }
+                bucket.push({key:k, value: v});
+                return null;
+            } else {
+                bucket = [];
+                buckets[index] = bucket;
+                bucket.push({key:k, value: v});
+                return null;
+            }
+        };
+        //            });
+        //        });
+    };
+})();
