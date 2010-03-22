15955317913a1e3319a084222cbbda31fcb53831
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-03-22 16:52:10 +0000

    Moving tag again.

--- icefaces/tags/icefaces-1.7.2-SP2/icefaces/component/src/com/icesoft/faces/component/paneltooltip/PanelTooltipRenderer.java
+++
@@ -1,7 +0,0 @@
-package com.icesoft.faces.component.paneltooltip;
-
-import com.icesoft.faces.component.panelpopup.PanelPopupRenderer;
-
-public class PanelTooltipRenderer extends PanelPopupRenderer{
-
-}
