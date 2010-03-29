684cd247e5f0039f1cddf84512f99374321fd2a1
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-03-29 20:43:40 +0000

    ICE-5524 Stop throwing exceptions when value lookup doesn't define the property name.

--- icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/application/WindowELResolver.java
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/application/WindowELResolver.java
@@ -2,7 +2,6 @@ package org.icefaces.application;
 
 import javax.el.ELContext;
 import javax.el.ELResolver;
-import javax.el.PropertyNotFoundException;
 import javax.faces.context.FacesContext;
 import java.beans.FeatureDescriptor;
 import java.util.Collections;
@@ -11,17 +10,14 @@ import java.util.Iterator;
 public class WindowELResolver extends ELResolver {
 
     public Object getValue(ELContext elContext, Object base, Object property) {
-        if (property == null) {
-            throw new PropertyNotFoundException();
-        }
-        if (base == null && WindowScopeManager.ScopeName.equals(property.toString())) {
+        if (base == null && WindowScopeManager.ScopeName.equals(property)) {
             WindowScopeManager.ScopeMap customScope = getScope(elContext);
             elContext.setPropertyResolved(true);
             return customScope;
         } else if (base != null && base instanceof WindowScopeManager.ScopeMap) {
-            return lookup(elContext, (WindowScopeManager.ScopeMap) base, property.toString());
+            return lookup(elContext, (WindowScopeManager.ScopeMap) base, property);
         } else if (base == null) {
-            return lookup(elContext, getScope(elContext), property.toString());
+            return lookup(elContext, getScope(elContext), property);
         }
         return null;
     }
@@ -55,10 +51,11 @@ public class WindowELResolver extends ELResolver {
     }
 
 
-    private Object lookup(ELContext elContext, WindowScopeManager.ScopeMap scope, String key) {
-        if (null == scope)  {
+    private Object lookup(ELContext elContext, WindowScopeManager.ScopeMap scope, Object key) {
+        if (null == scope) {
             return null;
         }
+
         Object value = scope.get(key);
         elContext.setPropertyResolved(value != null);
         return value;
