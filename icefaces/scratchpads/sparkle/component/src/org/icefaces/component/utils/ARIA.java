8e000caab162b6c764ef2afd4171c5ab569ce04c
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-03-30 22:06:04 +0000

    Utility added

---
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/utils/ARIA.java
@@ -0,0 +1,11 @@
+package org.icefaces.component.utils;
+
+public class ARIA {
+    public static final String TAB_ROLE = "tab";
+    public static final String TABLIST_ROLE = "tablist"; 
+    public static final String PRESENTATION_ROLE = "presentation";
+    public static final String TABPANEL_ROLE = "tabpanel";
+    public static final String LABELLEDBY_ATTR= "labelledby";  
+    public static final String ROLE_ATTR= "role";
+    
+}
