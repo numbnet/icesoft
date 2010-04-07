f01b8e53d71bc49c89b73946bdb65de1081de85c
philip.breau <philip.breau@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-07 18:03:34 +0000

    PUSH-67 add ability for JSP Region Tag to evaluate JS in fetched content

--- scratchpads/snowplow/core/src/main/javascript/application.js
+++ scratchpads/snowplow/core/src/main/javascript/application.js
@@ -107,6 +107,7 @@ if (!window.ice.icepush) {
         function calculateURI(uri) {
             return (namespace.push.configuration.uriPrefix || '') + uri + (namespace.push.configuration.uriSuffix || '');
         }
+        
 
         var currentNotifications = [];
         var apiChannel = Client(true);
@@ -196,6 +197,35 @@ if (!window.ice.icepush) {
                 }));
             },
 
+            searchAndEvaluateScripts: function(element) {
+            	var scripts = element.getElementsByTagName('script');
+            	for( s in scripts ){
+            		this.evaluateScript(scripts[s]);
+            	}
+            },
+
+            evaluateScript: function(script) {
+                var uri = script.src;
+                if (uri) {
+                    this.client.getSynchronously(uri, '', function(request) {
+                        try {
+                            if (window.execScript) window.execScript(request.content()); //ie
+                    		else top.eval(request.content()); //others
+                        } catch (e) {
+                        	warn(logger, 'Failed to evaluate script located at: ' + uri, e);
+                        }
+                    }.bind(this));
+                } else {
+                    var code = script.innerHTML;
+                    try {
+                    	if (window.execScript) window.execScript(code); //ie
+                		else top.eval(code); //others
+                    } catch (e) {
+                    	warn(logger, 'Failed to evaluate script: \n' + code, e);
+                    }
+                }
+            },
+            
             configuration: {
                 uriSuffix: '',
                 uriPrefix: ''
