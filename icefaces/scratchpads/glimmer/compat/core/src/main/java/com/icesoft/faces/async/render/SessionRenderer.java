cdaec709d7e17f5fbe77b7a7040b3e221800c4a5
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-12 21:50:36 +0000

    ICE-5609 Store a PortableRenderer in a static field to be later used in render calls that are not initiated on JSF threads.

--- icefaces/scratchpads/glimmer/compat/core/src/main/java/com/icesoft/faces/async/render/SessionRenderer.java
+++ icefaces/scratchpads/glimmer/compat/core/src/main/java/com/icesoft/faces/async/render/SessionRenderer.java
@@ -1,6 +1,31 @@
 package com.icesoft.faces.async.render;
 
+import org.icefaces.application.PortableRenderer;
 import org.icefaces.application.PushRenderer;
 
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+
 public class SessionRenderer extends PushRenderer {
+    private static PortableRenderer portableRenderer;
+
+    public static void render(String groupName) {
+        if (FacesContext.getCurrentInstance() == null) {
+            portableRenderer.render(groupName);
+        } else {
+            PushRenderer.render(groupName);
+        }
+    }
+
+    public static class StartupListener implements SystemEventListener {
+        public void processEvent(SystemEvent event) throws AbortProcessingException {
+            portableRenderer = PushRenderer.getPortableRenderer(FacesContext.getCurrentInstance());
+        }
+
+        public boolean isListenerForSource(Object source) {
+            return true;
+        }
+    }
 }
