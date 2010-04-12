051a48b6965de2686cadd4f0606d787de74749cc
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-12 17:24:45 +0000

    Comments added towards ICE-5616

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderRenderer.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderRenderer.java
@@ -13,24 +13,43 @@ import org.icefaces.component.utils.ARIA;
 import org.icefaces.component.utils.HTML;
 import org.icefaces.util.EnvUtils;
 
-
+/**
+ * The sliderRender renders following elements:
+ * 1. A div with a client id(e.g.)
+ *  <div id="xxx" />
+ *  which will be used by the YUI slider, as a slider holder
+ *  
+ * 2. A script node that makes a call to communicate with slider object on client
+ *   <script>
+ *        ice.yui.slider.updateProperties(clientId, yuiProps, jsfProps);
+ *   </script>
+ *   
+ *   In addition to the rendering the renderer performs decode as well. This component
+ *   doesn't use a hidden field for it value instead takes advantage of param support of JSF2
+ */
 public class SliderRenderer extends Renderer{
 
     public void decode(FacesContext facesContext, UIComponent uiComponent) {
         Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
+
+        //"ice.event.captured" should be holding the event source id
         if (requestParameterMap.containsKey("ice.event.captured")) {
             Slider slider = (Slider)uiComponent;
             String source = String.valueOf(requestParameterMap.get("ice.event.captured"));
             String clientId = uiComponent.getClientId();
+            //If I am a source of event?
             if (clientId.equals(source)) {
                 try {
                     int value = slider.getValue();
                     int submittedValue = Integer.parseInt((String.valueOf(requestParameterMap.get(clientId+"_value"))));
+                    //if there is a value change, queue a valueChangeEvent    
                     if (value != submittedValue) { 
                         uiComponent.queueEvent(new ValueChangeEvent (uiComponent, 
                                        new Integer(value), new Integer(submittedValue)));
                     }
-                } catch (Exception e) {}
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -40,6 +59,7 @@ public class SliderRenderer extends Renderer{
         String clientId = uiComponent.getClientId(facesContext);
         ResponseWriter writer = facesContext.getResponseWriter();        
         Slider slider = (Slider)uiComponent;
+        //YUI3 slider requires a div, where it renders slider component.
         writer.startElement(HTML.DIV_ELEM, uiComponent);
         writer.writeAttribute(HTML.ID_ATTR, clientId, HTML.ID_ATTR);      
         String styleClass = slider.getStyleClass();
@@ -48,12 +68,15 @@ public class SliderRenderer extends Renderer{
         }
         writer.endElement(HTML.DIV_ELEM);  
         
+        //make a call to YUI3 helper API to update 
         writer.startElement(HTML.SCRIPT_ELEM, uiComponent);
         writer.writeAttribute(HTML.ID_ATTR, clientId + "script", HTML.ID_ATTR); 
         StringBuilder call= new StringBuilder();
         call.append("ice.yui.slider.updateProperties('");
         call.append(clientId);
-        call.append("', {");
+        call.append("', ");
+        //pass through YUI slider properties 
+        call.append("{");
         call.append("min:");
         call.append(slider.getMin());
         call.append(", ");
@@ -68,7 +91,9 @@ public class SliderRenderer extends Renderer{
         call.append("', ");
         call.append("railSize:'");
         call.append(slider.getRailSize());
-        call.append("'},{");
+        call.append("'},");
+        //pass JSF component specific properties that would help in slider configuration 
+        call.append("{");
         if (slider.getThumbImage()!= null) {
             call.append("thumbImage:'");
             call.append(slider.getThumbImage());
@@ -88,8 +113,7 @@ public class SliderRenderer extends Renderer{
         call.append(", ");        
         call.append("tabindex:");
         call.append(slider.getTabindex());   
-        call.append("},{});");
-        System.out.println(call);        
+        call.append("});");
         writer.write(call.toString());
         writer.endElement(HTML.SCRIPT_ELEM);  
                 
