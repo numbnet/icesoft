f51e7cde4a0ed7a1d25b237a491f79b6687e3b2e
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-13 22:15:36 +0000

    comments added

--- icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.slider/slider.js
+++ icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.slider/slider.js
@@ -8,23 +8,31 @@ ice.yui.slider = {
         //set a callback to create slider component 
         ice.yui.use(function(Y){
                 obj = new Y.Slider({
-                    //following two properties has to be set initlization time
+                    //following two properties has to be set when initializing the componennt
                     axis: yuiProps.axis,
                     thumbImage: jsfProps.thumbImage 
                 }).render(root);
                 
                 var submitHandler = null;
 
-                //submit handler
+                //create a generic submit handler
                 submitHandler = function(event) {
+                    //get the value of singleSubmit property
                     var singleSubmit = jsfProps.singleSubmit;
+                    
+                    //get the current value of slider
                     var sliderValue = obj.get('value');
+                    
+                    //if aria is enabled, then set aria property so screen reader can pick it
                     if (jsfProps.aria) {
                         document.getElementById(clientId).firstChild.setAttribute("aria-valuenow", sliderValue);
                     } 
+                    
+                    //slider doesn't use a hidden field, so create a param
                     var params = function(parameter) {
                         parameter(clientId+'_value', sliderValue);
                     };
+                    
                     if (singleSubmit) {
                         ice.singleSubmit(event, root, params); 
                     } else {
@@ -32,22 +40,28 @@ ice.yui.slider = {
                     }                    
                 };
                                 
-                //name of the event that will invoke submit
+                //the value of submitOn property represents the event name that 
+                //is reponsible to invoke a submit. The valid values are slideStart,
+                //slideEnd and slideInterval.
                 var submitOn = jsfProps.submitOn;
 
+                //as "slideInterval" is not known by the YUI slider, and its 
+                //created by us, which internally uses "thumbDrag" event of slider.
                 if (submitOn == 'slideInterval') {
-                    //this event is not known by YUI3 slider, this represents "thumbDrag" event
-                    //now set the submitOn with "thumbDrag"
+
+                    //now set the submitOn with origional value "thumbDrag"
                     submitOn = 'thumbDrag';
 
+                    //get the slideInterval time 
                     var slideInterval = jsfProps.slideInterval;   
-                    //check range         
+                    
+                    //boundary test         
                     if (slideInterval < 100)
                         slideInterval = 100;
                     else if (slideInterval > 1000) {
                         slideInterval = 1000;
                     }    
-                    logger.info('sliderInterval '+ slideInterval);
+
                     //create a slider timeout handler
                     var sliderTimeoutHandler = null;
                     
@@ -67,22 +81,27 @@ ice.yui.slider = {
                         },  slideInterval);
                     }
                 } 
+                //register submit handler
                 obj.after(submitOn, submitHandler);
                 
                 //add aria support
                 if (jsfProps.aria) {
-                    //add roles and attributes to the YUI widget
+                    //add roles and attributes to the YUI slider widget
                     root = document.getElementById(clientId);
                     root.firstChild.setAttribute("role", "slider");
                     root.firstChild.setAttribute("aria-valuemin", yuiProps.min);
                     root.firstChild.setAttribute("aria-valuemax", yuiProps.max);
                     root.firstChild.setAttribute("aria-valuenow",yuiProps.value );
                     root.firstChild.setAttribute("tabindex",jsfProps.tabindex);
+
+                    //TODO shouldn't it be configurable?
                     var step = 5;
-                    //listen for keydown event, to react on left, right, home and end key 
+
+                    //listen for keydown event, to provide short-cut key support.
+                    //react on left, right, up, down, home and end key 
                     Y.on("keydown", function(event) {
                         //get the current value of the slider
-                        var valuebefor = valuenow = parseInt(root.firstChild.getAttribute("aria-valuenow"));
+                        var valuebefore = valuenow = parseInt(root.firstChild.getAttribute("aria-valuenow"));
                           
                         var isLeft = event.keyCode == 37;
                         var isUp = event.keyCode == 38;
@@ -92,21 +111,25 @@ ice.yui.slider = {
                         var isEnd = event.keyCode == 35;
 
                         if (isLeft || isDown) {
+                           //decrease slider value
                            if ((valuenow - step) >= yuiProps.min) {
                               valuenow -= step;
                            }
                         } else if (isRight || isUp) {
+                           //increase slider value
                            if (yuiProps.max >= valuenow + step) {
                               valuenow += step;
                            }
                         } else if (isHome) {
+                           //get the min value of slider 
                            valuenow = yuiProps.min;
                         } else if (isEnd) {
+                           //get the max value of slider 
                            valuenow = yuiProps.max;		
                         }
 
-                        //if value change?
-                        if (valuebefor != valuenow)  { 
+                        //if value changed?
+                        if (valuebefore != valuenow)  { 
                             //update slider value on client                       
                             obj.set('value', valuenow);
                             //notify server
@@ -116,16 +139,19 @@ ice.yui.slider = {
                         }
                     }, root);
                 }
+                //bind the initilized js component, so it can be reused for later calls
                 bindYUI(obj);
             });
    },
    
+   //delegate call to ice.yui.updateProperties(..)  with the reference of this lib
    updateProperties:function(clientId, yuiProps, jsfProps, events) {
-	     ice.yui.updateProperties(clientId, yuiProps, jsfProps, events, this);
+       ice.yui.updateProperties(clientId, yuiProps, jsfProps, events, this);
    },
-   
+ 
+   //delegate call to ice.yui.getInstance(..) with the reference of this lib 
    getInstance:function(clientId, callback) {
-        ice.yui.getInstance(clientId, callback, this );
+       ice.yui.getInstance(clientId, callback, this);
    }
 };
 
