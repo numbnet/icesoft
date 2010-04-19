3c866620d53814ad957032154705d20d881de947
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-19 21:07:20 +0000

    ICE-5166: Added tlddoc for the tag attributes.

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/selectinputdate/SelectInputDateMeta.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/selectinputdate/SelectInputDateMeta.java
@@ -7,22 +7,26 @@ import java.util.Date;
 
 @Component(componentClass = "org.icefaces.component.selectinputdate.SelectInputDate",
         tagName = "selectInputDate",
-        extendsClass ="javax.faces.component.UIInput",
-        componentFamily ="javax.faces.Input",
+        extendsClass = "javax.faces.component.UIInput",
+        componentFamily = "javax.faces.Input",
         componentType = "com.icesoft.faces.SelectInputDate",
-        rendererClass ="org.icefaces.component.selectinputdate.SelectInputDateRenderer",
-        rendererType ="com.icesoft.faces.Calendar",
+        rendererClass = "org.icefaces.component.selectinputdate.SelectInputDateRenderer",
+        rendererType = "com.icesoft.faces.Calendar",
         generatedClass = "org.icefaces.component.selectinputdate.SelectInputDateBase"
-        )
+)
 public class SelectInputDateMeta {
-
-    @Property
-    private Date selectedDate;
-    @Property(defaultValue="false")
+    @Property(defaultValue = "false", tlddoc = "Whether to render a text input field for a popup calendar.")
     private Boolean renderInputField;
-    @Property(defaultValue="false")
+
+    @Property(defaultValue = "false", tlddoc = "Whether to render the calendar inline or as a popup.")
     private Boolean renderAsPopup;
-    @Property(defaultValue="false")
+
+    @Property(defaultValue = "false", tlddoc = "Whether to use single or full submit.")
     private Boolean singleSubmit;
 
+    @Property(inherit = true, tlddoc = "ID of the component.")
+    private String id;
+
+    @Property(inherit = true, tlddoc = "Value of the component as a Date object.")
+    private Object value;
 }
