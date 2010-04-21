cff06606994a5b90fe52ad244ba8200b9b628a75
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-04-21 20:56:55 +0000

    Added a dynamic include for forcing a form element update on demand

--- icefaces/scratchpads/glimmer/samples/test/ICE-3182/src/main/java/org/icefaces/demo/basic/AjaxEcho.java
+++ icefaces/scratchpads/glimmer/samples/test/ICE-3182/src/main/java/org/icefaces/demo/basic/AjaxEcho.java
@@ -2,6 +2,9 @@ package org.icefaces.demo.basic;
 
 import javax.faces.bean.ViewScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ResponseWriter;
+import javax.faces.context.FacesContext;
+import java.util.Map;
 
 /**
  *
@@ -11,6 +14,10 @@ import javax.faces.bean.ManagedBean;
 public class AjaxEcho {
 
     private String str;
+
+    private static int count;
+
+    private String filepath;
 
     public String getStr() {
         return str;
@@ -22,5 +29,29 @@ public class AjaxEcho {
 
     public void reset() {
         this.str = "";
+    }
+
+     public void addChild() {
+
+        FacesContext fc = FacesContext.getCurrentInstance();
+         Map m = fc.getExternalContext().getRequestParameterMap();
+         m.put("javax.faces.partial.render", "@all");
+    }
+
+    public void toggleInclude() {
+
+        if (filepath == null) {
+            filepath = "snippet.xhtml";
+        } else {
+            filepath = null;
+        } 
     } 
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
 }
