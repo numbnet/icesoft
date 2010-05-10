42ee6859e46e37790363db2f37c37f5421d5276d
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-10 20:48:25 +0000

    Add session counter and fix mis-labeled tests

--- icefaces/scratchpads/glimmer/samples/test/bodyupdate/src/main/java/org/icefaces/demo/ajax/AjaxBean.java
+++ icefaces/scratchpads/glimmer/samples/test/bodyupdate/src/main/java/org/icefaces/demo/ajax/AjaxBean.java
@@ -2,33 +2,58 @@
 package org.icefaces.demo.ajax;
 
 import java.io.Serializable;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
+import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "ajaxBean")
 @ViewScoped
 public class AjaxBean implements Serializable {
 
-    int count = 0;
+    private final static String SESSION_COUNTER = "org.icefaces.test.sessionCounter";
+
+    int viewCount = 0;
 
     public void toggle(ActionEvent ae) {
-        count++;
+        viewCount++;
     }
 
-    public int getCount(){
-        return count;
+    public int getViewCount(){
+        return viewCount;
+    }
+
+    public int getSessionCount(){
+        Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        Object obj = sessionMap.get(SESSION_COUNTER);
+        if( obj == null ){
+            sessionMap.put(SESSION_COUNTER, new Integer(0));
+        }
+        return ((Integer)sessionMap.get(SESSION_COUNTER)).intValue();
+    }
+
+    private void incrementSessionCounter() {
+        Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        Object obj = sessionMap.get(SESSION_COUNTER);
+        if( obj == null ){
+            sessionMap.put(SESSION_COUNTER, new Integer(1));
+        } else {
+            sessionMap.put(SESSION_COUNTER,new Integer(((Integer)obj).intValue() + 1));
+        }
     }
 
     public String countAction(){
         System.out.println("AjaxBean.countAction");
-        count++;
+        viewCount++;
+        incrementSessionCounter();
         return "index";
     }
 
     public void countActionListener(ActionEvent event){
         System.out.println("AjaxBean.countActionListener");
-        count++;
+        viewCount++;
+        incrementSessionCounter();
     }
 
 }
