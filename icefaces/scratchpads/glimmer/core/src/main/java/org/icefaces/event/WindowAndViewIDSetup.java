164ca3933eba2e670b8e4412c181047538239f87
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-11 14:34:24 +0000

    ICE-5701 Create on the server side the hidden input elements that hold the window and view identifiers to make sure they are always present in the page.

---
+++ icefaces/scratchpads/glimmer/core/src/main/java/org/icefaces/event/WindowAndViewIDSetup.java
@@ -0,0 +1,83 @@
+/*
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * "The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2008 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"
+ * License), in which case the provisions of the LGPL License are
+ * applicable instead of those above. If you wish to allow use of your
+ * version of this file only under the terms of the LGPL License and not to
+ * allow others to use your version of this file under the MPL, indicate
+ * your decision by deleting the provisions above and replace them with
+ * the notice and other provisions required by the LGPL License. If you do
+ * not delete the provisions above, a recipient may use your version of
+ * this file under either the MPL or the LGPL License."
+ *
+ */
+
+package org.icefaces.event;
+
+import org.icefaces.application.WindowScopeManager;
+import org.icefaces.util.EnvUtils;
+
+import javax.faces.component.UIOutput;
+import javax.faces.component.html.HtmlForm;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PostAddToViewEvent;
+import javax.faces.event.SystemEvent;
+import javax.faces.event.SystemEventListener;
+import java.io.IOException;
+
+public class WindowAndViewIDSetup implements SystemEventListener {
+
+    public void processEvent(SystemEvent event) throws AbortProcessingException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (!EnvUtils.isICEfacesView(context)) {
+            return;
+        }
+
+        HtmlForm form = (HtmlForm) ((PostAddToViewEvent) event).getComponent();
+        UIOutput output = new UIOutput() {
+            public void encodeBegin(FacesContext context) throws IOException {
+                ResponseWriter writer = context.getResponseWriter();
+
+                writer.startElement("input", this);
+                writer.writeAttribute("type", "hidden", null);
+                writer.writeAttribute("name", "ice.window", null);
+                writer.writeAttribute("value", WindowScopeManager.lookup(context).lookupWindowScope().getId(), null);
+                writer.endElement("input");
+
+                writer.startElement("input", this);
+                writer.writeAttribute("type", "hidden", null);
+                writer.writeAttribute("name", "ice.view", null);
+                writer.writeAttribute("value", context.getExternalContext().getRequestMap().get(BridgeSetup.ViewState), null);
+                writer.endElement("input");
+            }
+        };
+        output.setTransient(true);
+        output.setId(form.getId() + "_windowviewid");
+        form.getChildren().add(0, output);
+    }
+
+    public boolean isListenerForSource(Object source) {
+        return source instanceof HtmlForm;
+    }
+}
