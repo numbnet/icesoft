dfba0818c1d4ac7622efe74dd9c6e85352c331a7
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-11 11:45:24 +0000

    JIRA PUSH-83 : ICEpush Java Client

--- scratchpads/snowplow/core/src/main/java/org/icepush/client/HttpClient.java
+++ scratchpads/snowplow/core/src/main/java/org/icepush/client/HttpClient.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,6 +18,7 @@ import org.icepush.util.ThreadFactory;
 public class HttpClient {
     private static final Logger LOGGER = Logger.getLogger(HttpClient.class.getName());
 
+    private final ReentrantLock lock = new ReentrantLock();
     private final ObjectPool<HttpConnection> connectionPool =
         new ObjectPool<HttpConnection>(2) {
             protected HttpConnection newObject() {
@@ -34,6 +36,8 @@ public class HttpClient {
         scheduledExecutorService = new ScheduledThreadPoolExecutor(2, _threadFactory);
     }
 
+    private boolean shutdown = false;
+
     public HttpClient() {
         this(new CookieHandler());
     }
@@ -44,72 +48,116 @@ public class HttpClient {
     }
 
     public void cancel(final HttpRequest request) {
-        synchronized (pendingRequestMap) {
-            synchronized (connectionMap) {
-                if (pendingRequestMap.containsKey(request)) {
-                    LOGGER.log(Level.INFO, "[Jack] Closing previous connection.");
-                    pendingRequestMap.remove(request).cancel(true);
-                    if (connectionMap.containsKey(request)) {
-                        HttpConnection _connection = connectionMap.remove(request);
-                        _connection.close();
-                        connectionPool.invalidateObject(_connection);
+        if (!shutdown) {
+            lock.lock();
+            try {
+                if (!shutdown) {
+                    if (pendingRequestMap.containsKey(request)) {
+                        LOGGER.log(Level.INFO, "[Jack] Closing previous connection.");
+                        pendingRequestMap.remove(request).cancel(true);
+                        if (connectionMap.containsKey(request)) {
+                            HttpConnection _connection = connectionMap.remove(request);
+                            _connection.close();
+                            connectionPool.invalidateObject(_connection);
+                        }
                     }
                 }
+            } finally {
+                lock.unlock();
             }
         }
+    }
+
+    public boolean isShutdown() {
+        return shutdown;
     }
 
     public void send(final HttpRequest request) {
-        synchronized (pendingRequestMap) {
-            synchronized (connectionMap) {
-                pendingRequestMap.put(
-                    request,
-                    scheduledExecutorService.schedule(
-                        new Runnable() {
-                            public void run() {
-                                HttpConnection _connection;
-                                synchronized (connectionMap) {
-                                    _connection = connectionPool.borrowObject();
-                                    connectionMap.put(request, _connection);
-                                }
-                                try {
-                                    request.onResponse((HttpResponse)_connection.send(request));
-                                } catch (MalformedURLException exception) {
-                                    if (LOGGER.isLoggable(Level.WARNING)) {
-                                        LOGGER.log(
-                                            Level.WARNING,
-                                            "",
-                                            exception);
-                                    }
-                                } catch (SocketException exception) {
-                                    // Do nothing.
-                                    // This is probably happening due to closing the connection.
-                                } catch (IOException exception) {
-                                    if (LOGGER.isLoggable(Level.WARNING)) {
-                                        LOGGER.log(
-                                            Level.WARNING,
-                                            "",
-                                            exception);
-                                    }
-                                } finally {
+        if (!shutdown) {
+            lock.lock();
+            try {
+                if (!shutdown) {
+                    pendingRequestMap.put(
+                        request,
+                        scheduledExecutorService.schedule(
+                            new Runnable() {
+                                public void run() {
+                                    HttpConnection _connection;
                                     synchronized (connectionMap) {
-                                        connectionMap.remove(request);
-                                        connectionPool.returnObject(_connection);
+                                        _connection = connectionPool.borrowObject();
+                                        connectionMap.put(request, _connection);
+                                    }
+                                    try {
+                                        // throws MalformedURLException, IOException
+                                        _connection.send(request);
+                                        request.onResponse(request.getResponse());
+                                    } catch (MalformedURLException exception) {
+                                        if (LOGGER.isLoggable(Level.WARNING)) {
+                                            LOGGER.log(
+                                                Level.WARNING,
+                                                "",
+                                                exception);
+                                        }
+                                    } catch (SocketException exception) {
+                                        // Do nothing.
+                                        // This is probably happening due to closing the connection.
+                                    } catch (IOException exception) {
+                                        if (LOGGER.isLoggable(Level.WARNING)) {
+                                            LOGGER.log(
+                                                Level.WARNING,
+                                                "",
+                                                exception);
+                                        }
+                                    } finally {
+                                        synchronized (connectionMap) {
+                                            connectionMap.remove(request);
+                                            connectionPool.returnObject(_connection);
+                                        }
                                     }
                                 }
-                            }
-                        },
-                        0,
-                        TimeUnit.MILLISECONDS));
+                            },
+                            0,
+                            TimeUnit.MILLISECONDS));
+                }
+            } finally {
+                lock.unlock();
             }
         }
     }
 
-    public Object sendNow(final HttpRequest request)
+    public void sendNow(final HttpRequest request)
     throws IOException, MalformedURLException {
-        HttpConnection _connection = connectionPool.borrowObject();
-        Object _response = _connection.send(request);
-        connectionPool.returnObject(_connection);
-        return _response;
+        if (!shutdown) {
+            lock.lock();
+            try {
+                if (!shutdown) {
+                    HttpConnection _connection = connectionPool.borrowObject();
+                    // throws MalformedURLException, IOException
+                    _connection.send(request);
+                    connectionPool.returnObject(_connection);
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void shutdown() {
+        if (!shutdown) {
+            lock.lock();
+            try {
+                if (!shutdown) {
+                    for (HttpRequest request : pendingRequestMap.keySet()) {
+                        cancel(request);
+                    }
+                    cookieHandler.clear();
+                    scheduledExecutorService.shutdownNow();
+                    connectionPool.shutdown();
+                    shutdown = true;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
