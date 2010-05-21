6e2d5082bd58dafe91a50426e1940ea7749821fa
mark.collette <mark.collette@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-21 02:09:07 +0000

    ICE-5032 : compat version of component-showcase requires new fileupload solution

---
+++ icefaces/scratchpads/sparkle/showcase/test/src/org/icefaces/inputFiles/FileBean.java
@@ -0,0 +1,29 @@
+package org.icefaces.inputFiles;
+
+import javax.faces.event.ActionEvent;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="fileBean")
+@SessionScoped
+public class FileBean {
+    private String regularText = "";
+    public String getRegularText() { return regularText; }
+    public void setRegularText(String t) { regularText = t; }
+    
+    public String getRegularTextUppercase() { return regularText.toUpperCase(); }
+    
+    public void regular(ActionEvent e) {
+        System.out.println("regular()  e: " + e);
+    }
+    
+    private String mixedText = "";
+    public String getMixedText() { return mixedText; }
+    public void setMixedText(String t) { mixedText = t; }
+    
+    public String getMixedTextUppercase() { return mixedText.toUpperCase(); }
+    
+    public void mixed(ActionEvent e) {
+        System.out.println("mixed()  e: " + e);
+    }
+}
