65b5e15b02c20bdeee16261fab12bac5069a5536
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-26 21:17:45 +0000

    ICE-5753 original check in for pushbutton component

---
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonMeta.java
@@ -0,0 +1,62 @@
+  package org.icefaces.component.pushbutton;
+
+import javax.faces.component.UIComponent;
+import javax.el.MethodExpression;
+import javax.faces.event.ActionListener;
+import org.icefaces.component.annotation.Component;
+import org.icefaces.component.annotation.Property;
+import java.util.List;
+
+@Component(
+        tagName ="pushbutton",
+        componentClass ="org.icefaces.component.pushbutton.PushButton",
+        rendererClass ="org.icefaces.component.pushbutton.PushButtonRenderer", 
+        componentType = "org.icefaces.PushButton", 
+        rendererType = "org.icefaces.PushButtonRenderer",            
+        extendsClass = "javax.faces.component.UICommand", 
+        generatedClass = "org.icefaces.component.pushbutton.PushButtonBase",
+		componentFamily="com.icesoft.faces.PushButton"
+        )
+        
+public class PushButtonMeta {
+    
+    @Property   
+    private String label;
+
+    @Property (inherit=true, useTemplate=true)
+    private String id;
+	
+	@Property
+    private String image;
+	
+	@Property(defaultValue="false",
+			tlddoc="Default is false, means uses full submit")
+    private Boolean singleSubmit;
+	
+    @Property (inherit=true, useTemplate=true)
+    private UIComponent binding;  
+    
+    @Property (defaultValue="false")
+    private Boolean disabled;
+    
+    @Property (inherit=true, defaultValue="true")
+    private Boolean rendered;
+    
+    @Property (defaultValue="0", tlddoc="tabindex of the component")
+    private Integer tabindex;  
+  
+    @Property(tlddoc="style class of the component, the renderer doesn't render any default class.")
+    private String styleClass;  
+
+    @Property(tlddoc="style of the component")
+    private String style;
+    
+    @Property(inherit=true, isMethodExpression=true)
+    private MethodExpression actionListener;
+ 
+    @Property(isMethodExpression=true, inherit=true, useTemplate=true	)
+    private MethodExpression action;
+    
+    @Property(defaultValue="false",inherit=true, useTemplate=true)
+    private Boolean immediate;
+}
---
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonRenderer.java
@@ -0,0 +1,116 @@
+package org.icefaces.component.pushbutton;
+
+import java.io.IOException;
+import java.util.*;
+import javax.faces.event.ActionEvent;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.Renderer;
+
+
+import org.icefaces.component.utils.HTML;
+import org.icefaces.util.EnvUtils;
+
+
+public class PushButtonRenderer extends Renderer {
+
+    public void decode(FacesContext facesContext, UIComponent uiComponent) {
+        Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
+        if (requestParameterMap.containsKey("ice.event.captured")) {
+//            PushButton pushButton = (PushButton) uiComponent;
+//            String source = String.valueOf(requestParameterMap.get("ice.event.captured"));
+//            String clientId = pushButton.getClientId();
+            uiComponent.queueEvent(new ActionEvent(uiComponent));
+//           if (clientId.equals(source)) {
+//        	   System.out.println("button pressed is "+source+" for clientId="+clientId);
+////               try {
+//            	   //do I need to check to see if it is disabled first?
+//            	   //ActionSource2 has a list of ActionListeners available
+//            	   //will that change this piece of code?
+//            	   if (!pushButton.isdisabled()){
+//                       uiComponent.queueEvent(new FacesEvent (uiComponent));
+//            	   }
+//               } catch (Exception e) {}
+//           }
+        }
+    }
+
+    public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
+    throws IOException {
+    	
+        ResponseWriter writer = facesContext.getResponseWriter();
+        String clientId = uiComponent.getClientId(facesContext);
+        PushButton pushButton = (PushButton) uiComponent;
+		System.out.println("in renderer and label="+pushButton.getLabel());
+		// root element
+        //writer.startElement(HTML.INPUT_ELEM, uiComponent);
+		writer.startElement(HTML.SPAN_ELEM, uiComponent);
+        writer.writeAttribute(HTML.ID_ATTR, clientId, null);
+		writer.writeAttribute(HTML.CLASS_ATTR, "yui-button yui-min yui-push-button", null);
+		
+		// first child
+		writer.startElement(HTML.SPAN_ELEM, uiComponent);
+		writer.writeAttribute(HTML.CLASS_ATTR, "first-child", null);
+		
+		// button element
+		writer.startElement(HTML.BUTTON_ELEM, uiComponent);
+		writer.writeAttribute(HTML.TYPE_ATTR, "button", null);
+		writer.writeAttribute(HTML.NAME_ATTR, clientId, null);
+		
+		// if there's an image, render label manually, don't rely on YUI, since it'd override button's contents
+		if (pushButton.getImage() == null) {
+			writer.startElement(HTML.SPAN_ELEM, uiComponent);
+			writer.write(pushButton.getLabel());
+			writer.endElement(HTML.SPAN_ELEM);
+		}else {	
+			writer.startElement(HTML.IMG_ELEM, uiComponent);
+			writer.writeAttribute(HTML.SRC_ATTR, pushButton.getImage(), null);
+			writer.endElement(HTML.IMG_ELEM);
+		}
+    }
+    
+    public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
+    throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+		String clientId = uiComponent.getClientId(facesContext);
+		PushButton pushButton = (PushButton) uiComponent;
+        writer.endElement(HTML.BUTTON_ELEM);
+		writer.endElement(HTML.SPAN_ELEM);
+		writer.endElement(HTML.SPAN_ELEM);
+		
+		// js call
+        StringBuilder call= new StringBuilder();
+        call.append("ice.component.pushbutton.updateProperties('");
+        call.append(clientId);
+        call.append("', ");
+        //pass through YUI  properties 
+        call.append("{");
+        call.append("type:'button'");
+        if (pushButton.getImage() == null) {
+            call.append(", label:'");  
+            call.append(pushButton.getLabel());
+            call.append("'");
+        }
+
+        call.append("},");
+        //pass JSF component specific properties that would help in slider configuration 
+        call.append("{");
+        call.append("singleSubmit:");
+        call.append(pushButton.isSingleSubmit());       
+   
+//        call.append(", ");        
+//        call.append("aria:");
+//        call.append(EnvUtils.isAriaEnabled(facesContext)); 
+        call.append(", ");        
+        call.append("tabindex:");
+        call.append(pushButton.getTabindex());   
+        call.append("});");
+ 
+        writer.startElement(HTML.SCRIPT_ELEM, uiComponent);
+        writer.writeAttribute(HTML.ID_ATTR, clientId + "script", HTML.ID_ATTR);             
+        writer.write(call.toString());
+        writer.endElement(HTML.SCRIPT_ELEM);
+    }
+}
