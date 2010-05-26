57126c57c2e7960d10d2f31bfec8edafaa570fbb
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-05-26 22:36:50 +0000

    removing incorrect branches

--- icefaces/scratchpads/patches/SF-8988/icefaces/core/src/com/icesoft/faces/webapp/http/common/ConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package com.icesoft.faces.webapp.http.common;
-
-public class ConfigurationException extends Exception {
-    public ConfigurationException(String message) {
-        super(message);
-    }
-}
