5573f213e3fd7563c1af2e04b918c0933a332501
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-02 20:33:51 +0000

    ICEpush Place - initial check in of jsp application

---
+++ scratchpads/snowplow/integration/jsp/samples/icepushplace/src/main/java/org/icepush/place/jsp/services/IcepushPlaceService.java
@@ -0,0 +1,13 @@
+package org.icepush.place.jsp.services;
+
+import org.icepush.place.jsp.view.model.Person;
+
+public interface IcepushPlaceService{
+
+    public void register();
+
+    public void login(Person person);
+
+    public void requestUpdate();
+
+}
