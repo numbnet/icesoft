7f53cb0989675f4458de9a1e089ef9c0043e0554
mark.collette <mark.collette@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-04 18:29:06 +0000

    ICE-5794 : Further document slider component

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderMeta.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderMeta.java
@@ -5,20 +5,42 @@ import javax.el.MethodExpression;
 import org.icefaces.component.annotation.Component;
 import org.icefaces.component.annotation.Property;
 
+// Each Meta class requires a @Component annotation
 @Component(
+        // The tag name, as it will be used in view definitions (.xhtml files)
         tagName ="slider",
+        // The end class that will be used by applications. Hand-coded.
+        // The componentClass extends generatedClass, which extends extendsClass.
         componentClass ="org.icefaces.component.slider.Slider",
+        // The renderer, which outputs the html markup and javascript. Hand-coded. 
         rendererClass ="org.icefaces.component.slider.SliderRenderer",
+        // Generated, to contain all of the properties, getters, setters, and
+        //  state saving methods. 
+        generatedClass = "org.icefaces.component.slider.SliderBase",
+        // The super-class of the component. Did not extend UIInput, because
+        //  none of the conversion nor validation facilities it provides are
+        //  relevant to a slider.
+        extendsClass = "javax.faces.component.UIComponentBase",
         componentType = "org.icesoft.faces.Slider",
         rendererType = "org.icesoft.faces.SliderRenderer",
-        extendsClass = "javax.faces.component.UIComponentBase",
-        generatedClass = "org.icefaces.component.slider.SliderBase",
         componentFamily="com.icesoft.faces.Slider"    
     )
 public class SliderMeta {
+    // Every java field in the Meta class can be annotated to become either a
+    //  Property, Field or Facet, in the generatedClass.
+    // @Property annotations are for properties that should have a StateHelper,
+    //  getter method, setter method, and state saving code generated for it.
+    //  It should be a public property for the component tag.
+    // @Field annotations are for fields that are internal to the component,
+    //  to maintain state through-out a lifecycle, or even between lifecycles,
+    //  if it is not transient, and so will participate in state saving.
+    // @Facet annotations, on fields, provide a means of describing and
+    //  documenting component facets, in a standard way. @Facets annotations,
+    //  on inner classes, provides a means of using a facet specific namespace
+    //  that will not collide with samely named properties or fields.
     
     @Property (defaultValue="x", 
-            tlddoc="It could be either x or y. default value is set to x. Pass trhough to YUI component")
+            tlddoc="It could be either x, for horizontal or y, for vertical. Default value is set to x. Pass through to YUI component")
     private String axis;
     
     @Property (defaultValue="0", 
@@ -39,13 +61,13 @@ public class SliderMeta {
     
     @Property (defaultValue="slideEnd",
             tlddoc="The 3 valid values are slideStart, slideEnd and slideInterval. " +
-            		"default value is slideEnd",
-            javadocGet="returns value of submitOn. Its not a pass through because sliderInterval is a custom event " +
+            		"Default value is slideEnd",
+            javadocGet="returns value of submitOn. It is not a pass through because slideInterval is a custom event " +
             		"which internally uses \"thumbMove\"")
     private String submitOn;   
     
     @Property (defaultValue="500",
-            tlddoc="This attribute has an effect only when \"submitOn\" is set to \"sliderInterval\". default value is 500 (mili seconds)")
+            tlddoc="This attribute has an effect only when \"submitOn\" is set to \"slideInterval\". Default value is 500 (milliseconds)")
     private Integer slideInterval; 
     
     @Property(defaultValue="false",
@@ -70,7 +92,9 @@ public class SliderMeta {
     @Property(defaultValue="true")
     private Boolean clickableRail;
     
-    
+    // A MethodExpression Property is a special type, that does not generate
+    //  the same code, as it does not use a ValueExpression, but instead
+    //  describes a method to be called, and the parameter to pass to it.
     @Property(isMethodExpression=true, methodExpressionArgument="javax.faces.event.ValueChangeEvent")
     private MethodExpression valueChangeListener;
 }
--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderRenderer.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/slider/SliderRenderer.java
@@ -28,15 +28,22 @@ import org.icefaces.util.EnvUtils;
  *   doesn't use a hidden field for it value instead takes advantage of param support of JSF2
  */
 public class SliderRenderer extends Renderer{
-
+    // The decode method, in the renderer, is responsible for taking the values
+    //  that have been submitted from the browser, and seeing if they correspond
+    //  to this particular component, and also for the correct row(s) of any
+    //  UIData container(s). So if there are 5 rows, and the user used the 3rd
+    //  row's slider, this slider will decode 5 times, but should only be affected
+    //  the 3rd time. Once it find that there is data relevant to it, it then
+    //  extracts that data, and passes it to the component.
     public void decode(FacesContext facesContext, UIComponent uiComponent) {
+        // The RequestParameterMap holds the values received from the browser
         Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
 
         //"ice.event.captured" should be holding the event source id
         if (requestParameterMap.containsKey("ice.event.captured")) {
             Slider slider = (Slider)uiComponent;
             String source = String.valueOf(requestParameterMap.get("ice.event.captured"));
-            String clientId = uiComponent.getClientId();
+            String clientId = uiComponent.getClientId(facesContext);
             //If I am a source of event?
             if (clientId.equals(source)) {
                 try {
@@ -54,6 +61,11 @@ public class SliderRenderer extends Renderer{
         }
     }
     
+    // The encodeBegin method, in the renderer, is responsible for rendering
+    //  the html markup, as well as the javacript necessary for initialising
+    //  the YUI javascript object, in the browser. Typically the encodeEnd(-)
+    //  method and possibly the encodeChildren(-) method would be used too,
+    //  but we've put all the rendering here, in this one method.
     public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
     throws IOException {
         String clientId = uiComponent.getClientId(facesContext);
@@ -61,17 +73,22 @@ public class SliderRenderer extends Renderer{
         Slider slider = (Slider)uiComponent;
         //YUI3 slider requires a div, where it renders slider component.
         writer.startElement(HTML.DIV_ELEM, uiComponent);
-        writer.writeAttribute(HTML.ID_ATTR, clientId, HTML.ID_ATTR);      
+        // The Java UIComponent clientId corresponds with the Javascript DOM id
+        writer.writeAttribute(HTML.ID_ATTR, clientId, HTML.ID_ATTR);
+        // If the application has specified a string of CSS class name(s), output it
         String styleClass = slider.getStyleClass();
-        if (styleClass != null) {
+        if (styleClass != null && styleClass.trim().length() > 0) {
             writer.writeAttribute(HTML.CLASS_ATTR, styleClass, HTML.CLASS_ATTR);
         }
+        // If the application has specified a string of CSS style(s), output it 
         String style = slider.getStyle();
-        if (style != null) {
+        if (style != null && style.trim().length() > 0) {
             writer.writeAttribute(HTML.STYLE_ATTR, style, HTML.STYLE_ATTR);
         }
         writer.endElement(HTML.DIV_ELEM);  
         
+        // After rendering the div tag, render a script tag, containing the
+        // javascript necessary to initialise the YUI slider object.
         //make a call to YUI3 helper API to update 
         writer.startElement(HTML.SCRIPT_ELEM, uiComponent);
         writer.writeAttribute(HTML.ID_ATTR, clientId + "script", HTML.ID_ATTR); 
@@ -101,9 +118,10 @@ public class SliderRenderer extends Renderer{
         call.append("'},");
         //pass JSF component specific properties that would help in slider configuration 
         call.append("{");
-        if (slider.getThumbUrl()!= null) {
+        String thumbUrl = slider.getThumbUrl(); 
+        if (thumbUrl != null && thumbUrl.trim().length() > 0) {
             call.append("thumbUrl:'");
-            call.append(slider.getThumbUrl());
+            call.append(thumbUrl);
             call.append("', ");
         }
         call.append("submitOn:'");
