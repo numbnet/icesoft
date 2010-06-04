ae57102bc72536f13d0218d693e7d336448f791a
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-04 21:18:18 +0000

    ICE-5798

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/checkbox/Checkbox.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/checkbox/Checkbox.java
@@ -1,58 +1,45 @@
 package org.icefaces.component.checkbox;
 
+
+
 import javax.faces.context.FacesContext;
+
 import javax.faces.context.ResponseWriter;
+
 import javax.faces.application.ResourceDependencies;
+
 import javax.faces.application.ResourceDependency;
+
 import javax.faces.component.UIOutput;
+
+
 
 import java.io.IOException;
 
+
+
 @ResourceDependencies({
-	@ResourceDependency(name="util.js",library="org.icefaces.component.util"),
-    @ResourceDependency(name="component.js",library="org.icefaces.component.util"),	
-    @ResourceDependency(name="checkbox.js",library="org.icefaces.component.checkbox")    
+
+	    @ResourceDependency(name = "out/sam/button/button.css", library = "org.icefaces.component.sprites"),
+	    @ResourceDependency(library = "yui/2_8_1", name = "yahoo-dom-event/yahoo-dom-event.js"),
+	    @ResourceDependency(library = "yui/2_8_1", name = "element/element-min.js"),
+	    @ResourceDependency(library = "yui/2_8_1", name = "button/button-min.js"),
+		@ResourceDependency(name="util.js",library="org.icefaces.component.util"),
+	    @ResourceDependency(name="component.js",library="org.icefaces.component.util"),	
+        @ResourceDependency(name="checkbox.js",library="org.icefaces.component.checkbox")    
+
 })
+
 public class Checkbox extends CheckboxBase {
 
-    public Checkbox() {
-        loadDependency(FacesContext.getCurrentInstance());     
-    }
+	// return false if current value is null
 
-	// load required yui libraries
-    private void loadDependency(FacesContext context) {
-        context.getViewRoot().addComponentResource(context, new UIOutput() {
-            public void encodeBegin(FacesContext context) throws IOException {
-                ResponseWriter writer = context.getResponseWriter();
-				writeCssExternFile(writer, "http://yui.yahooapis.com/2.7.0/build/button/assets/skins/sam/button.css");
-                writeJavascriptExternFile(writer, "http://yui.yahooapis.com/2.7.0/build/yahoo-dom-event/yahoo-dom-event.js");
-                writeJavascriptExternFile(writer, "http://yui.yahooapis.com/2.7.0/build/element/element-min.js");
-                writeJavascriptExternFile(writer, "http://yui.yahooapis.com/2.7.0/build/button/button-min.js");       
-            }
-        }, "head");        
-    }
-	
-    private void writeJavascriptExternFile(ResponseWriter writer, String url) throws IOException {
-        writer.startElement("script", this);
-        writer.writeAttribute("type", "text/javascript", null);
-        writer.writeAttribute("src", url, null);
-        writer.endElement("script");
-    }
-	
-    private void writeCssExternFile(ResponseWriter writer, String url) throws IOException {
-        writer.startElement("link", this);
-        writer.writeAttribute("rel", "stylesheet", null);
-        writer.writeAttribute("type", "text/css", null);        
-        writer.writeAttribute("href", url, null);
-        writer.endElement("link");
-    }  
-	
-	// return false if current value is null?
     public Object getValue() {
-	
+	    System.out.println("Checkbox component value="+super.getValue());	
 		Object value = super.getValue();
 		if (value == null) return Boolean.valueOf(false);
-		
 		return value;
 	}
+
 }
+
