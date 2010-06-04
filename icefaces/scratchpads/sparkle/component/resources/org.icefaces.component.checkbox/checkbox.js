718a767e5e44347848bb74f4809628017e15fc26
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-04 21:15:46 +0000

    ICE-5798

--- icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.checkbox/checkbox.js
+++ icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.checkbox/checkbox.js
@@ -1,36 +1,53 @@
 ice.component.checkbox = {
     initialize:function(clientId, jsProps, jsfProps, bindYUI) {
-		var Dom = YAHOO.util.Dom;
-		var button = new YAHOO.widget.Button(clientId, {type: jsProps.type});
-
-		if(jsProps.label) {
+	   var Dom = YAHOO.util.Dom;
+      
+       if (YAHOO.widget.Logger){
+	 	  YAHOO.widget.Logger.enableBrowserConsole();
+       }
+       
+	   var button = new YAHOO.widget.Button(clientId, {type: jsProps.type});
+	   
+	   if(jsProps.label) {
 			button.set('label', jsProps.label);
+	    }
+		
+		if (jsfProps.value) {
+			YAHOO.log("jsfProps.value="+jsfProps.value);
 		}
-
-		if(jsProps.checked) {
-			button.set('checked', jsProps.checked);
-		}
+		var singleSubmit=jsfProps.singleSubmit;	
+		YAHOO.log ("jsProps.checked=-"+jsProps.checked);	
+  		if(jsProps.checked) {	
+   			button.set('checked', jsProps.checked);
+ 		}
 	   
 		var onCheckedChange = function (e) { 
-			e.target = document.getElementById(clientId);
+			e.target = document.getElementById(clientId);	
+			//get the current value of checked
+ 	        var submittedValue =  e.newValue;	
+
+//YAHOO.log("in onCheckedChange and e.newValue="+e.newValue+" button checked="+checkedValue);
 
 			var params = function(parameter) {
-				parameter(clientId+'_value', e.newValue);
+				parameter(clientId+'_value', submittedValue);
 			};
-
-			ice.submit(e, e.target, params);
-			logger.info('checkbox event submitted');
+	        if (singleSubmit) {
+//	         	YAHOO.log("not single submit");
+                ice.se(e, e.target, params); 
+            } else {
+//            	YAHOO.log("single Submit is false");
+                ice.s(e, e.target, params);                    
+            }           
 		};
 
-		button.addListener("checkedChange", onCheckedChange);
-
-		logger.info('checkbox initialized');
+		button.on("checkedChange", onCheckedChange);
 
 		bindYUI(button);
 	},
 	
    //delegate call to ice.yui.updateProperties(..)  with the reference of this lib
    updateProperties:function(clientId, jsProps, jsfProps, events) {
+		YAHOO.log("updateProperties for clientId="+clientId+" checked="+jsProps.checked);
        ice.component.updateProperties(clientId, jsProps, jsfProps, events, this);
    },
  
