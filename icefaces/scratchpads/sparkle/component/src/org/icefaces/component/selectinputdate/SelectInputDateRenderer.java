c9dfcd5c3a3729ce43116199e189a47f015472d9
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-14 16:36:03 +0000

    ICE-5166: Changed namespace to ice.component.calendar.

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/selectinputdate/SelectInputDateRenderer.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/selectinputdate/SelectInputDateRenderer.java
@@ -124,7 +124,7 @@ public class SelectInputDateRenderer extends Renderer {
         writer.startElement(HTML.SCRIPT_ELEM, component);
         writer.writeAttribute(HTML.SCRIPT_TYPE_ATTR, HTML.SCRIPT_TYPE_TEXT_JAVASCRIPT, null);
 //        writer.write("YAHOO.icefaces.calendar.init(" + params + ");");
-        writer.write("YAHOO.icefaces.calendar.updateProperties(" + params + ");");
+        writer.write("ice.component.calendar.updateProperties(" + params + ");");
         writer.endElement(HTML.SCRIPT_ELEM);
         writer.endElement(HTML.SPAN_ELEM);
     }
