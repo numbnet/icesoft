98231c962ece76af904908451300159d8fd05020
brad.kroeger <brad.kroeger@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-06-17 22:11:35 +0000

    ICEpush Place Grails - Commit so Ted can take a look, having problem with icep:region tag:
    Caused by: java.lang.NullPointerException: Cannot get property 'northAmerica' on null object
            at C__ossrepo_scratchpads_snowplow_integration_grails_samples_icepushplace_grails_grails_app_views_world_northAmerica_gsp.run(C__ossrepo_scratchpads_snowplow_integration_grails_samples_icepushplace_grails_grails_app_views_world_northAmerica_gsp:18)

---
+++ scratchpads/snowplow/integration/grails/samples/icepushplace-grails/src/java/org/icepush/place/grails/view/model/Person.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.icepush.place.grails.view.model;
+
+/**
+ *
+ * @author bkroeger
+ */
+public class Person {
+    
+    private String nickname = "";
+    private String mood = "";
+    private String comment = "";
+    private String region = "";
+    private String technology = "";
+    private String messageIn = "";
+    private String messageOut = "";
+
+    public Person() {
+
+    }
+
+    public String getNickname() {
+	    return nickname;
+    }
+    
+    public void setNickname(String nn) {
+	    nickname = nn;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(String technology) {
+        this.technology = technology;
+    }
+
+    public String getMessageIn() {
+        return messageIn;
+    }
+
+    public void setMessageIn(String messageIn) {
+        this.messageIn = messageIn;
+    }
+
+    public String getMessageOut() {
+        return messageOut;
+    }
+
+    public void setMessageOut(String messageOut) {
+        this.messageOut = messageOut;
+    }
+}
