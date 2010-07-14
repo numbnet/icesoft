64085a5e421c58b12ccd5c041b8bad0a623c3560
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-14 02:25:14 +0000

    Fix for ICE-5748(Javadoc sparkle annotations)

--- icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/annotation/Property.java
+++ icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/annotation/Property.java
@@ -5,19 +5,97 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Property annotation allows to add JSF managed properties to the generated component. All
+ * fields are optional on this annotation.
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Property {
     final String Null = "null";
+    
+    /**
+     * if generated property is a method expression, then this field can be set to true. (e.g.)
+	 * <pre>
+	 * @Property(isMethodExpression=true, methodExpressionArgument="javax.faces.event.ValueChangeEvent")
+	 * public MethodExpression valueChangeListener;
+	 * </pre>
+     * @return boolean value.
+     */
     boolean isMethodExpression() default false;
+    
+    /**
+     * Allows to define method expression argument if any.
+     * @return fully qualified name of the argument class.
+     */
     String methodExpressionArgument() default Null;
+    
+    /**
+     * Default value of the property.
+     * @return default value.
+     */
     String defaultValue() default Null;
-    String tlddoc() default Null;
-    String javadocGet() default Null;    
-    String javadocSet() default Null;
-    //tld requires this info
-    boolean required() default false;
-    boolean useTemplate() default false;
-    boolean inherit() default false;
+    
+    /**
+     * By default the value being assigned to the property as string  literal (e.g.)
+     * <pre>
+     * @Property (value="Car")
+     * String type;
+     * 
+     * The generated property would look something like this:
+     * 
+     * String type = "Car";
+     * 
+     * But what if you want to define some other type then string or a constant, expression etc. You don't want
+     * value to be quoted in that case. So you would set this attribute to false. (e.g)
+     * 
+     * @Property (value="10", defaultValueIsStringLiteral=false)
+     * Integer count;      
+     * </pre>
+     * @return 
+     */    
     boolean defaultValueIsStringLiteral() default true;
+    
+    
+    /**
+     * TLDDoc for this property
+     * @return property tlddoc
+     */
+    String tlddoc() default Null;
+    
+    /**
+     * javadoc for the getter.
+     * @return getter javadoc.
+     */    
+    String javadocGet() default Null;  
+    
+    /**
+     * javadoc for the setter.
+     * @return setter javadoc.
+     */     
+    String javadocSet() default Null;
+    
+    /**
+     * Attribute that goes inside the TLD for each attribute. It also helps IDEs
+     * 
+     */
+    boolean required() default false;
+    
+    /**
+     * the "org.icefaces.component.annotation.PropertyTemplate" class keeps template for 
+     * commonly used properties. Which usually defines javadoc for properties. The PropertyTemplate
+     * can be used by any property annotation by just setting this filed to true.
+     * @return boolean value
+     */
+    boolean useTemplate() default false;
+    
+    /**
+     * Only those attribute goes inside the tag class which are defined in 
+     * Meta class, so if you have to expose some of the parent class attribute in tag 
+     * class then you can set this property to true. inherit true tells generator to 
+     * create setter in tag class as well as add the attribute info in TLD for tlddocs 
+     * and IDEs
+     * @return
+     */
+    boolean inherit() default false;
 }
