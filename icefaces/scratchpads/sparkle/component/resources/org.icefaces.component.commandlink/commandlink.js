35a0163d6b2ba039f431096e23318071b05364f2
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-14 16:44:29 +0000

    ICE-5814 Use JSContext to fetch PSFProps in onclick handler.

--- icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.commandlink/commandlink.js
+++ icefaces/scratchpads/sparkle/component/resources/org.icefaces.component.commandlink/commandlink.js
@@ -7,64 +7,45 @@ ice.component.commandlink = {
     });
 
 
-    var spanId = clientId + "_span";
-	var oLinkButton = new YAHOO.widget.Button(spanId,{label: jsProps.label }, {type: jsProps.type});
+        var spanId = clientId + "_span";
+	    var oLinkButton = new YAHOO.widget.Button(spanId,{ label: jsProps.label }, {type: jsProps.type});
 
-	// logger.log("params singleSubmit"+jsfProps.singleSubmit);
-		
-		if(jsProps.label) {
-			oLinkButton.set('label', jsProps.label);
-		}
-
-		if(jsProps.type) {
-			oLinkButton.set('button', jsProps.type);
-		}
-	    var isSingle = jsfProps.singleSubmit;
-	    //alert("Single submit is: " + this.isSingle + ", origninal " +  jsfProps.singleSubmit);
-	    YAHOO.log(" - local isSingle: " + this.isSingle + " original " + jsfProps.singleSubmit);
         root = document.getElementById(spanId);
 
-        if (jsfProps.doAction) {
-            root.onclick = this.actionClickHandler;
-        }
-        //oLinkButton.onclick = this.theInsideClick;
-
-		//oLinkButton.on("click", onClick);
-		// Set the aria role for the link. No further attributes supported.
 		root.firstChild.setAttribute("role", "link");
-// 		oLinkButton.addListener("onclick", onButtonClick);
 		bindYUI(oLinkButton);
 	},
 	
-   //delegate call to ice.yui.updateProperties(..)  with the reference of this lib
-   updateProperties:function(clientId, jsProps, jsfProps, events) {
-       ice.component.updateProperties(clientId, jsProps, jsfProps, events, this);
-   },
+    //delegate call to ice.yui.updateProperties(..)  with the reference of this lib
+    updateProperties:function(clientId, jsProps, jsfProps, events) {
+        ice.component.updateProperties(clientId, jsProps, jsfProps, events, this);
+    },
  
-   //delegate call to ice.yui.getInstance(..) with the reference of this lib 
-   getInstance:function(clientId, callback) {
-       ice.component.getInstance(clientId, callback, this);
-   }, 
+    //delegate call to ice.yui.getInstance(..) with the reference of this lib
+    getInstance:function(clientId, callback) {
+        ice.component.getInstance(clientId, callback, this);
+    },
 
+    // Click handler visible from Renderer code 
+    clickHandler : function (e, clientId) {
 
-   // handle clickage
-   actionClickHandler:function (e) {
-       //alert("ice.s, return false click handler");
-       //e.returnValue=false;
+        var JSContext = ice.component.getJSContext(clientId);
+        var singleSubmit = JSContext..getJSFProps().singleSubmit;
+        var doAction = JSContext.getJSFProps().doAction;
+        var divRoot = document.getElementById(clientId);
 
-       var divRoot = document.getElementById(this.spanId);
-       YAHOO.log(" ++=+++++ How about: " + this.isSingle + "?" );
+        YAHOO.log("--> Button.doAction = " + doAction);
 
-       if (this.isSingle) {
-           YAHOO.log(" ++=+++++ SINGLE SUBMIT onClick and e.target="+e.target + ", while divRoot = " + divRoot);
-           ice.se(e, e.target);
-       } else {
-           YAHOO.log(" Full Submit onClick and e.target="+e.target + ", while divRoot = " + divRoot);
-           ice.s(e, e.target);
-       }
-      // YAHOO.util.stopEvent(e);
-       return false;
-	},
-
-
+        if (singleSubmit) {
+            YAHOO.log("Single Submit on element: " + divRoot);
+            ice.se(e, divRoot );
+        } else {
+            YAHOO.log("Full Submit on element: " + divRoot);
+            ice.s(e, divRoot );
+        }
+        // If there are actionListeners, don't do default behaviour
+        if (doAction) {
+            return false;
+        }
+    }
 };
