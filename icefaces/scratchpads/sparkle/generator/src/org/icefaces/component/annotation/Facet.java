cfb2f4fc81c1a329e46f0a4f02c01f0bce6cdbf3
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-14 02:25:14 +0000

    Fix for ICE-5748(Javadoc sparkle annotations)

--- icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/annotation/Facet.java
+++ icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/annotation/Facet.java
@@ -5,10 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allows to add facet support to the generated component. It can be used in conjunction 
+ * with @Facets annotation.
+ *
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Facet {
-    String name() default "";
-    String javadocGet() default "";
-    String javadocSet() default "";
+	final String EMPTY = "";
+	
+	/**
+	 * Name of the facet, if not defined one the property name will be used instead.
+	 * @return facet name.
+	 */
+    String name() default EMPTY;
+    
+    /**
+     * Java doc for generated getter
+     * @return getter javadoc
+     */
+    String javadocGet() default EMPTY;
+    
+    /**
+     * Javadoc for generated setter
+     * @return setter javadoc
+     */
+    String javadocSet() default EMPTY;
 }
