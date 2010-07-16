5ccea0d0d5e888349d3b31f8e2e2dd08faf74559
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-16 04:27:30 +0000

    ICE-5749 Created Ant task around cssurlmapper tool

---
+++ icefaces/scratchpads/sparkle/component/util/cssurlmapper/src/org/icefaces/util/cssurlmapper/ant/CssUrlMapperTask.java
@@ -0,0 +1,83 @@
+package org.icefaces.util.cssurlmapper.ant;
+
+import java.io.*;
+import java.util.*;
+import org.icefaces.util.cssurlmapper.*;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class CssUrlMapperTask {
+
+    private String libraryname = "";
+	private String inputdir = "";
+	private String outputdir = "";
+	private String referencedir = "";
+
+    public void setLibraryname(String libraryname) {
+        this.libraryname = libraryname;
+    }
+	
+    public void setInputdir(String inputdir) {
+        this.inputdir = inputdir;
+    }
+	
+    public void setOutputdir(String outputdir) {
+        this.outputdir = outputdir;
+    }
+	
+    public void setReferencedir(String referencedir) {
+        this.referencedir = referencedir;
+    }
+	
+    public void execute() throws BuildException {
+        System.out.println("libraryname: " + libraryname);
+		System.out.println("inputdir: " + inputdir);
+		System.out.println("outputdir: " + outputdir);
+		System.out.println("referencedir: " + referencedir);
+		
+		try {
+			if (libraryname.equals("") || inputdir.equals("") || outputdir.equals("")) {
+				throw new BuildException("cssurlmapper: libraryname, inputdir and outputdir are required.");
+			}
+			
+			File inputdirFile = new File(inputdir);
+			if (!inputdirFile.exists()) {
+				throw new BuildException("cssurlmapper: inputdir does not exist.");
+			} else if (!inputdirFile.isDirectory()) {
+				throw new BuildException("cssurlmapper: inputdir is not a real directory.");
+			}
+			
+			File outputdirFile = new File(outputdir);
+			outputdirFile.mkdirs();
+			if (!outputdirFile.exists()) {
+				throw new BuildException("cssurlmapper: could not create output directory.");
+			} else if (!outputdirFile.isDirectory()) {
+				throw new BuildException("cssurlmapper: outputdir is not a real directory.");
+			}
+			
+			File referencedirFile = null;
+			if (!referencedir.equals("")) {
+				referencedirFile = new File(referencedir);
+				if (!referencedirFile.exists()) {
+					throw new BuildException("ERROR: referencedir does not exist.");
+				} else if (!inputdirFile.isDirectory()) {
+					throw new BuildException("cssurlmapper: referencedir is not a real directory.");
+				}
+				String referencedirPath = referencedirFile.getCanonicalPath();
+				String inputdirPath = inputdirFile.getCanonicalPath();
+				if (!inputdirPath.startsWith(referencedirPath)) {
+					throw new BuildException("cssurlmapper: referencedir must be an ancestor of inputdir.");
+				}
+			}
+			
+			if (referencedirFile != null) {
+				Main.processDirectory(inputdirFile, libraryname, referencedirFile, outputdirFile);
+			} else {
+				Main.processDirectory(inputdirFile, libraryname, inputdirFile, outputdirFile);
+			}
+		} catch (IOException e) {
+			throw new BuildException(e);
+		}
+    }
+}
