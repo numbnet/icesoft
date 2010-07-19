730dc6999d3e16fcd1176ce02002aa0830433688
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-19 22:07:06 +0000

    ICEfaces 2.0.0 Beta 1 release tag.

---
+++ icefaces2/tags/icefaces-2.0.0-beta1/icefaces/compat/core/src/main/java/com/icesoft/faces/webapp/parser/AttributeElement.java
@@ -0,0 +1,30 @@
+package com.icesoft.faces.webapp.parser;
+
+public class AttributeElement {
+
+    private String name;
+    private String required;
+    private String description;
+
+    
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRequired() {
+		return required;
+	}
+	public void setRequired(String required) {
+		this.required = required;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
+
