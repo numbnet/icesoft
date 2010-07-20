d3fb16311d16c7f4fdcaf6995ded7cb1d10c823a
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-20 22:35:48 +0000

    ICE-5844 Hide Prototype and Scriptaculous variable/function definitions by wrapping in a closure that is executed immediately. Make functions used by component renderers public, but namespace them to avoid naming collisions. Change renderers to use these new function names.

--- icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/selectinputtext/SelectInputTextRenderer.java
+++ icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/selectinputtext/SelectInputTextRenderer.java
@@ -22,30 +22,24 @@
 
 package com.icesoft.faces.component.selectinputtext;
 
-import com.icesoft.faces.component.ExtendedAttributeConstants;
 import com.icesoft.faces.context.DOMContext;
 import com.icesoft.faces.context.effects.JavascriptContext;
 import com.icesoft.faces.renderkit.dom_html_basic.DomBasicInputRenderer;
 import com.icesoft.faces.renderkit.dom_html_basic.HTML;
 import com.icesoft.faces.renderkit.dom_html_basic.PassThruAttributeRenderer;
-import org.icefaces.util.DOMUtils;
+import com.icesoft.util.pooling.ClientIdPool;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.icefaces.util.DOMUtils;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.Text;
 
 import javax.faces.component.UIComponent;
-import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
-
-import com.icesoft.util.pooling.ClientIdPool;
 
 public class SelectInputTextRenderer extends DomBasicInputRenderer {
     private static final String AUTOCOMPLETE_DIV = "_div";
@@ -55,8 +49,8 @@ public class SelectInputTextRenderer extends DomBasicInputRenderer {
 
     //private static final String[] passThruAttributes = ExtendedAttributeConstants.getAttributes(ExtendedAttributeConstants.ICE_SELECTINPUTTEXT);
     //handle         HTML.ONKEYDOWN_ATTR, HTML.ONKEYUP_ATTR, HTML.ONFOCUS_ATTR, HTML.ONBLUR_ATTR
-    private static final String[] passThruAttributes = new String[]{ HTML.ACCESSKEY_ATTR,  HTML.ALT_ATTR,  HTML.DIR_ATTR,  HTML.LANG_ATTR,  HTML.MAXLENGTH_ATTR,  HTML.ONCHANGE_ATTR,  HTML.ONCLICK_ATTR,  HTML.ONDBLCLICK_ATTR,   HTML.ONKEYPRESS_ATTR,   HTML.ONMOUSEDOWN_ATTR,  HTML.ONMOUSEMOVE_ATTR,  HTML.ONMOUSEOUT_ATTR,  HTML.ONMOUSEOVER_ATTR,  HTML.ONMOUSEUP_ATTR,  HTML.ONSELECT_ATTR,  HTML.ROWS_ATTR,  HTML.SIZE_ATTR,  HTML.STYLE_ATTR,  HTML.TABINDEX_ATTR,  HTML.TITLE_ATTR,  HTML.WIDTH_ATTR };                        
-           
+    private static final String[] passThruAttributes = new String[]{HTML.ACCESSKEY_ATTR, HTML.ALT_ATTR, HTML.DIR_ATTR, HTML.LANG_ATTR, HTML.MAXLENGTH_ATTR, HTML.ONCHANGE_ATTR, HTML.ONCLICK_ATTR, HTML.ONDBLCLICK_ATTR, HTML.ONKEYPRESS_ATTR, HTML.ONMOUSEDOWN_ATTR, HTML.ONMOUSEMOVE_ATTR, HTML.ONMOUSEOUT_ATTR, HTML.ONMOUSEOVER_ATTR, HTML.ONMOUSEUP_ATTR, HTML.ONSELECT_ATTR, HTML.ROWS_ATTR, HTML.SIZE_ATTR, HTML.STYLE_ATTR, HTML.TABINDEX_ATTR, HTML.TITLE_ATTR, HTML.WIDTH_ATTR};
+
     public boolean getRendersChildren() {
         return true;
     }
@@ -73,8 +67,8 @@ public class SelectInputTextRenderer extends DomBasicInputRenderer {
         String clientId = uiComponent.getClientId(facesContext);
         String divId = ClientIdPool.get(clientId + AUTOCOMPLETE_DIV);
         String call = " new Ice.Autocompleter('" + clientId + "','" + divId +
-                      "', " + component.getOptions() + " ,'" + component.getRowClass() + "','" +
-                      component.getSelectedRowClass() + "');";
+                "', " + component.getOptions() + " ,'" + component.getRowClass() + "','" +
+                component.getSelectedRowClass() + "');";
 
         if (!domContext.isInitialized()) {
             Element root = domContext.createRootElement(HTML.DIV_ELEM);
@@ -84,56 +78,56 @@ public class SelectInputTextRenderer extends DomBasicInputRenderer {
             root.appendChild(input);
             input.setAttribute(HTML.NAME_ATTR, clientId);
             input.setAttribute(HTML.CLASS_ATTR, component.getInputTextClass());
-            
-            String mousedownScript = (String)uiComponent.getAttributes().get(HTML.ONMOUSEDOWN_ATTR);
+
+            String mousedownScript = (String) uiComponent.getAttributes().get(HTML.ONMOUSEDOWN_ATTR);
             input.setAttribute(HTML.ONMOUSEDOWN_ATTR, combinedPassThru(mousedownScript, "this.focus();"));
 
             String inputStyle = component.getWidthAsStyle();
-            if(inputStyle != null && inputStyle.length() > 0)
+            if (inputStyle != null && inputStyle.length() > 0)
                 input.setAttribute(HTML.STYLE_ATTR, inputStyle);
             else
                 input.removeAttribute(HTML.STYLE_ATTR);
             input.setAttribute("autocomplete", "off");
             Element div = domContext.createElement(HTML.DIV_ELEM);
-            String listClass =  component.getListClass();
+            String listClass = component.getListClass();
 
             div.setAttribute(HTML.ID_ATTR, divId);
-            if(listClass == null){
+            if (listClass == null) {
                 div.setAttribute(HTML.STYLE_ATTR,
-                             "display:none;border:1px solid black;background-color:white;z-index:500;");
-            }else{
+                        "display:none;border:1px solid black;background-color:white;z-index:500;");
+            } else {
                 div.setAttribute(HTML.CLASS_ATTR, listClass);
             }
             root.appendChild(div);
-            
+
             Element index = domContext.createElement(HTML.INPUT_ELEM);
             index.setAttribute(HTML.TYPE_ATTR, HTML.INPUT_TYPE_HIDDEN);
             String indexId = ClientIdPool.get(clientId + AUTOCOMPLETE_INDEX);
             index.setAttribute(HTML.NAME_ATTR, indexId);
             root.appendChild(index);
-            
+
             String rootStyle = component.getStyle();
-            if(rootStyle != null && rootStyle.length() > 0)
+            if (rootStyle != null && rootStyle.length() > 0)
                 root.setAttribute(HTML.STYLE_ATTR, rootStyle);
             else
                 root.removeAttribute(HTML.STYLE_ATTR);
             root.setAttribute(HTML.CLASS_ATTR, component.getStyleClass());
-            
-          //  Element script = domContext.createElement(HTML.SCRIPT_ELEM);
-          //  script.setAttribute(HTML.SCRIPT_LANGUAGE_ATTR,
-                              //  HTML.SCRIPT_LANGUAGE_JAVASCRIPT);
-       //     String scriptCode = "window.onLoad(function(){" + call + "});";
-         //   Node node = domContext.createTextNode(scriptCode);
-          //  script.appendChild(node);
-          //  root.appendChild(script);
+
+            //  Element script = domContext.createElement(HTML.SCRIPT_ELEM);
+            //  script.setAttribute(HTML.SCRIPT_LANGUAGE_ATTR,
+            //  HTML.SCRIPT_LANGUAGE_JAVASCRIPT);
+            //     String scriptCode = "window.onLoad(function(){" + call + "});";
+            //   Node node = domContext.createTextNode(scriptCode);
+            //  script.appendChild(node);
+            //  root.appendChild(script);
             if (log.isDebugEnabled()) {
                 log.debug(
                         "SelectInputText:encodeBegin():component created with the following id : " +
-                        clientId);
+                                clientId);
             }
             PassThruAttributeRenderer.renderHtmlAttributes(facesContext, uiComponent, passThruAttributes);
-            PassThruAttributeRenderer.renderBooleanAttributes(facesContext, 
-                    uiComponent, input, PassThruAttributeRenderer.EMPTY_STRING_ARRAY);            
+            PassThruAttributeRenderer.renderBooleanAttributes(facesContext,
+                    uiComponent, input, PassThruAttributeRenderer.EMPTY_STRING_ARRAY);
         }
 //        Set excludes = new HashSet();
 //        excludes.add(HTML.ONKEYDOWN_ATTR);
@@ -195,87 +189,87 @@ public class SelectInputTextRenderer extends DomBasicInputRenderer {
 
     public void populateList(FacesContext facesContext, SelectInputText component)
             throws IOException {
-            if (log.isTraceEnabled()) {
-                log.trace("populateList");
+        if (log.isTraceEnabled()) {
+            log.trace("populateList");
+        }
+        component.populateItemList();
+        Iterator matchs = component.getItemList();
+        int rows = component.getRows();
+        int rowCounter = 0;
+        if (component.getSelectFacet() != null) {
+            if (log.isDebugEnabled()) {
+                log.debug(
+                        "SelectInputText:populateList(): \"selectInputText\" facet found, generate generic html for list");
             }
-            component.populateItemList();
-            Iterator matchs = component.getItemList();
-            int rows = component.getRows();
-            int rowCounter = 0;             
-            if (component.getSelectFacet() != null) {
-                if (log.isDebugEnabled()) {
-                    log.debug(
-                            "SelectInputText:populateList(): \"selectInputText\" facet found, generate generic html for list");
-                }
-                UIComponent facet = component.getSelectFacet();
-                DOMContext domContext =
-                        DOMContext.getDOMContext(facesContext, component);
+            UIComponent facet = component.getSelectFacet();
+            DOMContext domContext =
+                    DOMContext.getDOMContext(facesContext, component);
 
-                Element listDiv = domContext.createElement(HTML.DIV_ELEM);
-                Map requestMap =
-                        facesContext.getExternalContext().getRequestMap();
-                //set index to 0, so child components can get client id from autoComplete component
-                component.setIndex(0);
-                while (matchs.hasNext() && (rowCounter++ < rows || rows == 0)) {
-                    Element div = domContext.createElement(HTML.DIV_ELEM);
-                    SelectItem item = (SelectItem) matchs.next();
-                    requestMap.put(component.getListVar(), item.getValue());
-                    listDiv.appendChild(div);
-                    // When HTML is display we still need a selected value. Hidding the value in a hidden span
-                    // accomplishes this.
-                    Element spanToDisplay =
-                            domContext.createElement(HTML.SPAN_ELEM);
-                    spanToDisplay.setAttribute(HTML.CLASS_ATTR, "informal");
-                    div.appendChild(spanToDisplay);
-                    domContext.setCursorParent(spanToDisplay);
-                    encodeParentAndChildren(facesContext, facet);
-                    Element spanToSelect =
-                            domContext.createElement(HTML.SPAN_ELEM);
-                    spanToSelect.setAttribute(HTML.STYLE_ATTR,
-                                              "visibility:hidden;display:none;");
-                    String itemLabel = item.getLabel();
-                    if(itemLabel == null) {
-                        itemLabel = converterGetAsString(
+            Element listDiv = domContext.createElement(HTML.DIV_ELEM);
+            Map requestMap =
+                    facesContext.getExternalContext().getRequestMap();
+            //set index to 0, so child components can get client id from autoComplete component
+            component.setIndex(0);
+            while (matchs.hasNext() && (rowCounter++ < rows || rows == 0)) {
+                Element div = domContext.createElement(HTML.DIV_ELEM);
+                SelectItem item = (SelectItem) matchs.next();
+                requestMap.put(component.getListVar(), item.getValue());
+                listDiv.appendChild(div);
+                // When HTML is display we still need a selected value. Hidding the value in a hidden span
+                // accomplishes this.
+                Element spanToDisplay =
+                        domContext.createElement(HTML.SPAN_ELEM);
+                spanToDisplay.setAttribute(HTML.CLASS_ATTR, "informal");
+                div.appendChild(spanToDisplay);
+                domContext.setCursorParent(spanToDisplay);
+                encodeParentAndChildren(facesContext, facet);
+                Element spanToSelect =
+                        domContext.createElement(HTML.SPAN_ELEM);
+                spanToSelect.setAttribute(HTML.STYLE_ATTR,
+                        "visibility:hidden;display:none;");
+                String itemLabel = item.getLabel();
+                if (itemLabel == null) {
+                    itemLabel = converterGetAsString(
                             facesContext, component, item.getValue());
-                    }
-                    Text label = domContext.createTextNode(DOMUtils.escapeAnsi(itemLabel));
-                    spanToSelect.appendChild(label);
-                    div.appendChild(spanToSelect);
-                    component.resetId(facet);
                 }
-                component.setIndex(-1);
-
-                String nodeValue =
-                        DOMUtils.nodeToString(listDiv).replaceAll("\n", "");
-                String call = "Autocompleter.Finder.find('" +
-                              component.getClientId(facesContext) +
-                              "').updateNOW('" + nodeValue + "');";
-                JavascriptContext.addJavascriptCall(facesContext, call);
-
-            } else {
-                if (log.isDebugEnabled()) {
-                    log.debug(
-                            "SelectInputText:populateList(): \"selectItem(s)\" found, generate plain-text for list");
-                }
-                if (matchs.hasNext()) {
-                    StringBuffer sb = new StringBuffer("<div>");
-                    SelectItem item = null;
-                    while (matchs.hasNext() && (rowCounter++ < rows || rows == 0)) {
-                        item = (SelectItem) matchs.next();
-                        String itemLabel = item.getLabel();
-                        if(itemLabel == null) {
-                            itemLabel = converterGetAsString(
-                                facesContext, component, item.getValue());
-                        }
-                        sb.append("<div>").append(DOMUtils.escapeAnsi(itemLabel))
-                                .append("</div>");
-                    }
-                    sb.append("</div>");
-                    String call = "Autocompleter.Finder.find('" +
-                                  component.getClientId(facesContext) +
-                                  "').updateNOW('" + sb.toString() + "');";
-                    JavascriptContext.addJavascriptCall(facesContext, call);
-                }
+                Text label = domContext.createTextNode(DOMUtils.escapeAnsi(itemLabel));
+                spanToSelect.appendChild(label);
+                div.appendChild(spanToSelect);
+                component.resetId(facet);
             }
+            component.setIndex(-1);
+
+            String nodeValue =
+                    DOMUtils.nodeToString(listDiv).replaceAll("\n", "");
+            String call = "Ice.Scriptaculous.Autocompleter.Finder.find('" +
+                    component.getClientId(facesContext) +
+                    "').updateNOW('" + nodeValue + "');";
+            JavascriptContext.addJavascriptCall(facesContext, call);
+
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug(
+                        "SelectInputText:populateList(): \"selectItem(s)\" found, generate plain-text for list");
+            }
+            if (matchs.hasNext()) {
+                StringBuffer sb = new StringBuffer("<div>");
+                SelectItem item = null;
+                while (matchs.hasNext() && (rowCounter++ < rows || rows == 0)) {
+                    item = (SelectItem) matchs.next();
+                    String itemLabel = item.getLabel();
+                    if (itemLabel == null) {
+                        itemLabel = converterGetAsString(
+                                facesContext, component, item.getValue());
+                    }
+                    sb.append("<div>").append(DOMUtils.escapeAnsi(itemLabel))
+                            .append("</div>");
+                }
+                sb.append("</div>");
+                String call = "Ice.Scriptaculous.Autocompleter.Finder.find('" +
+                        component.getClientId(facesContext) +
+                        "').updateNOW('" + sb.toString() + "');";
+                JavascriptContext.addJavascriptCall(facesContext, call);
+            }
+        }
     }
 }
