48ab7df7479a7e4ac84a7fdfe24170f31312783f
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-07-28 21:59:55 +0000

    ICE-5853 Factor out initial parameter calculation into a separate function, ice.calculateInitalParameters(). Renderers belonging to 'javax.faces.Form' family are decorated, a 'script' element is rendered right before the 'form' end tag to make sure ice.calculateInitalParameters executes correctly.

--- icefaces2/trunk/icefaces/core/src/main/java/org/icefaces/render/DOMRenderKit.java
+++ icefaces2/trunk/icefaces/core/src/main/java/org/icefaces/render/DOMRenderKit.java
@@ -22,27 +22,31 @@
 
 package org.icefaces.render;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
-import java.io.Writer;
-
-import javax.faces.render.RenderKit;
-import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
-
+import org.icefaces.application.ExternalContextConfiguration;
+import org.icefaces.application.ProductInfo;
 import org.icefaces.context.DOMResponseWriter;
 import org.icefaces.event.MainEventListener;
+import org.icefaces.push.Configuration;
 import org.icefaces.util.EnvUtils;
-import org.icefaces.application.ProductInfo;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.convert.ConverterException;
+import javax.faces.render.RenderKit;
 import javax.faces.render.RenderKitWrapper;
+import javax.faces.render.Renderer;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DOMRenderKit extends RenderKitWrapper {
-
     private static Logger log = Logger.getLogger(DOMRenderKit.class.getName());
 
     MainEventListener mainEventListener = new MainEventListener();
     RenderKit delegate;
+    private boolean deltaSubmit;
 
     //Announce ICEfaces 2.0
     static {
@@ -51,12 +55,19 @@ public class DOMRenderKit extends RenderKitWrapper {
         }
     }
 
-    public DOMRenderKit(RenderKit delegate)  {
+    public DOMRenderKit(RenderKit delegate) {
         this.delegate = delegate;
+        Configuration configuration = new ExternalContextConfiguration("org.icefaces", FacesContext.getCurrentInstance().getExternalContext());
+        deltaSubmit = configuration.getAttributeAsBoolean("deltaSubmit", false);
     }
 
     public RenderKit getWrapped() {
         return delegate;
+    }
+
+    public void addRenderer(String family, String rendererType, Renderer r) {
+        Renderer renderer = deltaSubmit && "javax.faces.Form".equals(family) ? new FormBoost(r) : r;
+        super.addRenderer(family, rendererType, renderer);
     }
 
     public ResponseWriter createResponseWriter(Writer writer, String contentTypeList, String encoding) {
@@ -64,7 +75,49 @@ public class DOMRenderKit extends RenderKitWrapper {
         if (!EnvUtils.isICEfacesView(FacesContext.getCurrentInstance())) {
             return parentWriter;
         }
-        return new DOMResponseWriter(parentWriter,parentWriter.getCharacterEncoding(),parentWriter.getContentType());
+        return new DOMResponseWriter(parentWriter, parentWriter.getCharacterEncoding(), parentWriter.getContentType());
     }
 
+    private class FormBoost extends Renderer {
+        private Renderer renderer;
+
+        private FormBoost(Renderer renderer) {
+            this.renderer = renderer;
+        }
+
+        public void decode(FacesContext context, UIComponent component) {
+            renderer.decode(context, component);
+        }
+
+        public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+            renderer.encodeBegin(context, component);
+        }
+
+        public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
+            super.encodeChildren(context, component);
+        }
+
+        public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+            ResponseWriter writer = context.getResponseWriter();
+            writer.startElement("script", component);
+            writer.writeAttribute("type", "text/javascript", null);
+            writer.writeText("ice.calculateInitialParameters('", null);
+            writer.writeText(component.getClientId(context), null);
+            writer.writeText("');", null);
+            writer.endElement("script");
+            renderer.encodeEnd(context, component);
+        }
+
+        public String convertClientId(FacesContext context, String clientId) {
+            return renderer.convertClientId(context, clientId);
+        }
+
+        public boolean getRendersChildren() {
+            return renderer.getRendersChildren();
+        }
+
+        public Object getConvertedValue(FacesContext context, UIComponent component, Object submittedValue) throws ConverterException {
+            return renderer.getConvertedValue(context, component, submittedValue);
+        }
+    }
 }
