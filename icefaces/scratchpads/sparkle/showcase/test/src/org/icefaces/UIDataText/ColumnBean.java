bdd134159d145eede5a2ccc7d47642605b1abf8e
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-08-09 21:16:43 +0000

    initial check in

---
+++ icefaces/scratchpads/sparkle/showcase/test/src/org/icefaces/UIDataText/ColumnBean.java
@@ -0,0 +1,75 @@
+package org.icefaces.UIDataText;
+
+import org.icefaces.component.commandlink.CommandLink;
+import org.icefaces.UIDataText.StyleHolder;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.component.UIComponent;
+
+
+/**
+ * The ColumnsBean object generates data for the ice:columns example.
+ *
+ * @since 1.5
+ */
+@ManagedBean (name="columnBean")
+@SessionScoped
+public class ColumnBean implements Serializable {
+
+	private String label = "ActionListener";
+
+    private String myStyle;
+
+    private String STYLES[] = {
+            "text-decoration: overline;",
+            "text-decoration: line-through;",
+            "text-decoration: underline;",
+            "text-decoration: blink;",
+            "text-decoration: none;"
+     };
+
+    List styleData = new ArrayList();
+    private int styleIndex;
+
+    public ColumnBean(){
+        for (int idx = 0; idx < STYLES.length; idx ++ ) {
+            System.out.println("Adding new style: " + STYLES[idx]);
+            styleData.add( new StyleHolder( STYLES[idx] ) );
+        }
+   }
+    
+    public void incrementStyleIndex(ActionEvent e) {
+        UIComponent uic = e.getComponent();
+        CommandLink cl = (CommandLink) uic;
+        //cl.setStyle("black_line");
+        int index = styleIndex++ % styleData.size();
+        String newStyle = styleData.get( index ).toString();
+        System.out.println("--> Setting style to: " + newStyle + " at " + index);
+        cl.setStyle( newStyle );
+    }
+
+
+    public String getMyStyle() {
+        return myStyle;
+    }
+
+    public void setMyStyle(String style ) {
+        myStyle = style;
+    }
+
+    public String getStyleClass() {
+        return styleData.get(0).toString();
+    }
+
+    public List getStyleHolderData() {
+        return styleData;
+    }
+}
+
