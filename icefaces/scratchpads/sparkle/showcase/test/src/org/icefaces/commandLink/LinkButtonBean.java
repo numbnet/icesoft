80bfe6365c12cbbc690055d284532911d1484952
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-08-09 21:31:25 +0000

    trivial changes

--- icefaces/scratchpads/sparkle/showcase/test/src/org/icefaces/commandLink/LinkButtonBean.java
+++ icefaces/scratchpads/sparkle/showcase/test/src/org/icefaces/commandLink/LinkButtonBean.java
@@ -1,12 +1,16 @@
 package org.icefaces.commandLink;
 
+import org.icefaces.component.commandlink.CommandLink;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 
 import javax.faces.bean.ViewScoped;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
+import javax.faces.component.UIComponent;
 
 
 @ManagedBean (name="linkBean")
@@ -17,13 +21,13 @@ public class LinkButtonBean implements Serializable {
 	private String label = "ActionListener";
 	private boolean rendered = false;
 
+
   /** @PostConstruct
    public void init(){
-System.out.println("creating RadioButtonBean version="+this);
+       System.out.println("creating RadioButtonBean version="+this);
    } **/
 
-   public LinkButtonBean(){
-      System.out.println("creating LinkButtonBean version= "+this);
+    public LinkButtonBean(){
    }
 
     public boolean isChecked() {
@@ -49,14 +53,20 @@ System.out.println("creating RadioButtonBean version="+this);
     	return " ";
     }
     public void actionListenerMethod(ActionEvent e) {
-        System.out.println("++++++-- ++++> actionListenerMethod() ");
+        System.out.println("_____  Full submit actionListener ");
+        rendered = !rendered;
+    }
+
+     public void singleSubmitListenerMethod(ActionEvent e) {
+        System.out.println("___----- SingleSubmit actionListener ");
         rendered=!rendered;
     }
 
+
     public void hrefAndActionMethod(ActionEvent e) {
-        System.out.println("----------> hrefAndActionMethod called");
+        System.out.println("-----> hrefAndActionMethod called");
         rendered=!rendered;
-    } 
+    }
 
     public boolean isRendered(){
     	return this.rendered;
