c19eb98f56a24e8902dfe5c7698f33e5bb4fd6b3
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-08-17 20:00:26 +0000

    ICE-5997: adding test case for various session invalidation strategies

---
+++ icefaces2/trunk/icefaces/samples/test/invalidate/src/main/java/org/icefaces/test/invalidate/TestBean.java
@@ -0,0 +1,131 @@
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2010 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+package org.icefaces.test.invalidate;
+
+import org.icefaces.util.EnvUtils;
+
+import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.util.logging.Logger;
+
+@ManagedBean(name = "bean")
+//@RequestScoped
+//@ViewScoped
+@SessionScoped
+//@CustomScoped(value="#{window}")
+public class TestBean implements Serializable {
+
+    private static final long serialVersionUID = -5395176310517774113L;
+    private static Logger log = Logger.getLogger(TestBean.class.getName());
+
+    private String message = "";
+    private static String scope = "[unknown]";
+
+    static {
+        Annotation[] anns = TestBean.class.getAnnotations();
+        for (int index = 0; index < anns.length; index++) {
+            Annotation ann = anns[index];
+            if (ann instanceof RequestScoped) {
+                scope = "Request";
+            } else if (ann instanceof ViewScoped) {
+                scope = "View";
+            } else if (ann instanceof SessionScoped) {
+                scope = "Session";
+            } else if (ann instanceof CustomScoped) {
+                scope = "Window";
+            }
+        }
+    }
+
+    public TestBean() {
+        log.info("TestBean created: " + this);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getThisBean() {
+        return this.toString();
+    }
+
+    public void setThisBean(String bean) {
+    }
+
+    public String getThisSession() {
+        Object sessObj = FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+        if( sessObj != null ){
+            return sessObj.toString();
+        }
+        return "[no active session]";
+    }
+
+    public String getResponseWriter() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        return fc.getResponseWriter().toString();
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setThisSession(String session) {
+    }
+
+    public void submit(ActionEvent event) {
+        log.info("TestBean.submit: called for " + this);
+    }
+
+    public void invalidateStandard(ActionEvent event) {
+        log.info("TestBean.invalidateStandard: called for " + this);
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+        ec.invalidateSession();
+    }
+
+    public void invalidateFilter(ActionEvent event) {
+        log.info("TestBean.invalidateFilter: called for " + this);
+        FacesContext fc = FacesContext.getCurrentInstance();
+        ExternalContext ec = fc.getExternalContext();
+        ec.getRequestMap().put(EnvUtils.SESSION_INVALIDATION, "true");
+    }
+
+
+//    public void invalidateAfterRender(PhaseEvent event){
+//        if( event.getPhaseId().equals(PhaseId.RENDER_RESPONSE)){
+//            log.info("TestBean.afterPhase [" + event.getPhaseId() + "] : called for " + this);
+//            event.getFacesContext().getExternalContext().invalidateSession();
+//        }
+//    }
+
+
+}
