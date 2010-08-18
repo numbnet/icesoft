7e2c8f7f7196e5e8676ec313451b338acbde930e
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-08-18 17:10:51 +0000

    update radiobutton for examples

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/radiobutton/RadioButton.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/radiobutton/RadioButton.java
@@ -1,12 +1,7 @@
 package org.icefaces.component.radiobutton;
 
 import javax.faces.context.FacesContext;
-import javax.faces.context.ResponseWriter;
-import javax.faces.component.UIOutput;
-import javax.faces.model.SelectItemGroup;
-import javax.faces.model.SelectItem;
 
-import java.io.IOException;
 
 public class RadioButton extends RadioButtonBase {
 
