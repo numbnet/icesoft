2854def719c5c061cc84704472a80b47462de38b
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-08-31 23:06:03 +0000

    Added id field

--- icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/baseMeta/UIComponentBaseMeta.java
+++ icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/baseMeta/UIComponentBaseMeta.java
@@ -1,8 +1,13 @@
 package org.icefaces.component.baseMeta;
 
+import org.icefaces.component.annotation.Property;
+
 /**
  * 
  */
 public class UIComponentBaseMeta extends UIComponentMeta {
+
+    @Property ( tlddoc="The component id")
+    private String id;
 
 }
