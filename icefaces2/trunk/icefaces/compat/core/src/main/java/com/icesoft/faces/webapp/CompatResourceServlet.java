56af1e607ed4b5e9acd4d22db6b8d8d68f3661f4
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-09-09 19:54:22 +0000

    ICE-6027 Modified CompatResourceServlet to serve up any resource from com/icesoft/faces/resources . Added 'blank' HTML file into com/icesoft/faces/resources directory.

--- icefaces2/trunk/icefaces/compat/core/src/main/java/com/icesoft/faces/webapp/CompatResourceServlet.java
+++ icefaces2/trunk/icefaces/compat/core/src/main/java/com/icesoft/faces/webapp/CompatResourceServlet.java
@@ -25,31 +25,29 @@ package com.icesoft.faces.webapp;
 import org.icefaces.impl.util.Util;
 import org.icefaces.util.EnvUtils;
 
-import java.text.ParseException;
-import java.util.Date;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.IOException;
-import java.net.URI;
-
+import java.text.ParseException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CompatResourceServlet extends HttpServlet {
     private static Logger log = Logger.getLogger(CompatResourceServlet.class.getName());
-    private static final String BASE_PATH = "com/icesoft/faces/resources/css/";
+    private static final String BASE_PATH = "com/icesoft/faces/resources";
     private ClassLoader loader;
     private final Date lastModified = new Date();
     private final String STARTUP_TIME = Util.HTTP_DATE.format(lastModified);
     private ServletContext servletContext;
 
-    public void init(final ServletConfig servletConfig) throws ServletException  {
+    public void init(final ServletConfig servletConfig) throws ServletException {
         super.init(servletConfig);
         this.loader = this.getClass().getClassLoader();
         this.servletContext = servletConfig.getServletContext();
@@ -58,32 +56,31 @@ public class CompatResourceServlet extends HttpServlet {
     public void service(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
         String modifedHeader = httpServletRequest
                 .getHeader("If-Modified-Since");
-        if (null != modifedHeader)  {
+        if (null != modifedHeader) {
             try {
                 Date modifiedSince = Util.HTTP_DATE.parse(modifedHeader);
-                if (modifiedSince.getTime() + 1000 > lastModified.getTime() ) {
+                if (modifiedSince.getTime() + 1000 > lastModified.getTime()) {
                     //respond with a not-modifed
                     httpServletResponse.setStatus(304);
                     //TODO: calculate ETag
-        //            httpServletResponse.setHeader(
-        //                    "ETag", encode(resource));
+                    //            httpServletResponse.setHeader(
+                    //                    "ETag", encode(resource));
                     httpServletResponse.setDateHeader(
                             "Date", new Date().getTime());
                     httpServletResponse.setDateHeader(
                             "Last-Modified", lastModified.getTime());
                     return;
                 }
-            } catch (ParseException e)  {
+            } catch (ParseException e) {
                 //if the headers are corrupted, still just serve the resource
-                log.log(Level.WARNING, "failed to parse date: " + modifedHeader , e);
+                log.log(Level.WARNING, "failed to parse date: " + modifedHeader, e);
             }
         }
 
         String path = httpServletRequest.getPathInfo();
-        String file = path.substring(path.lastIndexOf("css/") + 4, path.length());
-        final InputStream in = loader.getResourceAsStream(BASE_PATH + file);
-        if (null == in)  {
-            httpServletResponse.setStatus(404, "CSS resource not found");
+        final InputStream in = loader.getResourceAsStream(BASE_PATH + path);
+        if (null == in) {
+            httpServletResponse.setStatus(404, "Resource not found");
             return;
         }
         String mimeType = servletContext.getMimeType(path);
@@ -93,11 +90,11 @@ public class CompatResourceServlet extends HttpServlet {
         OutputStream out = httpServletResponse.getOutputStream();
 
         boolean compressResources = !"false".equalsIgnoreCase(
-                servletContext.getInitParameter(EnvUtils.COMPRESS_RESOURCES) );
+                servletContext.getInitParameter(EnvUtils.COMPRESS_RESOURCES));
         String acceptHeader = httpServletRequest.getHeader("Accept-Encoding");
-        boolean acceptGzip = (null != acceptHeader) && 
-            (acceptHeader.indexOf("gzip") >=0);
-        if (acceptGzip && compressResources && Util.shouldCompress(mimeType) )  {
+        boolean acceptGzip = (null != acceptHeader) &&
+                (acceptHeader.indexOf("gzip") >= 0);
+        if (acceptGzip && compressResources && Util.shouldCompress(mimeType)) {
             httpServletResponse.setHeader("Content-Encoding", "gzip");
             Util.compressStream(in, out);
         } else {
