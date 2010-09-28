f471905387c6d5de01704ab46f1330133a20d6a4
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-09-28 20:10:38 +0000

    ICE-5711 Use introspection to load classes

--- icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/facelets/IceComponentHandler.java
+++ icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/facelets/IceComponentHandler.java
@@ -40,19 +40,14 @@ import javax.faces.el.MethodBinding;
 import javax.faces.context.FacesContext;
 import java.lang.reflect.InvocationTargetException;
 
-import com.icesoft.faces.component.dragdrop.DragEvent;
-import com.icesoft.faces.component.dragdrop.DropEvent;
-import com.icesoft.faces.component.ext.RowSelectorEvent;
-import com.icesoft.faces.component.ext.ClickActionEvent;
-import com.icesoft.faces.component.panelpositioned.PanelPositionedEvent;
-import com.icesoft.faces.component.paneltabset.TabChangeEvent;
-import com.icesoft.faces.component.outputchart.OutputChart;
-import com.icesoft.faces.component.DisplayEvent;
-import com.icesoft.faces.component.selectinputtext.TextChangeEvent;
-
 import java.util.EventObject;
 
 /**
+ *
+ * !! NOTE!!!
+ * There is a duplicate of this file in icefaces core. Any changes to this file need to be duplicated
+ * there as well!
+ *
  * @author Mark Collette
  * @since 1.6
  */
@@ -68,35 +63,65 @@ public class IceComponentHandler extends ComponentHandler {
             tag.getNamespace().equals("http://www.icesoft.com/icefaces/component") )
         {
             if( tag.getLocalName().equals("inputFile") ) {
-                m.addRule( new MethodRule("progressListener", null, new Class[] {EventObject.class}) );
+                try { 
+                m.addRule( new MethodRule("progressListener", null, new Class[] {
+                        Class.forName("java.util.EventObject" ) }) );
+                } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("outputChart") ) {
-                m.addRule( new MethodRule("renderOnSubmit", Boolean.TYPE, new Class[] {OutputChart.class}) );
+                try {
+                    m.addRule( new MethodRule("renderOnSubmit", Boolean.TYPE,
+                                              new Class[] {Class.forName( "com.icesoft.faces.component.outputchart.OutputChart")
+                                              }) );
+                 } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("panelGroup") ) {
-                m.addRule( new MethodRule("dragListener", null, new Class[] {DragEvent.class}) );
-                m.addRule( new MethodRule("dropListener", null, new Class[] {DropEvent.class}) );
+                try {
+                m.addRule( new MethodRule("dragListener", null, new Class[] {
+                        Class.forName("com.icesoft.faces.component.dragdrop.DragEvent") }) );
+                m.addRule( new MethodRule("dropListener", null, new Class[] {
+                        Class.forName("com.icesoft.faces.component.dragdrop.DropEvent") }) );
+             } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("panelPositioned") ) {
-                m.addRule( new MethodRule("listener", null, new Class[] {PanelPositionedEvent.class}) );
+                try { 
+                m.addRule( new MethodRule("listener", null, new Class[] {
+                        Class.forName("com.icesoft.faces.component.panelpositioned.PanelPositionedEvent")}) );
+         } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("panelTabSet") ) {
-                m.addRule( new MethodRule("tabChangeListener", null, new Class[] {TabChangeEvent.class}) );
+                try {
+                    m.addRule( new MethodRule("tabChangeListener", null, new Class[] {
+                            Class.forName("com.icesoft.faces.component.paneltabset.TabChangeEvent") }) );
+                } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("rowSelector") ) {
-                m.addRule( new MethodRule("selectionListener", null, new Class[] {RowSelectorEvent.class}) );
-                m.addRule( new MethodRule("selectionAction", null, new Class[0]) );
-                m.addRule( new MethodRule("clickListener", null, new Class[] {ClickActionEvent.class}) );
-                m.addRule( new MethodRule("clickAction", null, new Class[0]) );
+                try {
+                    m.addRule( new MethodRule("selectionListener", null, new Class[] {
+                            Class.forName("com.icesoft.faces.component.ext.RowSelectorEvent") }) );
+                    m.addRule( new MethodRule("selectionAction", null, new Class[0]) );
+                    m.addRule( new MethodRule("clickListener", null, new Class[] {
+                            Class.forName("com.icesoft.faces.component.ext.ClickActionEvent") }) );
+                    m.addRule( new MethodRule("clickAction", null, new Class[0]) );
+                } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("panelTooltip") ) {
-                m.addRule( new MethodRule("displayListener", null, new Class[] {DisplayEvent.class}) );
+                try {
+                    m.addRule( new MethodRule("displayListener", null, new Class[] {
+                            Class.forName( "com.icesoft.faces.component.DisplayEvent" )}) );
+                } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("menuPopup") ) {
-                m.addRule( new MethodRule("displayListener", null, new Class[] {DisplayEvent.class}) );
+                try {
+                    m.addRule( new MethodRule("displayListener", null, new Class[] {
+                            Class.forName("com.icesoft.faces.component.DisplayEvent") }) );
+                } catch (ClassNotFoundException nfe) {}
             }
             else if( tag.getLocalName().equals("selectInputText") ) {
-                m.addRule( new MethodRule("textChangeListener", null, new Class[] {TextChangeEvent.class}) );
+                try {
+                    m.addRule( new MethodRule("textChangeListener", null, new Class[] {
+                            Class.forName("com.icesoft.faces.component.selectinputtext.TextChangeEvent") }) );
+                } catch (ClassNotFoundException nfe) {}
             }
         }
         return m;
