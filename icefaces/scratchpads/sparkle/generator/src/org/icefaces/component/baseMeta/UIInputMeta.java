415e6bc5577ba0df8b726f67ec239da75bee9d30
greg.dick <greg.dick@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-10-01 20:45:50 +0000

    ICE-5984 Restored some properties overenthusiastically removed

--- icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/baseMeta/UIInputMeta.java
+++ icefaces/scratchpads/sparkle/generator/src/org/icefaces/component/baseMeta/UIInputMeta.java
@@ -12,6 +12,15 @@ public class UIInputMeta extends UIOutputMeta {
                                             "for this component. Default value must be false.")
     private boolean required;
 
+
+    @Property( tlddoc="If there has been a call to setRequiredMessage() on this instance, return the message.  Otherwise, call (getValueExpression() )" +
+                      " passing the key 'requiredMessage', get the result of the expression, and return it")
+    private String requiredMessage;
+
+    @Property(  tlddoc="If there has been a call to setValidatorMessage() on this instance, return the message.  Otherwise," +
+                       " call getValueExpression() passing the key 'validatorMessage', get the result of the expression, and return it")
+    private String validatorMessage;
+
     @Property(defaultValue="false",  tlddoc="A flag indicating that conversion and validation of this component’s value should occur during Apply" +
                                             "Request Values phase instead of Process Validations phase")
     private boolean immediate;
