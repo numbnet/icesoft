896c9257747ac4962e7117d4ae71ecce1fccabfa
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-10-07 14:33:36 +0000

    ICE-5841 remove image property as should be done with skinning

--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonMeta.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonMeta.java
@@ -23,7 +23,6 @@ import org.icefaces.component.baseMeta.UICommandMeta;
         )
 		
 @ResourceDependencies({
-    @ResourceDependency(name = "sam/button/button.css", library = "org.icefaces.component.sprites"),
     @ResourceDependency(name="yui/yui-min.js",library="yui/3_1_1"),    
     @ResourceDependency(library = "yui/2_8_1", name = "yahoo-dom-event/yahoo-dom-event.js"),
     @ResourceDependency(library = "yui/2_8_1", name = "element/element-min.js"),
--- icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonRenderer.java
+++ icefaces/scratchpads/sparkle/component/src/org/icefaces/component/pushbutton/PushButtonRenderer.java
@@ -19,7 +19,8 @@ import org.icefaces.util.EnvUtils;
 public class PushButtonRenderer extends Renderer {
 
     public void decode(FacesContext facesContext, UIComponent uiComponent) {
-        Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
+      if (uiComponent.isRendered()){ 
+    	Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
         if (requestParameterMap.containsKey("ice.event.captured")) {
             PushButton pushButton = (PushButton) uiComponent;
             String source = String.valueOf(requestParameterMap.get("ice.event.captured"));
@@ -27,22 +28,19 @@ public class PushButtonRenderer extends Renderer {
        	    System.out.println("PBR:decode() button pressed is "+source+" for clientId="+clientId);
              if (clientId.equals(source)) { //won't always be the same ??
                 try {
-             	   //do I need to check to see if it is disabled first?
-             	   //ActionSource2 has a list of ActionListeners available
-             	   //will that change this piece of code?
              	   if (!pushButton.isDisabled()){
                         uiComponent.queueEvent(new ActionEvent (uiComponent));
 
              	   }
                 } catch (Exception e) {}
              }
-//            else System.out.println("PBR: clientId!=source");
         }
+      }
     }
 
     public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
     throws IOException {
-    	
+      if (uiComponent.isRendered()){	
         ResponseWriter writer = facesContext.getResponseWriter();
         String clientId = uiComponent.getClientId(facesContext);
         PushButton pushButton = (PushButton) uiComponent;
@@ -65,23 +63,15 @@ public class PushButtonRenderer extends Renderer {
 		writer.writeAttribute(HTML.TYPE_ATTR, "button", null);
 		writer.writeAttribute(HTML.NAME_ATTR, clientId+"_button", null);
 		writer.writeAttribute(HTML.ID_ATTR, clientId+"_button", null);		
-		// if there's an image, render label manually, don't rely on YUI, since it'd override button's contents
-		if (pushButton.getImage() == null) {
-			writer.startElement(HTML.SPAN_ELEM, uiComponent);
-			writer.write(pushButton.getLabel());
-			writer.endElement(HTML.SPAN_ELEM);
-		}else {	
-			writer.startElement(HTML.SPAN_ELEM, uiComponent);
-			writer.writeAttribute(HTML.ID_ATTR, clientId+"_btn_img", null);
-			writer.startElement(HTML.IMG_ELEM, uiComponent);
-			writer.writeAttribute(HTML.SRC_ATTR, pushButton.getImage(), null);
-			writer.endElement(HTML.IMG_ELEM);
-			writer.endElement(HTML.SPAN_ELEM);
-		}
+		writer.startElement(HTML.SPAN_ELEM, uiComponent);
+		writer.write(pushButton.getLabel());
+		writer.endElement(HTML.SPAN_ELEM);		
+      }
     }
     
     public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
     throws IOException {
+      if (uiComponent.isRendered()){
         ResponseWriter writer = facesContext.getResponseWriter();
 		String clientId = uiComponent.getClientId(facesContext);
 		PushButton pushButton = (PushButton) uiComponent;
@@ -91,23 +81,18 @@ public class PushButtonRenderer extends Renderer {
 		 
 		// js call using JSONBuilder utility ICE-5831 and ScriptWriter ICE-5830
 	    String label = "";
-	    String ifImage = "";
+	    String builder = "";
 	    String valueString = String.valueOf(pushButton.getValue());
 	    if (null!=pushButton.getLabel())label=pushButton.getLabel();
 	    else if (label.equals(""))
 	    	label=String.valueOf(pushButton.getValue());
 	   // need to worry if label isn't set?
-	    if (pushButton.getImage()==null){
-	        ifImage = JSONBuilder.create().beginMap().
-	        entry("type", "button").
-	        entry("label", label).endMap().toString();
-	    }
-	    else {
-	        ifImage = JSONBuilder.create().beginMap().
-	        entry("type", "button").endMap().toString();  
-	    }
+	    builder = JSONBuilder.create().beginMap().
+	    entry("type", "button").
+	    entry("label", label).endMap().toString();
+
 	    String params = "'" + clientId + "'," +
-        ifImage
+        builder
            + "," +
            JSONBuilder.create().
            beginMap().
@@ -123,4 +108,5 @@ public class PushButtonRenderer extends Renderer {
             
         writer.endElement(HTML.DIV_ELEM);
     }
+  }
 }
