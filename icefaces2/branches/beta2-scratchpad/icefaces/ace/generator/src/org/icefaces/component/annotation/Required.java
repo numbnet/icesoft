1a126ffe0607abbdbab6db2f351b613dd9362577
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-10-28 15:58:50 +0000

    Removing temporary scratchpad branch.

--- icefaces2/branches/beta2-scratchpad/icefaces/ace/generator/src/org/icefaces/component/annotation/Required.java
+++
@@ -1,9 +0,0 @@
-package org.icefaces.component.annotation;
-
-public enum Required {
-	REQUIRED,
-	OPTIONAL,
-	UNSET;
-	
-	public static final Required DEFAULT = Required.OPTIONAL;
-}
--- icefaces2/branches/beta2-scratchpad/icefaces/ace/generator/src/org/icefaces/component/baseMeta/UISelectManyMeta.java
+++
@@ -1,9 +0,0 @@
-package org.icefaces.component.baseMeta;
-
-
-/**
- * These are the properties for javax.faces.component.UISelectMany
- */
-public class UISelectManyMeta extends UIInputMeta {
-    //TODO selectedValues is an alias for value
-}
