1ad0c0f94f4c5b58cbdc93154278b4dfca878409
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-11-10 19:08:41 +0000

    ICE-4951 - Added ICEpushplace example using ICEfaces 2.0.

---
+++ icepush/trunk/integration/icefaces2/samples/icepushplace-icefaces2/src/org/icepush/integration/icepushplace/service/PersonService.java
@@ -0,0 +1,7 @@
+package org.icepush.integration.icepushplace.service;
+
+import org.icepush.ws.samples.icepushplace.PersonType;
+
+public interface PersonService {
+	public PersonType loginPerson(PersonType person);
+}
