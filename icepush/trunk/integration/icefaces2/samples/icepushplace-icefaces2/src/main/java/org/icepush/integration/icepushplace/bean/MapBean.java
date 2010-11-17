28d3199765acd600c518337723fd8534aed1d779
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-11-17 18:15:33 +0000

    ICE-4951 - Added a few more tidbits of user information as 'title' attributes to their floating popup

--- icepush/trunk/integration/icefaces2/samples/icepushplace-icefaces2/src/main/java/org/icepush/integration/icepushplace/bean/MapBean.java
+++ icepush/trunk/integration/icefaces2/samples/icepushplace-icefaces2/src/main/java/org/icepush/integration/icepushplace/bean/MapBean.java
@@ -4,9 +4,11 @@ import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
 
+import java.io.Serializable;
+
 @ManagedBean(name="mapBean")
 @CustomScoped(value = "#{window}")
-public class MapBean {
+public class MapBean implements Serializable {
     private boolean consolePopupVisible = true;
     
     public boolean getConsolePopupVisible() {
