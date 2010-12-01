7e303bca8327a331eb3d8761d866613a23f7b05d
adnan.durrani <adnan.durrani@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-01 03:55:49 +0000

    Broken build (Missing renderer changes added)

--- icefaces2/trunk/icefaces/ace/component/src/org/icefaces/component/sliderentry/SliderEntry.java
+++ icefaces2/trunk/icefaces/ace/component/src/org/icefaces/component/sliderentry/SliderEntry.java
@@ -59,16 +59,6 @@ public class SliderEntry extends SliderEntryBase{
         super.queueEvent(event);
     } 
 
-    public String getSubmitOn() {
-if (true) throw new UnsupportedOperationException();
-        return "undefined";
-    }
-
-    public String getSlideInterval() {
-if (true) throw new UnsupportedOperationException();
-        return "undefined";
-    }
-
     
     public String getThumbUrl() {
     	String thumbUrl = super.getThumbUrl();
