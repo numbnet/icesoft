1fe3240f19da1750e4954f5c3802527466acbca2
patrick.corless <patrick.corless@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-16 15:56:48 +0000

    ICE-6388 changed SUCCESS navigation string from "success" to null to avoid navigation errors in jsf 2 when Project_stage=Development.

--- icefaces2/trunk/icefaces/samples/compat/auctionMonitor/src/main/java/com/icesoft/applications/faces/auctionMonitor/beans/AuctionBean.java
+++ icefaces2/trunk/icefaces/samples/compat/auctionMonitor/src/main/java/com/icesoft/applications/faces/auctionMonitor/beans/AuctionBean.java
@@ -48,7 +48,7 @@ public class AuctionBean implements Serializable  {
     private static Log log = LogFactory.getLog(AuctionBean.class);
     private static int userCount = 0;
     public static final String RENDERER_NAME = "demand";
-    private static final String SUCCESS = "success";
+    private static final String SUCCESS = null;
     private String queryItemID;
     private String queryString;
     private AuctionMonitorItemBean[] searchItemBeans;
