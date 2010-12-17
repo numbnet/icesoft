3196a8fcd9e8555dd69db3408303357f174796cb
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-17 21:46:53 +0000

    Repo branch for ICE-6403 work.

---
+++ icefaces2/branches/ICE-6403/icefaces/ace/test/src/org/icefaces/UIDataText/StyleHolder.java
@@ -0,0 +1,26 @@
+package org.icefaces.UIDataText;
+
+
+/**
+ *
+ */
+public class StyleHolder {
+
+    private String style;
+
+    public StyleHolder(String style) {
+        this.style = style;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String toString() {
+        return style;
+    } 
+}
