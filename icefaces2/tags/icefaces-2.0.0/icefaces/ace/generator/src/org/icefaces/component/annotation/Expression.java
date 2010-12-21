d8450b102cd57010e2c377360f06541b16b358be
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-21 02:12:16 +0000

    Moving tag.

--- icefaces2/tags/icefaces-2.0.0/icefaces/ace/generator/src/org/icefaces/component/annotation/Expression.java
+++
@@ -1,9 +0,0 @@
-package org.icefaces.component.annotation;
-
-public enum Expression {
-	METHOD_EXPRESSION,
-	VALUE_EXPRESSION,
-	UNSET;
-	
-	public static final Expression DEFAULT = Expression.VALUE_EXPRESSION;
-}
