a73769472ef6aaad3e0dca3728c7ac24e3e34d82
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-21 02:09:42 +0000

    ICEfaces 2.0.0 (final) official release tag.

---
+++ icefaces2/tags/icefaces-2.0.0-final/icefaces/ace/test/src/org/icefaces/calendar/CalendarBean.java
@@ -0,0 +1,65 @@
+package org.icefaces.calendar;
+
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+@ManagedBean
+@SessionScoped
+public class CalendarBean implements Serializable {
+    private Date selectedDate;
+    private boolean renderAsPopup;
+    private boolean renderInputField;
+    private boolean singleSubmit = false;
+    private String pattern = "MMM/dd/yyyy hh:mm a";
+
+//    public CalendarBean() throws ParseException {
+    public CalendarBean() {
+//        selectedDate = new SimpleDateFormat("yyyy-M-d H:m z").parse("2008-4-30 13:9 Pacific Daylight Time");
+    }
+
+    public Date getSelectedDate() {
+        return selectedDate;
+    }
+
+    public void setSelectedDate(Date selectedDate) {
+        this.selectedDate = selectedDate;
+    }
+
+    public boolean isRenderAsPopup() {
+        return renderAsPopup;
+    }
+
+    public void setRenderAsPopup(boolean renderAsPopup) {
+        this.renderAsPopup = renderAsPopup;
+    }
+
+    public boolean isRenderInputField() {
+        return renderInputField;
+    }
+
+    public void setRenderInputField(boolean renderInputField) {
+        this.renderInputField = renderInputField;
+    }
+
+    public boolean isSingleSubmit() {
+        return singleSubmit;
+    }
+
+    public void setSingleSubmit(boolean singleSubmit) {
+        this.singleSubmit = singleSubmit;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+//        System.out.println("\nCalendarBean.setPattern");
+//        System.out.println("pattern = " + pattern);
+        this.pattern = pattern;
+    }
+}
---
+++ icefaces2/tags/icefaces-2.0.0-final/icefaces/samples/core/auction/src/main/java/org/icefaces/demo/auction/view/controllers/IntervalPushRenderer.java
@@ -0,0 +1,128 @@
+/*
+ * Version: MPL 1.1
+ *
+ * "The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2010 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ *
+ */
+package org.icefaces.demo.auction.view.controllers;
+
+import org.icefaces.application.PortableRenderer;
+import org.icefaces.application.PushRenderer;
+import org.icefaces.demo.auction.view.util.FacesUtils;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The interval render setup up a timer task that is responsible for initiating
+ * the server side push at set intervals. The interval is configurable with
+ * the context parameter org.icefaces.sample.auction.interval.
+ * <p/>
+ * The timer will shutdown its self when there server shuts down via @PreDestroy.
+ *
+ * @author ICEsoft Technologies Inc.
+ * @since 2.0
+ */
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class IntervalPushRenderer {
+
+    private static Logger log = Logger.getLogger(IntervalPushRenderer.class.getName());
+
+    public static final String INTERVAL_RENDER_GROUP = "auctionInterval";
+
+    private static int POLLING_INTERVAL = 1000;
+
+    static {
+        String interval = FacesUtils.getFacesParameter(
+                "org.icefaces.sample.auction.interval");
+        if (interval != null) {
+            try {
+                POLLING_INTERVAL = Integer.parseInt(interval);
+            } catch (NumberFormatException e) {
+                log.log(Level.WARNING, "Error applying org.icefaces.demo.auction.interval, " +
+                        "must be valid integer.", e);
+            }
+        }
+
+    }
+
+    private Timer intervalTimer;
+    private TimerTask renderTask;
+
+    /**
+     * Constructs an new instance of the intervalTimer and sets up the timer
+     * task.  The timer tasks main goal is to flush the service layer cache
+     * as well as initiate a server push the render group INTERVAL_RENDER_GROUP.
+     */
+    public IntervalPushRenderer() {
+
+        final PortableRenderer renderer = PushRenderer.getPortableRenderer(
+                FacesContext.getCurrentInstance());
+
+        renderTask = new TimerTask() {
+            public void run() {
+                try {
+                    // NOTE: this thread doesn't 
+
+                    // make the push call to everyone in the interval group
+                    renderer.render(INTERVAL_RENDER_GROUP);
+                    if (log.isLoggable(Level.FINEST)) {
+                        log.finest("Render done for 'auction' using " + intervalTimer);
+                    }
+                } catch (Throwable e) {
+                    log.log(Level.WARNING, "Error running interval timer task.", e);
+                }
+            }
+        };
+    }
+
+    /**
+     * Starts our interval timer task. Intended to keep running until this
+     * Application scoped bean is shutdown by the server.
+     */
+    @PostConstruct
+    public void initializeIntervalRender() {
+        if (null != intervalTimer) {
+            intervalTimer.cancel();
+        }
+        intervalTimer = new Timer(true);
+        intervalTimer.schedule(renderTask, 0, POLLING_INTERVAL);
+    }
+
+    /**
+     * Stops the interval timer, generally only for server shutdown.
+     */
+    @PreDestroy
+    public void cleanup() {
+        if (null != intervalTimer) {
+            intervalTimer.purge();
+            intervalTimer.cancel();
+        }
+        if (log.isLoggable(Level.FINEST)) {
+            log.finest("cleaning up " + intervalTimer);
+        }
+    }
+}
