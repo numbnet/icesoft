4201579cf72c10be185e085ffc20b79cedebf88d
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2010-12-21 02:03:14 +0000

    Cleaning up.

--- icefaces2/tags/trunktest/icefaces/ace/generator/src/org/icefaces/component/baseMeta/UISelectBooleanMeta.java
+++
@@ -1,8 +0,0 @@
-package org.icefaces.component.baseMeta;
-
-/**
- * These are the properties for javax.faces.component.UISelectBoolean
- */
-public class UISelectBooleanMeta extends UIInputMeta {
-    //TODO selected is an alias for value
-}
