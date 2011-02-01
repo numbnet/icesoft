e2d1f8aa2561b096a8c5724d9b916af22a4256e4
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-02-01 22:42:03 +0000

    Test branch creation.

---
+++ icefaces2/scratchpads/testing/icefaces/ace/component/src/org/icefaces/component/checkboxbutton/CheckboxButtonRenderer.java
@@ -0,0 +1,210 @@
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+package org.icefaces.component.checkboxbutton;
+
+
+
+import java.io.IOException;
+import java.util.*;
+
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIParameter;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.convert.ConverterException;
+import javax.faces.render.Renderer;
+import javax.faces.event.ValueChangeEvent;
+
+import org.icefaces.component.utils.HTML;
+import org.icefaces.component.utils.JSONBuilder;
+import org.icefaces.component.utils.ScriptWriter;
+import org.icefaces.component.utils.Utils;
+
+import org.icefaces.util.EnvUtils;
+import org.icefaces.render.MandatoryResourceComponent;
+
+@MandatoryResourceComponent("org.icefaces.component.checkboxbutton.CheckboxButton")
+public class CheckboxButtonRenderer extends Renderer {
+
+     List <UIParameter> uiParamChildren;
+
+	public CheckboxButtonRenderer(){
+		super();
+	}
+
+    public void decode(FacesContext facesContext, UIComponent uiComponent) {
+	        Map requestParameterMap = facesContext.getExternalContext().getRequestParameterMap();
+	        CheckboxButton checkbox = (CheckboxButton) uiComponent;
+	        String source = String.valueOf(requestParameterMap.get("ice.event.captured"));
+	        String clientId = uiComponent.getClientId();
+	        //update with hidden field
+		    String hiddenValue = String.valueOf(requestParameterMap.get(clientId+"_hidden"));
+			boolean submittedValue = isChecked(hiddenValue);
+			checkbox.setSubmittedValue(submittedValue);           	
+    }
+
+
+	public void encodeBegin(FacesContext facesContext, UIComponent uiComponent)
+    throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+        String clientId = uiComponent.getClientId(facesContext);
+        CheckboxButton checkbox = (CheckboxButton) uiComponent;
+
+         // capture any children UIParameter (f:param) parameters.
+        uiParamChildren = Utils.captureParameters( checkbox );
+
+		// root element
+        writer.startElement(HTML.DIV_ELEM, uiComponent);
+        writer.writeAttribute(HTML.ID_ATTR, clientId, null);
+		writer.writeAttribute(HTML.CLASS_ATTR, "ice-checkboxbutton", null);
+        
+		writer.startElement(HTML.SPAN_ELEM, uiComponent);
+        writer.writeAttribute(HTML.ID_ATTR, clientId+"_span", null);      
+        String yuiBaseClass= "yui-button yui-min yui-checkboxButton-button";
+        Object styleClass = checkbox.getStyleClass();
+        if (null!=styleClass){
+             yuiBaseClass +=  " " + String.valueOf(styleClass);
+        }
+		writer.writeAttribute(HTML.STYLE_CLASS_ATTR, yuiBaseClass, null);
+
+        String style = checkbox.getStyle();
+        if (style != null && style.trim().length() > 0) {
+            writer.writeAttribute(HTML.STYLE_ATTR, style, HTML.STYLE_ATTR);
+        }
+		// first child
+		writer.startElement(HTML.SPAN_ELEM, uiComponent);
+		writer.writeAttribute(HTML.CLASS_ATTR, "first-child", null);
+	 	writer.writeAttribute(HTML.ID_ATTR, clientId+"_s2", null);
+	 	//labelling  should be label images are skinned
+
+		String label=this.findCheckboxLabel(checkbox);
+
+			
+			// should be either a label or an image as a minimum
+/*
+			writer.startElement(HTML.LABEL_ELEM, uiComponent);
+    		writer.writeAttribute(HTML.FOR_ATTR, clientId+"_span-button", null );
+            writer.writeText(label,null);
+	   		writer.endElement("label");		*/
+	
+	 	
+		// button element
+		writer.startElement(HTML.BUTTON_ELEM, uiComponent);
+		writer.writeAttribute(HTML.TYPE_ATTR, "button", null);
+		writer.writeAttribute(HTML.NAME_ATTR, clientId+"_button", null);
+		writer.writeAttribute(HTML.ID_ATTR, clientId+"_button", null);
+    }
+    
+    public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
+    throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+		String clientId = uiComponent.getClientId(facesContext);
+		CheckboxButton checkbox = (CheckboxButton) uiComponent;
+ 		String label=this.findCheckboxLabel(checkbox);
+
+        writer.endElement(HTML.BUTTON_ELEM);
+		writer.endElement(HTML.SPAN_ELEM);  
+	    
+		writer.endElement(HTML.SPAN_ELEM);			
+		//hidden input for single submit=false
+		
+	    writer.startElement("input", uiComponent);
+	    writer.writeAttribute("type", "hidden", null);
+	    writer.writeAttribute("name",clientId+"_hidden", null);
+	    writer.writeAttribute("id",clientId+"_hidden", null);
+	    writer.writeAttribute("value",checkbox.getValue(), null);
+	    writer.endElement("input");	
+     
+		// js call using JSONBuilder utility ICE-5831 and ScriptWriter ICE-5830
+	    //note that ScriptWriter takes care of the span tag surrounding the script
+	    String boxValue = String.valueOf(checkbox.getValue());
+	    boolean isChecked = this.isChecked(boxValue);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(checkbox.getStyle()).
+           append(checkbox.getStyleClass());
+
+        String builder="";
+	    JSONBuilder.create().beginMap().entry("nothing", label).toString();
+
+	        builder = JSONBuilder.create().beginMap().
+	        entry("type", "checkbox").
+	        entry("checked", isChecked).
+            entry("disabled", checkbox.isDisabled()).
+            entry("tabindex", checkbox.getTabindex()).
+	        entry("label", label).endMap().toString();
+	
+
+        JSONBuilder jBuild = JSONBuilder.create().
+           beginMap().
+               entry("singleSubmit", checkbox.isSingleSubmit()).
+               entry("hashCode", sb.toString().hashCode()).
+               entry("ariaEnabled", EnvUtils.isAriaEnabled(facesContext));
+
+        if (uiParamChildren != null) {
+            jBuild.entry("postParameters",  Utils.asStringArray(uiParamChildren) );
+        } 
+
+	    String params = "'" + clientId + "'," +
+        builder
+           + "," + jBuild.endMap().toString();
+  //        System.out.println("params = " + params);	    
+
+        String finalScript = "ice.component.checkboxbutton.updateProperties(" + params + ");";
+        ScriptWriter.insertScript(facesContext, uiComponent,finalScript);        		  
+        
+       writer.endElement(HTML.DIV_ELEM);
+    }
+    
+    private String findCheckboxLabel(CheckboxButton checkbox){
+    	String label="";
+        String checkLabel = checkbox.getLabel();
+		if (null!=checkLabel && !checkLabel.equals("")){
+			label=checkLabel;
+		}
+		return label;
+    }
+ 
+ 
+    /**
+     * support similar return values as jsf component
+     * so can use strings true/false, on/off, yes/no to
+     * support older browsers
+     * @param hiddenValue
+     * @return
+     */
+    private boolean isChecked(String hiddenValue) {
+		return hiddenValue.equalsIgnoreCase("on") ||
+		       hiddenValue.equalsIgnoreCase("yes") ||
+		       hiddenValue.equalsIgnoreCase("true");
+	}
+    
+    //forced converter support. It's either a boolean or string.   
+    @Override
+    public Object getConvertedValue(FacesContext facesContext, UIComponent uiComponent,
+    		                        Object submittedValue) throws ConverterException{
+    	if (submittedValue instanceof Boolean)return submittedValue;
+    	else return Boolean.valueOf(submittedValue.toString());
+    }
+    
+}
---
+++ icefaces2/scratchpads/testing/icefaces/core/src/main/javascript/blockui.js
@@ -0,0 +1,104 @@
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+(function() {
+    var off = operator();
+
+    function Overlay(element) {
+        var container = element || document.body;
+        var overlay = container.ownerDocument.createElement('iframe');
+        overlay.setAttribute('src', 'about:blank');
+        overlay.setAttribute('frameborder', '0');
+        var overlayStyle = overlay.style;
+        overlayStyle.position = 'absolute';
+        overlayStyle.backgroundColor = 'white';
+        overlayStyle.zIndex = '28000';
+        overlayStyle.top = '0';
+        overlayStyle.left = '0';
+        overlayStyle.opacity = 0.22;
+        overlayStyle.filter = 'alpha(opacity=22)';
+
+        if (container.tagName.toLowerCase() == 'body') {
+            overlayStyle.width = Math.max(document.documentElement.scrollWidth, document.body.scrollWidth) + 'px';
+            overlayStyle.height = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight) + 'px';
+        } else {
+            overlayStyle.width = container.offsetWidth + 'px';
+            overlayStyle.height = container.offsetHeight + 'px';
+        }
+
+        container.appendChild(overlay);
+
+        return object(function(method) {
+            method(off, function(self) {
+                if (overlay) container.removeChild(overlay);
+            });
+        });
+    }
+
+    //in the future this function can be modified to read configuration set per component container element 
+    function isBlockUIEnabled(source) {
+        return configurationOf(source).blockUIOnSubmit;
+    }
+
+    var stopBlockingUI = noop;
+    namespace.onBeforeSubmit(function(source) {
+        //don't block UI for the retrieveUpdate requests
+        if (viewIDOf(source) != source.id && isBlockUIEnabled(source)) {
+            debug(logger, 'blocking UI');
+            var blockUIOverlay = Overlay();
+            var rollbacks = inject(['input', 'select', 'textarea', 'button', 'a'], [], function(result, type) {
+                return concatenate(result, asArray(collect(document.body.getElementsByTagName(type), function(e) {
+                    var onkeypress = e.onkeypress;
+                    var onkeyup = e.onkeyup;
+                    var onkeydown = e.onkeydown;
+                    var onclick = e.onclick;
+                    e.onkeypress = none;
+                    e.onkeyup = none;
+                    e.onkeydown = none;
+                    e.onclick = none;
+
+                    return function() {
+                        try {
+                            e.onkeypress = onkeypress;
+                            e.onkeyup = onkeyup;
+                            e.onkeydown = onkeydown;
+                            e.onclick = onclick;
+                        } catch (ex) {
+                            //don't fail if element is not present anymore
+                        }
+                    };
+                })));
+            });
+
+            stopBlockingUI = function() {
+                broadcast(rollbacks);
+                off(blockUIOverlay);
+                debug(logger, 'unblocked UI');
+            };
+        } else {
+            stopBlockingUI = noop;
+        }
+    });
+
+    namespace.onAfterUpdate(function() {
+        stopBlockingUI();
+    });
+})();
