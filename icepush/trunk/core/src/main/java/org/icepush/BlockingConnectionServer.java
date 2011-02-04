08ca749727dcd1ab5808bbdc3703abb563f891e4
deryk.sinotte <deryk.sinotte@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-02-04 17:07:16 +0000

    MOBI-1: undoing work to support MOBI as it's on a separate branch

--- icepush/trunk/core/src/main/java/org/icepush/BlockingConnectionServer.java
+++ icepush/trunk/core/src/main/java/org/icepush/BlockingConnectionServer.java
@@ -39,6 +39,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BlockingConnectionServer extends TimerTask implements Server, Observer {
+    private static final long initialHeartbeat = 5000;
+    private static final long minHeartbeat = 1000;
+    private static final long maxHeartbeat = 20000;
     private static final Logger log = Logger.getLogger(BlockingConnectionServer.class.getName());
     private static final ResponseHandler CloseResponse = new CloseConnectionResponseHandler();
     //Define here to avoid classloading problems after application exit
@@ -47,9 +50,6 @@ public class BlockingConnectionServer extends TimerTask implements Server, Obser
 
     private final BlockingQueue pendingRequest = new LinkedBlockingQueue(1);
     private long heartbeat;
-    private long minHeartbeat = 5000;
-    private long maxHeartbeat = 45000;
-
     private long heartbeatUpdateTime = System.currentTimeMillis();
     private long responseTimeoutTime;
     private Server activeServer;
@@ -62,10 +62,7 @@ public class BlockingConnectionServer extends TimerTask implements Server, Obser
     private int sequenceNo = 0;
 
     public BlockingConnectionServer(final PushGroupManager pushGroupManager, final Timer monitorRunner, final Configuration configuration, final boolean terminateBlockingConnectionOnShutdown) {
-        this.heartbeat = configuration.getAttributeAsLong("heartbeatTimeout", 15000);
-        this.minHeartbeat = configuration.getAttributeAsLong("minHeartbeatInterval", heartbeat / 3);
-        this.maxHeartbeat = configuration.getAttributeAsLong("maxHeartbeatInterval", Math.round(3 * heartbeat));
-
+        this.heartbeat = configuration.getAttributeAsLong("heartbeatTimeout", initialHeartbeat);
         this.pushGroupManager = pushGroupManager;
         //add monitor
         monitorRunner.scheduleAtFixedRate(this, 0, 1000);
@@ -185,8 +182,6 @@ public class BlockingConnectionServer extends TimerTask implements Server, Obser
     }
 
     private class AdjustHeartbeat implements ResponseHandler {
-        private static final double IntervalExpansionFactor = 1.1;
-        private static final double IntervalReductionFactor = 0.75;
         private final Request request;
         private final ResponseHandler handler;
 
@@ -209,11 +204,11 @@ public class BlockingConnectionServer extends TimerTask implements Server, Obser
                 //increase interval since heartbeat still runs great
                 //increase interval only after the previous interval has elapsed to avoid increasing the value too rapidly
                 if (updateHeartbeat()) {
-                    heartbeat = Math.min(maxHeartbeat, Math.round(heartbeat * IntervalExpansionFactor));
+                    heartbeat = Math.min(maxHeartbeat, Math.round(heartbeat * 1.1));
                 }
             } else {
                 //decrease interval since heartbeat was lost
-                heartbeat = Math.max(minHeartbeat, Math.round(heartbeat * IntervalReductionFactor));
+                heartbeat = Math.max(minHeartbeat, Math.round(heartbeat * 0.75));
             }
             ++sequenceNo;
 
