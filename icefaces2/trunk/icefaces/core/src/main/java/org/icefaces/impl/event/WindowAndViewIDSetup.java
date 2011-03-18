8a85784a2c52247e987894b07e731116f66d65fc
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-03-18 20:49:32 +0000

    reverting due to regression (ICE-6643)

--- icefaces2/trunk/icefaces/core/src/main/java/org/icefaces/impl/event/WindowAndViewIDSetup.java
+++ icefaces2/trunk/icefaces/core/src/main/java/org/icefaces/impl/event/WindowAndViewIDSetup.java
@@ -30,7 +30,6 @@ import javax.faces.component.html.HtmlForm;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.event.AbortProcessingException;
-import javax.faces.event.PhaseId;
 import javax.faces.event.PostAddToViewEvent;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
@@ -51,13 +50,6 @@ public class WindowAndViewIDSetup implements SystemEventListener {
     public void processEvent(SystemEvent event) throws AbortProcessingException {
         final FacesContext context = FacesContext.getCurrentInstance();
         if (!EnvUtils.isICEfacesView(context)) {
-            return;
-        }
-
-        if( context.getCurrentPhaseId() != PhaseId.RENDER_RESPONSE ){
-            //ICE-6643: In certain circumstances, it's possible that the PostAddToViewEvent is
-            //          triggered more than once in the same lifecycle.  We only want it added
-            //          during the render phase.
             return;
         }
 
