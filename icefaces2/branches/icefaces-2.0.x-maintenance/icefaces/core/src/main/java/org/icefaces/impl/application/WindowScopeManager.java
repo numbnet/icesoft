5913be0d3240844e936073c3544d3b812745b450
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-04-12 14:50:41 +0000

    Fixed JIRA ICE-6784 : WindowScopeManager's 'dummy-bean' value is not Serializable

--- icefaces2/branches/icefaces-2.0.x-maintenance/icefaces/core/src/main/java/org/icefaces/impl/application/WindowScopeManager.java
+++ icefaces2/branches/icefaces-2.0.x-maintenance/icefaces/core/src/main/java/org/icefaces/impl/application/WindowScopeManager.java
@@ -43,6 +43,7 @@ import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
