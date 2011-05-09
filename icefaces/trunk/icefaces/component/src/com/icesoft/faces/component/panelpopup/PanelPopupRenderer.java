3df567b6c4d82fe699586272b4cb59eff47ded56
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-05-09 16:48:36 +0000

    ICE-6690: Allow positionOnLoadOnly to disable popup auto centering when the page is scrolled.

--- icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopupRenderer.java
+++ icefaces/trunk/icefaces/component/src/com/icesoft/faces/component/panelpopup/PanelPopupRenderer.java
@@ -291,7 +291,7 @@ public class PanelPopupRenderer extends GroupRenderer {
                 String x = positions.substring(0, positions.indexOf(','));
                 String y = positions.substring(positions.indexOf(',') + 1);
                 autoPositionJS = "Ice.autoPosition.start('" + clientId + "'," + x
-                        + "," + y + ");";
+                        + "," + y + "," + positionOnLoadOnly + ");";
             }
 
         } else {
@@ -303,8 +303,7 @@ public class PanelPopupRenderer extends GroupRenderer {
         boolean autoCentre = panelPopup.isAutoCentre();
         String centreJS;
         if (autoCentre && (!positionOnLoadOnly || (positionOnLoadOnly && !dragged))) {
-            centreJS = "Ice.autoCentre.start('" + clientId + "');";
-
+            centreJS = "Ice.autoCentre.start('" + clientId + "'," + positionOnLoadOnly + ");";
         } else {
             centreJS = "Ice.autoCentre.stop('" + clientId + "');";
         }
@@ -345,8 +344,9 @@ public class PanelPopupRenderer extends GroupRenderer {
                 }
 
                 String autoPosition = (String) uiComponent.getAttributes().get("autoPosition");
+                boolean positionOnLoadOnly = ((Boolean) uiComponent.getAttributes().get("positionOnLoadOnly")).booleanValue();
                 call = "Ice.modal.start('" + clientId + "', '" + iframeUrl
-                        + "', '" + trigger + "'," + "manual".equalsIgnoreCase(autoPosition) + ");" +
+                        + "', '" + trigger + "'," + "manual".equalsIgnoreCase(autoPosition) + "," + positionOnLoadOnly + ");" +
                         "Ice.registeredPopups = Ice.registeredPopups ? Ice.registeredPopups : [];" +
                         "if (!Ice.registeredPopups.include('" + clientId + "')) {Ice.registeredPopups.push('" + clientId + "');" +
                         "Ice.onAsynchronousReceive('" + clientId + "', function(){if (!document.getElementById('" + clientId + "')) Ice.modal.stop('" + clientId + "');});}";
