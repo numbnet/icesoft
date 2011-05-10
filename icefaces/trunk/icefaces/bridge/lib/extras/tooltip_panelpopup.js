8236a36ce2e4eda87410d9974978024b66f1b1b2
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-05-10 07:57:15 +0000

    ICE-6285 Added test that verifies if the position of the mouse is moved outside the tooltip enabled element, and only then act on the triggered 'mouseout' event.

--- icefaces/trunk/icefaces/bridge/lib/extras/tooltip_panelpopup.js
+++ icefaces/trunk/icefaces/bridge/lib/extras/tooltip_panelpopup.js
@@ -29,212 +29,217 @@
  * not delete the provisions above, a recipient may use your version of
  * this file under either the MPL or the LGPL License."
  */
- var visibleTooltipList = new Array();
- 
+var visibleTooltipList = new Array();
+
 ToolTipPanelPopup = Class.create({
-  initialize: function(srcComp, tooltipCompId, event, hideOn, delay, dynamic, formId, ctxValue, iFrameUrl, displayOn, moveWithMouse) {
-    //tooltip is not rendered 
-    if (!$(tooltipCompId)) return;
-    this.src = srcComp;
-    this.delay = delay || 500;
-    this.dynamic = (dynamic == "true");
-    this.tooltipCompId = tooltipCompId;
-    this.srcCompId = srcComp.id;
-    this.hideOn = hideOn;
-    this.x = Event.pointerX(event);
-    this.y = Event.pointerY(event);
-    this.formId = formId;
-    this.ctxValue = ctxValue
-    this.iFrameUrl = iFrameUrl;
-    this.moveWithMouse = moveWithMouse;
-    this.displayOn = displayOn;
-    this.event = event;
-    //cancel bubbling
-    event.cancelBubble = true;
-    //attach events
+    initialize: function(srcComp, tooltipCompId, event, hideOn, delay, dynamic, formId, ctxValue, iFrameUrl, displayOn, moveWithMouse) {
+        //tooltip is not rendered
+        if (!$(tooltipCompId)) return;
+        this.src = srcComp;
+        this.delay = delay || 500;
+        this.dynamic = (dynamic == "true");
+        this.tooltipCompId = tooltipCompId;
+        this.srcCompId = srcComp.id;
+        this.hideOn = hideOn;
+        this.x = Event.pointerX(event);
+        this.y = Event.pointerY(event);
+        this.formId = formId;
+        this.ctxValue = ctxValue
+        this.iFrameUrl = iFrameUrl;
+        this.moveWithMouse = moveWithMouse;
+        this.displayOn = displayOn;
+        this.event = event;
+        //cancel bubbling
+        event.cancelBubble = true;
+        //attach events
 
-    if (this.hideOn == "mousedown") {
-        this.hideEvent = this.hidePopupOnMouseClick.bindAsEventListener(this);
-    } else if (this.hideOn == "mouseout") {
-        this.hideEvent = this.hidePopupOnMouseOut.bindAsEventListener(this);
-    } else {
-        this.hideOn = "none";
-    }
-
-    this.eventMouseMove = this.updateCordinate.bindAsEventListener(this);
-    this.clearTimerEvent = this.clearTimer.bindAsEventListener(this);
-    Event.observe(document, "mouseout" , this.clearTimerEvent);
-    Event.observe(document, this.hideOn , this.hideEvent);
-    Event.observe(document, "mousemove", this.eventMouseMove);
-      if (displayOn == "hover") {
-          this.timer = setTimeout(this.showPopup.bind(this), parseInt(this.delay));
-      } else {
-          this.showPopup.bind(this)();
-          //ICE-5406 : outputLink does not work inside a panelTooltip with displayOn="click"
-          //Event.extend(event).stop();
-      }
-  },
-
-  showPopup: function() {
-    if (this.isTooltipVisible()) return;
-    if (this.dynamic) {
-         //its a dynamic tooltip, so remove all its childres
-        var tooltip = this.getTooltip();
-        if(tooltip) {
-	        tooltip.style.visibility = "hidden";        
-	        var table = tooltip.childNodes[0];
-	        if (table) {
-	            tooltip.removeChild(table);
-	        }
+        if (this.hideOn == "mousedown") {
+            this.hideEvent = this.hidePopupOnMouseClick.bindAsEventListener(this);
+        } else if (this.hideOn == "mouseout") {
+            this.hideEvent = this.hidePopupOnMouseOut.bindAsEventListener(this);
+        } else {
+            this.hideOn = "none";
         }
-    //dynamic? set status=show, populatefields, and submit
-      this.submit("show");
-      if (this.hideOn == "none") {
-        //reset the info
-        this.populateFields(true);
-      }
-    } else {
-        //static? just set the visibility= true 
-       var tooltip = this.getTooltip();
-        tooltip.style.visibility = "visible";
-        tooltip.style.position = "absolute" ;
-        tooltip.style.display = "";
-        var srcComp = $(this.srcCompId);
-        var cumulativeOffset = srcComp.cumulativeOffset();
-        var positionedOffset =  srcComp.positionedOffset();
-        tooltip.style.top = this.y - tooltip.offsetHeight - 4 - cumulativeOffset.top + positionedOffset.top + "px";
-        tooltip.style.left = this.x+4 - cumulativeOffset.left + positionedOffset.left +"px";
-        ToolTipPanelPopupUtil.adjustPosition(tooltip);
-        Ice.iFrameFix.start(this.tooltipCompId, this.iFrameUrl);
-    }
-    this.addToVisibleList(); 
-    //prohibits to open browser's context menu, as 'altclick' uses onmenucontext handler    
-    if (this.event && this.displayOn == 'altclick') {
-        Event.extend(this.event).stop();
-    }
-  },
 
-  hidePopupOnMouseOut: function(event) {
-    if (!this.isTooltipVisible()) return;
-    if (Position.within($(this.tooltipCompId), Event.pointerX(event), Event.pointerY(event))) return; //ICE-3521
-    this.hidePopup(event);
-    this.state = "hide";
-    this.populateFields();
-    if (this.hideOn == "mouseout") {
-        this.removedFromVisibleList();
-    }
-    this.dispose(event);
-  },
+        this.eventMouseMove = this.updateCordinate.bindAsEventListener(this);
+        this.clearTimerEvent = this.clearTimer.bindAsEventListener(this);
+        Event.observe(document, "mouseout", this.clearTimerEvent);
+        Event.observe(document, this.hideOn, this.hideEvent);
+        Event.observe(document, "mousemove", this.eventMouseMove);
+        if (displayOn == "hover") {
+            this.timer = setTimeout(this.showPopup.bind(this), parseInt(this.delay));
+        } else {
+            this.showPopup.bind(this)();
+            //ICE-5406 : outputLink does not work inside a panelTooltip with displayOn="click"
+            //Event.extend(event).stop();
+        }
+    },
 
-  hidePopupOnMouseClick: function(event) {
-    if (!this.isTooltipVisible() || !Event.isLeftClick(event)) return;
-    var eventSrc = Event.element(event);
-    if(this.srcOrchildOfSrcElement(eventSrc)) {
-        return;
-    } else {
+    showPopup: function() {
+        if (this.isTooltipVisible()) return;
+        if (this.dynamic) {
+            //its a dynamic tooltip, so remove all its childres
+            var tooltip = this.getTooltip();
+            if (tooltip) {
+                tooltip.style.visibility = "hidden";
+                var table = tooltip.childNodes[0];
+                if (table) {
+                    tooltip.removeChild(table);
+                }
+            }
+            //dynamic? set status=show, populatefields, and submit
+            this.submit("show");
+            if (this.hideOn == "none") {
+                //reset the info
+                this.populateFields(true);
+            }
+        } else {
+            //static? just set the visibility= true
+            var tooltip = this.getTooltip();
+            tooltip.style.visibility = "visible";
+            tooltip.style.position = "absolute";
+            tooltip.style.display = "";
+            var srcComp = $(this.srcCompId);
+            var cumulativeOffset = srcComp.cumulativeOffset();
+            var positionedOffset = srcComp.positionedOffset();
+            tooltip.style.top = this.y - tooltip.offsetHeight - 4 - cumulativeOffset.top + positionedOffset.top + "px";
+            tooltip.style.left = this.x + 4 - cumulativeOffset.left + positionedOffset.left + "px";
+            ToolTipPanelPopupUtil.adjustPosition(tooltip);
+            Ice.iFrameFix.start(this.tooltipCompId, this.iFrameUrl);
+        }
+        this.addToVisibleList();
+        //prohibits to open browser's context menu, as 'altclick' uses onmenucontext handler
+        if (this.event && this.displayOn == 'altclick') {
+            Event.extend(this.event).stop();
+        }
+    },
+
+    hidePopupOnMouseOut: function(event) {
+        if (!this.isTooltipVisible()) return;
+        var x = Event.pointerX(event);
+        var y = Event.pointerY(event);
+        if (Position.within($(this.tooltipCompId), x, y)) return; //ICE-3521
+        if (Position.within(this.src, x, y)) return; //ICE-6285
         this.hidePopup(event);
-    }
-    if (this.hideOn == "mousedown") {
-        this.removedFromVisibleList();
-    }
-    this.dispose(event);
-  },
+        this.state = "hide";
+        this.populateFields();
+        if (this.hideOn == "mouseout") {
+            this.removedFromVisibleList();
+        }
+        this.dispose(event);
+    },
+
+    hidePopupOnMouseClick: function(event) {
+        if (!this.isTooltipVisible() || !Event.isLeftClick(event)) return;
+        var eventSrc = Event.element(event);
+        if (this.srcOrchildOfSrcElement(eventSrc)) {
+            return;
+        } else {
+            this.hidePopup(event);
+        }
+        if (this.hideOn == "mousedown") {
+            this.removedFromVisibleList();
+        }
+        this.dispose(event);
+    },
 
 
- dispose: function(event) {
-    Event.stopObserving(document, this.hideOn, this.hideEvent);
-    Event.stopObserving(document, "mousemove", this.eventMouseMove);
+    dispose: function(event) {
+        Event.stopObserving(document, this.hideOn, this.hideEvent);
+        Event.stopObserving(document, "mousemove", this.eventMouseMove);
 
-   },
+    },
 
-  hidePopup:function(event) {
-    if(this.dynamic) {
-    //dynamic? set status=hide, populatefiels and submit 
-        this.submit("hide");
-    } else {
-        //static? set visibility = false;
-        tooltip =  this.getTooltip();
-        tooltip.style.visibility = "hidden";
-        tooltip.style.display = "none";
-    }
-  },
-  
-  
-  submit:function(state, event) {
-      if (!event) event = new Object();
-      this.state = state;
-      this.populateFields();
-      var element = $(this.srcCompId);
-      try {
-        var form = Ice.util.findForm(element);
-        iceSubmitPartial(form,element,event);
-      } catch (e) {logger.info("Form not found" + e);}
-  },
-  
-  clearTimer:function() {
-     //   $(action).innerHTML += "<br/> Clearing the event";
+    hidePopup:function(event) {
+        if (this.dynamic) {
+            //dynamic? set status=hide, populatefiels and submit
+            this.submit("hide");
+        } else {
+            //static? set visibility = false;
+            tooltip = this.getTooltip();
+            tooltip.style.visibility = "hidden";
+            tooltip.style.display = "none";
+        }
+    },
+
+
+    submit:function(state, event) {
+        if (!event) event = new Object();
+        this.state = state;
+        this.populateFields();
+        var element = $(this.srcCompId);
+        try {
+            var form = Ice.util.findForm(element);
+            iceSubmitPartial(form, element, event);
+        } catch (e) {
+            logger.info("Form not found" + e);
+        }
+    },
+
+    clearTimer:function() {
+        //   $(action).innerHTML += "<br/> Clearing the event";
         Event.stopObserving(document, "mouseout", this.clearTimerEvent);
         clearTimeout(this.timer);
 
-  },
-
-  updateCordinate: function(event) {
-    if (Event.element(event) != this.src && !Element.descendantOf(event.element(), this.src)) return;
-    this.x = Event.pointerX(event);
-    this.y = Event.pointerY(event);
-      if (!this.isTooltipVisible() || !this.moveWithMouse) return;
-      var tooltip = this.getTooltip();
-      tooltip.style.top = this.y - tooltip.offsetHeight - 4 + "px";
-      tooltip.style.left = this.x + 4 + "px";
-      ToolTipPanelPopupUtil.adjustPosition(tooltip);
-      Ice.iFrameFix.start(this.tooltipCompId, this.iFrameUrl);
-  },
-
-  srcOrchildOfSrcElement: function(ele) {
-     var tooltip =  this.getTooltip();
-     if (tooltip  == ele) return true;
-     while (ele.parentNode) {
-        ele = ele.parentNode;
-        if (tooltip  == ele){
-            return true;
-        }
-     }
-  },
-
-  getTooltip: function () {
-      return $(this.tooltipCompId);
-  },
-  
-  populateFields: function(reset) {
-  // the following field should be rendered by the panelPoupRenderer if rendered as tooltip
-
-
-	    var form = $(this.formId);
-	    if (form == null) return;
-	    var iceTooltipInfo = form.getElements().find( function(element) {
-	        if (element.id == "iceTooltipInfo") return element;
-	    });
-	    if (!iceTooltipInfo) { 
-		    iceTooltipInfo = document.createElement('input');
-		    iceTooltipInfo.id="iceTooltipInfo";
-		    iceTooltipInfo.name="iceTooltipInfo";            
-		    iceTooltipInfo.type="hidden";
-	        form.appendChild(iceTooltipInfo);
-	    }  else {
-	 
-	    }
-	    if (reset) {
-	       iceTooltipInfo.value = "";
-	    } else {
-	       iceTooltipInfo.value = "tooltip_id=" + this.tooltipCompId + 
-	                     "; tooltip_src_id="+ this.src.id+ 
-	                     "; tooltip_state="+ this.state +
-	                     "; tooltip_x="+ this.x +
-	                     "; tooltip_y="+ this.y +
-	                     "; cntxValue="+ this.ctxValue;
-	    }
     },
-    
+
+    updateCordinate: function(event) {
+        if (Event.element(event) != this.src && !Element.descendantOf(event.element(), this.src)) return;
+        this.x = Event.pointerX(event);
+        this.y = Event.pointerY(event);
+        if (!this.isTooltipVisible() || !this.moveWithMouse) return;
+        var tooltip = this.getTooltip();
+        tooltip.style.top = this.y - tooltip.offsetHeight - 4 + "px";
+        tooltip.style.left = this.x + 4 + "px";
+        ToolTipPanelPopupUtil.adjustPosition(tooltip);
+        Ice.iFrameFix.start(this.tooltipCompId, this.iFrameUrl);
+    },
+
+    srcOrchildOfSrcElement: function(ele) {
+        var tooltip = this.getTooltip();
+        if (tooltip == ele) return true;
+        while (ele.parentNode) {
+            ele = ele.parentNode;
+            if (tooltip == ele) {
+                return true;
+            }
+        }
+    },
+
+    getTooltip: function () {
+        return $(this.tooltipCompId);
+    },
+
+    populateFields: function(reset) {
+        // the following field should be rendered by the panelPoupRenderer if rendered as tooltip
+
+
+        var form = $(this.formId);
+        if (form == null) return;
+        var iceTooltipInfo = form.getElements().find(function(element) {
+            if (element.id == "iceTooltipInfo") return element;
+        });
+        if (!iceTooltipInfo) {
+            iceTooltipInfo = document.createElement('input');
+            iceTooltipInfo.id = "iceTooltipInfo";
+            iceTooltipInfo.name = "iceTooltipInfo";
+            iceTooltipInfo.type = "hidden";
+            form.appendChild(iceTooltipInfo);
+        } else {
+
+        }
+        if (reset) {
+            iceTooltipInfo.value = "";
+        } else {
+            iceTooltipInfo.value = "tooltip_id=" + this.tooltipCompId +
+                    "; tooltip_src_id=" + this.src.id +
+                    "; tooltip_state=" + this.state +
+                    "; tooltip_x=" + this.x +
+                    "; tooltip_y=" + this.y +
+                    "; cntxValue=" + this.ctxValue;
+        }
+    },
+
     addToVisibleList: function() {
         if (!this.isTooltipVisible()) {
             this.removedFromVisibleList('all');
@@ -242,35 +247,35 @@ ToolTipPanelPopup = Class.create({
         } else {
         }
     },
-    
+
     removedFromVisibleList: function(all) {
         if (this.isTooltipVisible() || all) {
-	        var newList = new Array();
-		    var index = -1;
-		    for (i=0; i < visibleTooltipList.length; i++) {
-		        if (visibleTooltipList[i].tooltipId != this.tooltipCompId) {
-		            index = parseInt(index)+ 1;
-		            newList[index] = visibleTooltipList[i];
-		        }else {
-		        }
-		    }
-		    visibleTooltipList = newList;
-		} else {
-		}
-    },
-    
-    isTooltipVisible: function(onlyTooltip) {
-        for (i=0; i < visibleTooltipList.length; i++) {
-            if (onlyTooltip) {
-                if (visibleTooltipList[i].tooltipId== this.tooltipCompId) {
-                    return true;
-                }             
-            } else {
-                if (visibleTooltipList[i].tooltipId== this.tooltipCompId && visibleTooltipList[i].srcCompId == this.srcCompId) {
-                    return true;
-                } 
+            var newList = new Array();
+            var index = -1;
+            for (i = 0; i < visibleTooltipList.length; i++) {
+                if (visibleTooltipList[i].tooltipId != this.tooltipCompId) {
+                    index = parseInt(index) + 1;
+                    newList[index] = visibleTooltipList[i];
+                } else {
+                }
             }
-  
+            visibleTooltipList = newList;
+        } else {
+        }
+    },
+
+    isTooltipVisible: function(onlyTooltip) {
+        for (i = 0; i < visibleTooltipList.length; i++) {
+            if (onlyTooltip) {
+                if (visibleTooltipList[i].tooltipId == this.tooltipCompId) {
+                    return true;
+                }
+            } else {
+                if (visibleTooltipList[i].tooltipId == this.tooltipCompId && visibleTooltipList[i].srcCompId == this.srcCompId) {
+                    return true;
+                }
+            }
+
         }
         return false;
     }
@@ -280,11 +285,11 @@ ToolTipPanelPopupUtil = {
     removeFromVisibleList:function(comp_id) {
         var newList = new Array();
         var index = -1;
-        for (i=0; i < visibleTooltipList.length; i++) {
+        for (i = 0; i < visibleTooltipList.length; i++) {
             if (visibleTooltipList[i].tooltipId != comp_id) {
-                index = parseInt(index)+ 1;
+                index = parseInt(index) + 1;
                 newList[index] = visibleTooltipList[i];
-            }else {
+            } else {
             }
         }
         visibleTooltipList = newList;
@@ -305,7 +310,7 @@ ToolTipPanelPopupUtil = {
             diff = - Math.min(diff, (elementOffsets.left - viewportScrollOffsets.left));
         }
         element.style.left = positionedOffset.left + diff + "px";
-        
+
         diff = 0;
         if (elementOffsets.top < viewportScrollOffsets.top) {
             diff = viewportScrollOffsets.top - elementOffsets.top;
