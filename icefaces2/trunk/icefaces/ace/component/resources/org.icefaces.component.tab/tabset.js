62b01e3d8dc94ac339f1c46c631b6430de99b7f6
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-05-11 15:01:57 +0000

    ICE-6859 : ACE javascript conflict with compat in window.Ice namespace

--- icefaces2/trunk/icefaces/ace/component/resources/org.icefaces.component.tab/tabset.js
+++ icefaces2/trunk/icefaces/ace/component/resources/org.icefaces.component.tab/tabset.js
@@ -47,6 +47,18 @@ ice.component.tabset = {
     		   tabview.set('activeIndex', jsfProps.selectedIndex);      
     	   }
        }
+       else {
+           //alert("server side init");
+           if(!ice.component.clientState.has(clientId)) {
+               //alert("server side init - no context");
+               tabview.set('activeIndex', jsfProps.selectedIndex);
+               /*
+                       tabview.removeListener('activeTabChange');
+                       tabview.selectTab(currentIndex);
+                       tabview.addListener('activeTabChange', tabChange);
+               */
+           }
+       }
        
        /*
         if (!Ice.component.registeredComponents[clientId]) {
@@ -63,7 +75,7 @@ ice.component.tabset = {
        //add hover effect 
        if (jsfProps.hover) {
            hoverEffect = function(event, attributes) {  
-               target = Ice.eventTarget(event);
+               target = ice.component_util.eventTarget(event);
                target = target.parentNode;
                if ("selected" == target.parentNode.className) return;         
                anim = new YAHOO.util.ColorAnim(target, attributes); 
@@ -91,10 +103,14 @@ ice.component.tabset = {
                
        //logger.info('3. tabset initialize');
        var tabChange=function(event) {
+            var rootElem = document.getElementById(clientId);
+            if ('suppressTabChange' in rootElem) {
+                return;
+            }
             var context = ice.component.getJSContext(clientId);
             var tabview = context.getComponent();
             var sJSFProps = context.getJSFProps();
-            event.target = document.getElementById(clientId);
+            event.target = rootElem;
             tbset = document.getElementById(clientId);
             currentIndex = tabview.getTabIndex(event.newValue);
             //YAHOO.log(" currentIndex="+currentIndex);
@@ -105,24 +121,10 @@ ice.component.tabset = {
                                 var context = ice.component.getJSContext(clientId);
                                 var tabview = context.getComponent();
                                 if (data.status == 'success') {
-                                        var lastKnownSelectedIndex = ice.component.getJSContext(clientId).getJSFProps().selectedIndex;   
-	                                    if (lastKnownSelectedIndex != currentIndex) {
-	                                            tabview.removeListener('activeTabChange');
-	                                            if (!jsfProps.isClientSide){
-	                                                tabview.set('activeIndex', lastKnownSelectedIndex);
-	                                            }else {
-		                                  	    	tabview.selectTab(currentIndex);
-	                                            }
-	                                            tabview.addListener('activeTabChange', tabChange); 
-	                                            currentIndex = lastKnownSelectedIndex; 
-	                                    }
+                                    //TODO
                                    try {
 									document.getElementById(event.newValue.get('element').firstChild.id).focus();		
                                    } catch(e) {}    
-								
-									 
- 
-                                                                         
                                 }
                             });
                         };
@@ -216,7 +218,7 @@ ice.component.tabset = {
             var context = ice.component.getJSContext(clientId);
             var tabview = context.getComponent();
             var target = Event.getTarget(event).parentNode;
-			if(Ice.isEventSourceInputElement(event)) {
+			if(ice.component_util.isEventSourceInputElement(event)) {
 				return true ;
 			}
 			//check for enter or space key
@@ -399,7 +401,7 @@ ice.component.tabset = {
 		   var f = document.getElementById(_form.id + 'yti');
 		   //if tabindex holder is not exist already, then create it lazily.
 		   if (!f) {
-			   f = this.createHiddenField(_form, _form.id + 'yti');
+			   f = ice.component.tabset.createHiddenField(_form, _form.id + 'yti');
 		   }
 	       return f 
 	   } else {
@@ -418,29 +420,324 @@ ice.component.tabset = {
  
    //delegate call to ice.yui.updateProperties(..)  with the reference of this lib
    updateProperties:function(clientId, jsProps, jsfProps, events) {
-	   if (jsfProps.isClientSide){
-		   var index = jsfProps.selectedIndex;
-	       var lastKnownIndex = 0;
-	       //if (index) YAHOO.log("updateProperties index="+index);
-	       var context = ice.component.getJSContext(clientId);
-	       if (context){
-	    	   //YAHOO.log("updateProperties has context");
-	    	   var tabviewObj = context.getComponent();	
-	    	   lastKnownIndex = context.getJSFProps().selectedIndex;
-	    	   //YAHOO.log("last know index is="+lastKnownIndex)
-	           if (index != lastKnownIndex) {
-	    		    //YAHOO.log("must switch indices");
-	    	    	tabviewObj.selectTab(index);
-	           }
-    	   }
+       var lib = this;
+       ice.yui3.use(function(Y){
+       Y.use('yui2-tabview', function(Yui) {
+       Y.on('domready', function(){
+
+       // Call handlePotentialTabChanges if we're NOT going to initialise
+       var oldJSFProps = null;
+       var context = ice.component.getJSContext(clientId);
+       if (context) {
+           oldJSFProps = context.getJSFProps();
        }
-       ice.yui3.updateProperties(clientId, jsProps, jsfProps, events, this);
-       
+       var requiresInitialise = ice.component.tabset.handlePotentialTabChanges(
+               Y, clientId, oldJSFProps, jsfProps);
+       // If the tab info changed sufficiently to require an initialise
+       if (context) {
+           if (requiresInitialise) {
+               // component.destroy() will remove all the markup fro the page
+               //var component = context.getComponent();
+               //if (component) {
+               //    component.destroy();
+               //}
+               var rootToReInit = document.getElementById(clientId);
+               if (rootToReInit) {
+                   rootToReInit['JSContext'] = null;
+                   rootToReInit.removeAttribute('JSContext');
+               }
+               //document.getElementById(clientId)['JSContext'] = null;
+               JSContext[clientId] = null;
+           }
+           else {
+               var tabviewObj = context.getComponent();
+               var lastKnownIndex = context.getJSFProps().selectedIndex;
+               var index = jsfProps.selectedIndex;
+               var objIndex = tabviewObj.get('activeIndex');
+
+               if (index != objIndex) {
+                   var rootElem = document.getElementById(clientId);
+                   rootElem.setAttribute('suppressTabChange', true);
+                   //tabviewObj.removeListener('activeTabChange');
+                   if (!jsfProps.isClientSide){
+                       tabviewObj.set('activeIndex', index);
+                   } else {
+                       tabviewObj.selectTab(index);
+                   }
+                   rootElem.removeAttribute('suppressTabChange');
+                   //tabviewObj.addListener('activeTabChange', tabChange);
+               }
+           }
+       }
+       ice.yui3.updateProperties(clientId, jsProps, jsfProps, events, lib);
+       });
+       });
+       });
    },
  
    //delegate call to ice.yui.getInstance(..) with the reference of this lib 
    getInstance:function(clientId, callback) {
        ice.component.getInstance(clientId, callback, this);
-   }   
+   },
+
+    // Used by updateProperties(-) when we're already initialised
+    // Updates the dom, re-parents the content, and triggers a new initialise
+    handlePotentialTabChanges : function(Y, clientId, oldJSFProps, newJSFProps) {
+        var oldSafeIds = ( (!oldJSFProps) ? null : oldJSFProps.safeIds );
+        var newSafeIds = ( (!newJSFProps) ? null : newJSFProps.safeIds );
+        if (!oldSafeIds) {
+            oldSafeIds = new Array();
+        }
+        if (!newSafeIds) {
+            newSafeIds = new Array();
+        }
+
+        var ret = false;
+
+        if (ice.component_util.arraysEqual(oldSafeIds, newSafeIds)) {
+            // We can have a scenario where the [client-side] tabSet is
+            // completely updated by the dom-diff, and nothing has changed
+            // with the tabs, but now the tab content is stored in the safe,
+            // but the old safeIds list is not null, and is exactly equal to
+            // the new safeIds list, so we don't know to re-parent the content
+            // into the content area. So we'll need to scan through the new
+            // safeIds list and see if the content is there, and handle it.
+            var contentDiv = document.getElementById(clientId + 'cnt');
+            if (contentDiv && !contentDiv.hasChildNodes()) {
+                var index;
+                for (index = 0; index < newSafeIds.length; index++) {
+                    var safeDiv = document.getElementById(newSafeIds[index]);
+                    if (safeDiv && safeDiv.hasChildNodes()) {
+                        var appendedDiv = document.createElement('div');
+                        contentDiv.appendChild(appendedDiv);
+
+                        // Reparent new safe-house entry into content area
+                        ice.component.tabset.moveSafeToContent(safeDiv, appendedDiv);
+
+                        ret = true;
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        var appendNewContent = new Array();
+        // [ [oldContent, newIndex where it should go or -1 for delete], É ]
+        var moveOldContent = new Array();
+        var skipNewIndexes = new Array();
+        var oldSafeIndex = 0;
+        var newSafeIndex = 0;
+
+        //var contentNode = Y.one('#' + clientId + 'cnt');//' .yui-content'); // Y.DOM.byId(clientId + 'cnt');
+        var contentDiv = document.getElementById(clientId + 'cnt');//Y.Node.getDOMNode(contentNode);
+
+        while (true) {
+            // (0) Detect if done
+            if (oldSafeIndex >= oldSafeIds.length && newSafeIndex >= newSafeIds.length) {
+                break;
+            }
+
+            // (3.5 skip) Skip past newSafeIndex if in skip list
+            if (ice.component_util.arrayIndexOf(skipNewIndexes, newSafeIndex, 0) >= 0) {
+                newSafeIndex++;
+                continue;
+            }
+
+            // (1) Detect if tab on end was deleted
+            // If past end of new list but more in old list
+            if (newSafeIndex >= newSafeIds.length && oldSafeIndex < oldSafeIds.length) {
+                // Everything left in old list has been deleted. Just delete one, then loop to remove any more
+                moveOldContent.push( [oldSafeIndex, -1] );
+                oldSafeIndex++;
+                ret = true;
+                continue;
+            }
+
+            // (2) Detect if appended to end
+            // If past end of old list, but at least another in new list
+            if (oldSafeIndex >= oldSafeIds.length && newSafeIndex < newSafeIds.length) {
+                // Current entry in new is appended
+                // Create new div and append it to content area.
+                var appendedDiv = document.createElement('div');
+                contentDiv.appendChild(appendedDiv);
+
+                // Reparent new safe-house entry into content area
+                ice.component.tabset.moveSafeIdToContent(newSafeIds[newSafeIndex], appendedDiv);
+
+                // Increment newSafeIndex, but not oldSafeIndex, and continue looping
+                newSafeIndex++;
+                ret = true;
+                continue;
+            }
+
+            // (3) Detect if non-end deleted, inserted, visited, moved
+            var oldsid = oldSafeIds[oldSafeIndex];
+            var newsid = newSafeIds[newSafeIndex];
+            if (oldsid !== newsid) {
+                // (3.5) Detect if old moved. Also covers unvisited inserts,
+                // which inadvertently move the pre-existing sections, which
+                // we'll try to avoid, since moving a section involves
+                // refreshing iframe content, which we need to avoid
+                // oldsid not null and in new list
+                // ?? Search from newSafeIndex onwards or beginning?? Just use beginning
+                var foundInNewIndex;
+                if (oldsid !== null &&
+                    (foundInNewIndex = ice.component_util.arrayIndexOf(newSafeIds, oldsid, 0)) >= 0)
+                {
+                    // Detect if newsid is unvisited/visiting insert
+                    var foundInOldIndex;
+                    if (newsid === null ||
+                        ( ((foundInOldIndex = ice.component_util.arrayIndexOf(oldSafeIds, newsid, 0)) < 0) &&
+                          document.getElementById(newsid).hasChildNodes()
+                        )) {
+                        var newDiv = document.createElement('div');
+                        var newIndex = contentDiv.childNodes.length;
+                        contentDiv.appendChild(newDiv);
+
+                        // Reparent new safe-house entry into content area
+                        // if newsid is not null
+                        ice.component.tabset.moveSafeIdToContent(newsid, newDiv);
+
+                        // Mark the new content div to be moved to it's proper insertion point
+                        appendNewContent.push( [newIndex, newSafeIndex] );
+
+                        // Increment newSafeIndex, but not oldSafeIndex, and continue looping
+                        newSafeIndex++;
+                        ret = true;
+                        continue;
+                    }
+
+                    // Mark the location in new list as something to skip over
+                    skipNewIndexes.push(foundInNewIndex);
+
+                    // Save the reference to the old content, and where it should end up
+                    moveOldContent.push( [oldSafeIndex, foundInNewIndex] );
+
+                    // Increment oldSafeIndex, but not newSafeIndex, and continue looping
+                    oldSafeIndex++;
+                    ret = true;
+                    continue;
+                }
+
+                // Have to detect which of non-end delete, insert, or visit
+
+                // (4) Detect if non-end deleted
+                // If newsid is null, it's a delete (oldsid is non-null since oldsid !== newsid)
+                if (newsid === null) {
+                    moveOldContent.push( [oldSafeIndex, -1] );
+                    oldSafeIndex++;
+                    ret = true;
+                    continue;
+                }
+
+                // (5) Detect if non-end deleted or instead insert/visit
+                // If new safe entry has no child markup, then is non-end delete
+                var safeDiv = document.getElementById(newsid);
+                if (!safeDiv.hasChildNodes()) {
+                    moveOldContent.push( [oldSafeIndex, -1] );
+                    oldSafeIndex++;
+                    ret = true;
+                    continue;
+                }
+                // If new safe entry has child markup, then is either insert or visit
+                else {// safeDiv.hasChildNodes()
+                    // If changed from null to non-null means visited
+                    if (oldsid === null && newsid !== null) {
+                        // Get the content area
+                        var visitedDiv = contentDiv.childNodes[oldSafeIndex];
+
+                        // Reparent new safe-house entry into content area
+                        ice.component.tabset.moveSafeToContent(safeDiv, visitedDiv);
+
+                        oldSafeIndex++;
+                        newSafeIndex++;
+                        continue;
+                    }
+
+                    // Inserted
+                    // We don't want to alter the content area indexing as we
+                    // go, so inserting right away is a no-go. We also don't
+                    // want to special case this. So, we'll treat this as an
+                    // append, where we'll subsequently move it to the
+                    // insertion point
+                    // Create new div and append it to content area.
+                    var newDiv = document.createElement('div');
+                    var newIndex = contentDiv.childNodes.length;
+                    contentDiv.appendChild(newDiv);
+
+                    // Reparent new safe-house entry into content area
+                    ice.component.tabset.moveSafeToContent(safeDiv, newDiv);
+
+                    // Mark the new content div to be moved to it's proper insertion point
+                    appendNewContent.push( [newIndex, newSafeIndex] );
+
+                    // Increment newSafeIndex, but not oldSafeIndex, and continue looping
+                    newSafeIndex++;
+                    ret = true;
+                    continue;
+                }
+            }
+            // If they're the same, move on to next. Increment both
+            else { // oldsid === newsid
+                oldSafeIndex++;
+                newSafeIndex++;
+                continue;
+            }
+        }
+
+        // moveOldContent assumes that as its index increases, the old content
+        // indexes increase as well. But when we append, we're putting larger
+        // old content indexes early towards the beginning of moveOldContent.
+        // So we'll buffer them in appendNewContent, and then use that to
+        // place them toward the end of moveOldContent.
+        var index;
+        for (index = 0; index < appendNewContent.length; index++) {
+            moveOldContent.push(appendNewContent[index]);
+        }
+
+        // moveOldContent, but our algorithm assumes that the the old indexes increase
+        // Iterate through moveOldContent in reverse, removing the divs from
+        // the content area, and setting the div into moveOldContent where
+        // the index had been
+        for (index = moveOldContent.length - 1; index >= 0; index--) {
+            var removeIndex = moveOldContent[index][0];
+            var removeDiv = contentDiv.childNodes[removeIndex];
+            moveOldContent[index][0] = removeDiv;
+            contentDiv.removeChild(removeDiv);
+        }
+        // Then iterate through that list in the original forward sequence,
+        // inserting the divs into their designated positions, unless the
+        // insert index is -1, in which case discard them.
+        for (index = 0; index < moveOldContent.length; index++) {
+            var fromTo = moveOldContent[index];
+            var insertDiv = fromTo[0];
+            var toIndex = fromTo[1];
+            if (toIndex >= 0) {
+                ice.component_util.insertElementAtIndex(contentDiv, insertDiv, toIndex);
+            }
+        }
+
+        return ret;
+    },
+
+    moveSafeIdToContent : function(safeId, tabContentDiv) {
+        if (safeId) {
+            var safeDiv = document.getElementById(safeId);
+            ice.component.tabset.moveSafeToContent(safeDiv, tabContentDiv);
+        }
+    },
+
+    moveSafeToContent : function(safeDiv, tabContentDiv) {
+        if (safeDiv.hasChildNodes()) {
+            // Clean out tabContentDiv, and put the div child of safeDiv into tabContentDiv
+            while (tabContentDiv.hasChildNodes()) {
+                tabContentDiv.removeChild(tabContentDiv.firstChild);
+            }
+            var contentsToMove = safeDiv.firstChild; //.getElementByTagName("div")[0];
+            safeDiv.removeChild(contentsToMove);
+            tabContentDiv.appendChild(contentsToMove);
+        }
+    }
 };
 
