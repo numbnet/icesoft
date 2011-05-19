da0b15da482bed88f522de5dfc190a82cd0c8476
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-05-19 21:07:45 +0000

    ICE-6892 Test for the existence of ICEpush library before setting up ICEpush, if not present ICEpushResourceHandler is configured as a delegating resource handler.  Introduced PushRendererMessage class (that matches the org.icepush.PushMessage class) to avoid any runtime dependency on ICEpush when PushRenderer is used.

---
+++ icefaces2/trunk/icefaces/core/src/main/java/org/icefaces/application/PushRendererMessage.java
@@ -0,0 +1,76 @@
+/*
+ * Version: MPL 1.1
+ *
+ * "The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2009 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+package org.icefaces.application;
+
+import org.icepush.PushMessage;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class PushRendererMessage {
+    private HashMap properties = new HashMap();
+
+    public Iterator getPropertyNames() {
+        return properties.keySet().iterator();
+    }
+
+    public void setProperty(String name, String value) {
+        properties.put(name, value);
+    }
+
+    public void setProperty(String name, long value) {
+        properties.put(name, value);
+    }
+
+    public void setProperty(String name, double value) {
+        properties.put(name, value);
+    }
+
+    public void setProperty(String name, boolean value) {
+        properties.put(name, value);
+    }
+
+    public String getPropertyAsString(String name) {
+        return String.valueOf(properties.get(name));
+    }
+
+    public long getPropertyAsLong(String name) {
+        return Long.parseLong(String.valueOf(properties.get(name)));
+    }
+
+    public double getPropertyAsDouble(String name) {
+        return Double.parseDouble(String.valueOf(properties.get(name)));
+    }
+
+    public boolean getPropertyAsBoolean(String name) {
+        return Boolean.parseBoolean(String.valueOf(properties.get(name)));
+    }
+
+    public String toString() {
+        return properties.toString();
+    }
+
+    public PushMessage toPushMessage() {
+        return new PushMessage(properties);
+    }
+}
