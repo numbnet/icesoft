b1fb4d8f23dcfba654556c964b2d1a744448a338
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-05-27 14:22:11 +0000

    ICE-6907, ICE-6908 Updated Scriptaculous code to version 1.9 to resolve the incompatibilities between 1.8 version and the new Prototype 1.7 code.

--- icefaces/trunk/icefaces/bridge/lib/scriptaculous/builder.js
+++ icefaces/trunk/icefaces/bridge/lib/scriptaculous/builder.js
@@ -1,4 +1,6 @@
-// Copyright (c) 2005-2007 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
+// script.aculo.us builder.js v1.9.0, Thu Dec 23 16:54:48 -0500 2010
+
+// Copyright (c) 2005-2010 Thomas Fuchs (http://script.aculo.us, http://mir.aculo.us)
 //
 // script.aculo.us is freely distributable under the terms of an MIT-style license.
 // For details, see the script.aculo.us web site: http://script.aculo.us/
@@ -24,8 +26,8 @@ var Builder = {
     //       due to a Firefox bug
     node: function(elementName) {
         elementName = elementName.toUpperCase();
-    
-    // try innerHTML approach
+
+        // try innerHTML approach
         var parentTag = this.NODEMAP[elementName] || 'div';
         var parentElement = document.createElement(parentTag);
         try { // prevent IE "feature": http://dev.rubyonrails.org/ticket/2707
@@ -33,33 +35,33 @@ var Builder = {
         } catch(e) {
         }
         var element = parentElement.firstChild || null;
-      
-    // see if browser added wrapping tags
+
+        // see if browser added wrapping tags
         if (element && (element.tagName.toUpperCase() != elementName))
             element = element.getElementsByTagName(elementName)[0];
-    
-    // fallback to createElement approach
+
+        // fallback to createElement approach
         if (!element) element = document.createElement(elementName);
-    
-    // abort if nothing could be created
+
+        // abort if nothing could be created
         if (!element) return;
 
-    // attributes (or text)
+        // attributes (or text)
         if (arguments[1])
             if (this._isStringOrNumber(arguments[1]) ||
-                (arguments[1] instanceof Array) ||
-                arguments[1].tagName) {
+                    (arguments[1] instanceof Array) ||
+                    arguments[1].tagName) {
                 this._children(element, arguments[1]);
             } else {
                 var attrs = this._attributes(arguments[1]);
                 if (attrs.length) {
                     try { // prevent IE "feature": http://dev.rubyonrails.org/ticket/2707
                         parentElement.innerHTML = "<" + elementName + " " +
-                                                  attrs + "></" + elementName + ">";
+                                attrs + "></" + elementName + ">";
                     } catch(e) {
                     }
                     element = parentElement.firstChild || null;
-            // workaround firefox 1.0.X bug
+                    // workaround firefox 1.0.X bug
                     if (!element) {
                         element = document.createElement(elementName);
                         for (attr in arguments[1])
@@ -70,11 +72,11 @@ var Builder = {
                 }
             }
 
-    // text, or array of children
+        // text, or array of children
         if (arguments[2])
             this._children(element, arguments[2]);
 
-        return element;
+        return $(element);
     },
     _text: function(text) {
         return document.createTextNode(text);
@@ -89,7 +91,7 @@ var Builder = {
         var attrs = [];
         for (attribute in attributes)
             attrs.push((attribute in this.ATTR_MAP ? this.ATTR_MAP[attribute] : attribute) +
-                       '="' + attributes[attribute].toString().escapeHTML().gsub(/"/, '&quot;') + '"');
+                    '="' + attributes[attribute].toString().escapeHTML().gsub(/"/, '&quot;') + '"');
         return attrs.join(" ");
     },
     _children: function(element, children) {
@@ -100,14 +102,14 @@ var Builder = {
         if (typeof children == 'object') { // array can hold nodes and text
             children.flatten().each(function(e) {
                 if (typeof e == 'object')
-                    element.appendChild(e)
+                    element.appendChild(e);
                 else
-                    if (Builder._isStringOrNumber(e))
-                        element.appendChild(Builder._text(e));
+                if (Builder._isStringOrNumber(e))
+                    element.appendChild(Builder._text(e));
             });
         } else
-            if (Builder._isStringOrNumber(children))
-                element.appendChild(Builder._text(children));
+        if (Builder._isStringOrNumber(children))
+            element.appendChild(Builder._text(children));
     },
     _isStringOrNumber: function(param) {
         return(typeof param == 'string' || typeof param == 'number');
@@ -121,16 +123,16 @@ var Builder = {
         if (typeof scope != 'object' && typeof scope != 'function') scope = window; //global scope
 
         var tags = ("A ABBR ACRONYM ADDRESS APPLET AREA B BASE BASEFONT BDO BIG BLOCKQUOTE BODY " +
-                    "BR BUTTON CAPTION CENTER CITE CODE COL COLGROUP DD DEL DFN DIR DIV DL DT EM FIELDSET " +
-                    "FONT FORM FRAME FRAMESET H1 H2 H3 H4 H5 H6 HEAD HR HTML I IFRAME IMG INPUT INS ISINDEX " +
-                    "KBD LABEL LEGEND LI LINK MAP MENU META NOFRAMES NOSCRIPT OBJECT OL OPTGROUP OPTION P " +
-                    "PARAM PRE Q S SAMP SCRIPT SELECT SMALL SPAN STRIKE STRONG STYLE SUB SUP TABLE TBODY TD " +
-                    "TEXTAREA TFOOT TH THEAD TITLE TR TT U UL VAR").split(/\s+/);
+                "BR BUTTON CAPTION CENTER CITE CODE COL COLGROUP DD DEL DFN DIR DIV DL DT EM FIELDSET " +
+                "FONT FORM FRAME FRAMESET H1 H2 H3 H4 H5 H6 HEAD HR HTML I IFRAME IMG INPUT INS ISINDEX " +
+                "KBD LABEL LEGEND LI LINK MAP MENU META NOFRAMES NOSCRIPT OBJECT OL OPTGROUP OPTION P " +
+                "PARAM PRE Q S SAMP SCRIPT SELECT SMALL SPAN STRIKE STRONG STYLE SUB SUP TABLE TBODY TD " +
+                "TEXTAREA TFOOT TH THEAD TITLE TR TT U UL VAR").split(/\s+/);
 
         tags.each(function(tag) {
             scope[tag] = function() {
                 return Builder.node.apply(Builder, [tag].concat($A(arguments)));
-            }
+            };
         });
     }
-}
+};
