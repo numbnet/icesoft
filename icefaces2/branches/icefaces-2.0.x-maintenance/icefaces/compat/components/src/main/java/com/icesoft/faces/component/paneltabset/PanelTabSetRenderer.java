e007109540bca087776eb4a608c462b10bbb3079
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-06-02 20:57:46 +0000

    ICE-6508: panelTabSet keyboard navigation not working when tabbed into.

--- icefaces2/branches/icefaces-2.0.x-maintenance/icefaces/compat/components/src/main/java/com/icesoft/faces/component/paneltabset/PanelTabSetRenderer.java
+++ icefaces2/branches/icefaces-2.0.x-maintenance/icefaces/compat/components/src/main/java/com/icesoft/faces/component/paneltabset/PanelTabSetRenderer.java
@@ -603,7 +603,8 @@ public class PanelTabSetRenderer
             link.setAttribute(HTML.ID_ATTR, linkId);
             link.setAttribute(HTML.HREF_ATTR, "javascript:;");
             link.setAttribute(HTML.CLASS_ATTR, "icePnlTbLblLnk");
-            
+            renderAttribute(tabSet, link, HTML.TABINDEX_ATTR, HTML.TABINDEX_ATTR);
+
             if (labelFacet == null) {
                 td_mid_mid.appendChild(link);
                 // set focus handler
@@ -630,11 +631,13 @@ public class PanelTabSetRenderer
                 Element div = domContext.createElement(HTML.DIV_ELEM); 
                 td_mid_mid.appendChild(div);
                 div.setAttribute(HTML.ONCLICK_ATTR, "if(!Ice.isEventSourceInputElement(event)) document.getElementById('"+ linkId+"').onclick();");
-				div.setAttribute(HTML.ONFOCUS_ATTR, "document.getElementById('"+ linkId+"').onclick();");				
+//				div.setAttribute(HTML.ONFOCUS_ATTR, "document.getElementById('"+ linkId+"').onclick();");
                 div.setAttribute(HTML.CLASS_ATTR, "ptfd");
+/*
                 if (active) {
                     renderAttribute(tabSet, div, HTML.TABINDEX_ATTR, HTML.TABINDEX_ATTR);
                 }
+*/
                 div.appendChild(link);
                 Node cursor = domContext.getCursorParent();
                 domContext.setCursorParent(div);
