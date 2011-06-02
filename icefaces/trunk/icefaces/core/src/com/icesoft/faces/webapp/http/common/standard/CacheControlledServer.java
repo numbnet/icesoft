97f29a9dc65b21affefa25c5d4e60aa2f515d41b
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-06-02 11:47:19 +0000

    ICE-6891 Stop caching of the responses that do not return 200 HTTP status code. (Commit done for Mircea)

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/common/standard/CacheControlledServer.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/common/standard/CacheControlledServer.java
@@ -32,12 +32,9 @@
 
 package com.icesoft.faces.webapp.http.common.standard;
 
-import com.icesoft.faces.webapp.http.common.Request;
-import com.icesoft.faces.webapp.http.common.RequestProxy;
-import com.icesoft.faces.webapp.http.common.Response;
-import com.icesoft.faces.webapp.http.common.ResponseHandler;
-import com.icesoft.faces.webapp.http.common.Server;
+import com.icesoft.faces.webapp.http.common.*;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
@@ -46,6 +43,7 @@ public class CacheControlledServer implements Server {
     private static final Date ExpirationDate = new Date(System.currentTimeMillis() + 2629743830l);
     private static final Collection cache = new HashSet();
     private static final Date StartupTime = new Date();
+    private final HashSet uncache = new HashSet();
     private Server server;
 
     public CacheControlledServer(Server server) {
@@ -54,18 +52,25 @@ public class CacheControlledServer implements Server {
     }
 
     public void service(Request request) throws Exception {
-        if (cache.contains(request.getHeader("If-None-Match"))) {
-            request.respondWith(new NotModifiedHandler(ExpirationDate));
+        if (uncache.contains(request.getHeader("If-None-Match"))) {
+            //do not cache, try serving up the requested resource
+            server.service(new EnhancedRequest(request));
         } else {
-            try {
-                Date modifiedSince = request.getHeaderAsDate("If-Modified-Since");
-                if (StartupTime.getTime() - modifiedSince.getTime() > 1000) {
+            if (cache.contains(request.getHeader("If-None-Match"))) {
+                request.respondWith(new NotModifiedHandler(ExpirationDate));
+            } else {
+                try {
+                    Date modifiedSince = request.getHeaderAsDate("If-Modified-Since");
+                    if (StartupTime.getTime() - modifiedSince.getTime() > 1000) {
+                        server.service(new EnhancedRequest(request));
+                    } else {
+                        request.respondWith(new NotModifiedHandler(ExpirationDate));
+                    }
+                } catch (IOException e) {
+                    request.respondWith(new NotFoundHandler(e.getMessage()));
+                } catch (Exception e) {
                     server.service(new EnhancedRequest(request));
-                } else {
-                    request.respondWith(new NotModifiedHandler(ExpirationDate));
                 }
-            } catch (Exception e) {
-                server.service(new EnhancedRequest(request));
             }
         }
     }
@@ -83,8 +88,7 @@ public class CacheControlledServer implements Server {
         public void respondWith(final ResponseHandler handler) throws Exception {
             request.respondWith(new ResponseHandler() {
                 public void respond(Response response) throws Exception {
-                    String eTag = Integer.toHexString(request.getURI().hashCode());
-                    cache.add(eTag);
+                    final String eTag = Integer.toHexString(request.getURI().hashCode());
                     response.setHeader("ETag", eTag);
                     //tell to IE to cache these resources
                     //see: http://mir.aculo.us/articles/2005/08/28/internet-explorer-and-ajax-image-caching-woes
@@ -92,7 +96,18 @@ public class CacheControlledServer implements Server {
                     //see: http://support.microsoft.com/default.aspx?scid=kb;en-us;319546
                     response.setHeader("Cache-Control", new String[]{"private", "max-age=2629743"});
                     response.setHeader("Last-Modified", StartupTime);
-                    handler.respond(response);
+                    handler.respond(new ResponseProxy(response) {
+                        public void setStatus(int code) {
+                            super.setStatus(code);
+                            //cache resource only when served up successfully
+                            if (code == 200) {
+                                cache.add(eTag);
+                                uncache.remove(eTag);
+                            } else {
+                                uncache.add(eTag);
+                            }
+                        }
+                    });
                 }
             });
         }
