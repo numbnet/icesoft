301f4cc3e782e9ca5905a9ec4a85955f9129ff23
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-07-04 13:33:37 +0000

    ICE-6987 Avoid rendering the path of the resource in the HTTP 404 response.

--- icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/FileServer.java
+++ icefaces/trunk/icefaces/core/src/com/icesoft/faces/webapp/http/core/FileServer.java
@@ -32,12 +32,7 @@
 
 package com.icesoft.faces.webapp.http.core;
 
-import com.icesoft.faces.webapp.http.common.FileLocator;
-import com.icesoft.faces.webapp.http.common.MimeTypeMatcher;
-import com.icesoft.faces.webapp.http.common.Request;
-import com.icesoft.faces.webapp.http.common.Response;
-import com.icesoft.faces.webapp.http.common.ResponseHandler;
-import com.icesoft.faces.webapp.http.common.Server;
+import com.icesoft.faces.webapp.http.common.*;
 import com.icesoft.faces.webapp.http.common.standard.NotFoundHandler;
 
 import java.io.File;
@@ -68,7 +63,7 @@ public class FileServer implements Server {
                 }
             });
         } else {
-            request.respondWith(new NotFoundHandler("Could not find file at " + path));
+            request.respondWith(new NotFoundHandler("Could not find resource."));
         }
     }
 
