98c5c4d6fb6bf79f8f061273884cc8e25ca0f64e
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-09-01 15:03:54 +0000

    removing samples from core icepush (PUSH-141)

--- icepush/trunk/integration/icefaces2/samples/icepushplace-icefaces2/src/main/java/org/icepush/integration/icepushplace/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package org.icepush.integration.icepushplace.service;
-
-import org.icepush.ws.samples.icepushplace.PersonType;
-
-public interface PersonService {
-	public PersonType loginPerson(PersonType person);
-	public void updatePerson(PersonType person);
-	public PersonType movePerson(int fromRegion, PersonType person);
-}
