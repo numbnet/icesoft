452d7909b1d845f925608eb57945818adbc01ecb
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-10-05 22:13:05 +0000

    Working branch for staging ACE updates for 2.1 Beta in.

---
+++ icefaces2/scratchpads/ace-merge/icefaces/compat/core/src/main/java/com/icesoft/faces/context/effects/EffectUtil.java
@@ -0,0 +1,56 @@
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+package com.icesoft.faces.context.effects;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ *Utility functions for effect
+ */
+public class EffectUtil {
+
+    /**
+     * build up a map from a css string
+     * key:value;
+     * @param css
+     * @return
+     */
+    public static Map buildCssMap(String css) {
+        Map map = new HashMap();
+        if (css == null || css.trim().length() == 0)
+            return map;
+        css = css.trim();
+        StringTokenizer st = new StringTokenizer(css, ";");
+        while (st.hasMoreTokens()) {
+            String token = st.nextToken();
+            int i = token.indexOf(":");
+            if (i == 0) throw new IllegalArgumentException(
+                    "Can't find : in [" + token + "]");
+            String name = token.substring(0, i);
+            String value = token.substring(++i);
+            map.put(name, value);
+        }
+        return map;
+    }
+}
---
+++ icefaces2/scratchpads/ace-merge/icefaces/compat/core/src/main/java/com/icesoft/faces/context/effects/Grow.java
@@ -0,0 +1,70 @@
+/*
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is ICEfaces 1.5 open source software code, released
+ * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
+ * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
+ * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ *
+ * Contributor(s): _____________________.
+ */
+
+package com.icesoft.faces.context.effects;
+
+import com.icesoft.faces.util.CoreUtils;
+
+/**
+ * script.aculo.us grow effect
+ * Grow an Element from nothing to its full size.
+ */
+public class Grow extends Effect {
+
+    private String direction = "center";
+
+    public Grow() {
+        ea.add("direction", direction);
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    /**
+     * Grow to current size center, top-left, top-right, bottom-left,
+     * bottom-right
+     *
+     * @param direction
+     */
+    public void setDirection(String direction) {
+        this.direction = direction;
+        ea.add("direction", direction);
+    }
+
+    public String getFunctionName() {
+        return "Ice.Scriptaculous.Effect.Grow";
+    }
+
+    public boolean equals(Object obj) {
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (!(obj instanceof Grow)) {
+            return false;
+        }
+        Grow effect = (Grow) obj;
+        if (!CoreUtils.objectsEqual(direction, effect.direction)) {
+            return false;
+        }
+        return true;
+    }
+}
