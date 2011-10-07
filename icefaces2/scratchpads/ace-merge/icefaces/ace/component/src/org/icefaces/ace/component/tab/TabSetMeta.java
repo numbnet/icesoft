7e3d6cc34539c7a6a781298b804fdeb72f224523
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-10-07 17:50:17 +0000

    Merge ace:tabSet changes back to icefaces2/trunk.

--- icefaces2/scratchpads/ace-merge/icefaces/ace/component/src/org/icefaces/ace/component/tab/TabSetMeta.java
+++ icefaces2/scratchpads/ace-merge/icefaces/ace/component/src/org/icefaces/ace/component/tab/TabSetMeta.java
@@ -101,7 +101,7 @@ public class TabSetMeta extends UIComponentBaseMeta {
     @Property(tlddoc = "If true then all tabs except the active one will be disabled and can not be selected.")
     private boolean disabled;
 
-    @Field(javadoc="Maintains the record of which tabs have been visited")
+    @Property(tlddoc="Maintains the record of which tabs have been visited")
     private List visitedTabClientIds;
 
     @Facets
