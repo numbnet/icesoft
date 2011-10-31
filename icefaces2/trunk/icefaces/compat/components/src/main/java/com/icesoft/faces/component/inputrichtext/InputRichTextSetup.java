a17e33b778469b732cf622a14f155c3be0eefcbb
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-10-31 14:54:14 +0000

    ICE-7293 Rollback changes since they do not work in JBoss and Glassfish.

--- icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/inputrichtext/InputRichTextSetup.java
+++ icefaces2/trunk/icefaces/compat/components/src/main/java/com/icesoft/faces/component/inputrichtext/InputRichTextSetup.java
@@ -1,10 +1,10 @@
 package com.icesoft.faces.component.inputrichtext;
 
+import com.icesoft.faces.context.*;
 import org.icefaces.impl.event.UIOutputWriter;
 import org.icefaces.util.EnvUtils;
 
-import javax.faces.application.Resource;
-import javax.faces.application.ResourceHandler;
+import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
@@ -12,17 +12,37 @@ import javax.faces.event.AbortProcessingException;
 import javax.faces.event.SystemEvent;
 import javax.faces.event.SystemEventListener;
 import java.io.*;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.jar.JarOutputStream;
+import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 
 public class InputRichTextSetup implements Serializable, SystemEventListener {
-    private static final String INPUTRICHTEXT_CKEDITOR_DIR = "inputrichtext/ckeditor/";
+    public static final Resource ICE_CK_EDITOR_JS = new FCKJarResource("com/icesoft/faces/component/inputrichtext/ckeditor_ext.js");
+    private static final String CK_EDITOR_ZIP = "com/icesoft/faces/component/inputrichtext/ckeditor.zip";
+
+    private final Date lastModified = new Date();
+    private final Map ZipEntryCacheCK = new HashMap();
+    private final ResourceLinker.Handler CK_LINKED_BASE = new LinkHandler();
+
+    private class LinkHandler implements ResourceLinker.Handler, Serializable{
+        public void linkWith(ResourceLinker linker) {
+            synchronized (ZipEntryCacheCK) {
+                if (ZipEntryCacheCK.isEmpty()) {
+                    loadZipEntryCache();
+                }
+            }
+            Iterator i = ZipEntryCacheCK.keySet().iterator();
+            while (i.hasNext()) {
+                String entryName = (String) i.next();
+                linker.registerRelativeResource(entryName, new RelativeResource(entryName));
+            }
+        }
+    }
 
     public boolean isListenerForSource(Object source) {
         return true;
@@ -31,248 +51,113 @@ public class InputRichTextSetup implements Serializable, SystemEventListener {
     public void processEvent(SystemEvent event) throws AbortProcessingException {
         final FacesContext context = FacesContext.getCurrentInstance();
         if (EnvUtils.isICEfacesView(context)) {
-            try {
-                UIViewRoot root = context.getViewRoot();
-                String code = loadResources(context);
-                MappingsWriter mappingsWriter = new MappingsWriter(code);
-                mappingsWriter.setTransient(true);
-                mappingsWriter.setId("inpRichTxt");
+            UIViewRoot root = context.getViewRoot();
 
-                List componentResources = root.getComponentResources(context, "head");
-                //make sure the script is always loaded first
-                if (componentResources.isEmpty()) {
-                    root.addComponentResource(context, mappingsWriter, "head");
-                } else {
-                    componentResources.add(0, mappingsWriter);
+            UIOutput inputTextJS = new UIOutputWriter() {
+                public void encode(ResponseWriter writer, FacesContext context) throws IOException {
+                    String clientID = getClientId(context);
+                    writer.startElement("script", this);
+                    writer.writeAttribute("id", clientID + "JS", null);
+                    writer.writeAttribute("src", getURI(context) + "ckeditor.js", "src");
+                    writer.endElement("script");
+                    writer.startElement("script", this);
+                    writer.writeAttribute("id", clientID + "JSWrpr", null);
+                    writer.writeAttribute("src", getURI(context), null);
+                    writer.endElement("script");
                 }
+            };
+            inputTextJS.setTransient(true);
+            inputTextJS.setId("inpRichTxt");
 
-            } catch (IOException e) {
-                throw new AbortProcessingException(e);
-            }
+            root.addComponentResource(context, inputTextJS, "head");
         }
     }
 
-    private String loadResources(FacesContext context) throws IOException {
-        Map applicationMap = context.getExternalContext().getApplicationMap();
+    private String getURI(FacesContext context) {
+        Map session = context.getExternalContext().getSessionMap();
 
-        Object value = applicationMap.get(InputRichTextSetup.class.getName());
-        if (value == null) {
-            List allResources = new ArrayList();
-            List cssResources = new ArrayList();
-            List imageResources = new ArrayList();
+        NonPersistentValue value = (NonPersistentValue) session.get(InputRichTextSetup.class.getName());
+        if (value == null || value.getObject() == null) {
+            ResourceRegistry registry = ResourceRegistryLocator.locate(FacesContext.getCurrentInstance());
+            URI ckBaseURI = registry.loadJavascriptCode(ICE_CK_EDITOR_JS, CK_LINKED_BASE);
+            registry.loadJavascriptCode(ICE_CK_EDITOR_JS);
 
-            ResourceHandler resourceHandler = context.getApplication().getResourceHandler();
-            Resource resource = resourceHandler.createResource(INPUTRICHTEXT_CKEDITOR_DIR + "ckeditor.js");
-
-            //collecting resource relative paths
-            URL url = resource.getURL();
-            String protocol = url.getProtocol();
-            if ("jar".equals(protocol)) {
-                String jarPath = url.getPath().substring(5, url.getPath().indexOf("!"));
-                JarFile jar = new JarFile(jarPath);
-                Enumeration<JarEntry> entries = jar.entries();
-                while (entries.hasMoreElements()) {
-                    JarEntry jarEntry = entries.nextElement();
-                    String name = jarEntry.getName();
-                    //create resources only for the files (excluding directories) contained in INPUTRICHTEXT_CKEDITOR_DIR
-                    if (name.contains(INPUTRICHTEXT_CKEDITOR_DIR) && !name.endsWith("/")) {
-                        String localPath = name.substring(name.indexOf(INPUTRICHTEXT_CKEDITOR_DIR));
-
-                        if (localPath.endsWith(".css")) {
-                            InputStream in = jar.getInputStream(jarEntry);
-                            cssResources.add(new CSSResourceEntry(localPath, readIntoByteArray(in)));
-                        } else if (localPath.endsWith(".png") || localPath.endsWith(".gif") || localPath.endsWith(".jpg")) {
-                            createResourceMapping(localPath, imageResources, resourceHandler);
-                        } else {
-                            createResourceMapping(localPath, allResources, resourceHandler);
-                        }
-                    }
-                }
-            } else {
-                throw new IOException("Cannot access InputRichText resources icefaces-compat.jar when exploded into the classpath.");
-            }
-
-            rewriteRelativeImagePaths(cssResources, imageResources);
-            File file = createCSSResourcesJar(context, cssResources);
-            addJarToClasspath(file);
-
-            //add CSS mappings that point to the rewritten CSS resources
-            Iterator<CSSResourceEntry> i = cssResources.iterator();
-            while (i.hasNext()) {
-                CSSResourceEntry css = i.next();
-                String resourcePath = css.getLocalPath().replace("css", "rewritten.css");
-                Resource r = resourceHandler.createResource(resourcePath);
-                allResources.add(new ResourceMapping(css.getRelativeLocalPath(), r.getRequestPath()));
-            }
-            //add image resources
-            allResources.addAll(imageResources);
-
-            StringBuffer code = new StringBuffer();
-            code.append("window.CKEDITOR_GETURL = function(r) { var mappings = [");
-            Iterator<ResourceMapping> entries = allResources.iterator();
-            while (entries.hasNext()) {
-                ResourceMapping next = entries.next();
-                code.append("{inbound: '");
-                code.append(next.getRelativeLocalPath());
-                code.append("', outbound: '");
-                code.append(next.getRequestPath());
-                code.append("'}");
-                if (entries.hasNext()) {
-                    code.append(",");
-                }
-            }
-            code.append("]; if (r.indexOf('http://') == 0) { var i = document.location.href.lastIndexOf('/'); r = r.substring(i + 1); }; for (var i = 0, l = mappings.length; i < l; i++) { var m = mappings[i]; if (m.inbound == r) { return m.outbound;} } return false; };");
-            applicationMap.put(InputRichTextSetup.class.getName(), code.toString());
-
-            return code.toString();
+            session.put(InputRichTextSetup.class.getName(), new NonPersistentValue(ckBaseURI));
+            return ckBaseURI.toString();
         } else {
-            return (String) value;
+            NonPersistentValue o = (NonPersistentValue) session.get(InputRichTextSetup.class.getName());
+            return o.getObject().toString();
         }
     }
 
-    private void addJarToClasspath(File file) throws IOException {
+    private class RelativeResource implements Resource, Serializable {
+        private final String entryName;
+
+        private RelativeResource(final String entryName) {
+            this.entryName = entryName;
+        }
+
+        public String calculateDigest() {
+            return String.valueOf(CK_EDITOR_ZIP + entryName);
+        }
+
+        public Date lastModified() {
+            return lastModified;
+        }
+
+        public InputStream open() throws IOException {
+            return new ByteArrayInputStream((byte[]) ZipEntryCacheCK.get(entryName));
+        }
+
+        public void withOptions(final Resource.Options options) {
+            options.setFileName(entryName);
+            options.setLastModified(lastModified);
+        }
+    }
+
+    private void loadZipEntryCache() {
         try {
-            Method method = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
-            method.setAccessible(true);
-            method.invoke(this.getClass().getClassLoader(), new Object[]{file.toURI().toURL()});
-        } catch (Throwable t) {
-            throw new IOException("Error, could not add URL to system classloader");
-        }
-    }
-
-    private File createCSSResourcesJar(FacesContext context, List cssList) throws IOException {
-        //using temporary directory as defined in spec: http://java.sun.com/developer/technicalArticles/Servlets/servletapi/
-        File tmpDir = (File) context.getExternalContext().getApplicationMap().get("javax.servlet.context.tempdir");
-        File file = File.createTempFile("remappedCSSResources", ".tmp.jar", tmpDir);
-        file.deleteOnExit();
-        JarOutputStream jar = new JarOutputStream(new FileOutputStream(file));
-        Iterator<CSSResourceEntry> i = cssList.iterator();
-        while (i.hasNext()) {
-            CSSResourceEntry next = i.next();
-            String path = next.getLocalPath();
-            byte[] content = next.getContent();
-            String resourcePath = path.replace("css", "rewritten.css");
-            jar.putNextEntry(new JarEntry("META-INF/resources/" + resourcePath));
-            jar.write(content);
-            jar.closeEntry();
-        }
-        jar.close();
-
-        return file;
-    }
-
-    private void rewriteRelativeImagePaths(List csss, List images) throws UnsupportedEncodingException {
-        Iterator<CSSResourceEntry> i = csss.iterator();
-        while (i.hasNext()) {
-            CSSResourceEntry css = i.next();
-            String content = css.getContentAsString("UTF-8");
-            String dir = css.getRelativeLocalDir();
-
-            Iterator<ResourceMapping> ri = images.iterator();
-            while (ri.hasNext()) {
-                ResourceMapping resourceMapping = ri.next();
-                String path = resourceMapping.getRelativeLocalPath();
-                if (path.startsWith(dir)) {
-                    String relativePath = path.substring(dir.length() + 1);
-                    String requestPath = resourceMapping.getRequestPath();
-                    content = content.replaceAll(relativePath, requestPath);
+            InputStream inCK = InputRichText.class.getClassLoader().getResourceAsStream(CK_EDITOR_ZIP);
+            ZipInputStream zipCK = new ZipInputStream(inCK);
+            ZipEntry entryCK;
+            while ((entryCK = zipCK.getNextEntry()) != null) {
+                if (!entryCK.isDirectory()) {
+                    ZipEntryCacheCK.put(entryCK.getName(), toByteArray(zipCK));
                 }
             }
-
-            css.setContentAsString(content, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    private void createResourceMapping(String path, List mappings, ResourceHandler resourceHandler) {
-        Resource resource = resourceHandler.createResource(path);
-        mappings.add(new ResourceMapping(path.substring(INPUTRICHTEXT_CKEDITOR_DIR.length()), resource.getRequestPath()));
+    private static byte[] toByteArray(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buf = new byte[4096];
+        int len = 0;
+        while ((len = input.read(buf)) > -1) output.write(buf, 0, len);
+        return output.toByteArray();
     }
 
-    private static class MappingsWriter extends UIOutputWriter {
-        private String code;
+    private static class NonPersistentValue implements Serializable {
+        private transient Object object;
 
-        public MappingsWriter(String code) {
-            super();
-            this.code = code;
-            this.setTransient(true);
+        private NonPersistentValue(Object object) {
+            this.object = object;
         }
 
-        public void encode(ResponseWriter writer, FacesContext context) throws IOException {
-            writer.startElement("script", this);
-            writer.writeAttribute("type", "text/javascript", null);
-            writer.writeText("try {" + code + " } catch (ex) { alert(ex) }", null);
-            writer.endElement("script");
-        }
-
-        //Convince PortletFaces Bridge that this is a valid script for
-        //inserting into the Portal head
-        public String getRendererType() {
-            return "javax.faces.resource.Script";
+        public Object getObject() {
+            return object;
         }
     }
+}
 
-    private static byte[] readIntoByteArray(InputStream in) throws IOException {
-        byte[] buffer = new byte[4096];
-        int bytesRead;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+class FCKJarResource extends JarResource {
 
-        while ((bytesRead = in.read(buffer)) != -1)
-            out.write(buffer, 0, bytesRead); // write
-        out.flush();
-
-        return out.toByteArray();
+    public FCKJarResource(String path) {
+        super(path);
     }
 
-    private static class ResourceMapping {
-        private String relativeLocalPath;
-        private String requestPath;
+    public void withOptions(Options options) throws IOException {
 
-        private ResourceMapping(String relativeLocalPath, String requestPath) {
-            this.relativeLocalPath = relativeLocalPath;
-            this.requestPath = requestPath;
-        }
-
-        public String getRelativeLocalPath() {
-            return relativeLocalPath;
-        }
-
-        public String getRequestPath() {
-            return requestPath;
-        }
-    }
-
-    private static class CSSResourceEntry {
-        private String localPath;
-        private byte[] content;
-
-        private CSSResourceEntry(String localPath, byte[] content) {
-            this.localPath = localPath;
-            this.content = content;
-        }
-
-        public String getLocalPath() {
-            return localPath;
-        }
-
-        public String getRelativeLocalPath() {
-            return localPath.substring(INPUTRICHTEXT_CKEDITOR_DIR.length());
-        }
-
-        public String getRelativeLocalDir() {
-            int position = localPath.lastIndexOf("/");
-            return INPUTRICHTEXT_CKEDITOR_DIR.length() > position ? "/" : localPath.substring(INPUTRICHTEXT_CKEDITOR_DIR.length(), position);
-        }
-
-        public byte[] getContent() {
-            return content;
-        }
-
-        public String getContentAsString(String encoding) throws UnsupportedEncodingException {
-            return new String(content, encoding);
-        }
-
-        public void setContentAsString(String newContent, String encoding) throws UnsupportedEncodingException {
-            content = newContent.getBytes(encoding);
-        }
     }
 }
