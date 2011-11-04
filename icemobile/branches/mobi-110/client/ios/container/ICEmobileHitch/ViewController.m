56b3d04fd0c4fdae16dd208acffe147e99dbb495
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-04 22:10:14 +0000

    ICEmobileHitch verified launching camera (MOBI-110)

---
+++ icemobile/branches/mobi-110/client/ios/container/ICEmobileHitch/ViewController.m
@@ -0,0 +1,69 @@
+//
+//  ViewController.m
+//  ICEmobileHitch
+//
+//  Created by Ted Goddard on 11-11-04.
+//  Copyright (c) 2011 ICEsoft Technologies, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NativeInterface.h"
+
+@implementation ViewController
+@synthesize nativeInterface;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.nativeInterface = [[NativeInterface alloc] init];
+    self.nativeInterface.controller = self;
+    self.nativeInterface.uploading = NO;
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
