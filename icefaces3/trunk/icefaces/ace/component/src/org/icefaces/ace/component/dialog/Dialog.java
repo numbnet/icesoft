d817e1a28cd75000d5322f0fb164e32f5d95edb0
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 18:57:32 +0000

    ICE-7047: Add new ace:dialog component.

---
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/dialog/Dialog.java
@@ -0,0 +1,118 @@
+/*
+ * Original Code developed and contributed by Prime Technology.
+ * Subsequent Code Modifications Copyright 2011 ICEsoft Technologies Canada Corp. (c)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM
+ *
+ * Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c).
+ *
+ * Code Modification 1: Integrated with ICEfaces Advanced Component Environment.
+ * Contributors: ICEsoft Technologies Canada Corp. (c)
+ *
+ * Code Modification 2: [ADD BRIEF DESCRIPTION HERE]
+ * Contributors: ______________________
+ * Contributors: ______________________
+ */
+
+/*
+ * Generated, Do Not Modify
+ */
+
+package org.icefaces.ace.component.dialog;
+
+import javax.faces.context.FacesContext;
+import javax.faces.component.UINamingContainer;
+import javax.el.ValueExpression;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import org.icefaces.ace.event.CloseEvent;
+import org.icefaces.ace.util.Constants;
+
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.FacesEvent;
+import javax.faces.event.PhaseId;
+
+public class Dialog extends DialogBase implements javax.faces.component.behavior.ClientBehaviorHolder {
+
+	private static final String OPTIMIZED_PACKAGE = "org.icefaces.ace.component.";
+
+    private final static String DEFAULT_EVENT = "close";
+
+    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList(DEFAULT_EVENT));
+
+    @Override
+    public Collection<String> getEventNames() {
+        return EVENT_NAMES;
+    }
+
+    @Override
+    public String getDefaultEventName() {
+        return DEFAULT_EVENT;
+    }
+
+    @Override
+    public void queueEvent(FacesEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        String eventName = context.getExternalContext().getRequestParameterMap().get(Constants.PARTIAL_BEHAVIOR_EVENT_PARAM);
+
+        if(eventName != null && eventName.equals("close") && event instanceof AjaxBehaviorEvent) {
+            setVisible(false);
+            CloseEvent closeEvent = new CloseEvent(this, ((AjaxBehaviorEvent) event).getBehavior());
+            closeEvent.setPhaseId(PhaseId.APPLY_REQUEST_VALUES);
+            super.queueEvent(closeEvent);
+            context.renderResponse();       //just process the close event and skip to response
+        } else {
+            super.queueEvent(event);
+        }
+    }
+
+    @Override
+    public void processDecodes(FacesContext context) {
+        if(isSelfRequest(context)) {
+            this.decode(context);
+        }
+        else {
+            super.processDecodes(context);
+        }
+    }
+
+    @Override
+    public void processValidators(FacesContext context) {
+        if(!isSelfRequest(context)) {
+            super.processValidators(context);
+        }
+    }
+
+    @Override
+    public void processUpdates(FacesContext context) {
+        if(!isSelfRequest(context)) {
+            super.processUpdates(context);
+        }
+    }
+
+    private boolean isSelfRequest(FacesContext context) {
+        return this.getClientId(context).equals(context.getExternalContext().getRequestParameterMap().get(Constants.PARTIAL_SOURCE_PARAM));
+    }
+
+	protected FacesContext getFacesContext() {
+		return FacesContext.getCurrentInstance();
+	}
+
+}
