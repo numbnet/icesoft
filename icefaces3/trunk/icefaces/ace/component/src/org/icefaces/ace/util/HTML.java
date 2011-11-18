8b8372009b6d311578f8b190fa123c2fc7b4769a
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 18:13:22 +0000

    Added / updated ACE component utility packages for ICEfaces 3.0.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/util/HTML.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/util/HTML.java
@@ -10,14 +10,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions andd
  * limitations under the License.
  */
 
 package org.icefaces.ace.util;
-
-import org.icefaces.ace.util.ArrayUtils;
-
 
 /**
  * Constant declarations for HTML rendering.
@@ -507,4 +504,117 @@ public static final String[] MESSAGE_PASSTHROUGH_ATTRIBUTES =
     public static final String HREF_PARAM_SEPARATOR = "&";
     public static final String HREF_PARAM_NAME_FROM_VALUE_SEPARATOR = "=";
 
+	public static String[] CLICK_EVENT = {"onclick"};
+
+	public static String[] BLUR_FOCUS_EVENTS = {
+		"onblur",
+		"onfocus"
+	};
+
+	public static String[] CHANGE_SELECT_EVENTS = {
+		"onchange",
+		"onselect"
+	};
+
+	public static String[] COMMON_EVENTS = {
+		"onclick",
+		"ondblclick",
+		"onkeydown",
+		"onkeypress",
+		"onkeyup",
+		"onmousedown",
+		"onmousemove",
+		"onmouseout",
+		"onmouseover",
+		"onmouseup"
+	};
+
+	//StyleClass is omitted
+	public static String[] IMG_ATTRS_WITHOUT_EVENTS = {
+		"alt",
+		"width",
+		"height",
+		"title",
+		"dir",
+		"lang",
+		"ismap",
+		"usemap",
+		"style"
+	};
+
+	//StyleClass is omitted
+	public static String[] LINK_ATTRS_WITHOUT_EVENTS = {
+		"accesskey",
+		"charset",
+		"coords",
+		"dir",
+		"disabled",
+		"hreflang",
+		"rel",
+		"rev",
+		"shape",
+		"tabindex",
+		"style",
+		"target",
+		"title",
+		"type"
+	};
+
+	//StyleClass is omitted
+	public static String[] BUTTON_ATTRS_WITHOUT_EVENTS = {
+		"accesskey",
+		"alt",
+		"dir",
+		"label",
+		"lang",
+		"style",
+		"tabindex",
+		"title",
+		"type"
+	};
+
+	//StyleClass is omitted
+	public static String[] MEDIA_ATTRS = {
+		"height",
+		"width",
+		"style"
+	};
+
+    //disabled, readonly, style, styleClass handles by component renderer
+	public static String[] INPUT_TEXT_ATTRS_WITHOUT_EVENTS = {
+		"accesskey",
+		"alt",
+        "autocomplete",
+		"dir",
+		"lang",
+		"maxlength",
+		"size",
+		"tabindex",
+		"title"
+	};
+
+    public static String[] SELECT_ATTRS_WITHOUT_EVENTS = {
+		"accesskey",
+		"dir",
+		"disabled",
+		"lang",
+		"readonly",
+		"style",
+		"tabindex",
+		"title"
+	};
+
+	public static String[] TEXTAREA_ATTRS = {
+		"cols",
+		"rows"
+	};
+
+	public static String[] LINK_EVENTS = ArrayUtils.concat(COMMON_EVENTS, BLUR_FOCUS_EVENTS);
+	public static String[] BUTTON_EVENTS = ArrayUtils.concat(LINK_EVENTS, CHANGE_SELECT_EVENTS);
+	public static String[] IMG_ATTRS = ArrayUtils.concat(IMG_ATTRS_WITHOUT_EVENTS, COMMON_EVENTS);
+	public static String[] LINK_ATTRS = ArrayUtils.concat(LINK_ATTRS_WITHOUT_EVENTS, LINK_EVENTS);
+	public static String[] BUTTON_ATTRS = ArrayUtils.concat(BUTTON_ATTRS_WITHOUT_EVENTS, BUTTON_EVENTS);
+	public static final String[] INPUT_TEXT_ATTRS = ArrayUtils.concat(INPUT_TEXT_ATTRS_WITHOUT_EVENTS, COMMON_EVENTS, CHANGE_SELECT_EVENTS, BLUR_FOCUS_EVENTS);
+    public static final String[] INPUT_TEXTAREA_ATTRS = ArrayUtils.concat(INPUT_TEXT_ATTRS, TEXTAREA_ATTRS);
+    public static final String[] SELECT_ATTRS = ArrayUtils.concat(SELECT_ATTRS_WITHOUT_EVENTS, COMMON_EVENTS, CHANGE_SELECT_EVENTS, BLUR_FOCUS_EVENTS);
 }
