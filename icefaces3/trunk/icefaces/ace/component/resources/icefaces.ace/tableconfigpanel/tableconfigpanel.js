b90e8703bb268ce251ad84fb0f15fc6470684d5f
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 18:02:51 +0000

    ICE-7005 - New Table Configuration Panel (ace:tableConfigPanel) component for ace:dataTable.

---
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/tableconfigpanel/tableconfigpanel.js
@@ -0,0 +1,251 @@
+if (!window.ice['ace']) {
+    window.ice.ace = {};
+}
+ice.ace.TableConf = function (id, cfg) {
+    this.id = ice.ace.escapeClientId(id.replace("_tableconf", ""));
+    this.cfg = cfg;
+    this.sortOrder = [];
+    ice.ace.jq(this.id+'_tableconf').draggable();
+
+    if (cfg.reorderable) {
+        // Return a helper with preserved width of cells
+        var fixHelper = function(e, ui) {
+            ui.children().each(function() {
+                ice.ace.jq(this).width(ice.ace.jq(this).width());
+            });
+            return ui;
+        };
+
+
+        ice.ace.jq(this.id+'_tableconf > .ui-tableconf-body > table > tbody')
+                .sortable({
+                    axis:'y',
+                    containment:'parent',
+                    helper: fixHelper,
+                    items:'tr:not(.ui-disabled)',
+                    handle:'.ui-sortable-handle:not(.ui-disabled)'
+                });
+    }
+
+    if (cfg.sortable) {
+        var _self = this;
+         ice.ace.jq(this.id+'_tableconf > .ui-tableconf-body tr:not(.ui-disabled) .ui-sortable-control')
+                .click(function(event, altY, altMeta) {
+                    event.stopPropagation();
+                    var $this = ice.ace.jq(this),
+                        topCarat = $this.find(".ui-icon-triangle-1-n")[0],
+                        bottomCarat = $this.find(".ui-icon-triangle-1-s")[0],
+                        controlCell =  $this.parent().parent(),
+                        controlRow  =  ice.ace.jq(controlCell).parent(),
+                        controlOffset = $this.offset(),
+                        controlHeight = !_self.cfg.singleSort ? $this.outerHeight() : 22,
+                        descending = false,
+                        metaKey = (altMeta == undefined) ? event.metaKey : altMeta;
+
+                    // altY and altMeta allow these event parameters to be optionally passed in
+                    // from an event triggering this event artificially
+                    var eventY = (altY == undefined) ? event.pageY : altY;
+                    if (eventY > (controlOffset.top + (controlHeight / 2)+2))
+                        descending = true;
+
+                    // If we are looking a freshly rendered DT initalize our JS sort state
+                    // from the state of the rendered controls
+                    if (_self.sortOrder.length == 0) {
+                        ice.ace.jq(_self.id+'_tableconf > .ui-tableconf-body .ui-sortable-control').each(function() {
+                            var $this = ice.ace.jq(this);
+                            if (ice.ace.getOpacity($this.find('.ui-icon-triangle-1-n')[0]) == 1 ||
+                                ice.ace.getOpacity($this.find('.ui-icon-triangle-1-s')[0]) == 1 )
+                                _self.sortOrder.splice(
+                                    parseInt($this.find('.ui-sortable-column-order').html())-1,
+                                    0,
+                                    $this.closest('.ui-header-column')
+                                );
+                        });
+                    }
+
+                     if (!metaKey || _self.cfg.singleSort) {
+                         // Remake sort criteria
+                         // Reset all other arrows
+                         _self.sortOrder = [];
+                         controlRow.siblings()
+                                 .find('.ui-icon-triangle-1-n, .ui-icon-triangle-1-s')
+                                 .animate({opacity : .2}, 200)
+                                 .removeClass('ui-toggled');
+
+                         if (!_self.cfg.singleSort) controlRow.siblings()
+                                 .find('.ui-sortable-column-order')
+                                 .html('&#160;');
+                         // remove previous gradients
+                         //headerCell.siblings().removeClass('ui-state-active').find('.ui-sortable-column-icon').removeClass('ui-icon-triangle-1-n ui-icon-triangle-1-s');
+                     }
+
+                    var rowFound = false;
+                    ice.ace.jq(_self.sortOrder).each(function() {
+                        if (controlRow.attr('class') === this.attr('class')) { rowFound = true; } });
+
+                    // if meta clicking a currently sorted row
+                    if (metaKey && rowFound) {
+                        // if deselecting
+                        if ((ice.ace.getOpacity(topCarat) == 1 && !descending) ||
+                            (ice.ace.getOpacity(bottomCarat) == 1 && descending)) {
+                             // Remove from sort order
+                             _self.sortOrder.splice(controlCell.find('.ui-sortable-column-order').html()-1,1);
+                             ice.ace.jq(bottomCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                             ice.ace.jq(topCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                             if (!_self.cfg.singleSort) {
+                                 controlCell.find('.ui-sortable-column-order').html('&#160;');
+                                 var i = 0;
+                                 ice.ace.jq(_self.sortOrder).each(function(){
+                                    this.find('.ui-sortable-column-order').html(parseInt(i++)+1);
+                                 });
+                             }
+                        } else {
+                            // Not a deselect, just meta toggle
+                            if (descending) {
+                                ice.ace.jq(bottomCarat).animate({opacity : 1},  200).addClass('ui-toggled');
+                                ice.ace.jq(topCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                            } else {
+                                ice.ace.jq(topCarat).animate({opacity : 1},  200).addClass('ui-toggled');
+                                ice.ace.jq(bottomCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                            }
+                        }
+                    // if not a deselect
+                    } else {
+                         // add header gradient
+                         //headerCell.addClass('ui-state-active');
+                         if (descending) {
+                             ice.ace.jq(bottomCarat).animate({opacity : 1},  200).addClass('ui-toggled');
+                             ice.ace.jq(topCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                         } else {
+                             ice.ace.jq(topCarat).animate({opacity : 1},  200).addClass('ui-toggled');
+                             ice.ace.jq(bottomCarat).animate({opacity : .2},  200).removeClass('ui-toggled');
+                         }
+
+                         // add to sort order
+                         var rowFound = false;
+                         ice.ace.jq(_self.sortOrder).each(
+                                 function() { if (controlRow.attr('class') === this.attr('class')) { rowFound = true; } });
+                         if (!rowFound) {
+                             var order = _self.sortOrder.push(controlRow);
+                             // write control display value
+                             if (!_self.cfg.singleSort) controlCell.find('.ui-sortable-column-order').html(order);
+                         }
+                    }
+                });
+
+        // Pre-fade and bind keypress to kb-navigable sort icons
+        ice.ace.jq(this.id+'_tableconf > .ui-tableconf-body tr:not(.ui-disabled) .ui-sortable-control')
+                .find('.ui-icon-triangle-1-n')
+                .keypress(function(event) {
+                    if (event.which == 32 || event.which == 13) {
+                        var $currentTarget = ice.ace.jq(event.currentTarget);
+                        $currentTarget.closest('.ui-sortable-control')
+                                .trigger('click', [$currentTarget.offset().top, event.metaKey]);
+                }}).not('.ui-toggled').fadeTo(0, 0.2);
+        ice.ace.jq(this.id+'_tableconf > .ui-tableconf-body tr:not(.ui-disabled) .ui-sortable-control')
+                .find('.ui-icon-triangle-1-s')
+                .keypress(function(event) {
+                    if (event.which == 32 || event.which == 13) {
+                        var $currentTarget = ice.ace.jq(event.currentTarget);
+                        $currentTarget.closest('.ui-sortable-control')
+                                .trigger('click', [$currentTarget.offset().top + 6, event.metaKey]);
+                }}).not('.ui-toggled').fadeTo(0, 0.2);
+    }
+}
+
+ice.ace.TableConf.prototype.getSortAscending= function() {
+    var sortAsc = {}, maxOrder = 0;
+    ice.ace.jq(this.id+'_tableconf tr[class^="ui-tableconf-row"] .ui-sortable-column-icon').each(function(i, val) {
+        var $val = ice.ace.jq(val),
+            $order = ice.ace.jq(ice.ace.jq($val.closest('.ui-sortable-control')).find('.ui-sortable-column-order')[0]),
+            topCarat = $val.find('.ui-icon-triangle-1-n')[0],
+            bottomCarat = $val.find('.ui-icon-triangle-1-s')[0];
+        if (ice.ace.getOpacity(topCarat) == 1 || ice.ace.getOpacity(bottomCarat) == 1) {
+            sortAsc[parseInt($order.html())] = (ice.ace.getOpacity(topCarat) == 1);
+            maxOrder++;
+        }
+    });
+
+    var ascList = [], i = 0;
+    while (i < maxOrder) {
+        i++;
+        ascList.push(sortAsc[i]);
+    }
+
+    return ascList;
+}
+
+ice.ace.TableConf.prototype.getSortOrder = function() {
+    var sortOrders = {},
+        _self = this,
+        maxOrder = 0;
+
+    // collect order and id for all sorted controls
+    ice.ace.jq(this.id+'_tableconf tr[class^="ui-tableconf-row"] .ui-sortable-column-icon').each(function(i, val) {
+        var $val = ice.ace.jq(val),
+            topCarat = $val.find('.ui-icon-triangle-1-n')[0],
+            bottomCarat = $val.find('.ui-icon-triangle-1-s')[0],
+            $row = ice.ace.jq($val.closest('tr')),
+            $order = ice.ace.jq($row.find('.ui-sortable-column-order')[0]);
+
+        if (ice.ace.getOpacity(topCarat) == 1 || ice.ace.getOpacity(bottomCarat) == 1) {
+            sortOrders[parseInt($order.html())] =
+                    (ice.ace.jq(_self.id + " thead tr .ui-header-column")
+                    [parseInt($row.attr('class').split(" ")[0].replace("ui-tableconf-row-",""))].getAttribute('id'));
+            maxOrder++;
+        }
+    });
+
+    // reorder ids according to order
+    var sortList = [], i = 0;
+    while (i < maxOrder) {
+        i++;
+        sortList.push(sortOrders[i]);
+    }
+
+    return sortList;
+}
+
+ice.ace.TableConf.prototype.getColOrder = function() {
+    var columnOrders = [];
+    ice.ace.jq(this.id+'_tableconf tr[class^="ui-tableconf-row"]').each(function(i, val) {
+        columnOrders.push(ice.ace.jq(val).attr('class').split(" ")[0].replace("ui-tableconf-row-",""));
+    });
+    return columnOrders;
+}
+
+ice.ace.TableConf.prototype.submitTableConfig = function (target) {
+    var id = this.id.replace("#","").replace(/\\/g,""),
+        body = ice.ace.jq(target).parents('.ui-tableconf').find('.ui-tableconf-body:first'),
+        options = {
+            source: id,
+            execute: id,
+            render: id
+        };
+
+    options.onsuccess = function(responseXML) {
+        ice.ace.selectCustomUpdates(responseXML, function(id, content) {
+            ice.ace.AjaxUtils.updateElement(id, content);
+        });
+        return false;
+    };
+
+    var params = {}, panelConf = "";
+    if (this.cfg.reorderable) panelConf += " colOrd";
+    if (this.cfg.sortable) panelConf += " colSor";
+    if (this.cfg.naming) panelConf += " colName";
+    if (this.cfg.visibility) panelConf += " colVis";
+    if (this.cfg.position == "first-col")
+        panelConf += " butPos-first-col";
+    else if (this.cfg.position == "last-col")
+        panelConf += " butPos-last-col";
+
+    params[id+"_tableconf"] = panelConf;
+    params[id+'_colorder'] = this.getColOrder();
+    params[id+'_sortKeys'] = this.getSortOrder();
+    params[id+'_sortDirs'] = this.getSortAscending();
+    params['ice.customUpdate'] = id;
+
+    options.params = params;
+    ice.ace.AjaxRequest(options);
+}
---
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/tableconfigpanel/TableConfigPanel.java
@@ -0,0 +1,39 @@
+package org.icefaces.ace.component.tableconfigpanel;
+
+
+import org.icefaces.ace.component.datatable.DataTable;
+
+import javax.faces.FacesException;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+public class TableConfigPanel extends TableConfigPanelBase {
+    // Find component cache
+    private DataTable table;
+
+    public DataTable getTargetedDatatable() {
+        if (this.table != null) return table;
+
+        String target = this.getFor();
+        if (target == null) {
+            // If nested in table
+            UIComponent parent = this;
+            while ((parent = parent.getParent()) != null)
+                if (parent instanceof DataTable) {
+                    ((DataTable) parent).setTableConfigPanel(this);
+                    this.table = (DataTable)parent;
+                    return (DataTable)parent;
+                }
+            throw new FacesException("TableConfigPanel: Must be nested within a DataTable or target one using the 'for' attribute.");
+        }
+
+        DataTable table = (DataTable)this.findComponent(target);
+        
+        if (table == null) throw new FacesException("TableConfigPanel: DataTable with clientId determined by 'for' attribute value '" + target + "' could not be found.");
+
+        table.setTableConfigPanel(this);
+        this.table = table;
+        return table;
+    }
+}
