7b0c6b4d8d496c725b29e755aeb45860a4a89ffd
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 15:34:49 +0000

    Establishing new icefaces3 trunk svn repository.

---
+++ icefaces3/trunk/trunk/icefaces/ace/test/src/org/icefaces/UIDataText/SliderDataBean.java
@@ -0,0 +1,98 @@
+package org.icefaces.UIDataText;
+
+import org.icefaces.ace.component.sliderentry.SliderEntry;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIColumn;
+
+
+/**
+ * Slider data backing bean for dynamic changing values
+ * @since 1.5
+ */
+@ManagedBean (name="sliderDataBean")
+@ViewScoped
+public class SliderDataBean implements Serializable {
+
+
+    List sliderValues = new ArrayList();
+
+    private String blueClass = "blue_box";
+    private String redClass = "red_box";
+    private String greenClass = "green_box";
+
+
+    public SliderDataBean(){
+        for (int idx = 0; idx < 3; idx ++ ) {
+            sliderValues.add( new SliderHolder() );
+        }
+    }
+
+    public void setToBlue(ActionEvent ae) {
+
+        UIComponent comp = ae.getComponent();
+        while ((comp = comp.getParent()) != null) {
+            if (comp instanceof UIColumn) {
+                UIColumn uic = (UIColumn) comp;
+                List<UIComponent> children = comp.getChildren();
+                for (UIComponent child: children) {
+                    if (child instanceof SliderEntry) {
+                        SliderEntry se = (SliderEntry) child;
+                        se.setStyleClass( blueClass );
+                        return;
+                    }
+                }
+            } 
+        }
+    }
+
+     public void setToRed(ActionEvent ae) {
+
+        UIComponent comp = ae.getComponent();
+        while ((comp = comp.getParent()) != null) {
+            if (comp instanceof UIColumn) {
+                UIColumn uic = (UIColumn) comp;
+                List<UIComponent> children = comp.getChildren();
+                for (UIComponent child: children) {
+                    if (child instanceof SliderEntry) {
+                        SliderEntry se = (SliderEntry) child;
+                        se.setStyleClass( redClass );
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void setToGreen(ActionEvent ae) {
+
+        UIComponent comp = ae.getComponent();
+        while ((comp = comp.getParent()) != null) {
+            if (comp instanceof UIColumn) {
+                UIColumn uic = (UIColumn) comp;
+                List<UIComponent> children = comp.getChildren();
+                for (UIComponent child: children) {
+                    if (child instanceof SliderEntry) {
+                        SliderEntry se = (SliderEntry) child;
+                        se.setStyleClass( greenClass );
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public List getSliderValues() {
+        return sliderValues;
+    }
+
+}
---
+++ icefaces3/trunk/trunk/icefaces/tutorials/internationalization/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>ICEfaces Internationalization Example</title>
+    </head>
+    <body>
+        <jsp:forward page="/main.jsf"/>
+    </body>
+</html>
