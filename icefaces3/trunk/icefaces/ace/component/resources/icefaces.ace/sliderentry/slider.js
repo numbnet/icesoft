7124bf47a97ad28a5fd6efc10414f5e98c83b4e4
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 19:39:40 +0000

    ICE-7462 added support for ace:ajax to ace:sliderEntry ICE-7209 re-implemented ace:sliderEntry, based on jquery, added ThemeRoller styling support also. ICE-6970 Adopted Apache 2.0 license source-code headers for ACE source-code.

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/sliderentry/slider.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/sliderentry/slider.js
@@ -1,285 +1,136 @@
-/*
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under
- * the License.
- *
- * The Original Code is ICEfaces 1.5 open source software code, released
- * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
- * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
- * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
- *
- * Contributor(s): _____________________.
+/* 
+* Original Code developed and contributed by Prime Technology. 
+* Subsequent Code Modifications Copyright 2011 ICEsoft Technologies Canada Corp. (c) 
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+* 
+* http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+* 
+* NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM 
+* 
+* Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c). 
+* 
+* Code Modification 1: Integrated with ICEfaces Advanced Component Environment. 
+* Contributors: ICEsoft Technologies Canada Corp. (c) 
+* 
+* Code Modification 2: [ADD BRIEF DESCRIPTION HERE] 
+* Contributors: ______________________ 
+* Contributors: ______________________ 
+* 
+*/
+
+/**
+ *  Slider Widget
  */
+ice.ace.Slider = function(id, cfg) {
+	this.id = id;
+	this.cfg = cfg;
+    this.jq = jQuery(ice.ace.escapeClientId(this.id));
+	this.input = jQuery(ice.ace.escapeClientId(this.cfg.input));
+	if(this.cfg.output) {
+		this.output = jQuery(ice.ace.escapeClientId(this.cfg.output));
+	}
+    var _self = this;
+    
+    //Create slider
+	if (this.cfg.clickableRail == false) {
+		this.jq.mousedown(function(event) { if (event.target == this) event.stopImmediatePropagation(); }).slider(this.cfg);
+	} else {
+		this.jq.slider(this.cfg);
+	}
+	var handle = jQuery(ice.ace.escapeClientId(this.id) + " > a");
+	if (this.cfg.tabindex) handle.attr('tabindex', this.cfg.tabindex);
 
-ice.component.slider = {
-   initialize:function(clientId, yuiProps, jsfProps, bindYUI) {
-        
-        //get the slider html element
-        var root = document.getElementById(clientId);
-        
-        //for short cut and to make it more like OOP
-        //var super = ice.yui3;
+    //Slide handler
+	this.jq.bind('slide', function(event, ui) {
+        _self.onSlide(event, ui);
+    });
 
-        var hiddenFieldId = clientId+"_hidden";
+    //Slide start handler
+    if(this.cfg.onSlideStart) {
+        this.jq.bind('slidestart', function(event, ui) {_self.cfg.onSlideStart.call(this, event, ui);});
+    }
+	if (this.cfg.behaviors) {
+		if (this.cfg.behaviors.slidestart) {
+			this.jq.bind('slidestart', function(event, ui) { _self.cfg.behaviors.slidestart.call(_self, event); });
+		}
+	}
 
-        //set a callback to create slider component 
-        ice.yui3.use(function(Y){
-            Y.use('slider', function(Yui) {
-        	    Y.on('domready', function(){
-				    var hiddenField = document.getElementById(clientId+"_hidden");
-				
-					try {
-			            var obj = new Y.Slider({
-							//following two properties has to be set when initializing componnent
-							axis: yuiProps.axis,
-							thumbUrl: jsfProps.thumbUrl
-							});
-	 
-						obj.updateLabels = function() {
-							obj.updateMinLabel();
-							obj.updateMaxLabel();
-							obj.updateMidLabel();
-						}	
-						obj.updateMinLabel = function(min) {
-							var nodes = obj.get('railRef').get('children');
-				 			nodes.item(0).setContent("<div class='sldrLblMn'>"+ yuiProps.min + '</div>');
-						}
-						
-						obj.updateMaxLabel = function(max) {
-							var nodes = obj.get('railRef').get('children');
-							nodes.item(1).setContent("<div class='sldrLblMx'>"+ yuiProps.max+ '</div>');
-						}
-						
-						obj.updateMidLabel = function(mid) {
-							if (!obj.get('midLblNode')) {
-							    if (yuiProps.axis == 'y') {
-									obj.get('railRef').append("<div class='sldrLblMid'></div>");	
-									obj.set('midLblNode', obj.get('railRef').get('children').item(3));								
-								} else {
-									obj.get('railRef').append("<center><div class='sldrLblMid'></div></center>");	
-									obj.set('midLblNode', obj.get('railRef').get('children').item(3).get('children').item(0));
-								}
-							}
-							obj.get('midLblNode').setContent(parseInt(((yuiProps.max - yuiProps.min)/2)+yuiProps.min));
-						}
-						if (jsfProps.showLabels) {						
-							obj.renderRail_ = obj.renderRail;
-							obj.renderRail = function() {
-								var railNode = obj.renderRail_();
-			
-							    if (!railNode.hasClass("sldrLbl")) {
-									railNode.addClass("sldrLbl");
-								}
-							 						
+    //Slide end handler
+    this.jq.bind('slidestop', function(event, ui) {_self.onSlideEnd(event, ui);});
+}
+
+ice.ace.Slider.prototype.onSlide = function(event, ui) {
+    //User callback
+    if(this.cfg.onSlide) {
+        this.cfg.onSlide.call(this, event, ui);
+    }
+
+    //Update input and output(if defined)
+	this.input.val(ui.value);
 	
-								obj.set('railRef', railNode);
-								return railNode;
-							}
-						}
-						
-						obj.render(root);
-                        obj.set('disabled', yuiProps.disabled);
-                        if (jsfProps.showLabels) {
-							obj.updateLabels();
-	                        obj.on('minChange', function(prop) {
-								yuiProps.min = prop.newVal;
-								obj.updateLabels();
-							});
-				            obj.on('maxChange', function(prop) {
-								yuiProps.max = prop.newVal;
-								obj.updateLabels();
-							});						
-                        }
-				
-					} catch(e){alert(e)}
-						
-					var invokeSubmit = function (event) {
-						var context = ice.component.getJSContext(clientId);
-						var sJSFProps = context.getJSFProps();
-						if (sJSFProps.singleSubmit) {
-							var sObj = context.getComponent();
-							var postParameters = sJSFProps.postParameters;
-							ice.se(event, root, function(param) {
-								param(hiddenFieldId, sObj.get('value'));
-								param('ice.focus', ice.focus);
-								param('onevent', function(data) { 
-									if (data.status == 'success') {
-										if (ice.focus == clientId) {
-											root.firstChild.focus();
-										}
-									}
-								});
-								if (postParameters != null) {
-									var argCount = postParameters.length / 2;
-									for (var idx =0; idx < argCount; idx ++ ) {
-										param( postParameters[idx*2], postParameters[(idx*2)+1] );
-									}
-								}
-							});
-						}  						
-					}
-						
-						obj.after("focus", function() {
-							ice.focus = clientId;
-						});
-						
-						obj.after("blur", function() {
-							ice.focus = "";
-						});
-						
-					    var eventName;
-						obj.after("valueChange", function(event) {
-                            var context = ice.component.getJSContext(clientId);
-                            var vcObj = context.getComponent();
-						    var sliderValue = vcObj.get('value');
-		                    //if aria is enabled, then set aria property so screen reader can pick it
-                            var vcJSFProps = context.getJSFProps();
-                            if (vcJSFProps.aria) {
-		                        // Because we're in a callback, to be safe we'll lookup
-		                        //  the current root element again, instead of using
-		                        //  the old root reference, which might be stale.
-		                        document.getElementById(clientId).firstChild.setAttribute("aria-valuenow", sliderValue);
-		                    }
+	if(this.output) {
+		this.output.html(ui.value);
+	}
+	
+	if (this.cfg.behaviors) {
+		if (this.cfg.behaviors.slide) {
+			this.cfg.behaviors.slide.call(this, event);
+		}
+	}
+}
 
-		                    // Strategy is now to use hidden field rather than
-		                    // request map value
+ice.ace.Slider.prototype.onSlideEnd = function(event, ui) {
+    //User callback
+    if(this.cfg.onSlideEnd) {
+        this.cfg.onSlideEnd.call(this, event, ui);
+    }
 
-                            var hiddenField = document.getElementById(hiddenFieldId);
-		                    hiddenField.value=sliderValue;
-                            var shouldSubmit = (eventName == 'railMouseDown');
-                            eventName = "";
-							if (shouldSubmit) {
-								invokeSubmit(event);
-							}
-						});
-						
-						obj.before("railMouseDown", function() {
-                            root.firstChild.focus();
-						});
-						obj.after("railMouseDown", function() {
-						  eventName = "railMouseDown";
-						});
+    if(this.cfg.ajaxSlide) {
+        var options = {
+            source : this.id,
+            execute: this.id + ' ' + this.cfg.input,
+            formId: this.cfg.formId
+        };
 
-						obj.after("slideEnd", function(event) {
-							invokeSubmit(event);
-						});
+        if(this.cfg.onSlideEndUpdate) {
+            options.render = this.cfg.onSlideEndUpdate;
+        }
 
+        var params = {};
+        params[this.id + '_ajaxSlide'] = true;
+        params[this.id + '_ajaxSlideValue'] = ui.value;
 
-                        if (Yui.Lang.isValue(jsfProps.tabindex)) {
-                            Yui.one(root).one(".yui3-slider-thumb").set("tabIndex", jsfProps.tabindex);
-                        }
-		                //add aria support
-		                if (jsfProps.aria) {
-		                    //add roles and attributes to the YUI slider widget
-		                    root = document.getElementById(clientId);
-		                    root.firstChild.setAttribute("role", "slider");
-		                    root.firstChild.setAttribute("aria-valuemin", yuiProps.min);
-		                    root.firstChild.setAttribute("aria-valuemax", yuiProps.max);
-		                    root.firstChild.setAttribute("aria-valuenow",yuiProps.value );
+        ice.ace.AjaxRequest(this.cfg.url, options, params);
+    }
+	
+	if (this.cfg.behaviors) {
+		if (this.cfg.behaviors.slideend) {
+			this.cfg.behaviors.slideend.call(this, event);
+		}
+	}
+}
 
-		                    //TODO shouldn't it be configurable?
-							var keydownTimeoutHandler = null;
-		                    //listen for keydown event, to provide short-cut key support.
-		                    //react on left, right, up, down, home and end key 
-		                    Y.on("keydown", function(event) {
-		                        //get the current value of the slider
-                                var context = ice.component.getJSContext(clientId);
-                                var kbObj = context.getComponent();
-                                var valuenow = kbObj.get('value');
-//                                var valuenow = parseInt(root.firstChild.getAttribute("aria-valuenow"));
-		                        var valuebefore = valuenow;
+ice.ace.Slider.prototype.getValue = function() {
+    return this.jq.slider('value');
+}
 
-                                var kdYuiProps = context.getJSProps();
-                                var kdMin = kdYuiProps.min;
-                                var kdMax = kdYuiProps.max;
+ice.ace.Slider.prototype.setValue = function(value) {
+    this.jq.slider('value', value);
+}
 
-                                var kdJsfProps = context.getJSFProps();
-                                var stepPercent = kdJsfProps.stepPercent;
-                                var step = Math.round(
-                                    Math.abs(kdMax - kdMin) * stepPercent / 100);
+ice.ace.Slider.prototype.enable = function() {
+    this.jq.slider('enable');
+}
 
-		                        var isLeft = event.keyCode == 37;
-		                        var isUp = event.keyCode == 38;
-		                        var isRight = event.keyCode == 39;
-		                        var isDown = event.keyCode == 40;                        
-		                        var isHome = event.keyCode == 36;
-		                        var isEnd = event.keyCode == 35;
-
-                                if (isLeft || isDown) {
-                                    //decrease slider value
-                                    valuenow -= step;
-                                    if (valuenow < kdMin) {
-                                        valuenow = kdMin;
-                                    }
-		                        } else if (isRight || isUp) {
-		                            //increase slider value
-                                    valuenow += step;
-		                            if (valuenow > kdMax) {
-		                                valuenow = kdMax;
-		                            }
-		                        } else if (isHome) {
-		                           //get the min value of slider 
-		                           valuenow = kdMin;
-		                        } else if (isEnd) {
-		                           //get the max value of slider 
-		                           valuenow = kdMax;		
-		                        }
-
-		                        //if value changed?
-		                        if (valuebefore != valuenow)  { 
-		                            //update slider value on client
-//                                    var kbObj = context.getComponent();
-		                            kbObj.set('value', valuenow);
-		                            //notify server
-							        clearTimeout(keydownTimeoutHandler);	
-							        keydownTimeoutHandler = setTimeout (function(){
-											//invoke submit
-											invokeSubmit(event);		                            
-											keydownTimeoutHandler = null;
-										},  300);									
-		                            //cancel event
-		                        }
-                                if (isLeft || isUp || isRight || isDown || isHome || isEnd) {
-                                    event.halt();
-                                }
-		                    }, root);
-		                } 
-
-						Y.on("click", function(event) {
-							root.firstChild.focus();
-						}, root);
-						//bind the initilized js component, so it can be reused for later calls
-		                bindYUI(obj);
-		        });
- 			});
-        });
-   },
-   
-   //delegate call to ice.yui3.updateProperties(..)  with the reference of this lib
-   updateProperties:function(clientId, yuiProps, jsfProps, events) {
-      var context = ice.component.getJSContext(clientId);
-      if (context && context.isAttached()) {
-          var prevJSFProps = context.getJSFProps();
-          if (prevJSFProps.hashCode != jsfProps.hashCode) {
-              context.getComponent().destroy();
-              document.getElementById(clientId)['JSContext'] = null;
-              JSContext[clientId] = null;
-          }
-      }
-      ice.yui3.updateProperties(clientId, yuiProps, jsfProps, events, this);
-   },
- 
-   //delegate call to ice.yui3.getInstance(..) with the reference of this lib 
-   getInstance:function(clientId, callback) {
-       ice.yui3.getInstance(clientId, callback, this);
-   }
-};
-
+ice.ace.Slider.prototype.disable = function() {
+    this.jq.slider('disable');
+}
--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/sliderentry/SliderEntryMeta.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/sliderentry/SliderEntryMeta.java
@@ -1,37 +1,35 @@
 /*
- * Version: MPL 1.1
+ * Copyright 2010-2011 ICEsoft Technologies Canada Corp.
  *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under
- * the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * The Original Code is ICEfaces 1.5 open source software code, released
- * November 5, 2006. The Initial Developer of the Original Code is ICEsoft
- * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
- * 2004-2011 ICEsoft Technologies Canada, Corp. All Rights Reserved.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *
- * Contributor(s): _____________________.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.icefaces.ace.component.sliderentry;
 
-import javax.el.MethodExpression;
-
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
 
-import org.icefaces.ace.meta.annotation.Expression;
-import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
 import org.icefaces.ace.meta.annotation.Component;
 import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.annotation.Expression;
+import org.icefaces.ace.meta.annotation.ClientBehaviorHolder;
+import org.icefaces.ace.meta.annotation.ClientEvent;
+import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+import org.icefaces.ace.api.IceClientBehaviorHolder;
 
-// Each Meta class requires a @Component annotation
+import javax.el.MethodExpression;
+
 @Component(
         // The tag name, as it will be used in view definitions (.xhtml files)
         tagName ="sliderEntry",
@@ -54,51 +52,60 @@ import org.icefaces.ace.meta.annotation.Property;
                 "horizontal or vertical axis. It can be used as a visual replacement for an input box that takes a " +
                 "number as input. For more information, see the " +
                 "<a href=\"http://wiki.icefaces.org/display/ICE/SliderEntry\">Wiki doc</a>."
-    )
+        )
 @ResourceDependencies({
-    @ResourceDependency(name="yui/yui-min.js",library="yui/3_3_0"),
-	@ResourceDependency(name="loader/loader-min.js",library="yui/3_3_0"),
-    @ResourceDependency(name="util/combined.js",library="icefaces.ace"),
-    @ResourceDependency(name="slider/assets/slider-core.css",library="yui/3_3_0"),
-	@ResourceDependency(name="util/combined.css",library="icefaces.ace")
+	@ResourceDependency(library="icefaces.ace", name="jquery/ui/jquery-ui.css"),
+	@ResourceDependency(library="icefaces.ace", name="jquery/jquery.js"),
+	@ResourceDependency(library="icefaces.ace", name="jquery/ui/jquery-ui.js"),
+	@ResourceDependency(library="icefaces.ace", name="core/core.js"),
+	@ResourceDependency(library="icefaces.ace", name="sliderentry/slider.js")
 })
-
+@ClientBehaviorHolder(events = {
+	@ClientEvent(name="slidestart", javadoc="...", tlddoc="...", defaultRender="@all", defaultExecute="@all"),
+	@ClientEvent(name="slide", javadoc="...", tlddoc="...", defaultRender="@all", defaultExecute="@all"),
+	@ClientEvent(name="slideend", javadoc="...", tlddoc="...", defaultRender="@all", defaultExecute="@all")
+})
 public class SliderEntryMeta extends UIComponentBaseMeta {
-    // Every java field in the Meta class can be annotated to become either a
-    //  Property, Field or Facet, in the generatedClass.
-    // @Property annotations are for properties that should have a StateHelper,
-    //  getter method, setter method, and state saving code generated for it.
-    //  It should be a public property for the component tag.
-    // @Field annotations are for fields that are internal to the component,
-    //  to maintain state through-out a lifecycle, or even between lifecycles,
-    //  if it is not transient, and so will participate in state saving.
-    // @Facet annotations, on fields, provide a means of describing and
-    //  documenting component facets, in a standard way. @Facets annotations,
-    //  on inner classes, provides a means of using a facet specific namespace
-    //  that will not collide with samely named properties or fields.
-    
-    @Property (defaultValue="x", 
-            tlddoc="It could be either x, for horizontal or y, for vertical. Default value is set to x.")
-    private String axis;
-    
-    @Property (defaultValue="0", 
-            tlddoc="The minimum value of slider, default is 0.")
-    private int min;
-    
-    @Property (defaultValue="100",
-            tlddoc="The maximum value of slider, default is 100.")
-    private int max;
 
-    @Property (defaultValue="10.0f",
-            tlddoc="The percentage of the range (max-min) to shift the " +
-                    "value by when the up, down, left or right arrow keys " +
-                    "are pressed. Expressed as a percentage from 0 to 100.")
-    private float stepPercent;
-    
-    @Property (defaultValue="0",
-            tlddoc="The value of slider, default is 0.")
-    private int value;    
-    
+	@Property(tlddoc="")
+	private String widgetVar;
+	
+	@Property(tlddoc="", defaultValue="0")
+	private int min;
+	
+	@Property(tlddoc="", defaultValue="100")
+	private int max;
+	
+	@Property(tlddoc="")
+	private String style;
+	
+	@Property(tlddoc="")
+	private String styleClass;
+	
+	@Property(tlddoc="", defaultValue="true")
+	private boolean animate;
+	
+	@Property(tlddoc="", defaultValue="x")
+	private String axis;
+	
+	@Property(tlddoc="", defaultValue="1f")
+	private float stepPercent;
+	
+	@Property(tlddoc="", defaultValue="false")
+	private boolean disabled;
+	
+	@Property(tlddoc="")
+	private String onSlideStart;
+	
+	@Property(tlddoc="")
+	private String onSlide;
+	
+	@Property(tlddoc="")
+	private String onSlideEnd;
+	
+    @Property (tlddoc="The value of slider, default is 0.", defaultValue="0")
+    private int value;
+	
     @Property (defaultValue="150px",
             tlddoc="The physical length of slider, in pixels, default is 150px. " +
             "Note: If the range of the slider (max-min) is greater than the length, " +
@@ -106,32 +113,27 @@ public class SliderEntryMeta extends UIComponentBaseMeta {
             "If the discrepancy is too great, then arrow key stepping will not " +
             "precisely reflect the stepPercent property.")
     private String length;
-    
+	
+    @Property(defaultValue="true", tlddoc="Allows clicking on the rail to move the thumb. Default is true.")
+    private boolean clickableRail;
+	
+    @Property(defaultValue="false",
+            tlddoc="If set to true, it will show min, mid and max value labels. The default value is false.")
+    private boolean showLabels;
+	
     @Property(defaultValue="false",
             tlddoc="When singleSubmit is true, changing the value of this component will submit and execute this " +
                     "component only (equivalent to &lt;f:ajax execute=\"@this\" render=\"@all\"&gt;). " +
                     "When singleSubmit is false, no submit will occur. The default value is false.")
     private boolean singleSubmit;
-    
-    @Property(tlddoc="style of the component")
-    private String style;
-    
-    @Property(tlddoc="style class of the component, the renderer doesn't render any default class.")
-    private String styleClass;  
-
-    @Property(tlddoc = "Path to the thumb image.")
-    private String thumbUrl; 
-
-    @Property (tlddoc="tabindex of the component")
-    private Integer tabindex;
-    
+	
     @Property(defaultValue="false", tlddoc="A flag indicating that conversion and validation of this component's value " +
             "should occur during Apply Request Values phase instead of Process Validations phase.")
     private boolean immediate;
 	
-    @Property(defaultValue="true", tlddoc="Allows clicking on the rail to move the thumb. Default is true.")
-    private boolean clickableRail;
-    
+    @Property (tlddoc="tabindex of the component")
+    private Integer tabindex;
+	
     // A MethodExpression Property is a special type, that does not generate
     //  the same code, as it does not use a ValueExpression, but instead
     //  describes a method to be called, and the parameter to pass to it.
@@ -142,11 +144,4 @@ public class SliderEntryMeta extends UIComponentBaseMeta {
             "with a return type of void. In the latter case, the method has no way of easily knowing what the new value " +
             "is, but this can be useful in cases where a notification is needed that \"this value changed\".")
     private MethodExpression valueChangeListener;
-
-    @Property (tlddoc="Boolean indicating if the component should be disabled.")
-    private boolean disabled;
-    
-    @Property(defaultValue="false",
-            tlddoc="If set to true, it will show min, mid and max value labels. The default value is false.")
-    private boolean showLabels;    
 }
