bb3c1bc59880e8b630515b1a8ea68a37e65d2175
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 18:21:26 +0000

    ICE-7113 New ace:confirmationDialog component.

---
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/confirmationdialog/ConfirmationDialogMeta.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2010-2011 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.icefaces.ace.component.confirmationdialog;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.baseMeta.UIPanelMeta;
+
+@Component(
+        tagName         = "confirmationDialog",
+        componentClass  = "org.icefaces.ace.component.confirmationdialog.ConfirmationDialog",
+        rendererClass   = "org.icefaces.ace.component.confirmationdialog.ConfirmationDialogRenderer",
+        generatedClass  = "org.icefaces.ace.component.confirmationdialog.ConfirmationDialogBase",
+        extendsClass    = "javax.faces.component.UIPanel",
+        componentType   = "org.icefaces.ace.component.ConfirmationDialog",
+        rendererType    = "org.icefaces.ace.component.ConfirmationDialogRenderer",
+		componentFamily = "org.icefaces.ace.component",
+		tlddoc = "Displays a dialog that asks users to confirm or cancel their actions."
+        )
+
+@ResourceDependencies({
+	@ResourceDependency(library="icefaces.ace", name="jquery/ui/jquery-ui.css"),
+	@ResourceDependency(library="icefaces.ace", name="jquery/jquery.js"),
+	@ResourceDependency(library="icefaces.ace", name="jquery/ui/jquery-ui.js"),
+	@ResourceDependency(library="icefaces.ace", name="core/core.js"),
+	@ResourceDependency(library="icefaces.ace", name="confirmationdialog/confirmationdialog.js")
+})
+
+public class ConfirmationDialogMeta extends UIPanelMeta {
+
+	@Property(tlddoc="Name of the widget to access client side api")
+	private String widgetVar;
+	
+	@Property(tlddoc="Text to be displayed in body.")
+	private String message;
+	
+	@Property(tlddoc="Text for the header.")
+	private String header;
+	
+	@Property(tlddoc="Message severity for the dislayed icon.", defaultValue="alert")
+	private String severity;
+	
+	@Property(tlddoc="Controls draggability.", defaultValue="true")
+	private boolean draggable;
+	
+	@Property(tlddoc="Boolean value that specifies whether the document should be shielded with a partially transparent mask to require the user to close the Panel before being able to activate any elements in the document.", defaultValue="false")
+	private boolean modal;
+	
+	@Property(tlddoc="Width of the dialog in pixels.", defaultValue="300")
+	private int width;
+	
+	@Property(tlddoc="Height of the dialog in pixels.", defaultValue="Integer.MIN_VALUE")
+	private int height;
+	
+	@Property(tlddoc="zindex property to control overlapping with other elements.", defaultValue="1000")
+	private int zindex;
+	
+	@Property(tlddoc="Style class of the dialog container.")
+	private String styleClass;
+	
+	@Property(tlddoc="Effect to use when showing the dialog.")
+	private String showEffect;
+	
+	@Property(tlddoc="Effect to use when hiding the dialog.")
+	private String hideEffect;
+	
+	@Property(tlddoc="Specifies where the dialog should be displayed. Possible values: \n1) a single string representing position within viewport: 'center', 'left', 'right', 'top', 'bottom'.\n2) an array containing an x,y coordinate pair in pixel offset from left, top corner of viewport (e.g. [350,100])\n3) an array containing x,y position string values (e.g. ['right','top'] for top right corner).")
+	private String position;
+	
+	@Property(tlddoc="Specifies if dialog should be closed when escape key is pressed.", defaultValue="true")
+	private boolean closeOnEscape;
+	
+	@Property(tlddoc="Specifies if close button should be displayed or not.", defaultValue="true")
+	private boolean closable;
+	
+	@Property(tlddoc="Appends dialog as a child of document body.", defaultValue="false")
+	private boolean appendToBody;
+}
