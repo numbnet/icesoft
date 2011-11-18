0b93100fdf5c472cf01debb0eaaf7221085d87a5
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-11-18 18:44:15 +0000

    ICE-7012 - Add new ace:dataExporter component.

---
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/dataexporter/DataExporterMeta.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2010-2011 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.icefaces.ace.component.dataexporter;
+
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+
+import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.DefaultValueType;
+import org.icefaces.ace.meta.annotation.Property;
+import org.icefaces.ace.meta.annotation.Required;
+import org.icefaces.ace.meta.annotation.Expression;
+
+import javax.el.ValueExpression;
+import javax.el.MethodExpression;
+import org.icefaces.ace.meta.baseMeta.UIComponentBaseMeta;
+
+@Component(
+        tagName       = "dataExporter",
+        componentClass  = "org.icefaces.ace.component.dataexporter.DataExporter",
+		rendererClass   = "org.icefaces.ace.component.dataexporter.DataExporterRenderer",
+        generatedClass  = "org.icefaces.ace.component.dataexporter.DataExporterBase",
+        extendsClass    = "javax.faces.component.UIComponentBase",
+        componentType   = "org.icefaces.ace.component.DataExporter",
+        rendererType    = "org.icefaces.ace.component.DataExporterRenderer",
+		componentFamily  = "org.icefaces.ace.component",
+		tlddoc = "Utility to export data from a datatable as an Excel, PDF, XML or CSV document. This component renders an HTML button. More components and HTML elements can be nested inside this tag to give a different look to the button.")
+@ResourceDependencies({
+	@ResourceDependency(library="icefaces.ace", name="jquery/jquery.js"),
+	@ResourceDependency(library="icefaces.ace", name="dataexporter/dataexporter.js")
+})
+public class DataExporterMeta extends UIComponentBaseMeta {
+
+	@Property(required=Required.yes, tlddoc="Server side id of the datatable whose date would be exported.")
+	private String target;
+	
+	@Property(required=Required.yes, tlddoc="Export type: \"xls\",\"pdf\",\"csv\", \"xml\".")
+	private String type;
+	
+	@Property(required=Required.yes, tlddoc="Filename of the generated export file, defaults to datatable server side id.")
+	private String fileName;
+
+	@Property(required=Required.no, tlddoc="The text that will appear on the button that will trigger the data export. Default value is 'Export' (only if the component contains no children).")
+	private String label;
+	
+	@Property(required=Required.no, tlddoc="Exports only current page instead of whole dataset.", defaultValue="false")
+	private boolean pageOnly;
+	
+	@Property(required=Required.no, tlddoc="Comma separated list(if more than one) of column indexes to be excluded from export.")
+	private String excludeColumns;
+	
+	@Property(required=Required.no, tlddoc="PreProcessor for the exported document.", expression = Expression.METHOD_EXPRESSION)
+	private MethodExpression preProcessor;
+	
+	@Property(required=Required.no, tlddoc="PostProcessor for the exported document.", expression = Expression.METHOD_EXPRESSION)
+	private MethodExpression postProcessor;
+	
+	@Property(required=Required.no, tlddoc="Character encoding to use.", defaultValue="UTF-8")
+	private String encoding;
+	
+	@Property(required=Required.no, tlddoc="Boolean value to specify whether column headers should be included at the top of the file.", defaultValue="true")
+	private boolean includeHeaders;
+	
+	@Property(required=Required.no, tlddoc="Boolean value to specify whether column footers should be included at the bottom of the file.", defaultValue="true")
+	private boolean includeFooters;
+	
+	@Property(required=Required.no, tlddoc="Exports only the currently selected rows instead of whole dataset.", defaultValue="false")
+	private boolean selectedRowsOnly;
+	
+	@Property(required=Required.no, tlddoc="When singleSubmit is true, no other components will be executed when triggering the data exporter action. Equivalent to <f:ajax execute='@this' render='@all'>.", defaultValue="false")
+	private boolean singleSubmit;
+	
+    @Property(required=Required.no, tlddoc="Style class of the button element.")
+    private String styleClass;  
+
+    @Property(required=Required.no, tlddoc="Style of the button element.")
+    private String style;
+}
---
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/dataexporter/XMLExporter.java
@@ -0,0 +1,220 @@
+/*
+ * Original Code developed and contributed by Prime Technology.
+ * Subsequent Code Modifications Copyright 2011 ICEsoft Technologies Canada Corp. (c)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM
+ *
+ * Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c).
+ *
+ * Code Modification 1: Integrated with ICEfaces Advanced Component Environment.
+ * Contributors: ICEsoft Technologies Canada Corp. (c)
+ *
+ * Code Modification 2: [ADD BRIEF DESCRIPTION HERE]
+ * Contributors: ______________________
+ * Contributors: ______________________
+ */
+package org.icefaces.ace.component.dataexporter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.el.MethodExpression;
+import javax.faces.component.UIColumn;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIData;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import org.icefaces.ace.component.datatable.DataTable;
+import org.icefaces.ace.component.column.Column;
+import org.icefaces.ace.component.columngroup.ColumnGroup;
+import org.icefaces.ace.component.row.Row;
+
+import org.icefaces.application.ResourceRegistry;
+import java.io.ByteArrayInputStream;
+import java.util.Map;
+
+public class XMLExporter extends Exporter {
+
+    @Override
+	public String export(FacesContext facesContext, DataTable table, String filename, boolean pageOnly, int[] excludeColumns, String encodingType, MethodExpression preProcessor, MethodExpression postProcessor, boolean includeHeaders, boolean includeFooters, boolean selectedRowsOnly) throws IOException {
+		StringBuilder builder = new StringBuilder();
+		
+		List<UIColumn> columns = getColumnsToExport(table, excludeColumns);
+    	
+		List<String> headers;
+		ColumnGroup columnGroup = getColumnGroupHeader(table);
+		if (columnGroup != null) {
+			headers = getHeadersFromColumnGroup(columnGroup, table, excludeColumns);
+		} else {
+			headers = getFacetTexts(table, ColumnType.HEADER);
+		}
+    	List<String> footers = getFacetTexts(table, ColumnType.FOOTER);
+    	String var = table.getVar().toLowerCase();
+    	
+    	builder.append("<?xml version=\"1.0\"?>\n");
+    	builder.append("<" + table.getId() + ">\n");
+    	
+		int rowCount = table.getRowCount();
+    	int first = pageOnly ? table.getFirst() : 0;
+    	int size = pageOnly ? (first + table.getRows()) : rowCount;
+		size = size > rowCount ? rowCount : size;
+    	
+		Object originalData = null;
+		if (selectedRowsOnly) {
+			originalData = table.getValue();
+			table.setValue(table.getStateMap().getSelected());
+			first = 0;
+			size = table.getRowCount();
+		}
+		
+    	for (int i = first; i < size; i++) {
+    		table.setRowIndex(i);
+    		
+    		builder.append("\t<" + var + ">\n");
+    		addColumnValues(builder, columns, headers);
+    		builder.append("\t</" + var + ">\n");
+		}
+		
+		if (selectedRowsOnly) {
+			table.setValue(originalData);
+		}
+
+        if (hasColumnFooter(columns) && includeFooters) {
+            builder.append("\t<footers>\n");
+            addFooterValues(builder, footers, headers);
+            builder.append("\t</footers>\n");
+        }
+    	
+    	builder.append("</" + table.getId() + ">");
+    	
+    	table.setRowIndex(-1);
+    	
+		byte[] bytes = builder.toString().getBytes();
+		//ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		//ExporterResource resource = new ExporterResource(bais);
+		ExporterResource resource = new ExporterResource(bytes);
+		resource.setContentType("text/xml");
+		Map<String, String> httpHeaders = resource.getResponseHeaders();
+		httpHeaders.put("Expires", "0");
+        httpHeaders.put("Cache-Control","must-revalidate, post-check=0, pre-check=0");
+        httpHeaders.put("Pragma", "public");
+        httpHeaders.put("Content-disposition", "attachment;filename=" + filename + ".xml");
+		String path = ResourceRegistry.addSessionResource(resource);
+		
+		return path;
+	}
+	
+	private void addColumnValues(StringBuilder builder, List<UIColumn> columns, List<String> headers) throws IOException {
+		for (int i = 0; i < columns.size(); i++) {
+            addColumnValue(builder, columns.get(i).getChildren(), headers.get(i));
+		}
+	}
+	
+	private void addFooterValues(StringBuilder builder, List<String> footers, List<String> headers) throws IOException {
+		for (int i = 0; i < footers.size(); i++) {
+			String footer = footers.get(i);
+			
+			if (!footer.isEmpty())
+				addColumnValue(builder, footer, headers.get(i));
+		}
+	}	
+	
+	private List<String> getFacetTexts(UIData data, ColumnType columnType) {
+		List<String> facets = new ArrayList<String>();
+		 
+        for (int i = 0; i < data.getChildCount(); i++) {
+            UIComponent child = (UIComponent) data.getChildren().get(i);
+            
+            if (child instanceof UIColumn && child.isRendered()) {
+            	UIColumn column = (UIColumn) child;
+				facets.add(extractValueToDisplay(column, columnType));
+            }
+        }
+        return facets;
+	}
+	
+	private String extractValueToDisplay(UIColumn column, ColumnType columnType) {
+		UIComponent facet = column.getFacet(columnType.facet());
+		
+		if (facet != null && facet.isRendered()) {
+			String value = exportValue(FacesContext.getCurrentInstance(), facet);
+			
+			return value;
+		} else {
+			String value = "";
+			if (column instanceof Column) {
+				Column _column = (Column) column;
+				if (columnType == ColumnType.HEADER) {
+					String headerText = _column.getHeaderText();
+					value = headerText != null ? headerText : "";
+				} else if (columnType == ColumnType.FOOTER) {
+					String footerText = _column.getFooterText();
+					value = footerText != null ? footerText : "";
+				}
+			}
+			return value;
+		}
+	}
+	
+	private List<String> getHeadersFromColumnGroup(ColumnGroup columnGroup, UIData data, int[] excludeColumns) {
+	
+		ArrayList<Row> rows = (ArrayList<Row>) getRows(columnGroup);
+		int size = rows.size();
+		if (size > 0) {
+			List<UIColumn> rowColumns = getRowColumnsToExport(rows.get(size-1), data, excludeColumns); // only use last row in column group
+			List<String> values = new ArrayList<String>();
+			for (UIColumn column : rowColumns) {
+				String value = extractValueToDisplay(column, ColumnType.HEADER);
+				values.add(value);
+			}
+			return values;
+		} else {
+			return getFacetTexts(data, ColumnType.HEADER);
+		}
+	}
+
+	private void addColumnValue(StringBuilder builder, List<UIComponent> components, String header) throws IOException {
+		StringBuilder builder1 = new StringBuilder();
+		String tag = header.toLowerCase();
+		builder.append("\t\t<" + tag + ">");
+
+		for (UIComponent component : components) {
+			if (component.isRendered()) {
+				String value = exportValue(FacesContext.getCurrentInstance(), component);
+
+				builder1.append(value);
+			}
+		}
+
+		builder.append(builder1.toString());
+		
+		builder.append("</" + tag + ">\n");
+	}
+	
+	private void addColumnValue(StringBuilder builder, String footer, String header) throws IOException {
+		String tag = header.toLowerCase();
+		builder.append("\t\t<" + tag + ">");
+
+		builder.append(footer.toLowerCase());
+		
+		builder.append("</" + tag + ">\n");
+	}
+	
+}
