b5966d91815f87063ff79cf01ba39ffdfb785d0f
esadovoi <esadovoi@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-12-15 18:11:43 +0000

    Resolved ICE-7533

--- icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/resizable/ResizeListenerBean.java
+++ icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/resizable/ResizeListenerBean.java
@@ -64,6 +64,7 @@ public class ResizeListenerBean extends ComponentExampleImpl<ResizeListenerBean>
     
     public void handleResizeEvent(ResizeEvent event)
     {
+        System.out.println("Hello world!");
         resizeParameters = "My size changed to: "+event.getWidth() +" x "+ event.getHeight();
     }
 
