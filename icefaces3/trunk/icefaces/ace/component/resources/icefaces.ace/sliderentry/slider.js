59829e03c5df57095bd9d74c596e08048d578730
nils.lundquist <nils.lundquist@8668f098-c06c-11db-ba21-f49e70c34f74>
2011-12-15 21:10:47 +0000

    ICE-7553 - Added case for slider rendering in disabled state.

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/sliderentry/slider.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/sliderentry/slider.js
@@ -67,10 +67,14 @@ ice.ace.Slider = function(id, cfg) {
     //Slide end handler
     this.jq.bind('slidestop', function(event, ui) {_self.onSlideEnd(event, ui);});
 
+    
     // This call required to init slider when inside tabset on IE browsers.
     // Else slider remains hidden until some tab action occurs.
     var jq = this.jq;
-    window.setTimeout(function () { jq.slider('enable'); } , 1);
+    if (this.cfg.disabled)
+        window.setTimeout(function () { jq.slider('disable'); } , 1);
+    else
+        window.setTimeout(function () { jq.slider('enable'); } , 1);
 }
 
 ice.ace.Slider.prototype.onSlide = function(event, ui) {
