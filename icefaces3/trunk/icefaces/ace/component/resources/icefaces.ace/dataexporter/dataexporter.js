50f230a4bd608d88304555a5797f3d5c853391a4
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-01-10 15:39:22 +0000

    ICE-7640 fixed problem by only showing one popup window if the export button is clicked multiple times; simplified popup messages; this fix also solves error mentioned in ICE-7317

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/dataexporter/dataexporter.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/dataexporter/dataexporter.js
@@ -20,33 +20,48 @@ ice.ace.DataExporters = {};
 
 ice.ace.DataExporter = function(id, behaviors) {
 	this.id = id;
-	ice.ace.DataExporters[this.id] = this;
-	if (ice.ace.DataExporter.shouldOpenPopUp()) {
-		this.window = window.open('','','width=400,height=150');
-		this.window.document.write('<html><head><title>Data export file loader</title></head><body id="body"></body></html>');
-		this.body = this.window.document.getElementById('body');
-		this.body.innerHTML = '<p>Please wait while the file you requested is generated...</p>';
-		this.window.focus();
+	if (!ice.ace.DataExporters[this.id]) {
+		ice.ace.DataExporters[this.id] = this;
+		if (ice.ace.DataExporter.shouldOpenPopUp()) {
+			if (!this.window) {
+				this.openWindow();
+			}
+		}
+	} else {
+		if (ice.ace.DataExporter.shouldOpenPopUp()) {
+			var instance = ice.ace.DataExporters[this.id];
+			if (instance.window.closed) {
+				instance.openWindow();
+			}
+			instance.body.innerHTML = '<p>Please wait while your file is generated...</p>';
+			instance.window.focus();
+		}
 	}
 	behaviors();
-}
+};
+ice.ace.DataExporter.prototype.openWindow = function() {
+	this.window = window.open('','','width=400,height=150');
+	this.window.document.write('<html><head><title>Data export file loader</title></head><body id="body"></body></html>');
+	this.body = this.window.document.getElementById('body');
+	this.body.innerHTML = '<p>Please wait while your file is generated...</p>';
+};
 ice.ace.DataExporter.prototype.url = function(url) {
 	if (ice.ace.DataExporter.shouldOpenPopUp()) {
-		this.body.innerHTML = this.body.innerHTML + '<p>The file is ready. Click link below to download.</p>'
-			+ '<a href="' + url + '">Download File</a>';
+		this.body.innerHTML = '<p>Click link below to download file.</p>'
+			+ '<a href="' + url + '">Download</a>';
 		this.window.focus();
 	} else {
 		var iframe = document.createElement('iframe');
 		iframe.setAttribute('src', url);
 		iframe.style.display = 'none';
 		document.body.appendChild(iframe);
+		ice.ace.DataExporters[this.id] = null;
 	}
-	ice.ace.DataExporters[this.id] = null;
-}
+};
 ice.ace.DataExporter.shouldOpenPopUp = function() {
 	if (ice.ace.jq.browser.msie) 
 		if (ice.ace.jq.browser.version < 8)
 			if (navigator.userAgent.indexOf("Trident/5") < 0) // detects IE9, regardless of compatibility mode
 				return true;
 	return false;
-}
+};
