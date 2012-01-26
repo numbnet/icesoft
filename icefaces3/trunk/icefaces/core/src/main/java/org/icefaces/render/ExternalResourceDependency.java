930663ee410e6c0af63e73b2f0898edb50e604a4
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-01-26 15:46:28 +0000

    ICE-7687 Annotated GMapRenderer as mandatory resource. Moved ExternalScript annotation to the component and changed its processing to be executed with the rest of mandatory resource dependencies. Renamed ExternalScript to ExternalResourceDependency. Updated component-showcase web.xml to include GMap component in the list of mandatoryResourceConfiguration parameter.

--- icefaces3/trunk/icefaces/core/src/main/java/org/icefaces/render/ExternalResourceDependency.java
+++ icefaces3/trunk/icefaces/core/src/main/java/org/icefaces/render/ExternalResourceDependency.java
@@ -22,16 +22,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *  ExternalScript annotation allows an external script declaration to be loaded
+ * ExternalResourceDependency annotation allows an external script declaration to be loaded
  * into the head portion of the page. If a second argument is specified,
- * it identifies a context param which acts as a check gate for this parameter.  
- *
+ * it identifies a context param which acts as a check gate for this parameter.
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ExternalScript {
+public @interface ExternalResourceDependency {
     final String Null = "Null";
-    
-    String contextParam() default Null;
-    String scriptURL();
+
+    String contextParameter() default Null;
+
+    String url();
 }
