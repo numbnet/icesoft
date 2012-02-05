90e54976e01cdbe16b72b9cc9435892ba9c65ebc
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-02-05 17:51:23 +0000

    ICE-7717 Invoke JQuery.noConflict with 'true' value as parameter to make sure both window.$ and window.jQuery global variables are restored to the previously loaded jQuery version.

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/core/core.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/core/core.js
@@ -16,7 +16,7 @@
 
 if (!window['ice']) window.ice = {};
 if (!window.ice['ace']) window.ice.ace = {};
-ice.ace.jq = jQuery.noConflict();
+ice.ace.jq = jQuery.noConflict(true);
 // After core is loaded after / as part of combined.js core will need to have
 // this object pulled apart so as no to overwrite existing members of ice.ace that
 // may come before it. Alternatively the generator could explictly add core.js earlier
