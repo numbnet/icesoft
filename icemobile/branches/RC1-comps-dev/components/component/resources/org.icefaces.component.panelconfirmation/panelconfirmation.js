c4eb171ba8418b4d6533d3c3f6f10f52920b1970
judy.guglielmin <judy.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-02-07 17:27:17 +0000

    MOBI-156 initial checkin of panel confirmation and test…didn't get committed with other files

---
+++ icemobile/branches/RC1-comps-dev/components/component/resources/org.icefaces.component.panelconfirmation/panelconfirmation.js
@@ -0,0 +1,64 @@
+if (!window['mobi']) {
+    window.mobi = {};
+}
+mobi.panelConf = {
+      opened: {},
+      cfg: {},
+      caller: {},
+	  init: function(clientId, callerId, cfgIn ){
+          this.cfg[clientId] = cfgIn;
+          this.caller[clientId] = callerId;
+          var idPanel = clientId+"_bg";
+          if (!document.getElementById(idPanel).className ){
+             document.getElementById(idPanel).className = 'mobi-date-bg-inv';
+          }
+          this.open(clientId);
+          this.opened[clientId]=true;
+	   },
+
+        confirm: function(clientId){
+            var event = this.cfg.event;
+            var hasBehaviors = false;
+            var behaviors = this.cfg.behaviors;
+            if (behaviors){
+                hasBehaviors = true;
+            }
+            if (hasBehaviors){
+                if (behaviors.accept){
+                    behaviors.accept();
+                }
+            }
+            if (!hasBehaviors){
+                var callerId = this.caller[clientId];
+                if (callerId){
+                    ice.s(event, callerId);
+                }
+            }
+            this.close(clientId);
+        },
+        toggle: function(clientId){
+            if (this.opened[clientId]==false){
+                this.open(clientId);
+            }  else {
+                this.close(clientId);
+            }
+        },
+        open: function(clientId){
+            var idPanel = clientId+"_bg";
+            document.getElementById(idPanel).className = "mobi-date-bg";
+            document.getElementById(clientId+"_popup").className = "mobi-date-container";
+            this.opened[clientId]= true;
+        },
+        close: function(clientId){
+            var idPanel = clientId+"_bg" ;
+            document.getElementById(idPanel).className = "mobi-date";
+            document.getElementById(clientId+"_popup").className = "mobi-date-container-hide";
+            this.opened[clientId]= false;
+        },
+        unload: function(clientId){
+            this.cfg[clientId] = null;
+            this.opened[clientId] = null;
+            this.caller[clientId] = null;
+        }
+
+}
