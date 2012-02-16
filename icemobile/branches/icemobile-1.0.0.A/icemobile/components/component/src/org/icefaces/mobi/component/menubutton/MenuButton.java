25bb5ce1f6eb2a22d50015502f788b1c5b518db0
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-02-16 17:47:40 +0000

    Maintenance branch for 1.0.0.A series patches.

---
+++ icemobile/branches/icemobile-1.0.0.A/icemobile/components/component/src/org/icefaces/mobi/component/menubutton/MenuButton.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icefaces.mobi.component.menubutton;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jguglielmin
+ * Date: 12-02-12
+ * Time: 12:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuButton extends MenuButtonBase {
+    public static final String BASE_STYLE_CLASS = "mobi-menu-btn ";
+    public static final String BUTTON_STYLE_CLASS = "mobi-menu-btn-btn ";
+    public static final String DISABLED_STYLE_CLASS = "mobi-button-dis ";
+    public static final String MENU_SELECT_CLASS =  "mobi-menu-btn-menu ";
+
+}
---
+++ icemobile/branches/icemobile-1.0.0.A/icemobile/components/component/src/org/icefaces/mobi/renderkit/BaseLayoutRenderer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobi.renderkit;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class BaseLayoutRenderer extends CoreRenderer {
+    protected static Map<String, StringBuilder> menuItemCfg = new HashMap<String, StringBuilder>() ;
+    /*
+        in order to use these must have empty encodeChildren method in Renderer.
+     */
+    protected void renderChildren(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+        for (UIComponent child : uiComponent.getChildren()) {
+            renderChild(facesContext, child);
+        }
+    }
+
+    protected void renderChild(FacesContext facesContext, UIComponent child) throws IOException {
+        if (!child.isRendered()) {
+            return;
+        }
+        //do we have to worry about encodeAll method???
+        child.encodeBegin(facesContext);
+        if (child.getRendersChildren()) {
+            child.encodeChildren(facesContext);
+        } else {
+            renderChildren(facesContext, child);
+        }
+        child.encodeEnd(facesContext);
+    }
+
+    protected static void addMenuItemCfg(String clientId,StringBuilder sb){
+        BaseLayoutRenderer.menuItemCfg.put(clientId, sb);
+    }
+
+}
