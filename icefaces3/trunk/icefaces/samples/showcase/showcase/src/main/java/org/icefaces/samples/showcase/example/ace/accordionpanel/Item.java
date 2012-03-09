a007250ddc10a9008e8e288e1e73eb64aad23f37
esadovoi <esadovoi@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-03-09 20:40:50 +0000

    resolved ICE-7787, pointed source code annotations on a set of beans to a new ImageSet.java location

--- icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/accordionpanel/Item.java
+++ icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/accordionpanel/Item.java
@@ -18,12 +18,13 @@
 package org.icefaces.samples.showcase.example.ace.accordionpanel;
 
 import java.io.Serializable;
+import org.icefaces.samples.showcase.dataGenerators.ImageSet;
 import org.icefaces.samples.showcase.example.compat.dragdrop.DragDropItem;
 
 public class Item extends DragDropItem implements Serializable
 {
-    public Item(int id, String name, String image, String type, double price, int quantity) 
+    public Item(ImageSet.ImageInfo image) 
     {
-        super(id, name, image, type, price, quantity);
+        super(image);
     }
 }
