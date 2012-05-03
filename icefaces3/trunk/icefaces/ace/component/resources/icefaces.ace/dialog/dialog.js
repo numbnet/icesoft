237b14809527e14d53d39f428cac895cd04bb6d1
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-05-03 23:26:38 +0000

    ICE-7999 Close dialog widget when its corresponding element is not present on the page anymore.

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/dialog/dialog.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/dialog/dialog.js
@@ -1,31 +1,31 @@
 /* 
-* Original Code Copyright Prime Technology.
-* Subsequent Code Modifications Copyright 2011-2012 ICEsoft Technologies Canada Corp. (c)
-* 
-* Licensed under the Apache License, Version 2.0 (the "License"); 
-* you may not use this file except in compliance with the License. 
-* You may obtain a copy of the License at 
-* 
-* http://www.apache.org/licenses/LICENSE-2.0 
-* 
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-* See the License for the specific language governing permissions and 
-* limitations under the License. 
-* 
-* NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM 
-* 
-* Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c). 
-* 
-* Code Modification 1: Integrated with ICEfaces Advanced Component Environment. 
-* Contributors: ICEsoft Technologies Canada Corp. (c) 
-* 
-* Code Modification 2: [ADD BRIEF DESCRIPTION HERE] 
-* Contributors: ______________________ 
-* Contributors: ______________________ 
-* 
-*/
+ * Original Code Copyright Prime Technology.
+ * Subsequent Code Modifications Copyright 2011-2012 ICEsoft Technologies Canada Corp. (c)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NOTE THIS CODE HAS BEEN MODIFIED FROM ORIGINAL FORM
+ *
+ * Subsequent Code Modifications have been made and contributed by ICEsoft Technologies Canada Corp. (c).
+ *
+ * Code Modification 1: Integrated with ICEfaces Advanced Component Environment.
+ * Contributors: ICEsoft Technologies Canada Corp. (c)
+ *
+ * Code Modification 2: [ADD BRIEF DESCRIPTION HERE]
+ * Contributors: ______________________
+ * Contributors: ______________________
+ *
+ */
 
 /**
  *  Dialog Widget
@@ -47,45 +47,52 @@ ice.ace.Dialog = function(id, cfg) {
     this.cfg = cfg;
     this.jqId = ice.ace.escapeClientId(id);
     this.jq = ice.ace.jq(this.jqId);
-    var _self = this,
-    closable = this.cfg.closable;
+    var _self = this, closable = this.cfg.closable;
 
-    if(closable == false) {
+    if (closable == false) {
         this.cfg.closeOnEscape = false;
     }
 
-	// disable unsupported effects
-	if (this.cfg.hide == 'pulsate') {
-		this.cfg.hide = null;
-	} else {
-		var browser = ice.ace.Dialog.browser();
-		if (browser == 'ie7' || browser == 'ie8') {
-			var hide = this.cfg.hide;
-			if (hide) {
-				if (hide == 'highlight' || hide == 'bounce')
-					this.cfg.hide = null;
-			}
-		}
-	}
+    // disable unsupported effects
+    if (this.cfg.hide == 'pulsate') {
+        this.cfg.hide = null;
+    } else {
+        var browser = ice.ace.Dialog.browser();
+        if (browser == 'ie7' || browser == 'ie8') {
+            var hide = this.cfg.hide;
+            if (hide) {
+                if (hide == 'highlight' || hide == 'bounce')
+                    this.cfg.hide = null;
+            }
+        }
+    }
 
     //Remove scripts to prevent duplicate widget issues
     this.jq.find("script").remove();
-    
+
     //Create the dialog
     this.cfg.autoOpen = false;
     this.jq.dialog(this.cfg);
 
+    ice.onElementRemove(id, function() {
+        _self.jq.dialog('close');
+    });
+
     //Event handlers
-    this.jq.bind('dialogclose', function(event, ui) {_self.onHide(event, ui);});
-    this.jq.bind('dialogopen', function(event, ui) {_self.onShow(event, ui);});
+    this.jq.bind('dialogclose', function(event, ui) {
+        _self.onHide(event, ui);
+    });
+    this.jq.bind('dialogopen', function(event, ui) {
+        _self.onShow(event, ui);
+    });
 
     //Hide close icon if dialog is not closable
-    if(closable == false) {
+    if (closable == false) {
         this.jq.parent().find('.ui-dialog-titlebar-close').hide();
     }
 
     //Hide header if showHeader is false
-    if(this.cfg.showHeader == false) {
+    if (this.cfg.showHeader == false) {
         this.jq.parent().children('.ui-dialog-titlebar').hide();
     }
 
@@ -95,7 +102,7 @@ ice.ace.Dialog = function(id, cfg) {
 //    }
 
     //Apply focus to first input initially
-    if(this.cfg.isVisible) {
+    if (this.cfg.isVisible) {
         this.jq.dialog("open");
         this.focusFirstInput();
     }
@@ -116,7 +123,7 @@ ice.ace.Dialog.prototype.hide = function() {
  * Invokes user provided callback
  */
 ice.ace.Dialog.prototype.onShow = function(event, ui) {
-    if(this.cfg.onShow) {
+    if (this.cfg.onShow) {
         this.cfg.onShow.call(this, event, ui);
     }
 };
@@ -126,16 +133,16 @@ ice.ace.Dialog.prototype.onShow = function(event, ui) {
  */
 ice.ace.Dialog.prototype.onHide = function(event, ui) {
     if (typeof event.originalEvent != 'undefined') {
-		if (event.originalEvent.synthetic) return;
+        if (event.originalEvent.synthetic) return;
     } else {
-		if (event.synthetic) return;
+        if (event.synthetic) return;
     }
 
-    if(this.cfg.onHide) {
+    if (this.cfg.onHide) {
         this.cfg.onHide.call(this, event, ui);
     }
 
-    if(this.cfg.behaviors) {
+    if (this.cfg.behaviors) {
         var closeBehavior = this.cfg.behaviors['close'];
 
         if (closeBehavior) {
@@ -149,13 +156,13 @@ ice.ace.Dialog.prototype.focusFirstInput = function() {
 };
 
 ice.ace.Dialog.browser = function() {
-	if (ice.ace.jq.browser.msie) 
-		if (ice.ace.jq.browser.version < 8) {
-			if (navigator.userAgent.indexOf("Trident/5") < 0) // detects IE9, regardless of compatibility mode
-				return 'ie7';
-		} else {
-			if (ice.ace.jq.browser.version < 9)
-				return 'ie8';
-		}
-	return '';
+    if (ice.ace.jq.browser.msie)
+        if (ice.ace.jq.browser.version < 8) {
+            if (navigator.userAgent.indexOf("Trident/5") < 0) // detects IE9, regardless of compatibility mode
+                return 'ie7';
+        } else {
+            if (ice.ace.jq.browser.version < 9)
+                return 'ie8';
+        }
+    return '';
 };
