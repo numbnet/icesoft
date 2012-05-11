6b7ad23ebb3cfd71928c65e6edf9d8353178a253
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-05-11 20:52:51 +0000

    ICEfaces 3.1.0 Alpha 2 tag.

---
+++ icefaces3/tags/icefaces-3.1.0.ALPHA2/icefaces/tutorials/conditional-row-messages/src/main/java/org/icefaces/tutorials/ace/Task.java
@@ -0,0 +1,38 @@
+package org.icefaces.tutorials.ace;
+
+/**
+ * Copyright 2010-2011 ICEsoft Technologies Canada Corp.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * <p/>
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p/>
+ * User: Nils
+ * Date: 12-03-29
+ * Time: 4:21 PM
+ */
+public class Task {
+    int id;
+
+    public Task(int id) {
+        this.id = id;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
