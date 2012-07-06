0ae8fc1d47787b300cda1ea8aa649b46ec7579c2
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-07-06 22:11:48 +0000

    ICE-8314: ace:menuItem - Clicking on menuItem resets browser scroll position in IE7.

--- icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/ace/component/src/org/icefaces/ace/component/menu/BaseMenuRenderer.java
+++ icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/ace/component/src/org/icefaces/ace/component/menu/BaseMenuRenderer.java
@@ -81,7 +81,7 @@ public abstract class BaseMenuRenderer extends CoreRenderer {
 				if(menuItem.getOnclick() != null) writer.writeAttribute("onclick", menuItem.getOnclick(), null);
 				if(menuItem.getTarget() != null) writer.writeAttribute("target", menuItem.getTarget(), null);
 			} else {
-				writer.writeAttribute("href", "#", null);
+				writer.writeAttribute("href", "javascript:void 0;", null);
 
 				UIComponent form = ComponentUtils.findParentForm(context, menuItem);
 				if(form == null) {
