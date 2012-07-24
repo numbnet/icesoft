d44365daefce0c8a57faa742bfec918db89eecf7
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-07-24 17:20:27 +0000

    ICE-8395 ace:autoCompleteEntry, removed blur ajax event from meta class; fixed submission with submit ajax event to adjust for change in client id

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/autocompleteentry/autocompleteentry.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/autocompleteentry/autocompleteentry.js
@@ -26,6 +26,7 @@ ice.ace.Autocompleter = function(id, updateId, rowClass, selectedRowClass, delay
 		if (behaviors.behaviors) {
 			if (behaviors.behaviors.submit)
 				this.ajaxSubmit = behaviors.behaviors.submit;
+				this.ajaxSubmit.source = this.ajaxSubmit.source + "_input";
 			if (behaviors.behaviors.blur)
 				this.ajaxBlur = behaviors.behaviors.blur;
 		}
