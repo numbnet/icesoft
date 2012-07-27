91ae9c590b5a8ee1615f213c3ede33701edc2cd8
patrick.corless <patrick.corless@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-07-27 20:11:01 +0000

    moving 1.1.0-Beta tag.

--- icemobile/tags/icemobile-1.1.0-BETA/icemobile/jsf/components/component/src/org/icefaces/mobi/component/contentnavbar/ContentNavBar.java
+++
@@ -1,8 +0,0 @@
-package org.icefaces.mobi.component.contentnavbar;
-
-
-public class ContentNavBar extends ContentNavBarBase {
-    public static final String CONTENTNAVBAR_BASE_CLASS = "mobi-contentnavbar-header";
-    public static final String CONTENTNAVBAR_BUTTON_CLASS = "mobi-contentnavbar-button";
-    public static final String CONTENTNAVBAR_BUTTON_MENU_CLASS = "mobi-contentnavbar-button menu";
-}
