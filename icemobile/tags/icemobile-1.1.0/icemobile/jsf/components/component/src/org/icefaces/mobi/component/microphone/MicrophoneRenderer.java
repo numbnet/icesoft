84e3054e23cf1c4e97a595ecc8122bf646642b10
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-08-09 19:42:07 +0000

    tagging icemobile-1.1.0 (MOBI-7)

---
+++ icemobile/tags/icemobile-1.1.0/icemobile/jsf/components/component/src/org/icefaces/mobi/component/microphone/MicrophoneRenderer.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobi.component.microphone;
+
+
+import org.icefaces.mobi.utils.HTML;
+import org.icefaces.mobi.utils.Utils;
+import org.icefaces.util.EnvUtils;
+
+
+import javax.faces.component.UIComponent;
+import javax.faces.render.Renderer;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.event.ValueChangeEvent;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+
+public class MicrophoneRenderer extends Renderer {
+    private static Logger logger = Logger.getLogger(MicrophoneRenderer.class.getName());
+
+    @Override
+    public void decode(FacesContext facesContext, UIComponent uiComponent) {
+        Microphone microphone = (Microphone) uiComponent;
+        String clientId = microphone.getClientId();
+        if (microphone.isDisabled()) {
+            return;
+        }
+        try {
+            //MOBI-18 requirement is to decode the file from the map
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            boolean valid = extractAudio(facesContext, map, clientId);
+            if (valid){
+               if (map !=null){
+                  microphone.setValue(map);
+             //   trigger valueChange and add map as newEvent value old event is NA
+                  uiComponent.queueEvent(new ValueChangeEvent(uiComponent,
+    		    		    null, map));
+                }
+            }
+        } catch (Exception e) {
+            logger.warning("Exception decoding audio stream: " + e);
+        }
+    }
+
+    public boolean extractAudio(FacesContext facesContext, Map map, String clientId) throws IOException {
+        return Utils.decodeComponentFile(facesContext, clientId, map);
+    }
+
+    public void encodeEnd(FacesContext facesContext, UIComponent uiComponent)
+            throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+        String clientId = uiComponent.getClientId(facesContext);
+        Microphone microphone = (Microphone) uiComponent;
+        boolean disabled = microphone.isDisabled();
+        // span as per MobI-18
+        boolean isEnhanced = EnvUtils.isEnhancedBrowser(facesContext);
+        boolean isAuxUpload = EnvUtils.isAuxUploadBrowser(facesContext);
+
+        if (!isEnhanced && !isAuxUpload) {
+            writer.startElement(HTML.SPAN_ELEM, uiComponent);
+            writer.startElement(HTML.INPUT_ELEM, uiComponent);
+            writer.writeAttribute(HTML.TYPE_ATTR, HTML.INPUT_TYPE_FILE, null);
+            writer.writeAttribute(HTML.ID_ATTR, clientId, null);
+            writer.writeAttribute(HTML.NAME_ATTR, clientId, null);
+            writer.endElement(HTML.INPUT_ELEM);
+            writer.endElement(HTML.SPAN_ELEM);
+            return;
+        }
+        int maxtime = microphone.getMaxtime();
+        // button element
+        writer.startElement("input", uiComponent);
+        writer.writeAttribute(HTML.TYPE_ATTR, "button", null);
+        writer.writeAttribute("id", clientId , null);
+        // write out style for input button, same as default device button.
+        Utils.writeConcatenatedStyleClasses(writer,
+                "mobi-button mobi-button-default",
+                microphone.getStyleClass());
+        writer.writeAttribute(HTML.STYLE_ATTR, microphone.getStyle(), HTML.STYLE_ATTR);
+        if (disabled) {
+            writer.writeAttribute("disabled", "disabled", null);
+        }
+        String script;
+        if (isAuxUpload)  {
+            script = Utils.getICEmobileSXScript("microphone", clientId);
+        } else {
+            script = writeJSCall(clientId, maxtime).toString();
+        }
+        writer.writeAttribute("onclick", script, null);
+
+        writer.writeAttribute("value", "record", null);
+        writer.endElement("input");
+    }
+
+    private StringBuilder writeJSCall(String clientId, int maxtime) {
+        final StringBuilder script = new StringBuilder();
+        if (maxtime != Integer.MIN_VALUE) {
+            script.append("ice.microphone( '").append(clientId).
+                    append(",'maxtime=").append(maxtime).append("');");
+        } else {
+            script.append("ice.microphone( '").append(clientId).append("');");
+        }
+        return script;
+    }
+}
