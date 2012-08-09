4763a7fe0031f80b419e9b2be76d96d2b7c71778
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-08-09 16:51:29 +0000

    tagging icemobile-1.1.0 (MOBI-7)

---
+++ icemobile/tags/icemobile-1.1.0/icemobile/jsp/src/main/java/org/icemobile/jsp/tags/PagePanelTag.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icemobile.jsp.tags;
+
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Logger;
+
+/**
+ *
+ */
+public class PagePanelTag extends TagSupport {
+
+    private static Logger LOG = Logger.getLogger(PagePanelTag.class.getName());
+
+    public int doStartTag() throws JspTagException {
+
+        Writer out = pageContext.getOut();
+
+        StringBuilder tag = new StringBuilder(TagUtil.DIV_TAG);
+        if (id != null && !"".equals(id)) {
+            tag.append(" id=\"").append(getId()).append("_pgPnl\"");
+        }
+        tag.append(">");
+
+        try {
+            out.write(tag.toString());
+        } catch (IOException ioe) {
+            LOG.severe("IOException writing PagePanelTag: " + ioe);
+        }
+        return EVAL_BODY_INCLUDE;
+    }
+
+
+    public int doEndTag() throws JspTagException {
+
+        Writer out = pageContext.getOut();
+        try {
+            out.write(TagUtil.DIV_TAG_END);
+        } catch (IOException ioe) {
+            LOG.severe("IOException closing OutputListTag: " + ioe);
+        }
+        return EVAL_PAGE;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
