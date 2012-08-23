1e1cf051fc53ad47e4cc078b454030a63f221e67
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-08-23 21:16:09 +0000

    ICE-8496 Clean up events listeners for the updated elements just before the updates are applied.

--- icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/core/src/main/javascript/fixjsf.js
+++ icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/core/src/main/javascript/fixjsf.js
@@ -152,4 +152,15 @@
         document.execCommand("BackgroundImageCache", false, true);
     } catch(err) {
     }
+
+    //fix potential memory leaks by clearing up the event handlers that replaced elements have had
+    namespace.onBeforeUpdate(function(updates) {
+        each(updates.getElementsByTagName('update'), function(update) {
+            var id = update.getAttribute('id');
+            var e = lookupElementById(id);
+            if (e) {
+                each(e.getElementsByTagName('*'), clearEventHandlers);
+            }
+        });
+    });
 })();
