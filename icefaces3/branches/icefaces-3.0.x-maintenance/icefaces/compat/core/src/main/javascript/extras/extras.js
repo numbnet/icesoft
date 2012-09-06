529be958a8c7a298b54d1671742c8349b3901d99
carlo.guglielmin <carlo.guglielmin@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-06 08:27:12 +0000

    ICE-8500 - Porting a minor fix to clear some vars in the tab navigator.

--- icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/compat/core/src/main/javascript/extras/extras.js
+++ icefaces3/branches/icefaces-3.0.x-maintenance/icefaces/compat/core/src/main/javascript/extras/extras.js
@@ -4947,8 +4947,10 @@ Ice.tabNavigator = function(event) {
             var preCell = ele.up('.icePnlTb').previousSibling;
             if (preCell) {
                 var lnk = preCell.down('.icePnlTbLblLnk');
+				preCell = null;
                 if (lnk) {
                     lnk.focus();
+					lnk = null;
                 }
             }
             if (ele.up('.icePnlTb')) {
@@ -4966,8 +4968,10 @@ Ice.tabNavigator = function(event) {
                 var lnk = nextCell.down('.icePnlTbLblLnk');
                 if (lnk) {
                     lnk.focus();
+					lnk = null;
                 }
             }
+			nextCell = null;
             if (ele.up('.icePnlTb')) {
                 if (event.preventDefault) {
                     event.preventDefault();
@@ -4976,8 +4980,8 @@ Ice.tabNavigator = function(event) {
                 }
             }
             break;
-
     }
+	ele = null;
 }
 
 Ice.pnlTabOnFocus = function(ele, facet, kbs) {
