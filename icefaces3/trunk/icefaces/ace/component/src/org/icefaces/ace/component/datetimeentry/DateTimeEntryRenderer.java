f2f07df6acfa6b0fa335306e4f3be7774688c7d2
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-11 23:18:05 +0000

    ICE-8558 Change configuration during postbacks so that 'onfocus' callbacks are not wired. Rewire the 'onfocus' callbacks with the newly DOM attached elements after a delay so that ice.applyFocus command has a chance to run.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/datetimeentry/DateTimeEntryRenderer.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/datetimeentry/DateTimeEntryRenderer.java
@@ -206,6 +206,12 @@ public class DateTimeEntryRenderer extends InputRenderer {
                     entry("buttonImageOnly", dateTimeEntry.isPopupIconOnly());
                 }
 
+                //do not send showOn=focus configuration during postbacks to avoid wiring the focus callbacks to early,
+                //before ice.applyFocus command has a chance to re-apply the focus to the input element
+                if (context.isPostback()) {
+                    json.entry("showOn", "click");
+                }
+
                 if(dateTimeEntry.isShowOtherMonths()) {
                     json.entry("showOtherMonths", true).
                     entry("selectOtherMonths", dateTimeEntry.isSelectOtherMonths());
