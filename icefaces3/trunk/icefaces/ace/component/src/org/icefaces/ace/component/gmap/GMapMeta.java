8d72fcf9026c825d0eb8d6a906df1395b1cf43d7
brennanm <brennanm@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-14 16:36:47 +0000

    ICE-7826 - Modifying the style and styleClass attributes of ace:gMap, updating demos to match.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/gmap/GMapMeta.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/gmap/GMapMeta.java
@@ -73,9 +73,9 @@ public class GMapMeta extends UIPanelMeta {
 	@Property(tlddoc="Map type to display by default. Possible values are HYBRID, ROADMAP, SATELLITE and TERRAIN, case insensitive", defaultValue="ROADMAP")
 	private String type;
 
-    @Property(tlddoc="Styling for the main gMap div", defaultValue="width: 100%; height: 100%")
+    @Property(tlddoc="Styling for the main gMap div", defaultValue="width: 800px; height: 500px")
     private String style;
 
-    @Property(tlddoc="The classname for the main gMap div", defaultValue="ice-ace-gmap")
+    @Property(tlddoc="The classname for the main gMap div")
     private String styleClass;
 }
