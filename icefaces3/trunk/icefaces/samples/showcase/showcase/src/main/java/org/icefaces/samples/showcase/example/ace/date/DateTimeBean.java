13049b78626399bfdb75adcb39cb07aa4240b6b2
matthew.cowell <matthew.cowell@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-18 00:46:25 +0000

    [ICE-8572] Added singleSubmit=false to the dateTimeEntry component.  The component now ignores the singleSubmit tag on the rest of the page and works correctly.
    Modified DateTimeBean so that when a date is only updated or a time is only updated, it does not affect the existing date / time.  Previously if we only updated the time it would clobber the date back to 1960 which looked a bit odd.

--- icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/date/DateTimeBean.java
+++ icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/example/ace/date/DateTimeBean.java
@@ -16,113 +16,131 @@
 
 package org.icefaces.samples.showcase.example.ace.date;
 
-import org.icefaces.samples.showcase.metadata.annotation.ComponentExample;
-import org.icefaces.samples.showcase.metadata.annotation.ExampleResource;
-import org.icefaces.samples.showcase.metadata.annotation.ExampleResources;
-import org.icefaces.samples.showcase.metadata.annotation.ResourceType;
-import org.icefaces.samples.showcase.metadata.context.ComponentExampleImpl;
-import org.icefaces.ace.event.DateSelectEvent;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.CustomScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ValueChangeEvent;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.Calendar;
-import java.util.Locale;
-import java.util.TimeZone;
 
-@ComponentExample(
-        parent = DateEntryBean.BEAN_NAME,
-        title = "example.ace.dateentry.timeentry.title",
-        description = "example.ace.dateentry.timeentry.description",
-        example = "/resources/examples/ace/date/datetimeentry.xhtml"
-)
-@ExampleResources(
-        resources ={
-            // xhtml
-            @ExampleResource(type = ResourceType.xhtml,
-                    title="datetimeentry.xhtml",
-                    resource = "/resources/examples/ace/date/datetimeentry.xhtml"),
-            // Java Source
-            @ExampleResource(type = ResourceType.java,
-                    title="DateTimeBean.java",
-                    resource = "/WEB-INF/classes/org/icefaces/samples/showcase/example/ace/date/DateTimeBean.java")
-        }
-)
-@ManagedBean(name= DateTimeBean.BEAN_NAME)
+import org.icefaces.samples.showcase.metadata.annotation.ComponentExample;
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResource;
+import org.icefaces.samples.showcase.metadata.annotation.ExampleResources;
+import org.icefaces.samples.showcase.metadata.annotation.ResourceType;
+import org.icefaces.samples.showcase.metadata.context.ComponentExampleImpl;
+
+@ComponentExample(parent = DateEntryBean.BEAN_NAME, title = "example.ace.dateentry.timeentry.title", description = "example.ace.dateentry.timeentry.description", example = "/resources/examples/ace/date/datetimeentry.xhtml")
+@ExampleResources(resources = {
+// xhtml
+		@ExampleResource(type = ResourceType.xhtml, title = "datetimeentry.xhtml", resource = "/resources/examples/ace/date/datetimeentry.xhtml"),
+		// Java Source
+		@ExampleResource(type = ResourceType.java, title = "DateTimeBean.java", resource = "/WEB-INF/classes/org/icefaces/samples/showcase/example/ace/date/DateTimeBean.java") })
+@ManagedBean(name = DateTimeBean.BEAN_NAME)
 @CustomScoped(value = "#{window}")
-public class DateTimeBean extends ComponentExampleImpl<DateTimeBean> implements Serializable {
-    public static final String BEAN_NAME = "dateTime";
-    
-    private static final String PATTERN_DATE = "MM/dd/yyyy";
-    private static final String PATTERN_TIME = "h:mm:ss a";
-    private static final String PATTERN_BOTH = PATTERN_DATE + " " + PATTERN_TIME;
-    
-    private Date selectedDate;
-    private String timeType = "both";
-    private String pattern = PATTERN_BOTH;
-    private boolean timeOnly = false;
+public class DateTimeBean extends ComponentExampleImpl<DateTimeBean> implements
+		Serializable {
+	public static final String BEAN_NAME = "dateTime";
 
-    public DateTimeBean() {
-        super(DateTimeBean.class);
-        
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Canada/Mountain"), Locale.getDefault());
-        selectedDate = calendar.getTime();
-    }
-    
-    public Date getSelectedDate() {
-        return selectedDate;
-    }
-    
-    public String getTimeType() {
-        return timeType;
-    }
-    
-    public String getPattern() {
-        return pattern;
-    }
-    
-    public boolean getTimeOnly() {
-        return timeOnly;
-    }
-    
-    public void setSelectedDate(Date selectedDate) {
-        this.selectedDate = selectedDate;
-    }
-    
-    public void setTimeType(String timeType) {
-        this.timeType = timeType;
-    }
-    
-    public void setPattern(String pattern) {
-        this.pattern = pattern;
-    }
-    
-    public void setTimeOnly(boolean timeOnly) {
-        this.timeOnly = timeOnly;
-    }
-    
-    @PostConstruct
-    public void initMetaData() {
-        super.initMetaData();
-    }
-    
-    public void typeChanged(ValueChangeEvent event) {
-        String val = event.getNewValue().toString();
-        
-        if ("time".equals(val)) {
-            pattern = PATTERN_TIME;
-            timeOnly = true;
-        }
-        else if ("date".equals(val)) {
-            pattern = PATTERN_DATE;
-            timeOnly = false;
-        }
-        else {
-            pattern = PATTERN_BOTH;
-            timeOnly = false;
-        }
-    }
+	private static final String PATTERN_DATE = "MM/dd/yyyy";
+	private static final String PATTERN_TIME = "h:mm:ss a";
+	private static final String PATTERN_BOTH = PATTERN_DATE + " "
+			+ PATTERN_TIME;
+
+	private Date selectedDate;
+	private String timeType = "both";
+	private String pattern = PATTERN_BOTH;
+	private boolean timeOnly = false;
+
+	public DateTimeBean() {
+		super(DateTimeBean.class);
+
+		Calendar calendar = Calendar.getInstance(
+				TimeZone.getTimeZone("Canada/Mountain"), Locale.getDefault());
+		selectedDate = calendar.getTime();
+	}
+
+	public Date getSelectedDate() {
+		return selectedDate;
+	}
+
+	public String getTimeType() {
+		return timeType;
+	}
+
+	public String getPattern() {
+		return pattern;
+	}
+
+	public boolean getTimeOnly() {
+		return timeOnly;
+	}
+
+	public void setSelectedDate(Date selectedDate) {
+		if (pattern.equals(PATTERN_TIME)) {
+			// Only update the time portion of the date
+			Calendar currentDate = Calendar.getInstance();
+			currentDate.setTime(this.selectedDate);
+
+			Calendar newDate = Calendar.getInstance();
+			newDate.setTime(selectedDate);
+			currentDate.set(Calendar.HOUR_OF_DAY,
+					newDate.get(Calendar.HOUR_OF_DAY));
+			currentDate.set(Calendar.MINUTE, newDate.get(Calendar.MINUTE));
+			currentDate.set(Calendar.SECOND, newDate.get(Calendar.SECOND));
+
+			this.selectedDate = currentDate.getTime();
+
+		} else if (pattern.equals(PATTERN_DATE)) {
+			// Only update the date portion of the date
+			Calendar currentDate = Calendar.getInstance();
+			currentDate.setTime(this.selectedDate);
+
+			Calendar newDate = Calendar.getInstance();
+			newDate.setTime(selectedDate);
+			currentDate.set(Calendar.DAY_OF_MONTH,
+					newDate.get(Calendar.DAY_OF_MONTH));
+			currentDate.set(Calendar.MONTH, newDate.get(Calendar.MONTH));
+			currentDate.set(Calendar.YEAR, newDate.get(Calendar.YEAR));
+
+		} else {
+			// Overwrite the whole object
+			this.selectedDate = selectedDate;
+		}
+	}
+
+	public void setTimeType(String timeType) {
+		this.timeType = timeType;
+	}
+
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+
+	public void setTimeOnly(boolean timeOnly) {
+		this.timeOnly = timeOnly;
+	}
+
+	@PostConstruct
+	public void initMetaData() {
+		super.initMetaData();
+	}
+
+	public void typeChanged(ValueChangeEvent event) {
+		String val = event.getNewValue().toString();
+
+		if ("time".equals(val)) {
+			pattern = PATTERN_TIME;
+			timeOnly = true;
+		} else if ("date".equals(val)) {
+			pattern = PATTERN_DATE;
+			timeOnly = false;
+		} else {
+			pattern = PATTERN_BOTH;
+			timeOnly = false;
+		}
+	}
 }
