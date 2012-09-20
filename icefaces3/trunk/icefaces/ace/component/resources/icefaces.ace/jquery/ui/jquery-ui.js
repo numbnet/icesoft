2833082feaf260b35a4e1a62c845a6749a4aa6e5
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-20 16:40:35 +0000

    ICE-8558 reverted for now. Can't easily fix problems created. See JIRA for details.

--- icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/jquery/ui/jquery-ui.js
+++ icefaces3/trunk/icefaces/ace/component/resources/icefaces.ace/jquery/ui/jquery-ui.js
@@ -7539,11 +7539,10 @@ $.extend(Datepicker.prototype, {
         var showOn = this._get(inst, 'showOn');
         if (showOn == 'focus' || showOn == 'both') // pop-up date picker when in the marked field
         {
-            input.bind('focus', this._showDatepicker);
+            input.focus(this._showDatepicker);
+            input.click(this._showDatepicker); // ICE-8552
         }
-        input.click(this._showDatepicker); // ICE-8552
-
-        if (showOn == 'button' || showOn == 'both') { // pop-up date picker when button clicked
+		if (showOn == 'button' || showOn == 'both') { // pop-up date picker when button clicked
 			var buttonText = this._get(inst, 'buttonText');
 			var buttonImage = this._get(inst, 'buttonImage');
 			inst.trigger = $(this._get(inst, 'buttonImageOnly') ?
