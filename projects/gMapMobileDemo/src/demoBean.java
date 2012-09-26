6b9f554c0b16b49ea1bd6c4a11a369ff4f9f0794
brennanm <brennanm@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-09-26 20:42:23 +0000

    Updating beta demo for Judy

--- projects/gMapMobileDemo/src/demoBean.java
+++ projects/gMapMobileDemo/src/demoBean.java
@@ -1,4 +1,3 @@
-
 import org.icefaces.util.EnvUtils;
 
 import javax.faces.bean.CustomScoped;
@@ -12,12 +11,9 @@ import javax.faces.model.SelectItem;
 import javax.faces.event.ActionEvent;
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.List;
+import java.util.*;
 
-@ManagedBean (name="demoBean")
+@ManagedBean(name = "demoBean")
 @CustomScoped(value = "#{window}")
 public class demoBean {
     private String autocompleteReturn;
@@ -30,6 +26,8 @@ public class demoBean {
     private String baseURL;
     private String address;
     private List<SearchedPosition> list = new ArrayList<SearchedPosition>();
+    private String types;
+    private String autoUrl;
 
     public String getAutocompleteReturn() {
         return autocompleteReturn;
@@ -37,10 +35,12 @@ public class demoBean {
 
     public void setAutocompleteReturn(String autocompleteReturn) {
         this.autocompleteReturn = autocompleteReturn;
-        String trimmed = autocompleteReturn.substring(1,autocompleteReturn.length()-2);
-        String[] split = trimmed.split(", ");
-        autoLat=split[0];
-        autoLong=split[1];
+        if (!autocompleteReturn.equals("null")) {
+            String trimmed = autocompleteReturn.substring(1, autocompleteReturn.length() - 2);
+            String[] split = trimmed.split(", ");
+            autoLat = split[0];
+            autoLong = split[1];
+        }
     }
 
     public String getGeoLat() {
@@ -76,7 +76,7 @@ public class demoBean {
     }
 
     public String getDistance() {
-        if(geoLat==null || autoLat == null){
+        if (geoLat == null || autoLat == null) {
             distance = "undefined";
             return distance;
         }
@@ -85,24 +85,24 @@ public class demoBean {
         double lon1 = Double.parseDouble(geoLong);
         double lon2 = Double.parseDouble(autoLong);
         int R = 6371; // km
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLon = Math.toRadians(lon2-lon1);
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
         lat1 = Math.toRadians(lat1);
         lat2 = Math.toRadians(lat2);
 
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         double d = R * c;
-        if(d>=1)
-        {
+        if (d >= 1) {
             BigDecimal rounded = new BigDecimal(Double.toString(d));
-            distance = rounded.setScale(2,BigDecimal.ROUND_HALF_UP).toString() + " Km";
+            distance = rounded.setScale(2, BigDecimal.ROUND_HALF_UP).toString() + " Km";
+            System.out.println(distance);
             return distance;
-        }
-        else{
+        } else {
             BigDecimal rounded = new BigDecimal(Double.toString(d * 1000));
-            distance = rounded.setScale(2,BigDecimal.ROUND_HALF_UP).toString() + " m";
+            distance = rounded.setScale(2, BigDecimal.ROUND_HALF_UP).toString() + " m";
+            System.out.println(distance);
             return distance;
         }
     }
@@ -112,7 +112,7 @@ public class demoBean {
     }
 
     public String getBearing() {
-        if(geoLat==null || autoLat == null){
+        if (geoLat == null || autoLat == null) {
             bearing = "undefined";
             return bearing;
         }
@@ -120,12 +120,12 @@ public class demoBean {
         double lat2 = Double.parseDouble(autoLat);
         double lon1 = Double.parseDouble(geoLong);
         double lon2 = Double.parseDouble(autoLong);
-        double dLon = Math.toRadians(lon2-lon1);
+        double dLon = Math.toRadians(lon2 - lon1);
         lat1 = Math.toRadians(lat1);
         lat2 = Math.toRadians(lat2);
         double y = Math.sin(dLon) * Math.cos(lat2);
-        double x = Math.cos(lat1)*Math.sin(lat2) -
-                Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+        double x = Math.cos(lat1) * Math.sin(lat2) -
+                Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
         double brng = Math.toDegrees(Math.atan2(y, x));
         if (brng < 0)
             brng += 360;
@@ -136,7 +136,8 @@ public class demoBean {
     public void setBearing(String bearing) {
         this.bearing = bearing;
     }
-    public String getBaseURL()  {
+
+    public String getBaseURL() {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         ExternalContext externalContext = facesContext.getExternalContext();
         String serverName = externalContext.getRequestHeaderMap()
@@ -155,7 +156,8 @@ public class demoBean {
         position.setLatitude(autoLat);
         position.setLongitude(autoLong);
         position.setLabel(getDistance());
-        position.setMarker(".\\\\Pin.png");
+
+        position.setIcon(chooseIcon());
         list.add(position);
 
         return list;
@@ -176,8 +178,46 @@ public class demoBean {
         this.address = address;
     }
 
-    public class SearchedPosition{
-        public String latitude,longitude,label,marker;
+    public String getTypes() {
+        return types;
+    }
+
+    public void setTypes(String types) {
+        System.out.println(types);
+        this.types = types;
+    }
+
+    public String getAutoUrl() {
+        return autoUrl;
+    }
+
+    public void setAutoUrl(String autoUrl) {
+        this.autoUrl = autoUrl;
+    }
+
+    public String chooseIcon() {
+        if (types != null && !types.equals("null")) {
+            String[] typeArray = types.split(",");
+            List<String> typeList = Arrays.asList(typeArray);
+            if (typeList.contains("country"))
+                return "/country.png";
+            if (typeList.contains("locality"))
+                return "/city.png";
+            if (typeList.contains("bar"))
+                return "/bar.png";
+            if (typeList.contains("restaurant"))
+                return "/restaurant.png";
+            if (typeList.contains("lodging"))
+                return "/lodging.png";
+            if (typeList.contains("art_gallery") || typeList.contains("amusement_park") || typeList.contains("aquarium") || typeList.contains("museum") || typeList.contains("natural_feature") || typeList.contains("zoo") || typeList.contains("point_of_interest"))
+                return "/sight.png";
+            return "/pin.png";
+        } else
+            return "/pin.png";
+    }
+
+    public class SearchedPosition {
+        public String latitude, longitude, label, icon;
 
         public String getLatitude() {
             return latitude;
@@ -195,20 +235,20 @@ public class demoBean {
             this.longitude = longitude;
         }
 
-        public String getMarker() {
-            return marker;
-        }
-
-        public void setMarker(String marker) {
-            this.marker = marker;
-        }
-
         public String getLabel() {
             return label;
         }
 
         public void setLabel(String label) {
             this.label = label;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public void setIcon(String icon) {
+            this.icon = icon;
         }
     }
 }
