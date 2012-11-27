7bca969de586413299538f4b997bc76838c6d88f
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-11-27 23:35:55 +0000

    Fix event listener cleanup for for elements by iterating over the form element types using getElementByTagName. The previous implementation was not running the cleanup on form elements when they were updated individually.

--- icefaces3/trunk/icefaces/core/src/main/javascript/fixjsf.js
+++ icefaces3/trunk/icefaces/core/src/main/javascript/fixjsf.js
@@ -245,14 +245,13 @@
             var id = update.getAttribute('id');
             var e = lookupElementById(id);
             if (e) {
-                each(['a', 'iframe'], function(type) {
+                each(['a', 'iframe', 'input', 'select', 'button', 'textarea'], function(type) {
                     each(e.getElementsByTagName(type), clearEventHandlers);
                 });
 
                 each(e.getElementsByTagName('form'), function(form) {
                     form.submit = null;
                     form.onsubmit = null;
-                    each(form.elements, clearEventHandlers);
                 });
             }
         });
