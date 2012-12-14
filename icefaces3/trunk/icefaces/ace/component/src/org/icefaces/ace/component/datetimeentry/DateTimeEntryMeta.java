ad15538c472b9d97a20e7768fc288f47821e7a7f
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2012-12-14 00:20:09 +0000

    ICE-8819: ace:dateTimeEntry - Incorrect date converted on client side.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/datetimeentry/DateTimeEntryMeta.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/datetimeentry/DateTimeEntryMeta.java
@@ -197,5 +197,5 @@ public class DateTimeEntryMeta extends UIInputMeta {
 
     @Property(defaultValue = "true", tlddoc = "With lenient parsing, the Java date parser may use heuristics to interpret" +
             " inputs that do not precisely match this date's format. With strict parsing, inputs must match this date's format.")
-    private boolean parsingLenient;
+    private boolean lenientParsing;
 }
