0b6a65387e500d76bf848f5d2ed9df1fd2f739e8
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-01-04 01:05:09 +0000

    Temporary new 3.2.0 release tag.

---
+++ icefaces3/tags/icefaces-3.2.0-temp/icefaces/ace/component/src/org/icefaces/ace/model/tree/TreeModelAdaptor.java
@@ -0,0 +1,11 @@
+package org.icefaces.ace.model.tree;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nils
+ * Date: 8/15/12
+ * Time: 7:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface TreeModelAdaptor {
+}
