c6428761eb85000c98cdd313a9dff8508fccd765
nils.lundquist <nils.lundquist@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-01-07 23:37:20 +0000

    ICE-8837 - Added ace:list broadcast to enable SelectionListener method expression attribute.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/list/ACEList.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/list/ACEList.java
@@ -1,5 +1,12 @@
 package org.icefaces.ace.component.list;
 
+import org.icefaces.ace.event.ListSelectEvent;
+
+import javax.el.MethodExpression;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.FacesEvent;
 import javax.faces.model.DataModel;
 import java.util.*;
 
@@ -22,7 +29,31 @@ public class ACEList extends ListBase {
             setSelections(set);
         }
         return set;
-   }
+    }
+
+    @Override
+    public void broadcast(FacesEvent event) throws AbortProcessingException {
+        super.broadcast(event);
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        String outcome = null;
+        MethodExpression me = null;
+
+        if (event instanceof ListSelectEvent) me = getSelectionListener();
+
+        if (me != null) {
+            if (!context.isValidationFailed()) {
+                outcome = (String) me.invoke(context.getELContext(), new Object[] {event});
+            }
+
+            if (outcome != null) {
+                NavigationHandler navHandler = context.getApplication().getNavigationHandler();
+                navHandler.handleNavigation(context, null, outcome);
+                context.renderResponse();
+            }
+        }
+    }
+
     // References to immigrant objects are gathered immediately
     List<ImmigrationRecord> immigrants;
 
