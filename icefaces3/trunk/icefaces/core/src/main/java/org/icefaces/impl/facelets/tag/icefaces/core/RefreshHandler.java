b6d1174718faa86a35fad8c74517ec0dbfb1bff1
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-01-11 21:49:41 +0000

    icecore:refresh taghandler now adding component directly to viewroot (ICE-8840)

--- icefaces3/trunk/icefaces/core/src/main/java/org/icefaces/impl/facelets/tag/icefaces/core/RefreshHandler.java
+++ icefaces3/trunk/icefaces/core/src/main/java/org/icefaces/impl/facelets/tag/icefaces/core/RefreshHandler.java
@@ -8,10 +8,6 @@ import javax.faces.component.UIOutput;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
-import javax.faces.event.AbortProcessingException;
-import javax.faces.event.PreRenderComponentEvent;
-import javax.faces.event.SystemEvent;
-import javax.faces.event.SystemEventListener;
 import javax.faces.view.facelets.FaceletContext;
 import javax.faces.view.facelets.TagAttribute;
 import javax.faces.view.facelets.TagConfig;
@@ -19,47 +15,63 @@ import javax.faces.view.facelets.TagHandler;
 import java.io.IOException;
 
 public class RefreshHandler extends TagHandler {
-    private long interval;
-    private long duration;
-    private boolean disabled;
-    private boolean receivingPreRenderEvents;
 
     public RefreshHandler(TagConfig config) {
         super(config);
-
-        TagAttribute intervalAttribute = getAttribute("interval");
-        TagAttribute durationAttribute = getAttribute("duration");
-        TagAttribute disabledAttribute = getAttribute("disabled");
-
-        interval = intervalAttribute == null ? 10000 : (Long.valueOf(intervalAttribute.getValue()) * 1000);//seconds
-        duration = durationAttribute == null ? -1 : (Long.valueOf(durationAttribute.getValue()) * 60 * 1000);//minutes
-        disabled = disabledAttribute == null ? false : Boolean.parseBoolean(disabledAttribute.getValue());
-        disabled = duration == 0 ? true : disabled;
     }
 
     public void apply(FaceletContext ctx, UIComponent parent) throws IOException {
-        if (!disabled && !receivingPreRenderEvents) {
-            ctx.getFacesContext().getApplication().subscribeToEvent(PreRenderComponentEvent.class, new RefreshSetup());
-            receivingPreRenderEvents = true;
-        }
-    }
-
-    private class RefreshSetup implements SystemEventListener {
-        public void processEvent(SystemEvent event) throws AbortProcessingException {
+        boolean disabled = getDisabled();
+        if (!disabled) {
             FacesContext context = FacesContext.getCurrentInstance();
-            UIOutput refreshSetup = new RefreshSetupOutput();
+            UIOutput refreshSetup = new RefreshSetupOutput(
+                getInterval(), getDuration(), disabled);
             UIViewRoot viewRoot = context.getViewRoot();
             refreshSetup.setTransient(true);
             refreshSetup.setId(viewRoot.createUniqueId(context, "_setupRefresh"));
             viewRoot.addComponentResource(context, refreshSetup, "body");
         }
-
-        public boolean isListenerForSource(Object source) {
-            return EnvUtils.isICEfacesView(FacesContext.getCurrentInstance()) && source instanceof UIViewRoot;
-        }
     }
 
+    public long getInterval()  {
+        TagAttribute intervalAttribute = getAttribute("interval");
+        long interval = intervalAttribute == null ? 10000 :
+            (Long.valueOf(intervalAttribute.getValue()) * 1000);
+            //seconds
+        return interval;
+    }
+
+    public long getDuration()  {
+        TagAttribute durationAttribute = getAttribute("duration");
+        long duration = durationAttribute == null ? -1 :
+            (Long.valueOf(durationAttribute.getValue()) * 60 * 1000);
+            //minutes
+        return duration;
+    }
+
+    public boolean getDisabled()  {
+        TagAttribute disabledAttribute = getAttribute("disabled");
+        boolean disabled = disabledAttribute == null ? false :
+            Boolean.parseBoolean(disabledAttribute.getValue());
+        disabled = getDuration() == 0 ? true : disabled;
+        return disabled;
+    }
+
+
     private class RefreshSetupOutput extends UIOutput {
+        //this component is transient so can use member fields
+        //rather than state saving
+        private long interval;
+        private long duration;
+        private boolean disabled;
+
+        public RefreshSetupOutput(long interval, long duration,
+                boolean disabled)  {
+            this.interval = interval;
+            this.duration = duration;
+            this.disabled = disabled;
+        }
+
         public void encodeBegin(FacesContext context) throws IOException {
             ResponseWriter writer = context.getResponseWriter();
             writer.startElement("script", this);
