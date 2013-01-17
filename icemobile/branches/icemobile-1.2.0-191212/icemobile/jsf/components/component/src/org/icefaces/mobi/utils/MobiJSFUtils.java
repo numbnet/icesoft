223b1648db50aef967e10b08c2c4cce10ff7718c
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-01-17 21:37:30 +0000

    move other javax.servlet.Part case inside try/catch (MOBI-614)

--- icemobile/branches/icemobile-1.2.0-191212/icemobile/jsf/components/component/src/org/icefaces/mobi/utils/MobiJSFUtils.java
+++ icemobile/branches/icemobile-1.2.0-191212/icemobile/jsf/components/component/src/org/icefaces/mobi/utils/MobiJSFUtils.java
@@ -82,16 +82,16 @@ public class MobiJSFUtils {
         Map auxMap = null;
         try {
             part = request.getPart(partUploadName);
+            if (null == part) {
+                auxMap = AuxUploadResourceHandler.getAuxRequestMap();
+                part = (Part) auxMap.get(partUploadName);
+            }
         } catch (IOException e) {
             throw e;
         } catch (Throwable t) {
             // ignore Throwable here since auxUpload is not multipart
             // and not-null part must be checked and we may not have
             // getPart API on Servlet 2.5
-        }
-        if (null == part) {
-            auxMap = AuxUploadResourceHandler.getAuxRequestMap();
-            part = (Part) auxMap.get(partUploadName);
         }
         if (null == part) {
             Map commonsMeta = (Map) request.getAttribute("org.icemobile.file." + clientId);
