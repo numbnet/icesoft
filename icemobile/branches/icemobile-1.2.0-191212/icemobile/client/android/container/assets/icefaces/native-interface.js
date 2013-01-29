3234782efde864bf2b2e3b3b950fe2ed1790d9c4
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-01-29 18:42:34 +0000

    do not serialize untyped HTML elements such as fieldset (MOBI-635)

--- icemobile/branches/icemobile-1.2.0-191212/icemobile/client/android/container/assets/icefaces/native-interface.js
+++ icemobile/branches/icemobile-1.2.0-191212/icemobile/client/android/container/assets/icefaces/native-interface.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the
@@ -296,7 +296,9 @@ if (!window.ice.mobile) {
                             }
                             break;
                         default:
-                            addField(prefix + el.name, el.value);
+                            if (el.type)  {
+                                addField(prefix + el.name, el.value);
+                            }
                     }
                 }
             }
