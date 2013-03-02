93bfae4c07ce8c1016c18991fde7385e345b9eb9
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-03-02 00:09:44 +0000

    ICE-8921: New ACE themeSelect component.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/themeselect/ThemeSelectRenderer.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/themeselect/ThemeSelectRenderer.java
@@ -29,25 +29,35 @@ import javax.faces.context.ResponseWriter;
 import javax.faces.render.Renderer;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.Enumeration;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @MandatoryResourceComponent(tagName = "themeSelect", value = "org.icefaces.ace.component.themeselect.ThemeSelect")
 public class ThemeSelectRenderer extends Renderer {
+
+    private static String THEME_LIST = Constants.THEME_PARAM + ".list";
+
     private FacesContext context;
     private ResponseWriter writer;
+    private ThemeSelect component;
+    private String clientId;
     private String selectedTheme;
+    private Map<String, String> themeList;
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         this.context = context;
+        this.component = (ThemeSelect) component;
         super.encodeEnd(context, component);
         if (!component.isRendered()) {
             return;
         }
         writer = context.getResponseWriter();
-        String clientId = component.getClientId(context);
+        clientId = component.getClientId(context);
         String selectId = "select_" + clientId;
 
         writer.startElement("span", component);
@@ -56,77 +66,90 @@ public class ThemeSelectRenderer extends Renderer {
         writer.startElement("select", component);
         writer.writeAttribute("id", selectId, "id");
         writer.writeAttribute("name", selectId, "id");
+        writer.writeAttribute("class", "ui-state-highlight", null);
         renderOptions();
         writer.endElement("select");
+
+        renderScript();
+
+        writer.startElement("span", null);
+        writer.writeAttribute("class", "ui-helper-hidden", null);
+        writer.write(String.valueOf(new Date().getTime()));
+        writer.endElement("span");
 
         writer.endElement("span");
     }
 
     private void renderOptions() throws IOException {
-        System.out.println("\nThemeSelectRenderer.renderOptions");
-        selectedTheme = getSelectedTheme();
+        getThemeList();
+        selectedTheme = (String) component.getValue();
+        selectedTheme = selectedTheme == null ? "" : selectedTheme.trim();
+        if (selectedTheme.equals("")) {
+            selectedTheme = getSelectedTheme();
+        }
+        if (themeList.get(selectedTheme) == null) {
+            selectedTheme = "sam";
+        }
+        context.getExternalContext().getSessionMap().put(Constants.THEME_PARAM, selectedTheme);
 
-        renderOption("none", "");
+        String theme, href;
+        for (Map.Entry<String, String> entry : themeList.entrySet()) {
+            theme = entry.getKey();
+            href = entry.getValue();
+            writer.startElement("option", null);
+            writer.writeAttribute("value", theme, null);
+            if (theme.equals(selectedTheme)) {
+                writer.writeAttribute("selected", "selected", null);
+            }
+            writer.writeAttribute("data-href", href, null);
+            writer.write(theme);
+            writer.endElement("option");
+        }
+    }
+
+    private void renderScript() throws IOException {
+        writer.startElement("script", null);
+        writer.writeAttribute("type", "text/javascript", null);
+        writer.write("ice.ace.ThemeSelect.singleEntry(\"" + clientId + "\");");
+        writer.endElement("script");
+    }
+
+    private void getThemeList() throws IOException {
+        Map<String, Object> appMap = context.getExternalContext().getApplicationMap();
+        themeList = (Map<String, String>) appMap.get(THEME_LIST);
+        if (themeList != null) {
+            return;
+        }
+        themeList = new TreeMap<String, String>();
+        themeList.put("none", "");
 
         ResourceHandler resourceHandler = context.getApplication().getResourceHandler();
         Resource resource;
         for (String theme : new String[]{"rime", "sam"}) {
             resource = resourceHandler.createResource("themes/" + theme + "/theme.css", "icefaces.ace");
             if (resource != null) {
-                renderOption(theme, resource);
+                themeList.put(theme, resource.getRequestPath());
             }
         }
-        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources("META-INF/resources");
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Enumeration<URL> urls = classLoader.getResources("META-INF/resources");
         URL url;
-        Pattern pattern = Pattern.compile("jar:.*/WEB-INF/lib/(?<jarName>.+)\\.jar!/META-INF/resources");
-        Matcher matcher = pattern.matcher("");
+        Matcher matcher = Pattern.compile("jar:.*/WEB-INF/lib/(.+)\\.jar!/META-INF/resources").matcher("");
         String theme;
         while (urls.hasMoreElements()) {
             url = urls.nextElement();
-//            System.out.println("url = " + url);
             if (matcher.reset(url.toString()).matches()) {
-//                theme = matcher.group("jarName");
                 theme = matcher.group(1);
-                System.out.println("theme = " + theme);
-                resource = resourceHandler.createResource("theme.css", "ace-" + theme);
-                if (resource != null) {
-                    renderOption(theme, resource);
+                url = classLoader.getResource("META-INF/resources/ace-" + theme);
+                if (url != null) {
+                    resource = resourceHandler.createResource("theme.css", "ace-" + theme);
+                    if (resource != null) {
+                        themeList.put(theme, resource.getRequestPath());
+                    }
                 }
             }
         }
-/*
-        ExternalContext extContext = FacesContext.getCurrentInstance().getExternalContext();
-        Set<String> libJars = extContext.getResourcePaths("/WEB-INF/lib");
-        if (libJars == null) {
-            return;
-        }
-        JarInputStream jarInputStream;
-        JarEntry jarEntry;
-        String entryName;
-        for (String jar : libJars) {
-            System.out.println("jar = " + jar);
-            jarInputStream = new JarInputStream(extContext.getResourceAsStream(jar));
-            while ((jarEntry = jarInputStream.getNextJarEntry()) != null) {
-                entryName = jarEntry.getName();
-                if (entryName.endsWith("theme.css")) {
-                    System.out.println("entryName = " + entryName);
-                }
-                jarInputStream.closeEntry();
-            }
-            jarInputStream.close();
-        }
-*/
-    }
-
-    private void renderOption(String theme, Object href) throws IOException {
-        writer.startElement("option", null);
-        writer.writeAttribute("value", theme, null);
-        if (theme.equals(selectedTheme)) {
-            writer.writeAttribute("selected", "selected", null);
-        }
-        writer.writeAttribute("data-href", href, null);
-        writer.write(theme);
-        writer.endElement("option");
+        appMap.put(THEME_LIST, themeList);
     }
 
     // Copied from org.icefaces.ace.renderkit.HeadRenderer.processEvent
