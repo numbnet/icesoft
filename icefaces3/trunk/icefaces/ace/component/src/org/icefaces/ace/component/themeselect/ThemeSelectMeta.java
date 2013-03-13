ab0470a0a244f757c471aefead9dc45dd15b1d0b
yip.ng <yip.ng@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-03-13 17:21:03 +0000

    ICE-8921: New ACE themeSelect component.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/themeselect/ThemeSelectMeta.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/themeselect/ThemeSelectMeta.java
@@ -18,6 +18,7 @@ package org.icefaces.ace.component.themeselect;
 import org.icefaces.ace.meta.annotation.ClientBehaviorHolder;
 import org.icefaces.ace.meta.annotation.ClientEvent;
 import org.icefaces.ace.meta.annotation.Component;
+import org.icefaces.ace.meta.annotation.Property;
 import org.icefaces.ace.meta.baseMeta.UIInputMeta;
 import org.icefaces.ace.resources.ACEResourceNames;
 import org.icefaces.resources.ICEResourceDependencies;
@@ -34,7 +35,7 @@ import org.icefaces.resources.ICEResourceLibrary;
         rendererType = "org.icefaces.ace.component.ThemeSelectRenderer",
         componentFamily = "org.icefaces.ace.ThemeSelect",
         tlddoc = "The themeSelect component can be used to dynamically change the current ACE ThemeRoller theme in the application. " +
-                "See <a href=\"http://www.icesoft.org/wiki/display/ICE/ace.theme\">Wiki Documentation</a> for more details on themes."
+                "<p>For more information, see the <a href=\"http://wiki.icefaces.org/display/ICE/ThemeSelect\">ThemeSelect Wiki Documentation</a>."
 )
 @ICEResourceLibrary(ACEResourceNames.ACE_LIBRARY)
 @ICEResourceDependencies({
@@ -48,4 +49,34 @@ import org.icefaces.resources.ICEResourceLibrary;
 }, defaultEvent = "valueChange")
 
 public class ThemeSelectMeta extends UIInputMeta {
+
+    @Property(tlddoc = "Access key that, when pressed, transfers focus to this element.")
+    private String accesskey;
+
+    @Property(tlddoc = "Direction indication for text that does not inherit directionality. Valid values are \"LTR\" (left-to-right) and \"RTL\" (right-to-left).")
+    private String dir;
+
+    @Property(tlddoc = "Flag indicating that this element must never receive focus or be included in a subsequent submit. A value of false causes no attribute to be rendered, while a value of true causes the attribute to be rendered as disabled=\"disabled\".")
+    private boolean disabled;
+
+    @Property(tlddoc = "Code describing the language used in the generated markup for this component.")
+    private String lang;
+
+    @Property(tlddoc = "A localized user presentable name for this component.")
+    private String label;
+
+    @Property(tlddoc = "CSS style(s) to be applied when this component is rendered.")
+    private String style;
+
+    @Property(tlddoc = "Space-separated list of CSS style class(es) to be applied when this element is rendered. This value must be passed through as the \"class\" attribute on generated markup.")
+    private String styleClass;
+
+    @Property(tlddoc = "Position of this element in the tabbing order for the current document. This value must be an integer between 0 and 32767.")
+    private String tabindex;
+
+    @Property(tlddoc = "Advisory title information about markup elements generated for this component.")
+    private String title;
+
+    @Property(tlddoc = "Alternate textual description of the element rendered by this component.")
+    private String alt;
 }
