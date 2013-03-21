511ac07fc97e512cbb56c51713951fc69d99f04f
mark.collette <mark.collette@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-03-21 21:15:15 +0000

    ICE-9034 : Remove tabSet state saving work-around since framework solution is in place

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/tabset/TabSetMeta.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/tabset/TabSetMeta.java
@@ -176,7 +176,7 @@ public class TabSetMeta extends UIComponentBaseMeta {
 
 
     @Field(javadoc="Maintains the record of which tabs have been visited")
-    private String visitedTabClientIds;
+    private List visitedTabClientIds;
 
     @Field(javadoc="This retains the selectedIndex from the beginning of " +
         "the lifecycle, so that when in server mode, only this tabPane's " +
