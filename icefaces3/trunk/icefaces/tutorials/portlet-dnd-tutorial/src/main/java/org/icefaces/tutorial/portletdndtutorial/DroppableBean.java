9a98b1361bf55ed8a681829dd54f54e4a836ff91
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-04-18 19:26:43 +0000

    ICE-9127 added inter-portlet drag and drop tutorial app; added 'liferay' property to common.xml to be able to build a war with the Liferay faces bridge

---
+++ icefaces3/trunk/icefaces/tutorials/portlet-dnd-tutorial/src/main/java/org/icefaces/tutorial/portletdndtutorial/DroppableBean.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.tutorial.portletdndtutorial;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.icefaces.ace.event.DragDropEvent;
+import org.icefaces.bean.WindowDisposed;
+import org.icefaces.application.PushRenderer;
+
+import java.util.*;
+
+@ManagedBean
+@WindowDisposed
+@ViewScoped
+public class DroppableBean implements java.io.Serializable {
+
+    @ManagedProperty(value = "#{windowScopedBean}")
+    private WindowScopedBean windowScopedBean;
+
+	private List<Item> items;
+	
+	@PostConstruct
+	public void postConstruct() {
+		items = new ArrayList<Item>();
+		PushRenderer.addCurrentView("tutorial");
+	}
+	
+	public void handleDrop(DragDropEvent e) {
+		Item item = windowScopedBean.getDraggedItem();
+		if (item != null) {
+			items.add(item);
+			item.removeFromOriginalList();
+		}
+		PushRenderer.render("tutorial");
+	}
+	
+	public List<Item> getItems() {
+		return items;
+	}
+	
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+	
+	public void setWindowScopedBean(WindowScopedBean windowScopedBean) {
+		this.windowScopedBean = windowScopedBean;
+	}
+}
