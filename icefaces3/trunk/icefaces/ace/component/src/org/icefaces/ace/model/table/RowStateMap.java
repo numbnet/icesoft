c9e2268980ddedf16558459e5e9b535532580994
nils.lundquist <nils.lundquist@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-07-11 23:08:55 +0000

    ICE-9411 - Completed javadoc.

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/model/table/RowStateMap.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/model/table/RowStateMap.java
@@ -25,6 +25,11 @@ import javax.faces.model.DataModel;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * A Map<Object, RowState> that defines the ace:dataTable feature state of the row objects of a given ace:dataTable.
+ *
+ * States can be added at initialization, or will be created as needed during row object rendering if no state exists.
+ */
 public class RowStateMap implements Map<Object, RowState>, Serializable {
     private Map<Object, RowState> map = new HashMap<Object, RowState>();
 
@@ -39,30 +44,63 @@ public class RowStateMap implements Map<Object, RowState>, Serializable {
     private static Predicate hasSelectedCellsPredicate = new SelectedCellsPredicate();
 
 
+    /**
+     * Put a new state for a specific row object. Overwrites old states.
+     * @param o row object
+     * @param s new state
+     * @return old state or null if none existed
+     */
     public RowState put(Object o, RowState s) {
         return map.put(o, s);
     }
 
+    /**
+     * Put a empty state for a specific row object. Overwrites old states.
+     * @param o row object
+     */
     public void add(Object o) {
         map.put(o, new RowState());
     }
 
+    /**
+     * Get the number of states held in this map
+     * @return map entry count
+     */
     public int size() {
         return map.size();
     }
 
+    /**
+     * Get if this map is empty
+     * @return if this map empty
+     */
     public boolean isEmpty() {
         return map.isEmpty();
     }
 
+    /**
+     * Get if this map contains a state for a particular row object
+     * @param key row object
+     * @return if this map contains the row object param
+     */
     public boolean containsKey(Object key) {
         return map.containsKey(key);
     }
 
+    /**
+     * Get if this map contains a particular row state
+     * @param value row state
+     * @return if this map contains the row state param
+     */
     public boolean containsValue(Object value) {
         return map.containsValue(value);
     }
 
+    /**
+     * Get the row state for a particular row object
+     * @param o row object
+     * @return row state
+     */
     public RowState get(Object o) {
         RowState s = map.get(o);
         if (s == null) {
@@ -72,30 +110,60 @@ public class RowStateMap implements Map<Object, RowState>, Serializable {
         return s;
     }
 
+    /**
+     * Remove the row state for a particular row object. A new empty state will
+     * be added for the row if it is rendered in a coming request.
+     * @param o
+     * @return
+     */
     public RowState remove(Object o) {
         return map.remove(o);
     }
 
+    /**
+     * Add all entries from the input map to this map.
+     * @param m input map
+     */
     public void putAll(Map m) {
         map.putAll(m);
     }
 
+    /**
+     * Remove all row states from this map.
+     */
     public void clear() {
         map.clear();
     }
 
+    /**
+     * Get the set of all row objects with attached RowStates
+     * @return set of row objects that have state
+     */
     public Set keySet() {
         return map.keySet();
     }
 
+    /**
+     * Get the collection of all RowStates
+     * @return the state of every row in this map
+     */
     public Collection values() {
         return map.values();
     }
 
+    /**
+     * Get the collection of all Map Entry objects, defining a key row object and value row state.
+     * @return row object state entry collection
+     */
     public Set entrySet() {
         return map.entrySet();
     }
 
+    /**
+     * Replace a row object with one that hashes identically but is a distinct copy.
+     * Useful for updating the row state map when replacing the data set with a copy.
+     * @param o
+     */
     public void updateData(Object o) {
         map.put(o, map.remove(o));
     }
@@ -164,6 +232,11 @@ public class RowStateMap implements Map<Object, RowState>, Serializable {
         keys.retainAll(rowDataList);
     }
 
+    /**
+     * Synchronize the expanded row state with the expanded state stored in the node of the data model.
+     * Data model expansion state overwrites pre-existing state.
+     * @param dataModel tree data model
+     */
     public void setExpandableByTreeModel(TreeDataModel dataModel) {
         String currentRootId = "";
         int rowCount = dataModel.getRowCount();
@@ -214,66 +287,159 @@ public class RowStateMap implements Map<Object, RowState>, Serializable {
         }
     }
 
+    /**
+     * Get the list of selected row objects.
+     * @return list of selected row objects
+     */
     public List getSelected() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), selectedPredicate));
     }
+
+    /**
+     * Get the list of row objects with cells that are selected.
+     * @return list of row objects with selected cells
+     */
     public List getRowsWithSelectedCells() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), hasSelectedCellsPredicate));
     }
+
+    /**
+     * Get row objects that are selectable
+     * @return the list of selectable row objects
+     */
     public List getSelectable() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), selectablePredicate));
     }
+
+    /**
+     * Get the row objects that are editable
+     * @return list of editable row objects
+     */
     public List getEditable() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), editablePredicate));
     }
+
+    /**
+     * Get the row objects that are expanded
+     * @return list of expanded row objects
+     */
     public List getExpanded() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), expandedPredicate));
     }
+
+    /**
+     * Get the row objects that are expandible
+     * @return list of expandible row objects
+     */
     public List getExpandable() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), expandablePredicate));
     }
+
+    /**
+     * Get the row objects that are not invisible
+     * @return list of visible row objects
+     */
     public List getVisible() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), visiblePredicate));
     }
+
+    /**
+     * Get the row objects that have activated a particular CellEditor.
+     * @param editor active CellEditor target
+     * @return list of row objects with active editors
+     */
     public List getEditing(UIComponent editor) {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), new EditingPredicate(editor)));
     }
+
+    /**
+     * Get the row objects that expand into other rows
+     * @return list of row expanding row objects
+     */
     public List getRowExpanders() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), rowExpansionPredicate));
     }
+
+    /**
+     * Get the row objects that expand into panels
+     * @return list of panel expanding row objects
+     */
     public List getPanelExpanders() {
         return EntrySetToKeyListTransformer.transform(CollectionUtils.select(map.entrySet(), panelExpansionPredicate));
     }
 
 
+    /**
+     * Set the selection state of all the row objects
+     * @param value selected
+     */
     public void setAllSelected(boolean value) {
         for (RowState s : map.values()) s.setSelected(value);
     }
+
+    /**
+     * Set the selectable state of all the row objects
+     * @param value selectable
+     */
     public void setAllSelectable(boolean value) {
         for (RowState s : map.values()) s.setSelectable(value);
     }
+
+    /**
+     * Set the editable state of all the row objects
+     * @param value editable
+     */
     public void setAllEditable(boolean value) {
         for (RowState s : map.values()) s.setEditable(value);
     }
+
+    /**
+     * Set the expanded state of all the row objects
+     * @param value expanded
+     */
     public void setAllExpanded(boolean value) {
         for (RowState s : map.values()) s.setExpanded(value);
     }
+
+    /**
+     * Set the expandable state of all the row objects
+     * @param value expandable
+     */
     public void setAllExpandable(boolean value) {
         for (RowState s : map.values()) s.setExpandable(value);
     }
+
+    /**
+     * Set the visible state of all the row objects
+     * @param value visible
+     */
     public void setAllVisible(boolean value) {
         for (RowState s : map.values()) s.setVisible(value);
     }
+
+    /**
+     * Set the expansion mode of all the row objects to row
+     */
     public void setAllRowExpansion() {
         for (RowState s : map.values()) {
             s.setExpansionType(RowState.ExpansionType.ROW);
         }
     }
+
+    /**
+     * Set the expansion mode of all the row objects to panel
+     */
     public void setAllPanelExpansion() {
     for (RowState s : map.values()) {
             s.setExpansionType(RowState.ExpansionType.PANEL);
         }
     }
+
+    /**
+     * Set the editing state for the given CellEditor
+     * @param editor the CellEditor
+     * @param add activate or remove
+     */
     public void setAllEditing(UIComponent editor, boolean add) {
         for (RowState s : map.values())
             if (add) s.addActiveCellEditor(editor);
