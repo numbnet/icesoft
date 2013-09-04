f6645698eaba3de377daf1f749f848d96d5666fc
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-09-04 19:55:00 +0000

    ICE-9553 added fallback to access the source code contents directly from the servlet object, without having to create a new http connection, when the request was made using SSL

--- icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/util/SourceCodeLoaderServlet.java
+++ icefaces3/trunk/icefaces/samples/showcase/showcase/src/main/java/org/icefaces/samples/showcase/util/SourceCodeLoaderServlet.java
@@ -39,6 +39,23 @@ public class SourceCodeLoaderServlet extends HttpServlet implements Serializable
     private static final Pattern JAVA_PATTERN =
             Pattern.compile("/\\*.*?\\*/", Pattern.DOTALL);
 
+	private static SourceCodeLoaderServlet instance = null;
+
+	public void init() {
+		if (instance == null) instance = this;
+	}
+
+	public static SourceCodeLoaderServlet getServlet() {
+		return instance;
+	}
+
+	public InputStream getSource(String path) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintWriter writer = new PrintWriter(baos);
+		getSource(path, null, writer);
+		byte[] bytes = baos.toByteArray();
+		return new ByteArrayInputStream(bytes);
+	}
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
         // Setting the context type to text/xml provides style
@@ -50,70 +67,74 @@ public class SourceCodeLoaderServlet extends HttpServlet implements Serializable
         String sourcePath = request.getParameter("path");
         
         if (sourcePath != null) {
-            InputStream sourceStream = getServletContext().getResourceAsStream(sourcePath);
-                    
-            if (sourceStream == null) {
-                try {
-                    // Work around for websphere
-                    sourceStream = new FileInputStream(new File(getServletContext().getRealPath(sourcePath)));
-                } catch (Exception failedWorkaround) {
-                    failedWorkaround.printStackTrace();
-                }
-            }
-            
-            if (sourceStream != null) {
-                PrintWriter responseStream = null;
-                
-                try {
-                    responseStream = response.getWriter();                    
-                    
-                    // Pass the source stream to the response stream
-                    StringBuffer stringBuffer = new StringBuffer();
-                    int ch;
-                    while ((ch = sourceStream.read()) != -1) {
-                        stringBuffer.append((char) ch);
-                    }
-                    
-                    // Remove the license from the source code
-                    Matcher m = JSPX_PATTERN.matcher(stringBuffer);
-                    String toReturn = "";
-                    if (m.find(0)) {
-                        toReturn = m.replaceFirst("// MPL License text (see http://www.mozilla.org/MPL/)");
-                    } else {
-                        m = JAVA_PATTERN.matcher(stringBuffer);
-                        toReturn = m.replaceFirst("/* MPL License text (see http://www.mozilla.org/MPL/) */\n");
-                    }
-                    
-                    // Check the extension
-                    String name = sourcePath.substring(sourcePath.lastIndexOf("/") + 1);
-                    String type = "";
-                    if (sourcePath.endsWith(".java")) {
-                        type = XhtmlRendererFactory.JAVA;
-                    } else if (sourcePath.endsWith(".xhtml")) {
-                        type = XhtmlRendererFactory.XHTML;
-                    }
-                    
-                    // Highlight properly and print
-                    String highlight = XhtmlRendererFactory.getRenderer(type).highlight(name, toReturn, "utf8", false);
-                    responseStream.print(highlight);
-                } catch (Exception failedWrite) {
-                    failedWrite.printStackTrace();
-                } finally {
-                    // Close the source
-                    if (sourceStream != null) {
-                        try{
-                            sourceStream.close();
-                        } catch (Exception ignoredClose) { }
-                    }
-                    // Close the response
-                    if (responseStream != null) {
-                        try{
-                            responseStream.close();
-                        } catch (Exception ignoredClose) { }
-                    }
-                }
-            }
-        }
+			getSource(sourcePath, response, null);
+		}
+	}
+	
+	public void getSource(String sourcePath, HttpServletResponse response, PrintWriter writer) { // either response or writer must be non-null
+		InputStream sourceStream = getServletContext().getResourceAsStream(sourcePath);
+				
+		if (sourceStream == null) {
+			try {
+				// Work around for websphere
+				sourceStream = new FileInputStream(new File(getServletContext().getRealPath(sourcePath)));
+			} catch (Exception failedWorkaround) {
+				failedWorkaround.printStackTrace();
+			}
+		}
+		
+		if (sourceStream != null) {
+			PrintWriter responseStream = null;
+			
+			try {
+				responseStream = response != null ? response.getWriter() : writer;
+				
+				// Pass the source stream to the response stream
+				StringBuffer stringBuffer = new StringBuffer();
+				int ch;
+				while ((ch = sourceStream.read()) != -1) {
+					stringBuffer.append((char) ch);
+				}
+				
+				// Remove the license from the source code
+				Matcher m = JSPX_PATTERN.matcher(stringBuffer);
+				String toReturn = "";
+				if (m.find(0)) {
+					toReturn = m.replaceFirst("// MPL License text (see http://www.mozilla.org/MPL/)");
+				} else {
+					m = JAVA_PATTERN.matcher(stringBuffer);
+					toReturn = m.replaceFirst("/* MPL License text (see http://www.mozilla.org/MPL/) */\n");
+				}
+				
+				// Check the extension
+				String name = sourcePath.substring(sourcePath.lastIndexOf("/") + 1);
+				String type = "";
+				if (sourcePath.endsWith(".java")) {
+					type = XhtmlRendererFactory.JAVA;
+				} else if (sourcePath.endsWith(".xhtml")) {
+					type = XhtmlRendererFactory.XHTML;
+				}
+				
+				// Highlight properly and print
+				String highlight = XhtmlRendererFactory.getRenderer(type).highlight(name, toReturn, "utf8", false);
+				responseStream.print(highlight);
+			} catch (Exception failedWrite) {
+				failedWrite.printStackTrace();
+			} finally {
+				// Close the source
+				if (sourceStream != null) {
+					try{
+						sourceStream.close();
+					} catch (Exception ignoredClose) { }
+				}
+				// Close the response
+				if (responseStream != null) {
+					try{
+						responseStream.close();
+					} catch (Exception ignoredClose) { }
+				}
+			}
+		}
     }
 
 }
