30f6f763db0e22a49908191a7cce3543c2c4d5bf
art.zambrano <art.zambrano@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-09-24 17:51:42 +0000

    ICE-9564 override validation methods to correctly compare converted values

--- icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/simpleselectonemenu/SimpleSelectOneMenu.java
+++ icefaces3/trunk/icefaces/ace/component/src/org/icefaces/ace/component/simpleselectonemenu/SimpleSelectOneMenu.java
@@ -21,10 +21,16 @@ import org.icefaces.component.Focusable;
 import javax.faces.model.SelectItem;
 import javax.faces.component.UISelectItem;
 import javax.faces.component.UISelectItems;
+import javax.faces.component.UISelectOne;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.event.FacesEvent;
 import java.util.*;
+
+import javax.faces.application.Application;
+import javax.faces.application.FacesMessage;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class SimpleSelectOneMenu extends SimpleSelectOneMenuBase implements Focusable {
 	
@@ -105,5 +111,122 @@ public class SimpleSelectOneMenu extends SimpleSelectOneMenuBase implements Focu
 
     public String getFocusedElementId() {
         return getClientId() + "_input";
+    }
+	
+	@Override
+	protected void validateValue(FacesContext facesContext, Object submittedValue) {
+	
+		boolean found = false;
+		populateItemList();
+		Iterator matches = getItemListIterator();
+		SelectItem item = null;
+		String convertedSubmittedValue = null;
+		if (submittedValue != null) {
+			try {
+				convertedSubmittedValue = ((SimpleSelectOneMenuRenderer) getRenderer(facesContext)).getConvertedValueForClient(facesContext, this, submittedValue);
+			} catch (Exception e) {
+				convertedSubmittedValue = submittedValue.toString();
+			}
+		}
+		while (matches.hasNext()) {
+			item = (SelectItem) matches.next();
+			Object value = item.getValue();
+			String convertedValue = null;
+			
+			if (value != null) {
+				try {
+					convertedValue = ((SimpleSelectOneMenuRenderer) getRenderer(facesContext)).getConvertedValueForClient(facesContext, this, value);
+				} catch (Exception e) {
+					convertedValue = value.toString();
+				}
+			}
+			
+			if (!item.isDisabled() && convertedValue != null && convertedSubmittedValue != null 
+				&& convertedValue.equals(convertedSubmittedValue.toString())) {
+				found = true;
+				break;
+			}
+		}
+		
+		if (found) {
+			setValid(true);
+		} else { // flag as invalid and add error message
+			Locale locale = null;
+			// facesContext.getViewRoot() may not have been initialized at this point.
+			if (facesContext != null && facesContext.getViewRoot() != null) {
+				locale = facesContext.getViewRoot().getLocale();
+				if (locale == null) {
+					locale = Locale.getDefault();
+				}
+			} else {
+				locale = Locale.getDefault();
+			}
+			
+			String summary = null;
+			String detail = null;       
+			ResourceBundle bundle;
+			String bundleName;
+			String messageId = UISelectOne.INVALID_MESSAGE_ID;
+			Application app = facesContext.getApplication();
+			Class appClass = app.getClass();
+			
+			// see if we have a user-provided bundle
+			if (null != (bundleName = app.getMessageBundle())) {
+				if (null != 
+					(bundle = 
+						ResourceBundle.getBundle(bundleName, locale,
+						 getCurrentLoader(appClass)))) {
+					// see if we have a hit
+					try {
+						summary = bundle.getString(messageId);
+						detail = bundle.getString(messageId + "_detail");
+					}
+					catch (Exception e) {
+						// ignore
+					}
+				}
+			}
+
+			// we couldn't find a summary in the user-provided bundle
+			if (null == summary) {
+				// see if we have a summary in the app provided bundle
+				if (null != 
+					(bundle = 
+						ResourceBundle.getBundle(FacesMessage.FACES_MESSAGES, 
+							locale, getCurrentLoader(appClass)))) {
+					try {
+						summary = bundle.getString(messageId);
+						detail = bundle.getString(messageId + "_detail");
+					} catch (Exception e) {
+						// ignore
+					}
+				}
+			}
+			
+			// use default message
+			if (null == summary) {
+				summary = "{0}: INVALID: Selected value didn't match one of the options.";
+				detail = summary;
+			}
+			
+			String _label = getLabel();
+			if (_label != null && !"".equals(_label)) {
+				summary = summary.replace("{0}", _label);
+				if (detail != null) detail = detail.replace("{0}", _label);
+			}
+
+			FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, detail);
+            facesContext.addMessage(getClientId(facesContext), message);
+            setValid(false);
+		}
+	}
+	
+    private static ClassLoader getCurrentLoader(Class fallbackClass) {
+        ClassLoader loader =
+            Thread.currentThread().getContextClassLoader();
+        if (loader == null) {
+            loader = fallbackClass.getClassLoader();
+        }
+        return loader;
     }
 }
