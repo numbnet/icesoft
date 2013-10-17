a5ccbafc5b048691dab86e634ad196e4bdaf64fb
mircea.toma <mircea.toma@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-10-17 17:37:04 +0000

    ICE-9653 Patch prototype.js library to make it work when getComputedStyle function is missing from some IE versions. Falling back to Element.currentStyle property.

--- icefaces3/trunk/icefaces/compat/core/src/main/javascript/prototype/prototype.js
+++ icefaces3/trunk/icefaces/compat/core/src/main/javascript/prototype/prototype.js
@@ -2428,8 +2428,14 @@ Element.Methods = {
         style = style == 'float' ? 'cssFloat' : style.camelize();
         var value = element.style[style];
         if (!value || value == 'auto') {
-            var css = document.defaultView.getComputedStyle(element, null);
-            value = css ? css[style] : null;
+            if (document.defaultView && document.defaultView.getComputedStyle) {
+                var css = document.defaultView.getComputedStyle(element, null);
+                value = css ? css[style] : null;
+            } else if (element.currentStyle) {
+                value = element.currentStyle[style];
+            } else {
+                value = element.style[style];
+            }
         }
         if (style == 'opacity') return value ? parseFloat(value) : 1.0;
         return value == 'auto' ? null : value;
