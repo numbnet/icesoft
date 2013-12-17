311d0b43b2743188c656fb0ac9ca1f2c6cfd1610
ted.goddard <ted.goddard@8668f098-c06c-11db-ba21-f49e70c34f74>
2013-12-17 10:56:55 +0000

    return Resource[] from handleRequest (NTFY-11)

--- icemobile/branches/icemobile-bridgeit-transition/icemobile/spring/src/main/java/org/icemobile/spring/handler/support/JdbcResourceStore.java
+++ icemobile/branches/icemobile-bridgeit-transition/icemobile/spring/src/main/java/org/icemobile/spring/handler/support/JdbcResourceStore.java
@@ -174,7 +174,7 @@ public class JdbcResourceStore implements ResourceStore<ByteArrayResource>{
      * @param request The HttpServletRequest
      * @param token Then user-specific token, such as the session id
      */
-	public void handleRequest(HttpServletRequest request, String token) {
+	public Resource[] handleRequest(HttpServletRequest request, String token) {
         Resource[] resources = null;
         if( resourceAdapter != null ){
             resources = resourceAdapter.handleRequest(request);
@@ -185,7 +185,8 @@ public class JdbcResourceStore implements ResourceStore<ByteArrayResource>{
                 resource.setStore(this);
                 this.add((ByteArrayResource)resource);
             }
-        } 
+        }
+        return resources;
     }
 
 	/**
