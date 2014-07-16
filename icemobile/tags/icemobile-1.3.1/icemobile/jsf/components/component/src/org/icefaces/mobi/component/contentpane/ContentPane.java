a91afef8edb614aa701acf59279d7979b7c1a358
ken.fyten <ken.fyten@8668f098-c06c-11db-ba21-f49e70c34f74>
2014-07-16 21:41:39 +0000

    ICEmobile 1.3.1 release tag.

---
+++ icemobile/tags/icemobile-1.3.1/icemobile/jsf/components/component/src/org/icefaces/mobi/component/contentpane/ContentPane.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icefaces.mobi.component.contentpane;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.el.ValueExpression;
+import javax.faces.component.StateHelper;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+
+import org.icefaces.mobi.api.ContentPaneController;
+import org.icefaces.mobi.utils.MobiJSFUtils;
+import org.icemobile.util.ClientDescriptor;
+
+@SuppressWarnings({"rawtypes","unchecked"})
+public class ContentPane extends ContentPaneBase{
+    
+    private static final Logger logger =
+            Logger.getLogger(ContentPane.class.toString());
+    
+    public static final String CONTENT_SELECTED = "mobi-contentpane ";
+    public static final String CONTENT_LEFT_SELECTED = "mobi-contentpane-left ";
+    public static final String CONTENT_HIDDEN = "mobi-contentpane-hidden ";
+    public static final String CONTENT_LEFT_HIDDEN = "mobi-contentpane-left-hidden";
+    public static final StringBuilder TABSET_TABS_CLASS = new StringBuilder("mobi-tabset-tabs ui-bar-b");
+    public static final String TABSET_SPAN_CLASS = "mobitab";
+    public static final String TABSET_HIDDEN_PAGECLASS = "mobi-tabpage-hidden";
+    public static final String TABSET_ACTIVE_CONTENT_CLASS= "mobi-tabpage";
+    
+    private transient Boolean first;
+
+    @Override
+    public void setClient(boolean client) {
+        ValueExpression ve = getValueExpression(PropertyKeys.client.name() );
+        if (ve != null) {
+            // map of style values per clientId
+            ve.setValue(getFacesContext().getELContext(), client );
+        } else {
+            StateHelper sh = getStateHelper();
+            if (isDisconnected(this))  {
+                String defaultKey = PropertyKeys.client.name() + "_defaultValues";
+                Map clientDefaults = (Map) sh.get(defaultKey);
+                if (clientDefaults == null) {
+                    clientDefaults = new HashMap();
+                    clientDefaults.put("defValue",client);
+                    sh.put(defaultKey, clientDefaults);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isClient() {
+        java.lang.Boolean retVal = false;
+        ValueExpression ve = getValueExpression( PropertyKeys.client.name() );
+        if (ve != null) {
+            Object o = ve.getValue( getFacesContext().getELContext() );
+            if (o != null) {
+                retVal = (java.lang.Boolean) o;
+            }
+        } else {
+            StateHelper sh = getStateHelper();
+            String defaultKey = PropertyKeys.client.name() + "_defaultValues";
+            Map defaultValues = (Map) sh.get(defaultKey);
+            if (defaultValues != null) {
+                if (defaultValues.containsKey("defValue" )) {
+                    retVal = (java.lang.Boolean) defaultValues.get("defValue");
+                }
+            }
+        }
+        return retVal;
+    }
+	private static boolean isDisconnected(UIComponent component) {
+		UIComponent parent = component.getParent();
+		if (parent != null && parent instanceof UIViewRoot) {
+			return false;
+		} else if (parent != null) {
+			return isDisconnected(parent);
+		} else {
+			return true;
+		}
+	}
+    
+    public ClientDescriptor getClient() {
+        return MobiJSFUtils.getClientDescriptor();
+    }
+    
+    public boolean isFirstPane(){
+        if( first == null ){
+            List<UIComponent> children = this.getParent().getChildren();
+            if( children != null && children.size() > 0 && children.get(0).equals(this)){
+                first = Boolean.TRUE;
+            }
+            else{
+                first = Boolean.FALSE;
+            }
+        }
+        return first.booleanValue();
+    }
+    
+    public boolean isSelected(){
+        UIComponent parent = this.getParent();
+        while( !(parent instanceof ContentPaneController) && parent != null ){
+            parent = parent.getParent();
+        }
+        
+        String selectedId= null;
+        if (parent != null){
+            ContentPaneController paneController = (ContentPaneController)parent;
+            selectedId = paneController.getSelectedId();
+            
+            if (null == selectedId){
+                UIComponent pComp = (UIComponent)parent;
+                logger.warning("Parent controller of contentPane must have value for selectedId="+pComp.getClientId());
+                return false;
+            }
+        }
+        else {
+            logger.warning("ContentPane must be nested within a ContentStack: " + this.getClientId());
+            return false;
+        }
+        String id = this.getId();
+        boolean result = false;
+        if( id != null ){
+            result = id.equals(selectedId);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ContentPane [first=" + first + ", getId()=" + getId() + "]";
+    }
+    
+  }
---
+++ icemobile/tags/icemobile-1.3.1/icemobile/jsf/components/tests/mobitest/src/main/java/org/icefaces/mobile/ImageBean.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2004-2014 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.icefaces.mobile;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.util.logging.Logger;
+
+@ManagedBean(name="imageBean")
+@SessionScoped
+public class ImageBean implements Serializable {
+	private static final Logger logger =
+        Logger.getLogger(ImageBean.class.toString());	
+	int cameraSampleUploadCount=1;
+
+	private int height;
+	private int width;
+	private byte[] fileInBytes;
+	
+	public ImageBean(){
+
+	}
+	@PostConstruct
+	public void prepFile() {
+
+        try {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            String sampleImagePath = facesContext.getExternalContext()
+                      .getRealPath("/images/DSC_2.JPG");
+            File imageFile = new File(sampleImagePath);
+            imageFile = new File(sampleImagePath);;
+           //Buffer some bytes.
+
+           FileInputStream fis = new FileInputStream(imageFile);
+           ByteArrayOutputStream bos = new ByteArrayOutputStream();
+           byte[] buf = new byte[1024];
+	       for (int readNum; (readNum = fis.read(buf)) != -1;) {
+	            bos.write(buf, 0, readNum); //no doubt here is 0
+                this.fileInBytes= bos.toByteArray();
+	       }
+	    } catch (Exception ex) {
+	       logger.warning("Error writing out stream");
+           ex.printStackTrace();
+	    }
+
+	 }
+
+	public int getCameraSampleUploadCount() {
+		return cameraSampleUploadCount;
+	}
+
+	public void setCameraSampleUploadCount(int cameraSampleUploadCount) {
+		this.cameraSampleUploadCount = cameraSampleUploadCount;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public byte[] getFileInBytes() {
+		return fileInBytes;
+	}
+
+	public void setFileInBytes(byte[] fileInBytes) {
+		this.fileInBytes = fileInBytes;
+	}
+
+}
